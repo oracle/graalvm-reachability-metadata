5f0c00353b74dd72532adbbbb4c4ef8b64b841b3
Lazar Mitrović <lazar.mitrovic@oracle.com>
2022-05-09 18:58:59 +0200

    Fix failing test for HdrHistogram

--- tests/src/org/hdrhistogram/HdrHistogram/2.1.12/src/test/java/hdrhistogram/HdrHistogramTest.java
+++ tests/src/org/hdrhistogram/HdrHistogram/2.1.12/src/test/java/hdrhistogram/HdrHistogramTest.java
@@ -1,3 +1,13 @@
+/*
+ * Licensed under Public Domain (CC0)
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * this code has waived all copyright and related or neighboring
+ * rights to this code.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 package hdrhistogram;
 
 import java.nio.ByteBuffer;
@@ -23,32 +33,32 @@ import org.junit.jupiter.api.Test;
  * @author Moritz Halbritter
  */
 class HdrHistogramTest {
-	@Test
-	void test() throws Exception {
-		encodeDecode(new AtomicHistogram(10, 0), AtomicHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new ConcurrentDoubleHistogram(0), ConcurrentDoubleHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new ConcurrentHistogram(0), ConcurrentHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new DoubleHistogram(0), DoubleHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new Histogram(0), Histogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new IntCountsHistogram(0), IntCountsHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new PackedConcurrentDoubleHistogram(0), PackedConcurrentDoubleHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new PackedConcurrentHistogram(0), PackedConcurrentHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new PackedDoubleHistogram(0), PackedDoubleHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new PackedHistogram(0), PackedHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new ShortCountsHistogram(0), ShortCountsHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new SynchronizedDoubleHistogram(0), SynchronizedDoubleHistogram::decodeFromCompressedByteBuffer);
-		encodeDecode(new SynchronizedHistogram(0), SynchronizedHistogram::decodeFromCompressedByteBuffer);
-	}
+    @Test
+    void test() throws Exception {
+        encodeDecode(new AtomicHistogram(10, 0), AtomicHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new ConcurrentDoubleHistogram(0), ConcurrentDoubleHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new ConcurrentHistogram(0), ConcurrentHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new DoubleHistogram(0), DoubleHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new Histogram(0), Histogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new IntCountsHistogram(0), IntCountsHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new PackedConcurrentDoubleHistogram(0), PackedConcurrentDoubleHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new PackedConcurrentHistogram(0), PackedConcurrentHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new PackedDoubleHistogram(0), PackedDoubleHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new PackedHistogram(0), PackedHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new ShortCountsHistogram(0), ShortCountsHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new SynchronizedDoubleHistogram(0), SynchronizedDoubleHistogram::decodeFromCompressedByteBuffer);
+        encodeDecode(new SynchronizedHistogram(0), SynchronizedHistogram::decodeFromCompressedByteBuffer);
+    }
 
-	private void encodeDecode(EncodableHistogram histogram, Decoder decoder) throws DataFormatException {
-		ByteBuffer buffer = ByteBuffer.allocate(histogram.getNeededByteBufferCapacity());
-		histogram.encodeIntoCompressedByteBuffer(buffer, 0);
-		buffer.flip();
-		Object decoded = decoder.decode(buffer, 0);
-		System.out.println(decoded);
-	}
+    private void encodeDecode(EncodableHistogram histogram, Decoder decoder) throws DataFormatException {
+        ByteBuffer buffer = ByteBuffer.allocate(histogram.getNeededByteBufferCapacity());
+        histogram.encodeIntoCompressedByteBuffer(buffer, 0);
+        buffer.flip();
+        Object decoded = decoder.decode(buffer, 0);
+        System.out.println(decoded);
+    }
 
-	private interface Decoder {
-		Object decode(ByteBuffer buffer, long minBarForHighestTrackableValue) throws DataFormatException;
-	}
+    private interface Decoder {
+        Object decode(ByteBuffer buffer, long minBarForHighestTrackableValue) throws DataFormatException;
+    }
 }
