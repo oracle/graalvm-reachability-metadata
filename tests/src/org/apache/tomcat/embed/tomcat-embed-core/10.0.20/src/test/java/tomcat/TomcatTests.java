b0be6aa5c29268a23bad3f3a88827b87a1988b5e
Lazar Mitrović <lazar.mitrovic@oracle.com>
2022-05-10 17:33:23 +0200

    Fix format in tomcat tests.

--- tests/src/org/apache/tomcat/embed/tomcat-embed-core/10.0.20/src/test/java/tomcat/TomcatTests.java
+++ tests/src/org/apache/tomcat/embed/tomcat-embed-core/10.0.20/src/test/java/tomcat/TomcatTests.java
@@ -1,3 +1,13 @@
+/*
+ * Licensed under Public Domain (CC0)
+ *
+ * To the extent possible under law, the person who associated CC0 with
+ * this code has waived all copyright and related or neighboring
+ * rights to this code.
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 package tomcat;
 
 import java.io.File;
@@ -23,60 +33,54 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class TomcatTests {
 
-	private static final int PORT = 8080;
+    private static final int PORT = 8080;
 
-	@ParameterizedTest
-	@ValueSource(strings = {
-			"HTTP/1.1",
-			"org.apache.coyote.http11.Http11NioProtocol",
-			"org.apache.coyote.http11.Http11Nio2Protocol"
-	})
-	void test(String protocol) throws Exception {
-		Tomcat tomcat = new Tomcat();
-		configureConnector(tomcat, protocol);
-		Context context = addContext(tomcat);
-		addServlet(context);
-		tomcat.start();
-		try {
-			HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(1)).build();
-			HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/hello")).GET().header("Accept", "text/plain")
-					.timeout(Duration.ofSeconds(1))
-					.build();
-			HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
-			assertThat(response.statusCode()).isEqualTo(200);
-			assertThat(response.body()).isEqualTo("Hello World\n");
-		}
-		finally {
-			tomcat.stop();
-			tomcat.destroy();
-		}
-	}
+    @ParameterizedTest
+    @ValueSource(strings = {"HTTP/1.1", "org.apache.coyote.http11.Http11NioProtocol", "org.apache.coyote.http11.Http11Nio2Protocol"})
+    void test(String protocol) throws Exception {
+        Tomcat tomcat = new Tomcat();
+        configureConnector(tomcat, protocol);
+        Context context = addContext(tomcat);
+        addServlet(context);
+        tomcat.start();
+        try {
+            HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(1)).build();
+            HttpRequest request = HttpRequest.newBuilder(URI.create("http://localhost:8080/hello"))
+                    .GET().header("Accept", "text/plain").timeout(Duration.ofSeconds(1)).build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            assertThat(response.statusCode()).isEqualTo(200);
+            assertThat(response.body()).isEqualTo("Hello World\n");
+        } finally {
+            tomcat.stop();
+            tomcat.destroy();
+        }
+    }
 
-	private Context addContext(Tomcat tomcat) {
-		Context context = tomcat.addContext("", new File(".").getAbsolutePath());
-		Tomcat.addDefaultMimeTypeMappings(context);
-		return context;
-	}
+    private Context addContext(Tomcat tomcat) {
+        Context context = tomcat.addContext("", new File(".").getAbsolutePath());
+        Tomcat.addDefaultMimeTypeMappings(context);
+        return context;
+    }
 
-	private void addServlet(Context context) {
-		Tomcat.addServlet(context, "hello", new MyServlet());
-		context.addServletMappingDecoded("/*", "hello");
-	}
+    private void addServlet(Context context) {
+        Tomcat.addServlet(context, "hello", new MyServlet());
+        context.addServletMappingDecoded("/*", "hello");
+    }
 
-	private void configureConnector(Tomcat tomcat, String protocol) {
-		Connector connector = new Connector(protocol);
-		connector.setPort(PORT);
-		tomcat.setConnector(connector);
-	}
+    private void configureConnector(Tomcat tomcat, String protocol) {
+        Connector connector = new Connector(protocol);
+        connector.setPort(PORT);
+        tomcat.setConnector(connector);
+    }
 
-	private static class MyServlet extends HttpServlet {
-		@Override
-		protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-			resp.setCharacterEncoding("UTF-8");
-			resp.setContentType("text/plain");
-			try (Writer writer = resp.getWriter()) {
-				writer.write("Hello World\n");
-			}
-		}
-	}
+    private static class MyServlet extends HttpServlet {
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+            resp.setCharacterEncoding("UTF-8");
+            resp.setContentType("text/plain");
+            try (Writer writer = resp.getWriter()) {
+                writer.write("Hello World\n");
+            }
+        }
+    }
 }
