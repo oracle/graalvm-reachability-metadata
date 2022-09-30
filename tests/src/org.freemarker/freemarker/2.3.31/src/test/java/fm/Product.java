546b18b837e81c1347bf3a20c1bf903c89d11282
Sébastien Deleuze <sdeleuze@vmware.com>
2022-09-30 17:25:28 +0200

    Add Freemarker metadata
    Closes gh-38

---
+++ tests/src/org.freemarker/freemarker/2.3.31/src/test/java/fm/Product.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package fm;
+
+public class Product {
+
+    private String url;
+    private String name;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
