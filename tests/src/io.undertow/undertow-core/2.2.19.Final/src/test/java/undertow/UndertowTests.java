e8efca73ed225b1e87218ea53b34ee097c34661f
Moritz Halbritter <mkammerer@vmware.com>
2022-10-10 16:02:34 +0200

    Add metadata for io.undertow:undertow-servlet:2.2.19.Final

--- tests/src/io.undertow/undertow-core/2.2.19.Final/src/test/java/undertow/UndertowTests.java
+++ tests/src/io.undertow/undertow-core/2.2.19.Final/src/test/java/undertow/UndertowTests.java
@@ -6,7 +6,13 @@
  */
 package undertow;
 
+import io.undertow.Handlers;
 import io.undertow.Undertow;
+import io.undertow.server.handlers.PathHandler;
+import io.undertow.servlet.Servlets;
+import io.undertow.servlet.api.DeploymentInfo;
+import io.undertow.servlet.api.DeploymentManager;
+import io.undertow.servlet.util.ImmediateInstanceFactory;
 import io.undertow.util.Headers;
 import org.junit.jupiter.api.Test;
 
@@ -23,7 +29,7 @@ public class UndertowTests {
     private static final int PORT = 8080;
 
     @Test
-    void test() throws Exception {
+    void core() throws Exception {
         // Start server
         Undertow server = Undertow.builder()
                 .addHttpListener(PORT, "localhost")
@@ -40,6 +46,39 @@ public class UndertowTests {
             HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
             assertThat(response.statusCode()).isEqualTo(200);
             assertThat(response.body()).isEqualTo("Hello World");
+        } finally {
+            // Cleanup
+            server.stop();
+        }
+    }
+
+    @Test
+    void servlet() throws Exception {
+        // Start server
+        DeploymentInfo servletBuilder = Servlets.deployment()
+                .setClassLoader(MessageServlet.class.getClassLoader())
+                .setContextPath("/myapp")
+                .setDeploymentName("test")
+                .addServlets(
+                        Servlets.servlet("MessageServlet", MessageServlet.class, new ImmediateInstanceFactory<>(new MessageServlet())).addMapping("/hello")
+                );
+        DeploymentManager manager = Servlets.defaultContainer().addDeployment(servletBuilder);
+        manager.deploy();
+        PathHandler path = Handlers.path(Handlers.redirect("/myapp"))
+                .addPrefixPath("/myapp", manager.start());
+        Undertow server = Undertow.builder()
+                .addHttpListener(PORT, "localhost")
+                .setHandler(path)
+                .build();
+        server.start();
+        try {
+            // Make request
+            HttpClient client = HttpClient.newBuilder().connectTimeout(Duration.ofSeconds(1)).build();
+            HttpRequest request = HttpRequest.newBuilder(URI.create(String.format("http://localhost:%d/myapp/hello", PORT)))
+                    .GET().header("Accept", "text/plain").timeout(Duration.ofSeconds(1)).build();
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            assertThat(response.statusCode()).isEqualTo(200);
+            assertThat(response.body()).isEqualTo("Hello world");
         } finally {
             // Cleanup
             server.stop();
