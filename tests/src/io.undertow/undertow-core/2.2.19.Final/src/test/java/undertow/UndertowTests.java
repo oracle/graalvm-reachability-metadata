c019c0ec565201a98e52b3f5bfd12853e769f3d3
Moritz Halbritter <mkammerer@vmware.com>
2022-10-11 14:37:09 +0200

    Add metadata for io.undertow:undertow-websockets-jsr:2.2.19.Final

--- tests/src/io.undertow/undertow-core/2.2.19.Final/src/test/java/undertow/UndertowTests.java
+++ tests/src/io.undertow/undertow-core/2.2.19.Final/src/test/java/undertow/UndertowTests.java
@@ -8,19 +8,41 @@ package undertow;
 
 import io.undertow.Handlers;
 import io.undertow.Undertow;
+import io.undertow.UndertowMessages;
+import io.undertow.client.UndertowClientMessages;
+import io.undertow.connector.ByteBufferPool;
+import io.undertow.server.DefaultByteBufferPool;
 import io.undertow.server.handlers.PathHandler;
 import io.undertow.servlet.Servlets;
+import io.undertow.servlet.UndertowServletLogger;
+import io.undertow.servlet.UndertowServletMessages;
 import io.undertow.servlet.api.DeploymentInfo;
 import io.undertow.servlet.api.DeploymentManager;
+import io.undertow.servlet.api.ServletContainer;
 import io.undertow.servlet.util.ImmediateInstanceFactory;
 import io.undertow.util.Headers;
+import io.undertow.websockets.client.WebSocketClient;
+import io.undertow.websockets.core.AbstractReceiveListener;
+import io.undertow.websockets.core.BufferedTextMessage;
+import io.undertow.websockets.core.WebSocketChannel;
+import io.undertow.websockets.core.WebSocketMessages;
+import io.undertow.websockets.core.WebSockets;
+import io.undertow.websockets.jsr.JsrWebSocketMessages;
+import io.undertow.websockets.jsr.WebSocketDeploymentInfo;
+import org.jboss.logging.Logger;
 import org.junit.jupiter.api.Test;
+import org.xnio.IoFuture;
+import org.xnio.XnioWorker;
 
+import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.time.Duration;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicReference;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -53,6 +75,21 @@ public class UndertowTests {
     }
 
     @Test
+    void loggersWork() {
+        UndertowServletLogger.ROOT_LOGGER.log(Logger.Level.INFO, "test");
+        UndertowServletLogger.REQUEST_LOGGER.log(Logger.Level.INFO, "test");
+    }
+
+    @Test
+    void messagesWork() {
+        assertThat(UndertowMessages.MESSAGES.expectedContinuationFrame()).isNotNull();
+        assertThat(UndertowClientMessages.MESSAGES.connectionClosed()).isNotNull();
+        assertThat(UndertowServletMessages.MESSAGES.asyncAlreadyStarted().getMessage()).isNotNull();
+        assertThat(WebSocketMessages.MESSAGES.channelClosed().getMessage()).isNotNull();
+        assertThat(JsrWebSocketMessages.MESSAGES.clientNotSupported().getMessage()).isNotNull();
+    }
+
+    @Test
     void servlet() throws Exception {
         // Start server
         DeploymentInfo servletBuilder = Servlets.deployment()
@@ -82,6 +119,94 @@ public class UndertowTests {
         } finally {
             // Cleanup
             server.stop();
+        }
+    }
+
+    @Test
+    void websocket() throws Exception {
+        // Start server
+        Undertow server = Undertow.builder()
+                .addHttpListener(PORT, "localhost")
+                .setHandler(Handlers.websocket((exchange, channel) -> {
+                    System.out.printf("onConnect: %s%n", channel);
+                    channel.getReceiveSetter().set(new AbstractReceiveListener() {
+                        @Override
+                        protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) {
+                            String data = message.getData();
+                            System.out.printf("Server: onFullTextMessage: %s %s%n", channel, data);
+                            WebSockets.sendText(data, channel, null);
+                        }
+                    });
+                    channel.resumeReceives();
+                })).build();
+        server.start();
+        try {
+            String received = sendWebSocketRequest(server.getWorker(), "/", "Hello websocket");
+            assertThat(received).isEqualTo("Hello websocket");
+        } finally {
+            // Cleanup
+            server.stop();
+        }
+    }
+
+    @Test
+    void websocketJsr() throws Exception {
+        // Start server
+        PathHandler path = Handlers.path();
+        Undertow server = Undertow.builder()
+                .addHttpListener(PORT, "localhost")
+                .setHandler(path)
+                .build();
+        ServletContainer container = ServletContainer.Factory.newInstance();
+
+        DeploymentInfo builder = new DeploymentInfo()
+                .setClassLoader(WebSocketHandler.class.getClassLoader())
+                .setContextPath("/")
+                .addServletContextAttribute(WebSocketDeploymentInfo.ATTRIBUTE_NAME,
+                        new WebSocketDeploymentInfo()
+                                .setBuffers(new DefaultByteBufferPool(true, 100))
+                                .addEndpoint(WebSocketHandler.class)
+                )
+                .setDeploymentName("websocket-jsr");
+        DeploymentManager manager = container.addDeployment(builder);
+        manager.deploy();
+        path.addPrefixPath("/", manager.start());
+        server.start();
+        try {
+            String received = sendWebSocketRequest(server.getWorker(), "/websocket", "Hello websocket");
+            assertThat(received).isEqualTo("Hello websocket");
+        } finally {
+            // Cleanup
+            server.stop();
+        }
+    }
+
+    private String sendWebSocketRequest(XnioWorker worker, String path, String text) throws IOException, InterruptedException {
+        try (ByteBufferPool pool = new DefaultByteBufferPool(false, 8192)) {
+            IoFuture<WebSocketChannel> future = WebSocketClient.connectionBuilder(worker, pool, URI.create(String.format("ws://localhost:%d%s", PORT, path))).connect();
+            IoFuture.Status status = future.await(1, TimeUnit.SECONDS);
+            if (status != IoFuture.Status.DONE) {
+                throw new IOException("Failed to establish websocket client connection");
+            }
+            try (WebSocketChannel channel = future.get()) {
+                CountDownLatch countDownLatch = new CountDownLatch(1);
+                AtomicReference<String> received = new AtomicReference<>();
+                channel.getReceiveSetter().set(new AbstractReceiveListener() {
+                    @Override
+                    protected void onFullTextMessage(WebSocketChannel channel, BufferedTextMessage message) throws IOException {
+                        String data = message.getData();
+                        System.out.printf("Client: onFullTextMessage: %s %s%n", channel, data);
+                        received.set(data);
+                        countDownLatch.countDown();
+                    }
+                });
+                channel.resumeReceives();
+                WebSockets.sendText(text, channel, null);
+                if (!countDownLatch.await(2, TimeUnit.SECONDS)) {
+                    throw new IOException("Failed to receive echo websocket message");
+                }
+                return received.get();
+            }
         }
     }
 }
