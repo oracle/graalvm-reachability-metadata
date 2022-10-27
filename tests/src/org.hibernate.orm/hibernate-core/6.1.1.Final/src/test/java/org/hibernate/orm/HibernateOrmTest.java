de98234401c59c63affe6bc5c41ac20bbca5013c
Christoph Strobl <cstrobl@vmware.com>
2022-10-27 09:39:10 +0200

    Provide testcases for hibernate-orm dialects loading.
    Use hibernate ClassLoaderServiceImpl to obtain database dialects for
    - hsql
    - h2
    - mariaDB
    - mysql
    - oracle
    - postgreSQL

--- tests/src/org.hibernate.orm/hibernate-core/6.1.1.Final/src/test/java/org/hibernate/orm/HibernateOrmTest.java
+++ tests/src/org.hibernate.orm/hibernate-core/6.1.1.Final/src/test/java/org/hibernate/orm/HibernateOrmTest.java
@@ -6,6 +6,8 @@
  */
 package org.hibernate.orm;
 
+import static org.junit.Assert.assertNotNull;
+
 import java.util.Collections;
 import java.util.Date;
 
@@ -14,7 +16,17 @@ import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.classloading.internal.ClassLoaderServiceImpl;
+import org.hibernate.boot.registry.selector.internal.StrategySelectorImpl;
+import org.hibernate.boot.registry.selector.spi.StrategySelector;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
+import org.hibernate.engine.jdbc.dialect.internal.DialectFactoryImpl;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+import org.hibernate.service.Service;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.spi.ServiceBinding;
+import org.hibernate.service.spi.ServiceRegistryImplementor;
 import org.junit.jupiter.api.Test;
 
 public class HibernateOrmTest {
@@ -88,6 +100,60 @@ public class HibernateOrmTest {
         sessionFactory.close();
     }
 
+    @Test
+    void hsqlDialect() {
+
+        DialectFactoryImpl dialectFactory = new DialectFactoryImpl();
+        dialectFactory.injectServices(new StubServiceRegistryImplementor());
+
+       assertNotNull(dialectFactory.buildDialect(Collections.singletonMap(AvailableSettings.DIALECT, "org.hibernate.dialect.HSQLDialect"), () -> StubDialectResolutionInfo.INSTANCE));
+    }
+
+    @Test
+    void h2Dialect() {
+
+        DialectFactoryImpl dialectFactory = new DialectFactoryImpl();
+        dialectFactory.injectServices(new StubServiceRegistryImplementor());
+
+        assertNotNull(dialectFactory.buildDialect(Collections.singletonMap(AvailableSettings.DIALECT, "org.hibernate.dialect.H2Dialect"), () -> StubDialectResolutionInfo.INSTANCE));
+    }
+
+    @Test
+    void mariaDbDialect() {
+
+        DialectFactoryImpl dialectFactory = new DialectFactoryImpl();
+        dialectFactory.injectServices(new StubServiceRegistryImplementor());
+
+        assertNotNull(dialectFactory.buildDialect(Collections.singletonMap(AvailableSettings.DIALECT, "org.hibernate.dialect.MariaDBDialect"), () -> StubDialectResolutionInfo.INSTANCE));
+    }
+
+    @Test
+    void mysqlDialect() {
+
+        DialectFactoryImpl dialectFactory = new DialectFactoryImpl();
+        dialectFactory.injectServices(new StubServiceRegistryImplementor());
+
+        assertNotNull(dialectFactory.buildDialect(Collections.singletonMap(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQLDialect"), () -> StubDialectResolutionInfo.INSTANCE));
+    }
+
+    @Test
+    void oracleDialect() {
+
+        DialectFactoryImpl dialectFactory = new DialectFactoryImpl();
+        dialectFactory.injectServices(new StubServiceRegistryImplementor());
+
+        assertNotNull(dialectFactory.buildDialect(Collections.singletonMap(AvailableSettings.DIALECT, "org.hibernate.dialect.OracleDialect"), () -> StubDialectResolutionInfo.INSTANCE));
+    }
+
+    @Test
+    void postgreSqlDialect() {
+
+        DialectFactoryImpl dialectFactory = new DialectFactoryImpl();
+        dialectFactory.injectServices(new StubServiceRegistryImplementor());
+
+        assertNotNull(dialectFactory.buildDialect(Collections.singletonMap(AvailableSettings.DIALECT, "org.hibernate.dialect.PostgreSQLDialect"), () -> StubDialectResolutionInfo.INSTANCE));
+    }
+
     private static Configuration h2Config(Class<?>... entities) {
 
         Configuration config = new Configuration();
@@ -103,5 +169,91 @@ public class HibernateOrmTest {
         }
 
         return config;
+    }
+
+    private static class StubDialectResolutionInfo implements DialectResolutionInfo {
+
+        static final DialectResolutionInfo INSTANCE = new StubDialectResolutionInfo();
+
+        @Override
+        public String getDatabaseName() {
+            return null;
+        }
+
+        @Override
+        public String getDatabaseVersion() {
+            return null;
+        }
+
+        @Override
+        public String getDriverName() {
+            return null;
+        }
+
+        @Override
+        public int getDriverMajorVersion() {
+            return 0;
+        }
+
+        @Override
+        public int getDriverMinorVersion() {
+            return 0;
+        }
+
+        @Override
+        public String getSQLKeywords() {
+            return null;
+        }
+
+        @Override
+        public int getDatabaseMajorVersion() {
+            return 0;
+        }
+
+        @Override
+        public int getDatabaseMinorVersion() {
+            return 0;
+        }
+    }
+
+    private static class StubServiceRegistryImplementor implements ServiceRegistryImplementor {
+
+        @Override
+        public <R extends Service> ServiceBinding<R> locateServiceBinding(Class<R> serviceRole) {
+            return null;
+        }
+
+        @Override
+        public void destroy() {
+
+        }
+
+        @Override
+        public void registerChild(ServiceRegistryImplementor child) {
+
+        }
+
+        @Override
+        public void deRegisterChild(ServiceRegistryImplementor child) {
+
+        }
+
+        @Override
+        public <T extends Service> T fromRegistryOrChildren(Class<T> serviceRole) {
+            return null;
+        }
+
+        @Override
+        public ServiceRegistry getParentServiceRegistry() {
+            return null;
+        }
+
+        @Override
+        public <R extends Service> R getService(Class<R> serviceRole) {
+            if (serviceRole == StrategySelector.class) {
+                return (R) new StrategySelectorImpl(new ClassLoaderServiceImpl());
+            }
+            return null;
+        }
     }
 }
