99f25c71224871a3e74b6c16a07bed81de3bdafb
Nemanja Mikic <n0t_l3ss@outlook.com>
2023-02-08 13:52:04 +0100

    Add opentelemetry version 1.19.0 (#215)
    * add opentelemetry version 1.19.0
    
    * add opentelemetry version 1.19.0
    
    * style change
    
    * add opentelemetry version 1.19.0
    
    * add opentelemetry version 1.19.0
    
    * style change
    
    * add missing entries into metadata/index.json
    
    * fix test
    
    * fix test
    
    * fix checkstyle
    
    * fix checkstyle
    
    * move AddAllCharsets to zipkin test

---
+++ tests/src/io.opentelemetry/opentelemetry-exporter-zipkin/1.19.0/src/test/java/opentelemetry/OpenTelemetryZipkinExporterTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package opentelemetry;
+
+import io.opentelemetry.api.trace.SpanContext;
+import io.opentelemetry.api.trace.SpanId;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.TraceFlags;
+import io.opentelemetry.api.trace.TraceId;
+import io.opentelemetry.api.trace.TraceState;
+import io.opentelemetry.exporter.zipkin.ZipkinSpanExporter;
+import io.opentelemetry.sdk.testing.trace.TestSpanData;
+import io.opentelemetry.sdk.trace.data.StatusData;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import java.util.List;
+
+
+public class OpenTelemetryZipkinExporterTest {
+
+    @Test
+    public void zipkinSpanExporterTest() {
+        TestSpanData span = TestSpanData.builder()
+                .setKind(SpanKind.INTERNAL)
+                .setName("test")
+                .setStatus(StatusData.ok())
+                .setSpanContext(SpanContext.create(TraceId.fromLongs(321, 123), SpanId.fromLong(12345), TraceFlags.getDefault(), TraceState.getDefault()))
+                .setStartEpochNanos(System.nanoTime())
+                .setEndEpochNanos(System.nanoTime())
+                .setHasEnded(true)
+                .build();
+
+        Assertions.assertTrue(isClassPresent("io.opentelemetry.exporter.zipkin.ZipkinSpanExporter"));
+
+        try (ZipkinSpanExporter exporter = ZipkinSpanExporter.builder().build()) {
+            exporter.export(List.of(span));
+        }
+    }
+
+    private boolean isClassPresent(String className) {
+        try {
+            Class.forName(className);
+            return true;
+        } catch (ClassNotFoundException ex) {
+            return false;
+        }
+    }
+}
