fb5283aaec7d03f0a4532fc2563b2ca96d861e0a
Corneil du Plessis <pcorneil@vmware.com>
2023-02-24 14:10:10 +0200

    Added kotlin.internal.jdk8.JDK8PlatformImplementations constructor. It is required when doing IO in a coroutine.

--- tests/src/org.jetbrains.kotlin/kotlin-reflect/1.7.10/src/test/kotlin/kotlinreflect/KotlinReflectTests.kt
+++ tests/src/org.jetbrains.kotlin/kotlin-reflect/1.7.10/src/test/kotlin/kotlinreflect/KotlinReflectTests.kt
@@ -1,7 +1,18 @@
 package kotlinreflect
 
+import kotlinx.coroutines.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import java.io.File
+import java.io.FileOutputStream
+import java.io.FileWriter
+import java.time.Instant
+import java.util.concurrent.ArrayBlockingQueue
+import java.util.concurrent.BlockingQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicBoolean
+import kotlin.concurrent.thread
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.createType
 import kotlin.reflect.full.memberProperties
@@ -39,6 +50,27 @@ class KotlinReflectTests {
         assertThat(annotatedProperty.annotations).hasSize(1)
         assertThat(annotatedProperty.annotations[0]).isInstanceOf(TestAnnotation::class.java)
         assertThat((annotatedProperty.annotations[0] as TestAnnotation).value).isEqualTo("annotation-on-field")
+    }
+    @Test
+    fun testCoroutine() {
+        val updated = AtomicBoolean(false)
+        CoroutineScope(Dispatchers.IO).launch {
+            val writer = FileWriter(File.createTempFile("tmp-couroutine", ".txt"))
+            writer.use {
+                val start = "start:" + Instant.now()
+                writer.append(start)
+                println(start)
+                delay(500)
+                updated.set(true)
+                val end = "end:" + Instant.now()
+                writer.append(end)
+                println(end)
+            }
+        }
+        println("sleep:" + Instant.now())
+        Thread.sleep(1000)
+        println("awake:" + Instant.now())
+        assertThat(updated.get()).isTrue
     }
 }
 
