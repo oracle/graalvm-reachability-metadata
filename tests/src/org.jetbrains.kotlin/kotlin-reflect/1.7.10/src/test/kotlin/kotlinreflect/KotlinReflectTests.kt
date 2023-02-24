8d0a1b59d4ae258abcc6c037eda29f191fb9d4d3
Corneil du Plessis <corneil.duplessis@gmail.com>
2023-02-24 14:31:35 +0200

    Update KotlinReflectTests.kt

--- tests/src/org.jetbrains.kotlin/kotlin-reflect/1.7.10/src/test/kotlin/kotlinreflect/KotlinReflectTests.kt
+++ tests/src/org.jetbrains.kotlin/kotlin-reflect/1.7.10/src/test/kotlin/kotlinreflect/KotlinReflectTests.kt
@@ -3,16 +3,7 @@ package kotlinreflect
 import kotlinx.coroutines.*
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.io.File
-import java.io.FileOutputStream
-import java.io.FileWriter
-import java.time.Instant
-import java.util.concurrent.ArrayBlockingQueue
-import java.util.concurrent.BlockingQueue
-import java.util.concurrent.ThreadPoolExecutor
-import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
-import kotlin.concurrent.thread
 import kotlin.reflect.KParameter
 import kotlin.reflect.full.createType
 import kotlin.reflect.full.memberProperties
@@ -54,23 +45,12 @@ class KotlinReflectTests {
     @Test
     fun testCoroutine() {
         val updated = AtomicBoolean(false)
-        CoroutineScope(Dispatchers.IO).launch {
-            val writer = FileWriter(File.createTempFile("tmp-couroutine", ".txt"))
-            writer.use {
-                val start = "start:" + Instant.now()
-                writer.append(start)
-                println(start)
-                delay(500)
-                updated.set(true)
-                val end = "end:" + Instant.now()
-                writer.append(end)
-                println(end)
-            }
+        CoroutineScope(Dispatchers.Default).launch {
+        delay(500)
+        updated.set(true)
         }
-        println("sleep:" + Instant.now())
         Thread.sleep(1000)
-        println("awake:" + Instant.now())
-        assertThat(updated.get()).isTrue
+        assertThat(updated.get()).isTrue    
     }
 }
 
