af2f1c3f4f1a762ec59093bc257486b5d1e4b80e
Milenko Supic <supic82@gmail.com>
2023-03-08 14:32:45 +0100

    Added metadata for hibernate 5

--- tests/src/org.hibernate/hibernate-core/5.6.14.Final/src/test/java/org_hibernate/hibernate_core/MSSQLDialectHibernateTest.java
+++ tests/src/org.hibernate/hibernate-core/5.6.14.Final/src/test/java/org_hibernate/hibernate_core/MSSQLDialectHibernateTest.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 package org_hibernate.hibernate_core;
 
 public class MSSQLDialectHibernateTest extends AbstractHibernateTest {
