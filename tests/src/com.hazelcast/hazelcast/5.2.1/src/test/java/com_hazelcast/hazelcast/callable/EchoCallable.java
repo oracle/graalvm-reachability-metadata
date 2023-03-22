948d2125a8f9e44b052d8b5884876fbb866c0e63
Fabio Niephaus <code@fniephaus.com>
2023-03-22 13:37:47 +0100

    Add missing copyright header to EchoCallable.java

--- tests/src/com.hazelcast/hazelcast/5.2.1/src/test/java/com_hazelcast/hazelcast/callable/EchoCallable.java
+++ tests/src/com.hazelcast/hazelcast/5.2.1/src/test/java/com_hazelcast/hazelcast/callable/EchoCallable.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
 package com_hazelcast.hazelcast.callable;
 
 import java.io.Serializable;
