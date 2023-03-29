ef9123211a5418578c12968cf57eaffeca28511c
David Nestorovic <david.nestorovic@oracle.com>
2023-03-29 12:19:50 +0200

    Add description

--- tests/src/org.eclipse.jetty/jetty-client/11.0.12/src/test/java/org_eclipse_jetty/jetty_client/JettyClientTest.java
+++ tests/src/org.eclipse.jetty/jetty-client/11.0.12/src/test/java/org_eclipse_jetty/jetty_client/JettyClientTest.java
@@ -14,6 +14,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class JettyClientTest {
 
+    // This test communicates with external service so it should be disabled until it is refactored
     private static final boolean DISABLE_TEST = true;
     @Test
     void test() throws Exception {
