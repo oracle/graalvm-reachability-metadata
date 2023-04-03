d4b95b3fd25efdd988d95511dc149cb63e7bf01d
Milenko Supic <supic82@gmail.com>
2023-04-03 16:55:06 +0200

    Adding metadata for hibernate 6.2.0.Final

--- tests/src/org.hibernate.orm/hibernate-core/6.2.0.Final/generate-metadata.sh
+++ tests/src/org.hibernate.orm/hibernate-core/6.2.0.Final/generate-metadata.sh
@@ -4,3 +4,6 @@
 ./gradlew test --tests "org_hibernate_orm.hibernate_core.HibernateH2Test" -Pagent metadataCopy
 ./gradlew test --tests "org_hibernate_orm.hibernate_core.HibernateDialectTest" -Pagent metadataCopy
 ./gradlew test --tests "org_hibernate_orm.hibernate_core.UrlMessageBundleTest" -Pagent metadataCopy
+./gradlew test --tests "org_hibernate_orm.hibernate_core.IdentifierGeneratorTest" -Pagent metadataCopy
+./gradlew test --tests "org_hibernate_orm.hibernate_core.OptimizerTest" -Pagent metadataCopy
+./gradlew updateGeneratedMetadata
