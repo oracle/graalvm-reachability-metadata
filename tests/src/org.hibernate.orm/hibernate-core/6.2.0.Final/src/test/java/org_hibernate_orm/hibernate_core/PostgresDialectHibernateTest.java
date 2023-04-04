8cf24c9321dad5771ffb2c195af414e82ff40970
Milenko Supic <supic82@gmail.com>
2023-04-04 18:36:49 +0200

    Added metadata for `org.hibernate.orm:hibernate-core:6.2.0.Final` (#263)
    * Adding metadata for hibernate 6.2.0.Final
    
    * Adding metadata for hibernate 6.2.0.Final
    
    * Adding metadata for hibernate 6.2.0.Final
    
    * Adding metadata for hibernate 6.2.0.Final
    
    * Adding metadata for hibernate 6.2.0.Final
    
    * Adding metadata for hibernate 6.2.0.Final
    
    * Adding metadata for hibernate 6.2.0.Final
    
    * Modified hibernate-core index.json file

---
+++ tests/src/org.hibernate.orm/hibernate-core/6.2.0.Final/src/test/java/org_hibernate_orm/hibernate_core/PostgresDialectHibernateTest.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_hibernate_orm.hibernate_core;
+
+public class PostgresDialectHibernateTest  extends AbstractHibernateTest {
+
+    @Override
+    protected String getJdbcUrl() {
+        return "jdbc:h2:mem:test;MODE=PostgreSQL;DATABASE_TO_LOWER=TRUE;DEFAULT_NULL_ORDERING=HIGH";
+    }
+
+    @Override
+    protected String getHibernateDialect() {
+        return "org.hibernate.dialect.PostgreSQLDialect";
+    }
+}
