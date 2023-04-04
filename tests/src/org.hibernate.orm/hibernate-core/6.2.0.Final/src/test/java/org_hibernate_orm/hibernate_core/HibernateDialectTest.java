4bb29c71b4b6b0582962b08212f100a1142619e5
Milenko Supic <supic82@gmail.com>
2023-04-04 13:12:38 +0200

    Adding metadata for hibernate 6.2.0.Final

--- tests/src/org.hibernate.orm/hibernate-core/6.2.0.Final/src/test/java/org_hibernate_orm/hibernate_core/HibernateDialectTest.java
+++ tests/src/org.hibernate.orm/hibernate-core/6.2.0.Final/src/test/java/org_hibernate_orm/hibernate_core/HibernateDialectTest.java
@@ -8,8 +8,13 @@ package org_hibernate_orm.hibernate_core;
 
 import org.hibernate.boot.registry.internal.BootstrapServiceRegistryImpl;
 import org.hibernate.cfg.AvailableSettings;
+import org.hibernate.dialect.DatabaseVersion;
 import org.hibernate.dialect.Dialect;
 import org.hibernate.engine.jdbc.dialect.internal.DialectFactoryImpl;
+import org.hibernate.engine.jdbc.dialect.spi.DatabaseMetaDataDialectResolutionInfoAdapter;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfo;
+import org.hibernate.engine.jdbc.dialect.spi.DialectResolutionInfoSource;
+import org.hibernate.engine.jdbc.env.internal.JdbcEnvironmentInitiator;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -31,11 +36,43 @@ public class HibernateDialectTest {
         configValues.put(AvailableSettings.DIALECT, hibernateDialect);
         DialectFactoryImpl dialectFactoryImpl = new DialectFactoryImpl();
         dialectFactoryImpl.injectServices(new BootstrapServiceRegistryImpl());
-        Dialect dialect = dialectFactoryImpl.buildDialect(configValues, null);
+        Dialect dialect = dialectFactoryImpl.buildDialect(configValues, new DialectResolutionInfoSource() {
+                    @Override
+                    public DialectResolutionInfo getDialectResolutionInfo() {
+                        return new DialectResolutionInfoTest();
+                    }
+                });
         assertThat(dialect).isNotNull();
     }
 
     private static Stream<Arguments> provideHibernateDialects() {
+        String dialectPackage = "org.hibernate.dialect.";
+
+        List<String> hibernateDialects = Arrays.asList(
+                "H2Dialect",
+                "MariaDBDialect",
+                "MySQLDialect",
+                "OracleDialect",
+                "PostgresPlusDialect",
+                "PostgreSQLDialect",
+                "SQLServerDialect"
+        );
+
+        return hibernateDialects.stream().map(dialect -> Arguments.of(dialectPackage + dialect));
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideDeprecatedHibernateDialects")
+    void testDeprecatedHibernateDialects(String hibernateDialect) {
+        Map<String, Object> configValues = new HashMap<>();
+        configValues.put(AvailableSettings.DIALECT, hibernateDialect);
+        DialectFactoryImpl dialectFactoryImpl = new DialectFactoryImpl();
+        dialectFactoryImpl.injectServices(new BootstrapServiceRegistryImpl());
+        Dialect dialect = dialectFactoryImpl.buildDialect(configValues, null);
+        assertThat(dialect).isNotNull();
+    }
+
+    private static Stream<Arguments> provideDeprecatedHibernateDialects() {
         String dialectPackage = "org.hibernate.dialect.";
 
         List<String> hibernateDialects = Arrays.asList(
@@ -44,7 +81,6 @@ public class HibernateDialectTest {
                 "MySQL8Dialect",
                 "MySQL57Dialect",
                 "Oracle12cDialect",
-                "PostgresPlusDialect",
                 "PostgreSQL10Dialect",
                 "SQLServer2008Dialect",
                 "SQLServer2012Dialect",
@@ -52,5 +88,48 @@ public class HibernateDialectTest {
         );
 
         return hibernateDialects.stream().map(dialect -> Arguments.of(dialectPackage + dialect));
+    }
+
+    private static class DialectResolutionInfoTest implements DialectResolutionInfo {
+
+        @Override
+        public String getDatabaseName() {
+            return "TestDB";
+        }
+
+        @Override
+        public String getDatabaseVersion() {
+            return "99";
+        }
+
+        @Override
+        public String getDriverName() {
+            return "TestDBDriver";
+        }
+
+        @Override
+        public int getDriverMajorVersion() {
+            return 0;
+        }
+
+        @Override
+        public int getDriverMinorVersion() {
+            return 0;
+        }
+
+        @Override
+        public String getSQLKeywords() {
+            return null;
+        }
+
+        @Override
+        public int getDatabaseMajorVersion() {
+            return 99;
+        }
+
+        @Override
+        public int getDatabaseMinorVersion() {
+            return 0;
+        }
     }
 }
