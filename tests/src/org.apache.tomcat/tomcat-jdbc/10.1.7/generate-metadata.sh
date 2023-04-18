efa73820997543a353c107056ff5403e36a48bbf
Milenko Supic <supic82@gmail.com>
2023-04-18 15:57:16 +0200

    Added more metadata for org.apache.tomcat:tomcat-jdbc:10.1.7

--- tests/src/org.apache.tomcat/tomcat-jdbc/10.1.7/generate-metadata.sh
+++ tests/src/org.apache.tomcat/tomcat-jdbc/10.1.7/generate-metadata.sh
@@ -3,3 +3,4 @@
 ./gradlew deleteGeneratedMetadata
 ./gradlew test --tests "org_apache_tomcat.tomcat_jdbc.TomcatJdbcTest" -Pagent metadataCopy
 ./gradlew test --tests "org_apache_tomcat.tomcat_jdbc.DriverTest" -Pagent metadataCopy
+./gradlew prettyPrintMetadata
