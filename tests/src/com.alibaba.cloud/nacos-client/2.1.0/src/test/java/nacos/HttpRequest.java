40e7a09c02e644844213983ae02e641c9bc9548b
yuluo <1481556636@qq.com>
2023-06-01 18:41:11 +0800

    add nacos-client metadata

---
+++ tests/src/com.alibaba.cloud/nacos-client/2.1.0/src/test/java/nacos/HttpRequest.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package nacos;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.List;
+import java.util.Map;
+
+public class HttpRequest {
+
+	private HttpRequest() {
+	}
+
+	/**
+	 * Send get request to URL address.
+	 * @param url request url
+	 * @param param params, format: name1=value1&name2=value2
+	 * @return response result
+	 */
+	public static String sendGet(String url, String param) {
+		StringBuilder result = new StringBuilder();
+		BufferedReader in = null;
+		try {
+			String urlNameString = url + "?" + param;
+			URL realUrl = new URL(urlNameString);
+			URLConnection connection = realUrl.openConnection();
+			connection.setRequestProperty("accept", "*/*");
+			connection.setRequestProperty("connection", "Keep-Alive");
+			connection.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			connection.connect();
+			Map<String, List<String>> map = connection.getHeaderFields();
+			for (String key : map.keySet()) {
+				System.out.println(key + "--->" + map.get(key));
+			}
+			in = new BufferedReader(new InputStreamReader(
+					connection.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result.append(line);
+			}
+		}
+		catch (Exception e) {
+			System.out.println("Send get request exception: [" + e + "]");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (in != null) {
+					in.close();
+				}
+			}
+			catch (Exception e2) {
+				e2.printStackTrace();
+			}
+		}
+		return result.toString();
+	}
+
+	/**
+	 * Send post request to URL address.
+	 * @param url request url
+	 * @param param params, format: name1=value1&name2=value2
+	 * @return response result
+	 */
+	public static String sendPost(String url, String param) {
+		PrintWriter out = null;
+		BufferedReader in = null;
+		StringBuilder result = new StringBuilder();
+		try {
+			URL realUrl = new URL(url);
+			URLConnection conn = realUrl.openConnection();
+			conn.setRequestProperty("accept", "*/*");
+			conn.setRequestProperty("connection", "Keep-Alive");
+			conn.setRequestProperty("user-agent",
+					"Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			out = new PrintWriter(conn.getOutputStream());
+			out.print(param);
+			out.flush();
+			in = new BufferedReader(
+					new InputStreamReader(conn.getInputStream()));
+			String line;
+			while ((line = in.readLine()) != null) {
+				result.append(line);
+			}
+		}
+		catch (Exception e) {
+			System.out.println("send post request exception: [" + e + "]");
+			e.printStackTrace();
+		}
+		finally {
+			try {
+				if (out != null) {
+					out.close();
+				}
+				if (in != null) {
+					in.close();
+				}
+			}
+			catch (IOException ex) {
+				ex.printStackTrace();
+			}
+		}
+		return result.toString();
+	}
+}
+
