3ba5c628eb4c8ce0d3e8e570e1c09b15633fef79
David Nestorovic <david.nestorovic@oracle.com>
2023-07-25 16:29:01 +0200

    Assert that dockerfiles will contain only one line

--- tests/tck-build-logic/src/main/java/org/graalvm/internal/tck/DockerUtils.java
+++ tests/tck-build-logic/src/main/java/org/graalvm/internal/tck/DockerUtils.java
@@ -2,6 +2,8 @@ package org.graalvm.internal.tck;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -12,7 +14,7 @@ import java.util.stream.Collectors;
 
 public class DockerUtils {
 
-    public static Set<String> getAllowedImages() throws IOException {
+    public static Set<String> getAllowedImages() throws IOException, URISyntaxException {
         String dockerfileDirectory = Paths.get("./tests/tck-build-logic/src/main/resources/allowed-docker-images").toString();
         File[] dockerFiles = new File(dockerfileDirectory).listFiles();
         if (dockerFiles == null) {
@@ -27,6 +29,10 @@ public class DockerUtils {
                     .filter(line -> line.startsWith(FROM))
                     .map(line -> line.substring(FROM.length()).trim())
                     .toList();
+            if (images.size() != 1) {
+                throw new RuntimeException("Dockerfile: " + dockerFile.getName() + " must contain only one FROM line, got '" + images.size() + "' (" + images+ "). Please read our documentation: "
+                + new URI("https://github.com/oracle/graalvm-reachability-metadata/blob/master/CONTRIBUTING.md#providing-the-tests-that-use-docker"));
+            }
 
             allowedImages.addAll(images);
         }
