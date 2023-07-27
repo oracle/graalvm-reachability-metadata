621bdf06a149e48e2106219cd91b6cc3848ec54c
Milenko Supic <supic82@gmail.com>
2023-07-27 13:19:16 +0200

    Added metadata for org.apache.kafka:kafka-clients:3.5.1

--- tests/src/org.apache.kafka/kafka-clients/3.5.1/src/test/java/org_apache_kafka/kafka_clients/KafkaClientsTest.java
+++ tests/src/org.apache.kafka/kafka-clients/3.5.1/src/test/java/org_apache_kafka/kafka_clients/KafkaClientsTest.java
@@ -22,15 +22,39 @@ import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.KafkaFuture;
+import org.apache.kafka.common.serialization.BooleanDeserializer;
+import org.apache.kafka.common.serialization.BooleanSerializer;
+import org.apache.kafka.common.serialization.ByteArrayDeserializer;
+import org.apache.kafka.common.serialization.ByteArraySerializer;
+import org.apache.kafka.common.serialization.ByteBufferDeserializer;
+import org.apache.kafka.common.serialization.ByteBufferSerializer;
+import org.apache.kafka.common.serialization.BytesDeserializer;
+import org.apache.kafka.common.serialization.BytesSerializer;
+import org.apache.kafka.common.serialization.DoubleDeserializer;
+import org.apache.kafka.common.serialization.DoubleSerializer;
+import org.apache.kafka.common.serialization.FloatDeserializer;
+import org.apache.kafka.common.serialization.FloatSerializer;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
+import org.apache.kafka.common.serialization.LongDeserializer;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.ShortDeserializer;
+import org.apache.kafka.common.serialization.ShortSerializer;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.apache.kafka.common.serialization.UUIDDeserializer;
+import org.apache.kafka.common.serialization.UUIDSerializer;
+import org.apache.kafka.common.serialization.VoidDeserializer;
+import org.apache.kafka.common.serialization.VoidSerializer;
 import org.apache.kafka.common.utils.Time;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -45,6 +69,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class KafkaClientsTest {
 
+    private static final Logger logger = LoggerFactory.getLogger("KafkaClientsTest");
+
     private static final String KAFKA_SERVER = "localhost:9092";
 
     private EmbeddedZookeeper zookeeper;
@@ -54,21 +80,25 @@ class KafkaClientsTest {
     @BeforeAll
     void beforeAll() {
         zookeeper = new EmbeddedZookeeper();
+        logger.info("Embedded zookeeper started");
         Properties brokerProperties = new Properties();
         brokerProperties.setProperty("zookeeper.connect", "localhost:" + zookeeper.port());
         brokerProperties.setProperty("log.dirs", TestUtils.tempDir().getPath());
         brokerProperties.setProperty("listeners", "PLAINTEXT://" + KAFKA_SERVER);
         brokerProperties.setProperty("offsets.topic.replication.factor", "1");
         kafkaServer = TestUtils.createServer(new KafkaConfig(brokerProperties), Time.SYSTEM);
+        logger.info("Embedded kafka server started");
     }
 
     @AfterAll
     void afterAll() {
         if (kafkaServer != null) {
             kafkaServer.shutdown();
+            logger.info("Embedded kafka server stopped");
         }
         if (zookeeper != null) {
             zookeeper.shutdown();
+            logger.info("Embedded zookeeper stopped");
         }
     }
 
@@ -116,5 +146,51 @@ class KafkaClientsTest {
         assertThat(receivedMessages)
                 .hasSize(2)
                 .containsExactly("message0", "message1");
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {
+            BooleanSerializer.class,
+            BytesSerializer.class,
+            ByteArraySerializer.class,
+            ByteBufferSerializer.class,
+            DoubleSerializer.class,
+            FloatSerializer.class,
+            IntegerSerializer.class,
+            LongSerializer.class,
+            ShortSerializer.class,
+            StringSerializer.class,
+            UUIDSerializer.class,
+            VoidSerializer.class})
+    void testSerializers(Class valueSerializer) {
+        Map<String, Object> producerProperties = new HashMap<>();
+        producerProperties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        producerProperties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, IntegerSerializer.class);
+        producerProperties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, valueSerializer);
+        KafkaProducer<Integer, String> producer = new KafkaProducer<>(producerProperties);
+        assertThat(producer).isNotNull();
+    }
+
+    @ParameterizedTest
+    @ValueSource(classes = {
+            BooleanDeserializer.class,
+            BytesDeserializer.class,
+            ByteArrayDeserializer.class,
+            ByteBufferDeserializer.class,
+            DoubleDeserializer.class,
+            FloatDeserializer.class,
+            IntegerDeserializer.class,
+            LongDeserializer.class,
+            ShortDeserializer.class,
+            StringDeserializer.class,
+            UUIDDeserializer.class,
+            VoidDeserializer.class})
+    void testDeserializers(Class valueDeserializer) {
+        Map<String, Object> consumerProperties = new HashMap<>();
+        consumerProperties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, KAFKA_SERVER);
+        consumerProperties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, IntegerDeserializer.class);
+        consumerProperties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, valueDeserializer);
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(consumerProperties);
+        assertThat(consumer).isNotNull();
     }
 }
