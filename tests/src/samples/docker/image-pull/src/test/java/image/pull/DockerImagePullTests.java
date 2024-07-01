5bc3e665403101712c3c16662bd5ee0d31d54248
David Nestorovic <david.nestorovic@oracle.com>
2024-07-01 16:53:05 +0200

    Bump mysql docker image version

--- tests/src/samples/docker/image-pull/src/test/java/image/pull/DockerImagePullTests.java
+++ tests/src/samples/docker/image-pull/src/test/java/image/pull/DockerImagePullTests.java
@@ -32,7 +32,7 @@ class DockerImagePullTests {
 
     @Test
     void pullAllowedImage() throws Exception {
-        runImage("container-registry.oracle.com/mysql/community-server:8.2", true);
+        runImage("container-registry.oracle.com/mysql/community-server:9.0.0", true);
     }
 
     @Test
