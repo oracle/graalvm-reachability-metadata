fd6496ac0baa9c6e6c2b073e551a83005f4a9444
David Nestorovic <david.nestorovic@oracle.com>
2024-10-04 13:05:26 +0200

    Update nats docker image

--- tests/src/io.nats/jnats/2.16.11/src/test/java/io_nats/jnats/JnatsTest.java
+++ tests/src/io.nats/jnats/2.16.11/src/test/java/io_nats/jnats/JnatsTest.java
@@ -30,7 +30,7 @@ class JnatsTest {
     @BeforeAll
     public void init() throws IOException {
         System.out.println("Starting NATS ...");
-        process = new ProcessBuilder("docker", "run", "--rm", "-p", "4222:4222", "nats:2.10.17").inheritIO().start();
+        process = new ProcessBuilder("docker", "run", "--rm", "-p", "4222:4222", "nats:2.10.21").inheritIO().start();
 
         waitUntil(() -> {
             openConnection().close();
