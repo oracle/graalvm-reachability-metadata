c452fe2d53ed6101dfd23d845395ef8733b8daf2
David Nestorovic <david.nestorovic@oracle.com>
2024-10-04 13:09:37 +0200

    Update mysql docker image

--- tests/src/samples/docker/image-pull/src/test/java/image/pull/DockerImagePullTests.java
+++ tests/src/samples/docker/image-pull/src/test/java/image/pull/DockerImagePullTests.java
@@ -32,7 +32,7 @@ class DockerImagePullTests {
 
     @Test
     void pullAllowedImage() throws Exception {
-        runImage("container-registry.oracle.com/mysql/community-server:9.0.0", true);
+        runImage("container-registry.oracle.com/mysql/community-server:9.0.1", true);
     }
 
     @Test
