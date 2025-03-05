c3af177ac42edb09443f49cfd17356b814270fc9
David Nestorovic <david.nestorovic@oracle.com>
2025-03-05 14:24:35 +0100

    Register contributing task

--- tests/tck-build-logic/src/main/java/org/graalvm/internal/tck/ContributionTask.java
+++ tests/tck-build-logic/src/main/java/org/graalvm/internal/tck/ContributionTask.java
@@ -1,2 +1,13 @@
-package org.graalvm.internal.tck;public class ContributionTask {
+package org.graalvm.internal.tck;
+
+import org.gradle.api.DefaultTask;
+import org.gradle.api.tasks.TaskAction;
+
+public abstract class ContributionTask extends DefaultTask {
+
+    @TaskAction
+    void run() {
+        System.out.println("Hello from contribution task!");
+    }
+
 }
