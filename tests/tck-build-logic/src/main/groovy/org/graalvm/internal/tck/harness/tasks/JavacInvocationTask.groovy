3bc97bf79bfde12ab089900378a79ea2d71f0322
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-11-05 12:18:14 +0100

    Distinguish failure types: javac, native-test

---
+++ tests/tck-build-logic/src/main/groovy/org/graalvm/internal/tck/harness/tasks/JavacInvocationTask.groovy
@@ -0,0 +1,35 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+package org.graalvm.internal.tck.harness.tasks
+
+import org.gradle.api.tasks.Input
+
+import javax.inject.Inject
+/**
+ * Task that is used to compile subprojects with javac.
+ */
+@SuppressWarnings("unused")
+abstract class JavacInvocationTask extends AbstractSubprojectTask {
+
+    @Inject
+    JavacInvocationTask(String coordinates) {
+        super(coordinates)
+    }
+
+    @Override
+    @Input
+    List<String> getCommand() {
+        return [tckExtension.repoRoot.get().asFile.toPath().resolve("gradlew").toString(), "compileTestJava"]
+    }
+
+    @Override
+    protected String getErrorMessage(int exitCode) {
+        "Compilation failed"
+    }
+
+}
