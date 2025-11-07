31ab4e925aa987545a9976b8a6de8b5430f74951
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-11-07 18:05:37 +0100

    Print the reproducer and commands

--- .github/workflows/run-consecutive-tests.sh
+++ .github/workflows/run-consecutive-tests.sh
@@ -6,7 +6,6 @@
 # work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 set -u
-set -x
 
 if [ $# -ne 2 ]; then
   echo "Usage: $0 <test-coordinates> <versions-json-array>"
@@ -23,43 +22,59 @@ VERSIONS_JSON="${VERSIONS_JSON%"${VERSIONS_JSON##*[!\']}"}"
 
 # Parse versions with jq
 readarray -t VERSIONS < <(echo "$VERSIONS_JSON" | jq -r '.[]')
+export DELIMITER="========================================================================================"
 
-for VERSION in "${VERSIONS[@]}"; do
-  # check if javac works for the new version
-  GVM_TCK_LV="$VERSION" ./gradlew clean javac -Pcoordinates="$TEST_COORDINATES"
-  RESULT=$?
-  if [ "$RESULT" -ne 0 ]; then
-    echo "FAILED [javac compile]:$VERSION"
-    break
-  fi
+run_multiple_attempts() {
+  local stage="$1"
+  local max_attempts="$2"
+  local gradle_command="$3"
 
-  # check if native-image can be built
-  GVM_TCK_LV="$VERSION" ./gradlew clean nativeTestCompile -Pcoordinates="$TEST_COORDINATES"
-  RESULT=$?
-    if [ "$RESULT" -ne 0 ]; then
-      echo "FAILED [native-image build]:$VERSION"
-      break
+  echo "$DELIMITER"
+  echo " $TEST_COORDINATES:$VERSION stage $stage"
+  echo "$DELIMITER"
+
+  local attempt=0
+  local result=0
+
+  while [ $attempt -lt "$max_attempts" ]; do
+    local cmd_str="GVM_TCK_LV=\"$VERSION\" ./gradlew clean $gradle_command -Pcoordinates=\"$TEST_COORDINATES\""
+    if [ $attempt -gt 0 ]; then
+      echo "Re-running stage '$stage' (attempt $((attempt + 1))/$max_attempts)"
     fi
 
-  echo "Running test with GVM_TCK_LV=$VERSION and coordinates=$TEST_COORDINATES"
-  GVM_TCK_LV="$VERSION" ./gradlew test -Pcoordinates="$TEST_COORDINATES"
-  RESULT=$?
-  ATTEMPTS=1
+    eval "$cmd_str"
+    result=$?
 
-  # maybe we failed because the test was flaky => try two more times to be sure
-  while [ "$RESULT" -ne 0 ] && [ $ATTEMPTS -le 2 ]; do
-    echo "Re-running the test with GVM_TCK_LV=$VERSION and coordinates=$TEST_COORDINATES"
-    GVM_TCK_LV="$VERSION" ./gradlew clean test -Pcoordinates="$TEST_COORDINATES"
-    RESULT=$?
-    ATTEMPTS=$((ATTEMPTS + 1))
+    if [ "$result" -eq 0 ]; then
+      return 0
+    fi
+
+    attempt=$((attempt + 1))
   done
 
-  if [ "$RESULT" -eq 0 ]; then
-    echo "PASSED:$VERSION"
-  else
-    echo "FAILED [native-image run]:$VERSION"
+  echo "FAILED [$stage][$VERSION][$cmd_str]"
+  return $result
+}
+
+for VERSION in "${VERSIONS[@]}"; do
+  echo "$DELIMITER"
+  echo " Testing $TEST_COORDINATES:$VERSION"
+  echo "$DELIMITER"
+
+
+  if ! run_multiple_attempts "javac compile" 1 javac; then
     break
   fi
+
+  if ! run_multiple_attempts "native-image build" 1 nativeTestCompile; then
+    break
+  fi
+
+  if ! run_multiple_attempts "native-image run" 3 test; then
+    break
+  fi
+
+  echo "PASSED:$VERSION"
 done
 
 exit 0
