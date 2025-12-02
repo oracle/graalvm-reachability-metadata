da3ebb02bfecf10c9416d9cb417a3dccfcf8599a
jvukicev <jovan.vukicevic@oracle.com>
2025-12-02 15:03:33 +0100

    Add license

--- check-library-support.sh
+++ check-library-support.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
+
+# Copyright and related rights waived via CC0
+#
+# You should have received a copy of the CC0 legalcode along with this
+# work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+
 set -euo pipefail
 
 if [ "$#" -ne 1 ]; then
