586fc26f0bc3fc78f360abdac715fcaf8e6c5edc
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-12-08 08:58:51 +0100

    Fix copyright

--- tests/src/org.hibernate/hibernate-core/5.6.14.Final/generate-metadata.sh
+++ tests/src/org.hibernate/hibernate-core/5.6.14.Final/generate-metadata.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+# Copyright and related rights waived via CC0
+#
+# You should have received a copy of the CC0 legalcode along with this
+# work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
 
 ./gradlew deleteGeneratedMetadata
 ./gradlew test --tests "org_hibernate.hibernate_core.H2DialectHibernateTest" -Pagent metadataCopy
