d0ea5b8bd7fd972f3b6c64fcd343b673d5d4b9ac
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-12-11 09:18:03 +0100

    tck-build-logic: migrate JavaTestInvocationTask from Groovy to Java; preserve task behavior

--- tests/tck-build-logic/src/main/groovy/org/graalvm/internal/tck/harness/tasks/JavaTestInvocationTask.java
+++ tests/tck-build-logic/src/main/groovy/org/graalvm/internal/tck/harness/tasks/JavaTestInvocationTask.java
@@ -4,21 +4,26 @@
  * You should have received a copy of the CC0 legalcode along with this
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
-package org.graalvm.internal.tck.harness.tasks
+package org.graalvm.internal.tck.harness.tasks;
+
+import java.util.List;
 
 /**
  * Task that is used to run JVM tests (Gradle 'test') on subprojects.
  */
 @SuppressWarnings("unused")
-abstract class JavaTestInvocationTask extends AllCoordinatesExecTask {
+public abstract class JavaTestInvocationTask extends AllCoordinatesExecTask {
 
     @Override
-    List<String> commandFor(String coordinates) {
-        return [tckExtension.repoRoot.get().asFile.toPath().resolve("gradlew").toString(), "test"]
+    public List<String> commandFor(String coordinates) {
+        return List.of(
+                tckExtension.getRepoRoot().get().getAsFile().toPath().resolve("gradlew").toString(),
+                "test"
+        );
     }
 
     @Override
     protected String errorMessageFor(String coordinates, int exitCode) {
-        "Java tests failed"
+        return "Java tests failed";
     }
 }
