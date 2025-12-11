8bfde85f387543c09e2d4cbb5b7682e550c527b5
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2025-12-11 09:19:37 +0100

    tck-build-logic: migrate NativeTestCompileInvocationTask from Groovy to Java; preserve task behavior

--- tests/tck-build-logic/src/main/groovy/org/graalvm/internal/tck/harness/tasks/NativeTestCompileInvocationTask.java
+++ tests/tck-build-logic/src/main/groovy/org/graalvm/internal/tck/harness/tasks/NativeTestCompileInvocationTask.java
@@ -4,21 +4,26 @@
  * You should have received a copy of the CC0 legalcode along with this
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
-package org.graalvm.internal.tck.harness.tasks
+package org.graalvm.internal.tck.harness.tasks;
+
+import java.util.List;
 
 /**
  * Task that is used to compile native tests (Gradle 'nativeTestCompile') on subprojects.
  */
 @SuppressWarnings("unused")
-abstract class NativeTestCompileInvocationTask extends AllCoordinatesExecTask {
+public abstract class NativeTestCompileInvocationTask extends AllCoordinatesExecTask {
 
     @Override
-    List<String> commandFor(String coordinates) {
-        return [tckExtension.repoRoot.get().asFile.toPath().resolve("gradlew").toString(), "nativeTestCompile"]
+    public List<String> commandFor(String coordinates) {
+        return List.of(
+                tckExtension.getRepoRoot().get().getAsFile().toPath().resolve("gradlew").toString(),
+                "nativeTestCompile"
+        );
     }
 
     @Override
     protected String errorMessageFor(String coordinates, int exitCode) {
-        "Native test compilation failed"
+        return "Native test compilation failed";
     }
 }
