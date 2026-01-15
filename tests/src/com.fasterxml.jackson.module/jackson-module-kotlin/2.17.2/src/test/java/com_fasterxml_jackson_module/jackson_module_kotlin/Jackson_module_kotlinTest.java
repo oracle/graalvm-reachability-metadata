fb58aef9af041b13c4227485b756ab377c0ec42c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-01-15 14:41:46 +0100

    Scaffold com.fasterxml.jackson.module:jackson-module-kotlin:2.17.2

---
+++ tests/src/com.fasterxml.jackson.module/jackson-module-kotlin/2.17.2/src/test/java/com_fasterxml_jackson_module/jackson_module_kotlin/Jackson_module_kotlinTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_fasterxml_jackson_module.jackson_module_kotlin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+class Jackson_module_kotlinTest {
+    @Test
+    void test() throws Exception {
+        fail("TODO: Add test logic here");
+    }
+}
