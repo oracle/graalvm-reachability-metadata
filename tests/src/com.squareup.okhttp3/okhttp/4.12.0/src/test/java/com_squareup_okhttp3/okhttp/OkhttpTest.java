9364701fa21fa71016f11c6d03f58a4e56d34441
Mihailo <mihailo.markovic@oracle.com>
2026-01-26 20:59:09 +0100

    Scaffold com.squareup.okhttp3:okhttp:4.12.0

---
+++ tests/src/com.squareup.okhttp3/okhttp/4.12.0/src/test/java/com_squareup_okhttp3/okhttp/OkhttpTest.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_squareup_okhttp3.okhttp;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.fail;
+
+class OkhttpTest {
+    @Test
+    void test() throws Exception {
+        fail("TODO: Add test logic here");
+    }
+}
