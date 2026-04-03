4c4fcd3c603dac761631d398aece0bec6590b5bc
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-03 15:30:22 +0200

    Scaffold dev.langchain4j:langchain4j:1.9.0

---
+++ tests/src/dev.langchain4j/langchain4j/1.9.0/src/test/java/dev_langchain4j/langchain4j/Langchain4jTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package dev_langchain4j.langchain4j;
+
+import org.junit.jupiter.api.Test;
+
+class Langchain4jTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
