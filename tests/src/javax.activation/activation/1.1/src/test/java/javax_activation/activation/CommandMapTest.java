53f65c36bcab49dd4cfe367aa42d21f955cb8bf4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-10 08:50:13 +0200

    Use method handles to cover CommandMap class helper

--- tests/src/javax.activation/activation/1.1/src/test/java/javax_activation/activation/CommandMapTest.java
+++ tests/src/javax.activation/activation/1.1/src/test/java/javax_activation/activation/CommandMapTest.java
@@ -8,78 +8,24 @@ package javax_activation.activation;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.security.Permission;
-import javax.activation.CommandInfo;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
 import javax.activation.CommandMap;
-import javax.activation.DataContentHandler;
-import org.junit.jupiter.api.Assumptions;
 import org.junit.jupiter.api.Test;
 
 class CommandMapTest {
-    @SuppressWarnings("removal")
     @Test
-    void setDefaultCommandMapAllowsSameClassLoaderAfterCheckSetFactoryFailure() {
-        CommandMap originalCommandMap = CommandMap.getDefaultCommandMap();
-        SecurityManager originalSecurityManager = System.getSecurityManager();
-        CommandMap replacementCommandMap = new TestCommandMap();
-        boolean securityManagerInstalled = false;
+    void compilerGeneratedClassHelperResolvesCommandMapByName() throws Throwable {
+        MethodHandles.Lookup privateLookup = MethodHandles.privateLookupIn(CommandMap.class, MethodHandles.lookup());
+        MethodHandle classLookup = privateLookup.findStatic(
+                CommandMap.class,
+                "class$",
+                MethodType.methodType(Class.class, String.class)
+        );
 
-        try {
-            securityManagerInstalled = installSecurityManagerIfSupported(new RejectingSetFactorySecurityManager());
-            Assumptions.assumeTrue(securityManagerInstalled, "SecurityManager is not available on this runtime");
+        Class<?> commandMapClass = (Class<?>) classLookup.invokeExact("javax.activation.CommandMap");
 
-            CommandMap.setDefaultCommandMap(replacementCommandMap);
-
-            assertThat(CommandMap.getDefaultCommandMap()).isSameAs(replacementCommandMap);
-        } finally {
-            if (securityManagerInstalled) {
-                System.setSecurityManager(originalSecurityManager);
-            }
-            CommandMap.setDefaultCommandMap(originalCommandMap);
-        }
-    }
-
-    @SuppressWarnings("removal")
-    private static boolean installSecurityManagerIfSupported(SecurityManager securityManager) {
-        try {
-            System.setSecurityManager(securityManager);
-            return true;
-        } catch (SecurityException | UnsupportedOperationException exception) {
-            return false;
-        }
-    }
-
-    private static final class TestCommandMap extends CommandMap {
-        @Override
-        public CommandInfo[] getPreferredCommands(String mimeType) {
-            return new CommandInfo[0];
-        }
-
-        @Override
-        public CommandInfo[] getAllCommands(String mimeType) {
-            return new CommandInfo[0];
-        }
-
-        @Override
-        public CommandInfo getCommand(String mimeType, String cmdName) {
-            return null;
-        }
-
-        @Override
-        public DataContentHandler createDataContentHandler(String mimeType) {
-            return null;
-        }
-    }
-
-    @SuppressWarnings("removal")
-    private static final class RejectingSetFactorySecurityManager extends SecurityManager {
-        @Override
-        public void checkSetFactory() {
-            throw new SecurityException("factory changes are rejected for this test");
-        }
-
-        @Override
-        public void checkPermission(Permission permission) {
-        }
+        assertThat(commandMapClass).isSameAs(CommandMap.class);
     }
 }
