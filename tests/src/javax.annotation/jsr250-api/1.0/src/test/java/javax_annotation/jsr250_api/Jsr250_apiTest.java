2dceb78de6b9fe8bf223c34661a3fbf2500fe917
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-13 10:55:01 +0200

    Scaffold javax.annotation:jsr250-api:1.0

---
+++ tests/src/javax.annotation/jsr250-api/1.0/src/test/java/javax_annotation/jsr250_api/Jsr250_apiTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package javax_annotation.jsr250_api;
+
+import org.junit.jupiter.api.Test;
+
+class Jsr250_apiTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
