20bd191641ea6816d2049d009a941b73c8f71d7c
jvukicev <jovan.vukicevic@oracle.com>
2026-04-14 11:16:12 +0200

    Update generated library support for org.checkerframework:checker-compat-qual:2.5.5

--- tests/src/org.checkerframework/checker-compat-qual/2.5.5/src/test/java/org_checkerframework/checker_compat_qual/Checker_compat_qualTest.java
+++ tests/src/org.checkerframework/checker-compat-qual/2.5.5/src/test/java/org_checkerframework/checker_compat_qual/Checker_compat_qualTest.java
@@ -6,9 +6,14 @@
  */
 package org_checkerframework.checker_compat_qual;
 
+import java.lang.reflect.AnnotatedArrayType;
 import java.lang.reflect.AnnotatedParameterizedType;
 import java.lang.reflect.AnnotatedType;
+import java.lang.reflect.AnnotatedWildcardType;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.lang.reflect.TypeVariable;
+import java.util.List;
 
 import org.assertj.core.api.Assertions;
 import org.checkerframework.checker.nullness.compatqual.KeyForDecl;
@@ -58,6 +63,31 @@ class Checker_compat_qualTest {
         Assertions.assertThat(keyForType.value()).containsExactly("entries");
     }
 
+    @Test
+    void typeUseAnnotationsAreVisibleOnArraysAndWildcardBounds() throws NoSuchFieldException, NoSuchMethodException {
+        Field field = MemberFixture.class.getDeclaredField("entries");
+        AnnotatedArrayType fieldType = (AnnotatedArrayType) field.getAnnotatedType();
+
+        Assertions.assertThat(fieldType.getAnnotation(NonNullType.class)).isNotNull();
+        Assertions.assertThat(fieldType.getAnnotatedGenericComponentType().getAnnotation(NullableType.class)).isNotNull();
+
+        Method method = MemberFixture.class.getDeclaredMethod("transform", String[].class, List.class);
+        AnnotatedArrayType returnType = (AnnotatedArrayType) method.getAnnotatedReturnType();
+        AnnotatedArrayType arrayParameterType = (AnnotatedArrayType) method.getAnnotatedParameterTypes()[0];
+        AnnotatedParameterizedType wildcardListType = (AnnotatedParameterizedType) method.getAnnotatedParameterTypes()[1];
+
+        Assertions.assertThat(returnType.getAnnotation(MonotonicNonNullType.class)).isNotNull();
+        Assertions.assertThat(returnType.getAnnotatedGenericComponentType().getAnnotation(PolyNullType.class)).isNotNull();
+        Assertions.assertThat(arrayParameterType.getAnnotation(NullableType.class)).isNotNull();
+        Assertions.assertThat(arrayParameterType.getAnnotatedGenericComponentType().getAnnotation(NonNullType.class))
+                .isNotNull();
+
+        AnnotatedWildcardType wildcardType = (AnnotatedWildcardType) wildcardListType.getAnnotatedActualTypeArguments()[0];
+        KeyForType keyForType = wildcardType.getAnnotatedUpperBounds()[0].getAnnotation(KeyForType.class);
+        Assertions.assertThat(keyForType).isNotNull();
+        Assertions.assertThat(keyForType.value()).containsExactly("entries");
+    }
+
     @NonNullDecl
     @NullableDecl
     @MonotonicNonNullDecl
@@ -75,5 +105,16 @@ class Checker_compat_qualTest {
     private static final class TypeUseFixture<@PolyNullType T>
             extends @NonNullType Base<@MonotonicNonNullType String>
             implements @NullableType Marker<@KeyForType("entries") String> {
+    }
+
+    private static final class MemberFixture {
+        private @NullableType String @NonNullType [] entries;
+
+        private @PolyNullType String @MonotonicNonNullType [] transform(
+                @NonNullType String @NullableType [] input,
+                List<? extends @KeyForType("entries") String> keys
+        ) {
+            return input;
+        }
     }
 }
