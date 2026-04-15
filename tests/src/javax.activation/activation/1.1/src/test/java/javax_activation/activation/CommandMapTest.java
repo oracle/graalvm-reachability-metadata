fcb64f2804adf29dbfcf29b9b9489237a6359619
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-15 22:17:25 +0200

    Run CommandMap JVM coverage on Java 17

--- tests/src/javax.activation/activation/1.1/src/test/java/javax_activation/activation/CommandMapTest.java
+++ tests/src/javax.activation/activation/1.1/src/test/java/javax_activation/activation/CommandMapTest.java
@@ -4,19 +4,50 @@
  * You should have received a copy of the CC0 legalcode along with this
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
-package javax.activation;
+package javax_activation.activation;
 
-import java.lang.reflect.Method;
+import java.security.Permission;
+
+import javax.activation.CommandMap;
+import javax.activation.MailcapCommandMap;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.DisabledIfSystemProperty;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CommandMapTest {
     @Test
-    void classDollarLoadsCommandMapByName() throws Exception {
-        Method classLookupMethod = CommandMap.class.getDeclaredMethod("class$", String.class);
+    @DisabledIfSystemProperty(named = "org.graalvm.nativeimage.imagecode", matches = "runtime")
+    void setDefaultCommandMapHandlesDeniedSetFactoryFromSameClassLoader() {
+        CommandMap originalCommandMap = CommandMap.getDefaultCommandMap();
+        SecurityManager originalSecurityManager = System.getSecurityManager();
+        MailcapCommandMap replacementCommandMap = new MailcapCommandMap();
 
-        assertThat(classLookupMethod.invoke(null, "javax.activation.CommandMap")).isSameAs(CommandMap.class);
+        try {
+            System.setSecurityManager(new DenyingSetFactorySecurityManager());
+
+            CommandMap.setDefaultCommandMap(replacementCommandMap);
+
+            assertThat(CommandMap.getDefaultCommandMap()).isSameAs(replacementCommandMap);
+        } finally {
+            System.setSecurityManager(originalSecurityManager);
+            CommandMap.setDefaultCommandMap(originalCommandMap);
+        }
+    }
+
+    private static final class DenyingSetFactorySecurityManager extends SecurityManager {
+        @Override
+        public void checkPermission(Permission permission) {
+        }
+
+        @Override
+        public void checkPermission(Permission permission, Object context) {
+        }
+
+        @Override
+        public void checkSetFactory() {
+            throw new SecurityException("setFactory denied for test coverage");
+        }
     }
 }
