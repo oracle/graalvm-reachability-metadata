d715fe1cc28a658b5f0f82e5ae318b3a7be376b8
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-17 01:36:46 +0200

    Scaffold com.graphql-java:graphql-java-extended-scalars:19.0

---
+++ tests/src/com.graphql-java/graphql-java-extended-scalars/19.0/src/test/java/com_graphql_java/graphql_java_extended_scalars/Graphql_java_extended_scalarsTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_graphql_java.graphql_java_extended_scalars;
+
+import org.junit.jupiter.api.Test;
+
+class Graphql_java_extended_scalarsTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
