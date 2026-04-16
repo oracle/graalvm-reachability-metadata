094ef70d72ba97fec3d663d15f97646145806bf1
jvukicev <jovan.vukicevic@oracle.com>
2026-04-16 23:38:19 +0200

    Add MapMaker serialization proxy coverage test

---
+++ tests/src/org.immutables/value/2.8.2/src/test/java/org/immutables/value/internal/$guava$/collect/$MapMakerInternalMap$AbstractSerializationProxyTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.immutables.value.internal.$guava$.collect;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.Map;
+import java.util.concurrent.ConcurrentMap;
+import org.junit.jupiter.api.Test;
+
+class $MapMakerInternalMap$AbstractSerializationProxyTest {
+    @Test
+    void serializesAndDeserializesCustomMapEntries() throws Exception {
+        final ConcurrentMap<String, Integer> source = new $MapMaker().weakKeys().makeMap();
+        source.put("alpha", 1);
+        source.put("beta", 2);
+
+        final ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+        try (ObjectOutputStream outputStream = new ObjectOutputStream(byteStream)) {
+            outputStream.writeObject(source);
+        }
+
+        final Map<?, ?> restored;
+        try (ObjectInputStream inputStream = new ObjectInputStream(
+                new ByteArrayInputStream(byteStream.toByteArray()))) {
+            restored = (Map<?, ?>) inputStream.readObject();
+        }
+
+        assertThat(source.getClass().getName())
+                .isEqualTo("org.immutables.value.internal.$guava$.collect.$MapMakerInternalMap");
+        assertThat(restored.getClass()).isEqualTo(source.getClass());
+        assertThat(restored).containsEntry("alpha", 1).containsEntry("beta", 2);
+    }
+}
