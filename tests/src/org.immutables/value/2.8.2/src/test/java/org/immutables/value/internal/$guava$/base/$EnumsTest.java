e9a8292295ce0e2b0211424ec3504e1a8a9247d9
jvukicev <jovan.vukicevic@oracle.com>
2026-04-17 00:41:47 +0200

    Add $Enums dynamic-access coverage test

---
+++ tests/src/org.immutables/value/2.8.2/src/test/java/org/immutables/value/internal/$guava$/base/$EnumsTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.immutables.value.internal.$guava$.base;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Test;
+
+class $EnumsTest {
+    @Test
+    void getsDeclaredFieldForEnumConstantWithSpecificClassBody() {
+        final Field field = $Enums.getField(SampleEnum.SPECIAL);
+
+        assertThat(field.getName()).isEqualTo("SPECIAL");
+        assertThat(field.getDeclaringClass()).isEqualTo(SampleEnum.class);
+        assertThat(field.isEnumConstant()).isTrue();
+    }
+
+    private enum SampleEnum {
+        PLAIN,
+        SPECIAL {
+        }
+    }
+}
