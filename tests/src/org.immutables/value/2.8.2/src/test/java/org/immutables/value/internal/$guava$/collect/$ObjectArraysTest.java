daf41237fa9c6445d9252ffbaf05bc82b2aa0db0
jvukicev <jovan.vukicevic@oracle.com>
2026-04-17 00:54:22 +0200

    Add $ObjectArrays dynamic-access test coverage

---
+++ tests/src/org.immutables/value/2.8.2/src/test/java/org/immutables/value/internal/$guava$/collect/$ObjectArraysTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.immutables.value.internal.$guava$.collect;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+class $ObjectArraysTest {
+    @Test
+    void createsTypedArrayFromComponentClass() {
+        final String[] values = $ObjectArrays.newArray(String.class, 3);
+
+        assertThat(values).hasSize(3).containsOnlyNulls();
+        assertThat(values).isInstanceOf(String[].class);
+        assertThat(values.getClass().getComponentType()).isEqualTo(String.class);
+    }
+}
