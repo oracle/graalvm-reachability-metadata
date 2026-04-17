465fec9b60b630b12a7aba0ac516a9044dfee58d
jvukicev <jovan.vukicevic@oracle.com>
2026-04-18 00:45:11 +0200

    Refine Seata AbstractDataSourceProvider coverage tests

---
+++ tests/src/org.apache.seata/seata-all/2.1.0/src/test/java/org_apache_seata/seata_all/AbstractDataSourceProviderTest.java
@@ -0,0 +1,156 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_seata.seata_all;
+
+import java.io.IOException;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Base64;
+import java.util.jar.JarEntry;
+import java.util.jar.JarOutputStream;
+
+import javax.sql.DataSource;
+
+import org.apache.seata.common.ConfigurationKeys;
+import org.apache.seata.config.ConfigurationCache;
+import org.apache.seata.core.store.db.AbstractDataSourceProvider;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+public class AbstractDataSourceProviderTest {
+    private static final String MYSQL8_DRIVER_CLASS_NAME = "com.mysql.cj.jdbc.Driver";
+    private static final String CONFIG_TYPE_PROPERTY = "config.type";
+    private static final String CONFIG_FILE_NAME_PROPERTY = "config.file.name";
+    private static final String MYSQL_DRIVER_CLASS_BYTES =
+            "yv66vgAAAEUADQoAAgADBwAEDAAFAAYBABBqYXZhL2xhbmcvT2JqZWN0AQAGPGluaXQ+AQADKClWBwAIAQAYY29tL215c3FsL2NqL2pkYmMvRHJpdmVyAQAEQ29kZQEAD0xpbmVOdW1iZXJUYWJsZQEAClNvdXJjZUZpbGUBAAtEcml2ZXIuamF2YQAhAAcAAgAAAAAAAQABAAUABgABAAkAAAAdAAEAAQAAAAUqtwABsQAAAAEACgAAAAYAAQAAAAIAAQALAAAAAgAM";
+
+    private String previousJavaClassPath;
+    private String previousDriverClassName;
+    private String previousConfigType;
+    private String previousConfigFileName;
+
+    @AfterEach
+    void restoreSystemProperties() {
+        ConfigurationCache.clear();
+        if (previousJavaClassPath == null) {
+            System.clearProperty("java.class.path");
+        } else {
+            System.setProperty("java.class.path", previousJavaClassPath);
+        }
+        if (previousDriverClassName == null) {
+            System.clearProperty(ConfigurationKeys.STORE_DB_DRIVER_CLASS_NAME);
+        } else {
+            System.setProperty(ConfigurationKeys.STORE_DB_DRIVER_CLASS_NAME, previousDriverClassName);
+        }
+        if (previousConfigType == null) {
+            System.clearProperty(CONFIG_TYPE_PROPERTY);
+        } else {
+            System.setProperty(CONFIG_TYPE_PROPERTY, previousConfigType);
+        }
+        if (previousConfigFileName == null) {
+            System.clearProperty(CONFIG_FILE_NAME_PROPERTY);
+        } else {
+            System.setProperty(CONFIG_FILE_NAME_PROPERTY, previousConfigFileName);
+        }
+    }
+
+    @Test
+    void getDriverClassLoaderBuildsAMysqlSpecificLoaderFromTheJdbcSiblingDirectory(@TempDir Path tempDir)
+            throws IOException {
+        Path classpathRoot = Files.createDirectories(tempDir.resolve("classpath-root"));
+        Path jdbcDirectory = Files.createDirectories(classpathRoot.resolve("jdbc"));
+        Path driverJar = jdbcDirectory.resolve("mysql-connector-java-test.jar");
+        writeMysqlDriverJar(driverJar);
+        configureSeata(tempDir.resolve("file.conf"), MYSQL8_DRIVER_CLASS_NAME, classpathRoot.toString());
+
+        TestDataSourceProvider provider = new TestDataSourceProvider();
+        ClassLoader driverClassLoader = provider.driverClassLoader();
+
+        assertThat(driverClassLoader).isInstanceOf(URLClassLoader.class);
+        assertThat(((URLClassLoader) driverClassLoader).getURLs())
+                .extracting(URL::getPath)
+                .contains(driverJar.toAbsolutePath().toString());
+    }
+
+    @Test
+    void validateUsesTheThreadContextClassLoaderToResolveTheConfiguredDriver(@TempDir Path tempDir)
+            throws IOException {
+        configureSeata(tempDir.resolve("file.conf"), DriverMarker.class.getName(), tempDir.toString());
+        ClassLoader originalContextClassLoader = Thread.currentThread().getContextClassLoader();
+        RecordingClassLoader recordingClassLoader = new RecordingClassLoader(originalContextClassLoader);
+
+        try {
+            Thread.currentThread().setContextClassLoader(recordingClassLoader);
+
+            assertThatCode(() -> new TestDataSourceProvider().validate()).doesNotThrowAnyException();
+            assertThat(recordingClassLoader.getLastRequestedClassName()).isEqualTo(DriverMarker.class.getName());
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextClassLoader);
+        }
+    }
+
+    private void configureSeata(Path configFile, String driverClassName, String javaClassPath) throws IOException {
+        Files.writeString(configFile, "# test configuration\n");
+        ConfigurationCache.clear();
+
+        previousJavaClassPath = System.getProperty("java.class.path");
+        previousDriverClassName = System.getProperty(ConfigurationKeys.STORE_DB_DRIVER_CLASS_NAME);
+        previousConfigType = System.getProperty(CONFIG_TYPE_PROPERTY);
+        previousConfigFileName = System.getProperty(CONFIG_FILE_NAME_PROPERTY);
+        System.setProperty("java.class.path", javaClassPath);
+        System.setProperty(ConfigurationKeys.STORE_DB_DRIVER_CLASS_NAME, driverClassName);
+        System.setProperty(CONFIG_TYPE_PROPERTY, "file");
+        System.setProperty(CONFIG_FILE_NAME_PROPERTY, configFile.toString());
+    }
+
+    private static void writeMysqlDriverJar(Path driverJar) throws IOException {
+        byte[] driverClassBytes = Base64.getDecoder().decode(MYSQL_DRIVER_CLASS_BYTES);
+        try (JarOutputStream outputStream = new JarOutputStream(Files.newOutputStream(driverJar))) {
+            outputStream.putNextEntry(new JarEntry("com/mysql/cj/jdbc/Driver.class"));
+            outputStream.write(driverClassBytes);
+            outputStream.closeEntry();
+        }
+    }
+
+    public static final class DriverMarker {
+    }
+
+    private static final class RecordingClassLoader extends ClassLoader {
+        private String lastRequestedClassName;
+
+        private RecordingClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            lastRequestedClassName = name;
+            return super.loadClass(name);
+        }
+
+        private String getLastRequestedClassName() {
+            return lastRequestedClassName;
+        }
+    }
+
+    public static final class TestDataSourceProvider extends AbstractDataSourceProvider {
+        public ClassLoader driverClassLoader() {
+            return getDriverClassLoader();
+        }
+
+        @Override
+        public DataSource doGenerate() {
+            return null;
+        }
+    }
+}
