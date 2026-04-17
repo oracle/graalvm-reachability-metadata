9fb2c657c998e3a37dfec7b4eb5f2c5574a6ecb0
jvukicev <jovan.vukicevic@oracle.com>
2026-04-17 21:34:49 +0200

    Add ReflectionUtil coverage tests for seata-all

---
+++ tests/src/org.apache.seata/seata-all/2.1.0/src/test/java/org_apache_seata/seata_all/ReflectionUtilTest.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_seata.seata_all;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import java.util.List;
+
+import org.apache.seata.common.util.ReflectionUtil;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionUtilTest {
+    @Test
+    void getClassByNameResolvesClassesFromTheContextClassLoader() throws ClassNotFoundException {
+        assertThat(ReflectionUtil.getClassByName(SampleChild.class.getName())).isSameAs(SampleChild.class);
+    }
+
+    @Test
+    void getAllFieldsReturnsOnlyInstanceFieldsAcrossTheHierarchy() {
+        Field[] fields = ReflectionUtil.getAllFields(SyntheticFieldChild.class);
+        List<String> fieldNames = Arrays.stream(fields).map(Field::getName).toList();
+
+        assertThat(fieldNames)
+                .containsExactlyInAnyOrder("childValue", "parentValue", "secret")
+                .doesNotContain("IGNORED_CHILD_STATIC", "IGNORED_PARENT_STATIC", "this$0");
+    }
+
+    @Test
+    void getFieldAndSetFieldValueTraverseSuperclasses() throws NoSuchFieldException {
+        SampleChild sample = new SampleChild();
+        Field field = ReflectionUtil.getField(SampleChild.class, "secret");
+
+        assertThat(field.getDeclaringClass()).isEqualTo(SampleParent.class);
+        assertThat(ReflectionUtil.getFieldValue(sample, field)).isEqualTo("initial");
+
+        ReflectionUtil.setFieldValue(sample, field, "updated");
+
+        assertThat(ReflectionUtil.getFieldValue(sample, "secret")).isEqualTo("updated");
+    }
+
+    @Test
+    void getMethodAndInvokeMethodTraverseSuperclasses() throws Exception {
+        SampleChild sample = new SampleChild();
+        Method method = ReflectionUtil.getMethod(SampleChild.class, "describe", String.class);
+
+        assertThat(method.getDeclaringClass()).isEqualTo(SampleParent.class);
+        assertThat(ReflectionUtil.invokeMethod(sample, method, "prefix")).isEqualTo("prefix-initial");
+    }
+
+    @Test
+    void modifyStaticFinalFieldUpdatesStaticFieldsByName() throws Exception {
+        assertThat(StaticFieldHolder.getValue()).isEqualTo("before");
+
+        try {
+            ReflectionUtil.modifyStaticFinalField(StaticFieldHolder.class, "VALUE", "after");
+            assertThat(StaticFieldHolder.getValue()).isEqualTo("after");
+        } finally {
+            ReflectionUtil.modifyStaticFinalField(StaticFieldHolder.class, "VALUE", "before");
+        }
+    }
+
+    private static class SampleParent {
+        private static final String IGNORED_PARENT_STATIC = "ignored";
+
+        private String parentValue = "parent";
+        private String secret = "initial";
+
+        private String describe(String prefix) {
+            return prefix + "-" + secret;
+        }
+    }
+
+    private static final class SampleChild extends SampleParent {
+    }
+
+    private final class SyntheticFieldChild extends SampleParent {
+        private static final String IGNORED_CHILD_STATIC = "ignored";
+
+        private String childValue = "child";
+    }
+
+    private static final class StaticFieldHolder {
+        private static String VALUE = "before";
+
+        private StaticFieldHolder() {
+        }
+
+        private static String getValue() {
+            return VALUE;
+        }
+    }
+}
