228edb432fdebb90dfab2192f09f900fa9742b2d
jvukicev <jovan.vukicevic@oracle.com>
2026-04-17 14:44:13 +0200

    Scaffold org.checkerframework:checker-qual:3.12.0

---
+++ tests/src/org.checkerframework/checker-qual/3.12.0/src/test/java/org_checkerframework/checker_qual/Checker_qualTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_checkerframework.checker_qual;
+
+import org.junit.jupiter.api.Test;
+
+class Checker_qualTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
