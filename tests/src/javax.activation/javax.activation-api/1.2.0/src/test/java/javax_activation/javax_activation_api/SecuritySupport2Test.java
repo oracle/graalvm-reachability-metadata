73401bd9a4de45f841a517f9d301e5dfd2595922
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-17 14:11:26 +0200

    Add SecuritySupport$2 resource loading test for javax.activation

---
+++ tests/src/javax.activation/javax.activation-api/1.2.0/src/test/java/javax_activation/javax_activation_api/SecuritySupport2Test.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package javax_activation.javax_activation_api;
+
+import javax.activation.MimetypesFileTypeMap;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SecuritySupport2Test {
+    @Test
+    void getContentTypeLoadsDefaultMimeTypesFromClasspathResource() {
+        MimetypesFileTypeMap fileTypeMap = new MimetypesFileTypeMap();
+
+        assertThat(fileTypeMap.getContentType("sample.securitysupport2"))
+                .isEqualTo("text/x-securitysupport2");
+    }
+}
