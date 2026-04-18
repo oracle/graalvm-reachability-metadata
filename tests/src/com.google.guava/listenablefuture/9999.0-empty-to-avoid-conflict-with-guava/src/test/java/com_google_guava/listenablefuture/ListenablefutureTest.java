6f50d3aa1f6b25b9b0573a279692d836d140dc12
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-18 04:00:29 +0200

    Checkpoint generated tests for com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava (1)

--- tests/src/com.google.guava/listenablefuture/9999.0-empty-to-avoid-conflict-with-guava/src/test/java/com_google_guava/listenablefuture/ListenablefutureTest.java
+++ tests/src/com.google.guava/listenablefuture/9999.0-empty-to-avoid-conflict-with-guava/src/test/java/com_google_guava/listenablefuture/ListenablefutureTest.java
@@ -8,9 +8,35 @@ package com_google_guava.listenablefuture;
 
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ListenablefutureTest {
+
     @Test
-    void test() throws Exception {
-        System.out.println("This is just a placeholder, implement your test");
+    void placeholderArtifactDoesNotPublishListenableFutureTypes() {
+        assertThat(classResource("com/google/common/util/concurrent/ListenableFuture.class")).isNull();
+        assertThat(classResource("com/google/common/util/concurrent/AbstractFuture.class")).isNull();
+        assertThat(classResource("com/google/common/util/concurrent/Futures.class")).isNull();
+        assertThat(classResource("com/google/common/util/concurrent/SettableFuture.class")).isNull();
+    }
+
+    @Test
+    void placeholderArtifactDoesNotPublishCoreGuavaTypes() {
+        assertThat(classResource("com/google/common/base/Optional.class")).isNull();
+        assertThat(classResource("com/google/common/collect/ImmutableList.class")).isNull();
+        assertThat(classResource("com/google/common/hash/Hashing.class")).isNull();
+        assertThat(classResource("com/google/common/io/Files.class")).isNull();
+    }
+
+    @Test
+    void placeholderArtifactDoesNotDefineCommonGuavaPackages() {
+        assertThat(classResource("com/google/common/base/package-info.class")).isNull();
+        assertThat(classResource("com/google/common/collect/package-info.class")).isNull();
+        assertThat(classResource("com/google/common/hash/package-info.class")).isNull();
+        assertThat(classResource("com/google/common/util/concurrent/package-info.class")).isNull();
+    }
+
+    private java.net.URL classResource(final String resourcePath) {
+        return getClass().getClassLoader().getResource(resourcePath);
     }
 }
