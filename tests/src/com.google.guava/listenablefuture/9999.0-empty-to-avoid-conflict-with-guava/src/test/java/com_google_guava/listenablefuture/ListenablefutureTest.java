533575b997e7196dd1f6fd8a112514022e1704f3
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-18 14:45:34 +0200

    Scaffold com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava

---
+++ tests/src/com.google.guava/listenablefuture/9999.0-empty-to-avoid-conflict-with-guava/src/test/java/com_google_guava/listenablefuture/ListenablefutureTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_google_guava.listenablefuture;
+
+import org.junit.jupiter.api.Test;
+
+class ListenablefutureTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
