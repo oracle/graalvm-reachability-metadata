ca21b337e4832cc216699ecf07795868e6d11311
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-20 20:43:43 +0200

    Add SnakeYAML custom class loader coverage test

---
+++ tests/src/org.yaml/snakeyaml/1.32/src/test/java/org_yaml/snakeyaml/CustomClassLoaderConstructorTest.java
@@ -0,0 +1,102 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_yaml.snakeyaml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.LinkedHashSet;
+import java.util.Set;
+import org.junit.jupiter.api.Test;
+import org.yaml.snakeyaml.Yaml;
+import org.yaml.snakeyaml.constructor.CustomClassLoaderConstructor;
+
+public class CustomClassLoaderConstructorTest {
+
+    @Test
+    void resolvesTaggedBeanWithProvidedClassLoaderWhenContextLoaderRejectsIt() {
+        ClassLoader originalContextClassLoader = Thread.currentThread().getContextClassLoader();
+        TrackingClassLoader trackingClassLoader =
+                new TrackingClassLoader(CustomClassLoaderConstructorTest.class.getClassLoader());
+        ClassLoader rejectingContextClassLoader = new RejectingClassLoader(
+                CustomClassLoaderConstructorTest.class.getClassLoader(),
+                TaggedBean.class.getName());
+        Yaml yaml = new Yaml(new CustomClassLoaderConstructor(trackingClassLoader));
+
+        Thread.currentThread().setContextClassLoader(rejectingContextClassLoader);
+        try {
+            TaggedBean bean = yaml.load(
+                    """
+                    !!%s
+                    name: Example
+                    quantity: 7
+                    """.formatted(TaggedBean.class.getName()));
+
+            assertThat(bean.getName()).isEqualTo("Example");
+            assertThat(bean.getQuantity()).isEqualTo(7);
+            assertThat(trackingClassLoader.getRequestedClassNames())
+                    .contains(TaggedBean.class.getName());
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalContextClassLoader);
+        }
+    }
+
+    private static final class TrackingClassLoader extends ClassLoader {
+        private final Set<String> requestedClassNames = new LinkedHashSet<>();
+
+        private TrackingClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            requestedClassNames.add(name);
+            return super.loadClass(name, resolve);
+        }
+
+        private Set<String> getRequestedClassNames() {
+            return requestedClassNames;
+        }
+    }
+
+    private static final class RejectingClassLoader extends ClassLoader {
+        private final String rejectedClassName;
+
+        private RejectingClassLoader(ClassLoader parent, String rejectedClassName) {
+            super(parent);
+            this.rejectedClassName = rejectedClassName;
+        }
+
+        @Override
+        protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            if (rejectedClassName.equals(name)) {
+                throw new ClassNotFoundException(name);
+            }
+            return super.loadClass(name, resolve);
+        }
+    }
+
+    public static final class TaggedBean {
+        private String name;
+        private int quantity;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getQuantity() {
+            return quantity;
+        }
+
+        public void setQuantity(int quantity) {
+            this.quantity = quantity;
+        }
+    }
+}
