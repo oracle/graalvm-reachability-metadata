a9d443e4c5908457966940693a6e03952389d247
jvukicev <jovan.vukicevic@oracle.com>
2026-04-20 19:41:17 +0200

    Add CommandInfo Beans coverage test for javax.activation-api

---
+++ tests/src/javax.activation/javax.activation-api/1.2.0/src/test/java/javax_activation/javax_activation_api/CommandInfoBeansTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package javax_activation.javax_activation_api;
+
+import javax.activation.CommandInfo;
+import javax.activation.CommandObject;
+import javax.activation.DataHandler;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CommandInfoBeansTest {
+    @Test
+    void getCommandObjectInstantiatesCommandObjectsViaJavaBeansInstantiate() throws Exception {
+        CommandInfo commandInfo = new CommandInfo("view", RecordingCommandObject.class.getName());
+        DataHandler dataHandler = new DataHandler("payload", "text/plain");
+
+        Object commandObject = commandInfo.getCommandObject(dataHandler, RecordingCommandObject.class.getClassLoader());
+
+        assertThat(commandObject).isInstanceOf(RecordingCommandObject.class);
+        RecordingCommandObject bean = (RecordingCommandObject) commandObject;
+        assertThat(bean.getVerb()).isEqualTo("view");
+        assertThat(bean.getDataHandler()).isSameAs(dataHandler);
+    }
+
+    public static final class RecordingCommandObject implements CommandObject {
+        private String verb;
+        private DataHandler dataHandler;
+
+        public RecordingCommandObject() {
+        }
+
+        @Override
+        public void setCommandContext(String verb, DataHandler dataHandler) {
+            this.verb = verb;
+            this.dataHandler = dataHandler;
+        }
+
+        public String getVerb() {
+            return verb;
+        }
+
+        public DataHandler getDataHandler() {
+            return dataHandler;
+        }
+    }
+}
