6c4436474749785a64f7c8f49f9ac13e55ccb0eb
jvukicev <jovan.vukicevic@oracle.com>
2026-04-21 23:00:29 +0200

    Add Streams ArrayCollector integration test

---
+++ tests/src/org.apache.commons/commons-lang3/3.11/src/test/java/org_apache_commons/commons_lang3/StreamsArrayCollectorTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_commons.commons_lang3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.stream.Stream;
+
+import org.apache.commons.lang3.Streams;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("deprecation")
+public class StreamsArrayCollectorTest {
+
+    @Test
+    public void toArrayCollectsElementsIntoTypedArrayInEncounterOrder() {
+        String[] result = Stream.of("alpha", "beta", "gamma").collect(Streams.toArray(String.class));
+
+        assertThat(result).isExactlyInstanceOf(String[].class).containsExactly("alpha", "beta", "gamma");
+    }
+
+    @Test
+    public void toArrayCreatesTypedEmptyArrayForEmptyStreams() {
+        String[] result = Stream.<String>empty().collect(Streams.toArray(String.class));
+
+        assertThat(result).isExactlyInstanceOf(String[].class).isEmpty();
+    }
+}
