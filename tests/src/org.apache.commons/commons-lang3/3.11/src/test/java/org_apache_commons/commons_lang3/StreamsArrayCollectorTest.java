e8347fbfba1b8c200104a466756d8351f5a5732a
jvukicev <jovan.vukicevic@oracle.com>
2026-04-21 23:27:19 +0200

    Add stream Streams ArrayCollector coverage tests

--- tests/src/org.apache.commons/commons-lang3/3.11/src/test/java/org_apache_commons/commons_lang3/StreamsArrayCollectorTest.java
+++ tests/src/org.apache.commons/commons-lang3/3.11/src/test/java/org_apache_commons/commons_lang3/StreamsArrayCollectorTest.java
@@ -8,12 +8,12 @@ package org_apache_commons.commons_lang3;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.List;
 import java.util.stream.Stream;
 
-import org.apache.commons.lang3.Streams;
+import org.apache.commons.lang3.stream.Streams;
 import org.junit.jupiter.api.Test;
 
-@SuppressWarnings("deprecation")
 public class StreamsArrayCollectorTest {
 
     @Test
@@ -21,6 +21,15 @@ public class StreamsArrayCollectorTest {
         String[] result = Stream.of("alpha", "beta", "gamma").collect(Streams.toArray(String.class));
 
         assertThat(result).isExactlyInstanceOf(String[].class).containsExactly("alpha", "beta", "gamma");
+    }
+
+    @Test
+    public void arrayCollectorFinisherCreatesTypedArrayForLists() {
+        Streams.ArrayCollector<Number> collector = new Streams.ArrayCollector<>(Number.class);
+
+        Number[] result = collector.finisher().apply(List.of(1, 2.5d, 3L));
+
+        assertThat(result).isExactlyInstanceOf(Number[].class).containsExactly(1, 2.5d, 3L);
     }
 
     @Test
