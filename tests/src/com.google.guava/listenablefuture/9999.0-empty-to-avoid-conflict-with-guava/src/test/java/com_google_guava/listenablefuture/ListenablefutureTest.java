d7026452db310d22777f6d2f3491664646e89531
jvukicev <jovan.vukicevic@oracle.com>
2026-04-21 20:58:44 +0200

    Checkpoint generated tests for com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava (1)

--- tests/src/com.google.guava/listenablefuture/9999.0-empty-to-avoid-conflict-with-guava/src/test/java/com_google_guava/listenablefuture/ListenablefutureTest.java
+++ tests/src/com.google.guava/listenablefuture/9999.0-empty-to-avoid-conflict-with-guava/src/test/java/com_google_guava/listenablefuture/ListenablefutureTest.java
@@ -8,9 +8,40 @@ package com_google_guava.listenablefuture;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.util.Collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 class ListenablefutureTest {
+    private static final String LISTENABLE_FUTURE_PACKAGE_RESOURCE = "com/google/common/util/concurrent/";
+    private static final String LISTENABLE_FUTURE_CLASS_RESOURCE =
+            "com/google/common/util/concurrent/ListenableFuture.class";
+
     @Test
-    void test() throws Exception {
-        System.out.println("This is just a placeholder, implement your test");
+    void placeholderDoesNotExposeTheStandaloneListenableFuturePackageLayout() {
+        assertThat(classLoader().getResource(LISTENABLE_FUTURE_PACKAGE_RESOURCE)).isNull();
+        assertThat(classLoader().getResource(LISTENABLE_FUTURE_CLASS_RESOURCE)).isNull();
+        assertThat(classLoader().getResourceAsStream(LISTENABLE_FUTURE_CLASS_RESOURCE)).isNull();
+    }
+
+    @Test
+    void placeholderDoesNotExposeTheStandaloneListenableFutureBytecodeThroughEnumerationLookups() throws IOException {
+        assertThat(Collections.list(classLoader().getResources(LISTENABLE_FUTURE_PACKAGE_RESOURCE))).isEmpty();
+        assertThat(Collections.list(classLoader().getResources(LISTENABLE_FUTURE_CLASS_RESOURCE))).isEmpty();
+    }
+
+    @Test
+    void threadContextClassLoaderAlsoSeesNoStandaloneListenableFutureBytecode() throws IOException {
+        ClassLoader contextClassLoader = Thread.currentThread().getContextClassLoader();
+
+        assertThat(contextClassLoader).isNotNull();
+        assertThat(contextClassLoader.getResource(LISTENABLE_FUTURE_CLASS_RESOURCE)).isNull();
+        assertThat(contextClassLoader.getResourceAsStream(LISTENABLE_FUTURE_CLASS_RESOURCE)).isNull();
+        assertThat(Collections.list(contextClassLoader.getResources(LISTENABLE_FUTURE_CLASS_RESOURCE))).isEmpty();
+    }
+
+    private static ClassLoader classLoader() {
+        return ListenablefutureTest.class.getClassLoader();
     }
 }
