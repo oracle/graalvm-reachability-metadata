94033d22468cf28866d1306b2ce19a1f9b4c2c73
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-23 00:31:58 +0200

    Add deeper JAXB API context path tests

--- tests/src/javax.xml.bind/jaxb-api/2.3.1/src/test/java/javax_xml_bind/jaxb_api/ContextFinderTest.java
+++ tests/src/javax.xml.bind/jaxb-api/2.3.1/src/test/java/javax_xml_bind/jaxb_api/ContextFinderTest.java
@@ -32,6 +32,17 @@ public class ContextFinderTest {
     }
 
     @Test
+    public void loadsContextPathFactoryFromJaxbPropertiesWithNullClassLoader() throws Exception {
+        JAXBContext context = JAXBContext.newInstance(
+                "javax_xml_bind.jaxb_api.objectfactorypath",
+                null,
+                Collections.singletonMap("path", "objectfactory"));
+
+        assertThat(context).isInstanceOf(StubJaxbContext.class);
+        assertThat(((StubJaxbContext) context).getSource()).isEqualTo("three-argument-context-factory");
+    }
+
+    @Test
     public void loadsDeprecatedServiceFactoryWithExplicitClassLoader() throws Exception {
         JAXBContext context = JAXBContext.newInstance(
                 "javax_xml_bind.jaxb_api.noprovider",
--- tests/src/javax.xml.bind/jaxb-api/2.3.1/src/test/java/javax_xml_bind/jaxb_api/ModuleUtilTest.java
+++ tests/src/javax.xml.bind/jaxb-api/2.3.1/src/test/java/javax_xml_bind/jaxb_api/ModuleUtilTest.java
@@ -6,7 +6,10 @@
  */
 package javax_xml_bind.jaxb_api;
 
+import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax_xml_bind.jaxb_api.support.StubJaxbContext;
@@ -18,23 +21,64 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ModuleUtilTest {
     @Test
     public void resolvesObjectFactoryClassesFromContextPath() throws Exception {
+        TrackingClassLoader classLoader = new TrackingClassLoader(getClass().getClassLoader());
+
         JAXBContext context = JAXBContext.newInstance(
                 "javax_xml_bind.jaxb_api.objectfactorypath",
-                getClass().getClassLoader(),
+                classLoader,
                 Collections.emptyMap());
 
         assertThat(context).isInstanceOf(StubJaxbContext.class);
         assertThat(((StubJaxbContext) context).getSource()).isEqualTo("three-argument-context-factory");
+        assertThat(classLoader.getLoadedClasses())
+                .contains("javax_xml_bind.jaxb_api.objectfactorypath.ObjectFactory");
     }
 
     @Test
     public void resolvesIndexedClassesFromContextPath() throws Exception {
+        TrackingClassLoader classLoader = new TrackingClassLoader(getClass().getClassLoader());
+
         JAXBContext context = JAXBContext.newInstance(
                 "javax_xml_bind.jaxb_api.indexpath",
-                getClass().getClassLoader(),
+                classLoader,
                 Collections.emptyMap());
 
         assertThat(context).isInstanceOf(StubJaxbContext.class);
         assertThat(((StubJaxbContext) context).getSource()).isEqualTo("three-argument-context-factory");
+        assertThat(classLoader.getResourceRequests())
+                .contains("javax_xml_bind/jaxb_api/indexpath/jaxb.index");
+        assertThat(classLoader.getLoadedClasses())
+                .contains(
+                        "javax_xml_bind.jaxb_api.indexpath.ObjectFactory",
+                        "javax_xml_bind.jaxb_api.indexpath.IndexedType");
+    }
+
+    private static final class TrackingClassLoader extends ClassLoader {
+        private final List<String> loadedClasses = new ArrayList<>();
+        private final List<String> resourceRequests = new ArrayList<>();
+
+        private TrackingClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            loadedClasses.add(name);
+            return super.loadClass(name);
+        }
+
+        @Override
+        public InputStream getResourceAsStream(String name) {
+            resourceRequests.add(name);
+            return super.getResourceAsStream(name);
+        }
+
+        private List<String> getLoadedClasses() {
+            return loadedClasses;
+        }
+
+        private List<String> getResourceRequests() {
+            return resourceRequests;
+        }
     }
 }
