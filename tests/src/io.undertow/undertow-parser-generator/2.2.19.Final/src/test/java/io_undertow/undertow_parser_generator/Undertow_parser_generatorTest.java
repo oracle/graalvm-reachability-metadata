44c14c8bfaa0c3176cc9f43d751b10eade2c40cc
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-22 23:54:20 +0200

    Scaffold io.undertow:undertow-parser-generator:2.2.19.Final

---
+++ tests/src/io.undertow/undertow-parser-generator/2.2.19.Final/src/test/java/io_undertow/undertow_parser_generator/Undertow_parser_generatorTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_undertow.undertow_parser_generator;
+
+import org.junit.jupiter.api.Test;
+
+class Undertow_parser_generatorTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
