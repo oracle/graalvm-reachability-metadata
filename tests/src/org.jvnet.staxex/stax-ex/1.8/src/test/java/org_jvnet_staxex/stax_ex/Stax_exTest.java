0828bdb01a587fb8855009ffe93583b65b2f52b4
Mihailo <mihailo.markovic@oracle.com>
2026-04-22 02:09:26 +0200

    Scaffold org.jvnet.staxex:stax-ex:1.8

---
+++ tests/src/org.jvnet.staxex/stax-ex/1.8/src/test/java/org_jvnet_staxex/stax_ex/Stax_exTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_jvnet_staxex.stax_ex;
+
+import org.junit.jupiter.api.Test;
+
+class Stax_exTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
