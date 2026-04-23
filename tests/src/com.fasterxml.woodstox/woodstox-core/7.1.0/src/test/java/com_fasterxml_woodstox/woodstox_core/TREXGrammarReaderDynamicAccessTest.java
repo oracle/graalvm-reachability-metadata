a26650a99215e5446ff4eea45780126da4d686a2
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-23 06:48:15 +0200

    Add TREXGrammarReader classic bundle coverage test

--- tests/src/com.fasterxml.woodstox/woodstox-core/7.1.0/src/test/java/com_fasterxml_woodstox/woodstox_core/TREXGrammarReaderDynamicAccessTest.java
+++ tests/src/com.fasterxml.woodstox/woodstox-core/7.1.0/src/test/java/com_fasterxml_woodstox/woodstox_core/TREXGrammarReaderDynamicAccessTest.java
@@ -8,22 +8,26 @@ package com_fasterxml_woodstox.woodstox_core;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import com.ctc.wstx.shaded.msv_core.grammar.ExpressionPool;
 import com.ctc.wstx.shaded.msv_core.reader.trex.classic.TREXGrammarReader;
 import org.junit.jupiter.api.Test;
 
 public class TREXGrammarReaderDynamicAccessTest {
     @Test
-    void loadsTrexGrammarReaderMessages() {
+    void loadsTrexGrammarReaderMessagesFromClassicBundle() {
         ExposedTREXGrammarReader reader = new ExposedTREXGrammarReader();
 
         assertThat(reader.localize("TREXGrammarReader.DuplicateDefinition", "patternName"))
-                .contains("defined more than once")
+                .contains("classic TREX grammar reader bundle")
                 .contains("patternName");
     }
 
     private static final class ExposedTREXGrammarReader extends TREXGrammarReader {
         private ExposedTREXGrammarReader() {
-            super(MsvTestSupport.recordingController());
+            super(
+                    MsvTestSupport.recordingController(),
+                    MsvTestSupport.namespaceAwareParserFactory(),
+                    new ExpressionPool());
         }
 
         private String localize(String propertyName, Object... arguments) {
