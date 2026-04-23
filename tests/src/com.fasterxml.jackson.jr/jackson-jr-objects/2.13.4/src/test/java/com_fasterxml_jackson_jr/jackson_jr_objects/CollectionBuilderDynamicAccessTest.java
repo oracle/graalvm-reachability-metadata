9bb6c8c30c43a0aa50af77ea3863465ed2d6615e
jvukicev <jovan.vukicevic@oracle.com>
2026-04-23 16:44:33 +0200

    Use runtime-resolved types in CollectionBuilder tests

--- tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.13.4/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/CollectionBuilderDynamicAccessTest.java
+++ tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.13.4/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/CollectionBuilderDynamicAccessTest.java
@@ -6,6 +6,7 @@
  */
 package com_fasterxml_jackson_jr.jackson_jr_objects;
 
+import com.fasterxml.jackson.jr.ob.JSON;
 import com.fasterxml.jackson.jr.ob.api.CollectionBuilder;
 import org.junit.jupiter.api.Test;
 
@@ -13,35 +14,56 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class CollectionBuilderDynamicAccessTest {
     @Test
-    void createsEmptyTypedArrays() {
+    void createsEmptyTypedArrays() throws Exception {
         CollectionBuilder builder = CollectionBuilder.defaultImpl();
+        Class<?> elementType = runtimeArrayElementType();
 
-        String[] values = builder.emptyArray(String.class);
+        Object[] values = builder.emptyArray((Class) elementType);
 
         assertThat(values).isEmpty();
-        assertThat(values.getClass().getComponentType()).isSameAs(String.class);
+        assertThat(values.getClass().getComponentType()).isSameAs(elementType);
     }
 
     @Test
-    void createsSingletonTypedArrays() {
+    void createsSingletonTypedArrays() throws Exception {
         CollectionBuilder builder = CollectionBuilder.defaultImpl();
+        Class<?> elementType = runtimeArrayElementType();
 
-        String[] values = builder.singletonArray(String.class, "solo");
+        Object[] values = builder.singletonArray(elementType, new ArrayElement("solo"));
 
-        assertThat(values).containsExactly("solo");
-        assertThat(values.getClass().getComponentType()).isSameAs(String.class);
+        assertThat(values).singleElement().isInstanceOf(ArrayElement.class);
+        assertThat(((ArrayElement) values[0]).name).isEqualTo("solo");
+        assertThat(values.getClass().getComponentType()).isSameAs(elementType);
     }
 
     @Test
-    void createsMultiValueTypedArrays() {
+    void createsMultiValueTypedArrays() throws Exception {
         CollectionBuilder builder = CollectionBuilder.defaultImpl();
+        Class<?> elementType = runtimeArrayElementType();
 
-        String[] values = builder.start()
-                .add("left")
-                .add("right")
-                .buildArray(String.class);
+        Object[] values = builder.start()
+                .add(new ArrayElement("left"))
+                .add(new ArrayElement("right"))
+                .buildArray((Class) elementType);
 
-        assertThat(values).containsExactly("left", "right");
-        assertThat(values.getClass().getComponentType()).isSameAs(String.class);
+        assertThat(values).hasSize(2);
+        assertThat(((ArrayElement) values[0]).name).isEqualTo("left");
+        assertThat(((ArrayElement) values[1]).name).isEqualTo("right");
+        assertThat(values.getClass().getComponentType()).isSameAs(elementType);
+    }
+
+    private static Class<?> runtimeArrayElementType() throws Exception {
+        return JSON.std.beanFrom(Class.class, '"' + ArrayElement.class.getName() + '"');
+    }
+
+    public static final class ArrayElement {
+        public String name;
+
+        public ArrayElement() {
+        }
+
+        public ArrayElement(String name) {
+            this.name = name;
+        }
     }
 }
