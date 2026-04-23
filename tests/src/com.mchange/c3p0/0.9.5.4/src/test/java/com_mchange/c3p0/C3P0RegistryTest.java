a7988ee07ca6c893e17d847ccc896cc06e8715b5
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-23 03:46:33 +0200

    Refine C3P0Registry management coordinator coverage

--- tests/src/com.mchange/c3p0/0.9.5.4/src/test/java/com_mchange/c3p0/C3P0RegistryTest.java
+++ tests/src/com.mchange/c3p0/0.9.5.4/src/test/java/com_mchange/c3p0/C3P0RegistryTest.java
@@ -9,7 +9,12 @@ package com_mchange.c3p0;
 import com.mchange.v2.c3p0.C3P0Registry;
 import com.mchange.v2.c3p0.ConnectionTester;
 import com.mchange.v2.c3p0.impl.DefaultConnectionTester;
+import com.mchange.v2.log.MLog;
 import org.junit.jupiter.api.Test;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.CodeSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,5 +33,34 @@ public class C3P0RegistryTest {
             .isInstanceOf(DefaultConnectionTester.class);
         assertThat(C3P0Registry.getConnectionCustomizer(TrackingConnectionCustomizer.class.getName()))
             .isInstanceOf(TrackingConnectionCustomizer.class);
+    }
+
+    @Test
+    void initializesRegistryWithDefaultManagementCoordinatorWhenNoPropertiesResourceIsVisible() throws Exception {
+        Thread thread = Thread.currentThread();
+        ClassLoader previousContextClassLoader = thread.getContextClassLoader();
+
+        try (URLClassLoader classLoader = new URLClassLoader(
+            new URL[] {
+                codeSourceUrl(C3P0Registry.class),
+                codeSourceUrl(MLog.class)
+            },
+            ClassLoader.getPlatformClassLoader()
+        )) {
+            thread.setContextClassLoader(classLoader);
+            Class<?> isolatedRegistry = Class.forName(C3P0Registry.class.getName(), true, classLoader);
+
+            assertThat(isolatedRegistry).isNotSameAs(C3P0Registry.class);
+            assertThat(isolatedRegistry.getClassLoader()).isSameAs(classLoader);
+            assertThat(classLoader.getResource("c3p0.properties")).isNull();
+        } finally {
+            thread.setContextClassLoader(previousContextClassLoader);
+        }
+    }
+
+    private static URL codeSourceUrl(Class<?> type) {
+        CodeSource codeSource = type.getProtectionDomain().getCodeSource();
+        assertThat(codeSource).isNotNull();
+        return codeSource.getLocation();
     }
 }
