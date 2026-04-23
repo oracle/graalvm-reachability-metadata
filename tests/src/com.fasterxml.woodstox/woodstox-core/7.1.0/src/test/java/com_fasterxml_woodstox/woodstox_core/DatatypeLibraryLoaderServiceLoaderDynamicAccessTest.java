7bbfb5434aa3252dd152f4ba00a3822cdfed6954
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-23 05:29:21 +0200

    Refine legacy DatatypeLibraryLoader Loader test

--- tests/src/com.fasterxml.woodstox/woodstox-core/7.1.0/src/test/java/com_fasterxml_woodstox/woodstox_core/DatatypeLibraryLoaderServiceLoaderDynamicAccessTest.java
+++ tests/src/com.fasterxml.woodstox/woodstox-core/7.1.0/src/test/java/com_fasterxml_woodstox/woodstox_core/DatatypeLibraryLoaderServiceLoaderDynamicAccessTest.java
@@ -9,10 +9,9 @@ package com_fasterxml_woodstox.woodstox_core;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.ctc.wstx.shaded.msv.relaxng_datatype.DatatypeLibraryFactory;
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.invoke.VarHandle;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.Enumeration;
 import org.junit.jupiter.api.Test;
@@ -20,44 +19,80 @@ import org.junit.jupiter.api.Test;
 public class DatatypeLibraryLoaderServiceLoaderDynamicAccessTest {
     private static final String LOADER_CLASS_NAME =
             "com.ctc.wstx.shaded.msv.relaxng_datatype.helpers.DatatypeLibraryLoader$Service$Loader";
+    private static final String SERVICE_CLASS_NAME =
+            "com.ctc.wstx.shaded.msv.relaxng_datatype.helpers.DatatypeLibraryLoader$Service";
     private static final String LOADER_CLASS_CACHE_FIELD =
             "class$org$relaxng$datatype$helpers$DatatypeLibraryLoader$Service$Loader";
     private static final String SERVICE_RESOURCE =
             "META-INF/services/" + DatatypeLibraryFactory.class.getName();
 
     @Test
-    void coversLegacyLoaderClassAndResourceResolutionPaths() throws Throwable {
+    void coversLegacyLoaderClassAndResourceResolutionPaths() throws Exception {
         Class<?> loaderClass = Class.forName(LOADER_CLASS_NAME);
-        MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(loaderClass, MethodHandles.lookup());
-        MethodHandle constructor = lookup.findConstructor(loaderClass, MethodType.methodType(void.class));
-        MethodHandle getResources =
-                lookup.findVirtual(loaderClass, "getResources", MethodType.methodType(Enumeration.class, String.class));
-        MethodHandle loadClass =
-                lookup.findVirtual(loaderClass, "loadClass", MethodType.methodType(Class.class, String.class));
-        VarHandle cacheField = lookup.findStaticVarHandle(loaderClass, LOADER_CLASS_CACHE_FIELD, Class.class);
-        Object loader = constructor.invoke();
-        Class<?> previousValue = (Class<?>) cacheField.get();
+        Class<?> serviceClass = Class.forName(SERVICE_CLASS_NAME);
+        Object loader = newInstance(loaderClass, new Class<?>[0], new Object[0]);
+        Object service = newInstance(serviceClass, new Class<?>[]{Class.class}, new Object[]{DatatypeLibraryFactory.class});
+        Field cacheField = accessibleField(loaderClass, LOADER_CLASS_CACHE_FIELD);
+        Field loaderField = accessibleField(serviceClass, "loader");
+        Object previousValue = cacheField.get(null);
+        loaderField.set(service, loader);
 
         try {
-            cacheField.set(null);
-            Enumeration<?> classLoaderResources = (Enumeration<?>) getResources.invoke(loader, SERVICE_RESOURCE);
+            cacheField.set(null, null);
+            Enumeration<?> classLoaderResources = invokeResources(loaderClass, loader);
             assertThat(classLoaderResources.hasMoreElements()).isTrue();
             URL classLoaderResource = (URL) classLoaderResources.nextElement();
             assertThat(classLoaderResource.toExternalForm()).contains("META-INF/services");
 
-            cacheField.set(String.class);
-            Enumeration<?> systemResources = (Enumeration<?>) getResources.invoke(loader, SERVICE_RESOURCE);
+            Enumeration<?> providers = invokeProviders(serviceClass, service);
+            assertThat(providers.hasMoreElements()).isTrue();
+            assertThat(providers.nextElement())
+                    .isInstanceOf(DatatypeLibraryLoaderServiceDynamicAccessTest.TestDatatypeLibraryFactory.class);
+
+            Class<?> factoryClass = invokeLoadClass(
+                    loaderClass,
+                    loader,
+                    DatatypeLibraryLoaderServiceDynamicAccessTest.TestDatatypeLibraryFactory.class.getName());
+            assertThat(factoryClass)
+                    .isEqualTo(DatatypeLibraryLoaderServiceDynamicAccessTest.TestDatatypeLibraryFactory.class);
+
+            cacheField.set(null, String.class);
+            Enumeration<?> systemResources = invokeResources(loaderClass, loader);
             assertThat(systemResources.hasMoreElements()).isTrue();
             URL systemResource = (URL) systemResources.nextElement();
             assertThat(systemResource.toExternalForm()).contains("META-INF/services");
-
-            Class<?> factoryClass = (Class<?>) loadClass.invoke(
-                    loader, DatatypeLibraryLoaderServiceDynamicAccessTest.TestDatatypeLibraryFactory.class.getName());
-            assertThat(factoryClass)
-                    .isEqualTo(DatatypeLibraryLoaderServiceDynamicAccessTest.TestDatatypeLibraryFactory.class);
-            assertThat(DatatypeLibraryFactory.class.isAssignableFrom(factoryClass)).isTrue();
         } finally {
-            cacheField.set(previousValue);
+            cacheField.set(null, previousValue);
         }
+    }
+
+    private static Field accessibleField(Class<?> type, String name) throws Exception {
+        Field field = type.getDeclaredField(name);
+        field.setAccessible(true);
+        return field;
+    }
+
+    private static Object newInstance(Class<?> type, Class<?>[] parameterTypes, Object[] arguments) throws Exception {
+        Constructor<?> constructor = type.getDeclaredConstructor(parameterTypes);
+        constructor.setAccessible(true);
+        return constructor.newInstance(arguments);
+    }
+
+    private static Enumeration<?> invokeProviders(Class<?> serviceClass, Object service) throws Exception {
+        Method method = serviceClass.getDeclaredMethod("getProviders");
+        method.setAccessible(true);
+        return (Enumeration<?>) method.invoke(service);
+    }
+
+    private static Enumeration<?> invokeResources(Class<?> loaderClass, Object loader) throws Exception {
+        Method method = loaderClass.getDeclaredMethod("getResources", String.class);
+        method.setAccessible(true);
+        return (Enumeration<?>) method.invoke(loader, SERVICE_RESOURCE);
+    }
+
+    private static Class<?> invokeLoadClass(Class<?> loaderClass, Object loader, String className) throws Exception {
+        Method method = loaderClass.getDeclaredMethod("loadClass", String.class);
+        method.setAccessible(true);
+        return (Class<?>) method.invoke(loader, className);
     }
 }
