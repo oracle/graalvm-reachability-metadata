e23e0607562ed1ac538f40f7c6af43abf42421bd
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-23 05:34:44 +0200

    Add JDK50Impl dynamic access coverage test

---
+++ tests/src/com.fasterxml.woodstox/woodstox-core/7.1.0/src/test/java/com_fasterxml_woodstox/woodstox_core/JDK50ImplDynamicAccessTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_fasterxml_woodstox.woodstox_core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.ctc.wstx.shaded.msv_core.datatype.xsd.regex.RegExp;
+import com.ctc.wstx.shaded.msv_core.datatype.xsd.regex.RegExpFactory;
+import java.lang.reflect.Constructor;
+import org.junit.jupiter.api.Test;
+
+public class JDK50ImplDynamicAccessTest {
+    private static final String FACTORY_CLASS_NAME =
+            "com.ctc.wstx.shaded.msv_core.datatype.xsd.regex.JDK50Impl";
+
+    @Test
+    void compilesPatternsThroughTheLegacyJdk50RegexpImplementation() throws Exception {
+        RegExpFactory factory = newFactory();
+        RegExp regExp = factory.compile("[A-Z]{2,5}-[0-9]{2}");
+
+        assertThat(regExp.matches("WSTX-71")).isTrue();
+        assertThat(regExp.matches("woodstox-71")).isFalse();
+    }
+
+    private static RegExpFactory newFactory() throws Exception {
+        Class<?> factoryClass = Class.forName(FACTORY_CLASS_NAME);
+        Constructor<?> constructor = factoryClass.getDeclaredConstructor();
+        constructor.setAccessible(true);
+        return (RegExpFactory) constructor.newInstance();
+    }
+}
