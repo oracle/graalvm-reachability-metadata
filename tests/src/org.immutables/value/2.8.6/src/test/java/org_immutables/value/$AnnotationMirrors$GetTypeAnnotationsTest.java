c0618642e38981ae7f4d9707734dbd58778661fe
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-25 15:58:37 +0200

    Update generated library support for org.immutables:value:2.8.6

--- tests/src/org.immutables/value/2.8.6/src/test/java/org_immutables/value/$AnnotationMirrors$GetTypeAnnotationsTest.java
+++ tests/src/org.immutables/value/2.8.6/src/test/java/org_immutables/value/$AnnotationMirrors$GetTypeAnnotationsTest.java
@@ -21,7 +21,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class $AnnotationMirrors$GetTypeAnnotationsTest {
+class AnnotationMirrorsGetTypeAnnotationsTest {
 
     @Test
     void fromUsesTypeMirrorGetAnnotationMirrorsWhenAvailable() {
