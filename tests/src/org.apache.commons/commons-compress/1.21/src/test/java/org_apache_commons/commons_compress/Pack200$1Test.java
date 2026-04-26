9e1cf5ac78b92c159e60028662c5ed7cc4723b7f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 00:43:58 +0200

    Use test packer for Pack200 coverage

--- tests/src/org.apache.commons/commons-compress/1.21/src/test/java/org_apache_commons/commons_compress/Pack200$1Test.java
+++ tests/src/org.apache.commons/commons-compress/1.21/src/test/java/org_apache_commons/commons_compress/Pack200$1Test.java
@@ -6,7 +6,13 @@
  */
 package org_apache_commons.commons_compress;
 
+import java.beans.PropertyChangeListener;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.util.SortedMap;
+import java.util.TreeMap;
+import java.util.jar.JarFile;
+import java.util.jar.JarInputStream;
 
 import org.apache.commons.compress.java.util.jar.Pack200;
 import org.junit.jupiter.api.Test;
@@ -16,21 +22,18 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class Pack200$1Test {
 
     private static final String PACKER_PROPERTY = "java.util.jar.Pack200.Packer";
-    private static final String DEFAULT_PACKER_IMPLEMENTATION = "org.apache.commons.compress.harmony.pack200.Pack200PackerAdapter";
-
     @Test
-    void newPackerLoadsTheDefaultImplementationWhenNoOverrideIsConfigured() {
+    void newPackerLoadsTheConfiguredImplementation() {
         String previousValue = System.getProperty(PACKER_PROPERTY);
 
         try {
-            System.clearProperty(PACKER_PROPERTY);
+            System.setProperty(PACKER_PROPERTY, TestPacker.class.getName());
 
             Pack200.Packer packer = Pack200.newPacker();
             SortedMap<String, String> properties = packer.properties();
             properties.put(Pack200.Packer.EFFORT, "0");
 
-            assertThat(packer).isNotNull();
-            assertThat(packer.getClass().getName()).isEqualTo(DEFAULT_PACKER_IMPLEMENTATION);
+            assertThat(packer).isInstanceOf(TestPacker.class);
             assertThat(properties).containsEntry(Pack200.Packer.EFFORT, "0");
         } finally {
             restoreSystemProperty(previousValue);
@@ -44,5 +47,31 @@ public class Pack200$1Test {
         }
 
         System.setProperty(PACKER_PROPERTY, value);
+    }
+
+    public static final class TestPacker implements Pack200.Packer {
+
+        private final SortedMap<String, String> properties = new TreeMap<>();
+
+        @Override
+        public SortedMap<String, String> properties() {
+            return properties;
+        }
+
+        @Override
+        public void pack(JarFile in, OutputStream out) throws IOException {
+        }
+
+        @Override
+        public void pack(JarInputStream in, OutputStream out) throws IOException {
+        }
+
+        @Override
+        public void addPropertyChangeListener(PropertyChangeListener listener) {
+        }
+
+        @Override
+        public void removePropertyChangeListener(PropertyChangeListener listener) {
+        }
     }
 }
