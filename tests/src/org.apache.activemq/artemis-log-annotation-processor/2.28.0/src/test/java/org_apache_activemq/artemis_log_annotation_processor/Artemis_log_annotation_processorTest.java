055a1dcbbebb3d5314f84712b68a9e75a7409353
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-26 23:58:42 +0200

    Scaffold org.apache.activemq:artemis-log-annotation-processor:2.28.0

---
+++ tests/src/org.apache.activemq/artemis-log-annotation-processor/2.28.0/src/test/java/org_apache_activemq/artemis_log_annotation_processor/Artemis_log_annotation_processorTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_activemq.artemis_log_annotation_processor;
+
+import org.junit.jupiter.api.Test;
+
+class Artemis_log_annotation_processorTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
