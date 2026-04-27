19e05e8bbf0810abd4bb1c306bed87ef3acfdf7a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 13:48:08 +0200

    Add picocli default factory coverage tests

---
+++ tests/src/info.picocli/picocli/4.7.6/src/test/java/groovy/lang/CommandLineInnerDefaultFactoryTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package groovy.lang;
+
+import java.lang.reflect.InvocationTargetException;
+import java.util.concurrent.Callable;
+
+import org.junit.jupiter.api.Test;
+
+import picocli.CommandLine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class CommandLineInnerDefaultFactoryTest {
+
+    @Test
+    void createsPublicNoArgInstancesWithDefaultFactory() throws Exception {
+        CommandLine.IFactory factory = CommandLine.defaultFactory();
+
+        Object result = factory.create(PublicNoArgCommand.class);
+
+        assertThat(result).isInstanceOf(PublicNoArgCommand.class);
+    }
+
+    @Test
+    void invokesDeclaredConstructorWhenPublicInstantiationFails() {
+        CommandLine.IFactory factory = CommandLine.defaultFactory();
+
+        assertThatThrownBy(() -> factory.create(ThrowingConstructorCommand.class))
+                .isInstanceOf(InvocationTargetException.class)
+                .hasCauseInstanceOf(IllegalStateException.class);
+    }
+
+    @Test
+    void createsInstancesWithPrivateNoArgConstructors() throws Exception {
+        CommandLine.IFactory factory = CommandLine.defaultFactory();
+
+        Object result = factory.create(PrivateNoArgCommand.class);
+
+        assertThat(result).isInstanceOf(PrivateNoArgCommand.class);
+    }
+
+    @Test
+    void invokesGroovyClosureConstructorAndCallThroughDefaultFactory() throws Exception {
+        CommandLine.IFactory factory = CommandLine.defaultFactory();
+
+        Object result = factory.create(CallableClosure.class);
+
+        assertThat(result).isEqualTo("created-by-closure");
+    }
+
+    public static class PublicNoArgCommand {
+    }
+
+    public static class ThrowingConstructorCommand {
+        public ThrowingConstructorCommand() {
+            throw new IllegalStateException("intentional constructor failure");
+        }
+    }
+
+    public static class PrivateNoArgCommand {
+        private PrivateNoArgCommand() {
+        }
+    }
+
+    public static class CallableClosure extends Closure {
+        public CallableClosure(Object owner, Object thisObject) {
+            super(owner, thisObject);
+        }
+
+        @Override
+        public Object call() {
+            return "created-by-closure";
+        }
+    }
+}
+
+abstract class Closure implements Callable<Object> {
+    protected Closure(Object owner, Object thisObject) {
+    }
+}
