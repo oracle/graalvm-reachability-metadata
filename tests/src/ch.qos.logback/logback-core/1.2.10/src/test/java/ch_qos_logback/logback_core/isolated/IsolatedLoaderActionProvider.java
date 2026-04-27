a9e4d87465e7c57f4dcc8b28f374ebd98ec87a4d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 14:29:27 +0200

    Add logback Loader dynamic access tests

---
+++ tests/src/ch.qos.logback/logback-core/1.2.10/src/test/java/ch_qos_logback/logback_core/isolated/IsolatedLoaderActionProvider.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package ch_qos_logback.logback_core.isolated;
+
+import ch.qos.logback.core.util.Loader;
+import ch_qos_logback.logback_core.IsolatedLoaderAction;
+
+public class IsolatedLoaderActionProvider implements IsolatedLoaderAction {
+
+    @Override
+    public String loadStringClass() {
+        try {
+            return Loader.loadClass(String.class.getName()).getName();
+        } catch (ClassNotFoundException ex) {
+            throw new IllegalStateException(ex);
+        }
+    }
+}
