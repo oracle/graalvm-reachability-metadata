54368d14669fb2a7e562a2eefbbe99e5e92d26c0
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 03:20:21 +0200

    Refine DetailPanel dynamic access test

--- tests/src/log4j/log4j/1.2.16/src/test/java/log4j/log4j/DetailPanelTest.java
+++ tests/src/log4j/log4j/1.2.16/src/test/java/log4j/log4j/DetailPanelTest.java
@@ -6,10 +6,6 @@
  */
 package log4j.log4j;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,17 +13,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class DetailPanelTest {
 
     @Test
-    void syntheticClassLookupResolvesTheDetailPanelTypeByName() throws Throwable {
+    void initializingDetailPanelLoadsItsOwnClassForLoggerInitialization() throws Exception {
         Class<?> detailPanelClass = Class.forName(
                 "org.apache.log4j.chainsaw.DetailPanel",
-                false,
+                true,
                 DetailPanelTest.class.getClassLoader()
         );
-        MethodHandle classLookup = MethodHandles.privateLookupIn(detailPanelClass, MethodHandles.lookup())
-                .findStatic(detailPanelClass, "class$", MethodType.methodType(Class.class, String.class));
 
-        Class<?> resolvedClass = (Class<?>) classLookup.invokeExact("org.apache.log4j.chainsaw.DetailPanel");
-
-        assertThat(resolvedClass).isSameAs(detailPanelClass);
+        assertThat(detailPanelClass.getName()).isEqualTo("org.apache.log4j.chainsaw.DetailPanel");
     }
 }
