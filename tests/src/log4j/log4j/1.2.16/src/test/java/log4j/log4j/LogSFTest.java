69fa43def17600e7a6fda0060f196d152264f975
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 02:35:05 +0200

    Add LogSF coverage tests for log4j 1.2.16

---
+++ tests/src/log4j/log4j/1.2.16/src/test/java/log4j/log4j/LogSFTest.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package log4j.log4j;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListResourceBundle;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogSF;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LogSFTest {
+
+    @Test
+    void formatsParameterizedMessages() {
+        Logger logger = Logger.getLogger(LogSFTest.class.getName() + "." + System.nanoTime());
+        RecordingAppender appender = new RecordingAppender();
+        logger.setAdditivity(false);
+        logger.setLevel(Level.INFO);
+        logger.addAppender(appender);
+
+        try {
+            LogSF.info(logger, "Hello {}", new Object[] { "native image" });
+
+            assertThat(appender.getMessages()).containsExactly("Hello native image");
+        } finally {
+            logger.removeAppender(appender);
+        }
+    }
+
+    @Test
+    void loadsMessagePatternsFromResourceBundles() {
+        Logger logger = Logger.getLogger(LogSFTest.class.getName() + "." + System.nanoTime());
+        RecordingAppender appender = new RecordingAppender();
+        logger.setAdditivity(false);
+        logger.setLevel(Level.INFO);
+        logger.addAppender(appender);
+
+        try {
+            LogSF.logrb(logger, Level.INFO, TestMessages.class.getName(), "message", new Object[] { "native image" });
+
+            assertThat(appender.getMessages()).containsExactly("Hello native image");
+        } finally {
+            logger.removeAppender(appender);
+        }
+    }
+
+    public static final class TestMessages extends ListResourceBundle {
+        @Override
+        protected Object[][] getContents() {
+            return new Object[][] {
+                    { "message", "Hello {}" }
+            };
+        }
+    }
+
+    private static final class RecordingAppender extends AppenderSkeleton {
+        private final List<String> messages = new ArrayList<>();
+
+        @Override
+        protected void append(LoggingEvent event) {
+            messages.add(event.getRenderedMessage());
+        }
+
+        @Override
+        public void close() {
+            closed = true;
+        }
+
+        @Override
+        public boolean requiresLayout() {
+            return false;
+        }
+
+        private List<String> getMessages() {
+            return messages;
+        }
+    }
+}
