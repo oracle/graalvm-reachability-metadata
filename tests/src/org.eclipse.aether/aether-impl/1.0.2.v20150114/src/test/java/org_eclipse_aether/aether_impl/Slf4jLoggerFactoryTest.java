656459997d4db682808d77128b6e9e707415cb71
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 00:06:55 +0200

    Add Slf4jLoggerFactory coverage test

---
+++ tests/src/org.eclipse.aether/aether-impl/1.0.2.v20150114/src/test/java/org_eclipse_aether/aether_impl/Slf4jLoggerFactoryTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_eclipse_aether.aether_impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.aether.internal.impl.slf4j.Slf4jLoggerFactory;
+import org.junit.jupiter.api.Test;
+
+public class Slf4jLoggerFactoryTest {
+    @Test
+    void reportsSlf4jAvailabilityDuringFactoryInitialization() {
+        Boolean slf4jAvailable = Slf4jLoggerFactory.isSlf4jAvailable();
+
+        assertThat(slf4jAvailable).isNotNull();
+        assertThat(new Slf4jLoggerFactory()).isNotNull();
+    }
+}
