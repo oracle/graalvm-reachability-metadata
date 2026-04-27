64413733d98f11fe3519b8eb4041659ce6e709e0
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-27 19:50:49 +0200

    Scaffold org.jetbrains.kotlin:kotlin-stdlib-common:1.6.20

---
+++ tests/src/org.jetbrains.kotlin/kotlin-stdlib-common/1.6.20/src/test/kotlin/org_jetbrains_kotlin/kotlin_stdlib_common/Kotlin_stdlib_commonTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_jetbrains_kotlin.kotlin_stdlib_common
+
+import org.junit.jupiter.api.Test
+
+class Kotlin_stdlib_commonTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
