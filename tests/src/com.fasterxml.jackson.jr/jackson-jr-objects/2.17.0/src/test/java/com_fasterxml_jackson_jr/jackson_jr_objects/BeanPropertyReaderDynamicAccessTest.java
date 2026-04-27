b990f9754f428a06ab249cc0aea902922dfa53d9
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 13:03:08 +0200

    Refine BeanPropertyReader dynamic access tests

--- tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.17.0/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/BeanPropertyReaderDynamicAccessTest.java
+++ tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.17.0/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/BeanPropertyReaderDynamicAccessTest.java
@@ -7,12 +7,36 @@
 package com_fasterxml_jackson_jr.jackson_jr_objects;
 
 import com.fasterxml.jackson.jr.ob.JSON;
+import com.fasterxml.jackson.jr.ob.impl.BeanPropertyReader;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BeanPropertyReaderDynamicAccessTest {
     private static final JSON JSON_WITH_FORCE_ACCESS = JSON.std.with(JSON.Feature.FORCE_REFLECTION_ACCESS);
+
+    @Test
+    void setsFieldBackedValuesThroughBeanPropertyReader() throws Exception {
+        BeanPropertyReader reader = new BeanPropertyReader("x", FieldBackedReaderBean.class.getField("x"), null);
+        FieldBackedReaderBean bean = new FieldBackedReaderBean();
+
+        reader.setValueFor(bean, new Object[]{3});
+
+        assertThat(bean.isConstructed()).isTrue();
+        assertThat(bean.x).isEqualTo(3);
+    }
+
+    @Test
+    void invokesSetterBackedValuesThroughBeanPropertyReader() throws Exception {
+        BeanPropertyReader reader = new BeanPropertyReader("name", null,
+                PublicSetterBackedReaderBean.class.getMethod("setName", String.class));
+        PublicSetterBackedReaderBean bean = new PublicSetterBackedReaderBean();
+
+        reader.setValueFor(bean, new Object[]{"Ada"});
+
+        assertThat(bean.getName()).isEqualTo("Ada");
+        assertThat(bean.getSetterCalls()).isEqualTo(1);
+    }
 
     @Test
     void populatesFieldBackedBeanProperties() throws Exception {
