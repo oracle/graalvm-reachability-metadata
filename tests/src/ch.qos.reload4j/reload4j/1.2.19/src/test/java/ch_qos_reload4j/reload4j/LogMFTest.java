3cc38bd1e2f6112c2fb52cd7d495c79f141271b9
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 00:03:19 +0200

    Add reload4j LogMF resource bundle coverage

---
+++ tests/src/ch.qos.reload4j/reload4j/1.2.19/src/test/java/ch_qos_reload4j/reload4j/LogMFTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package ch_qos_reload4j.reload4j;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.Level;
+import org.apache.log4j.LogMF;
+import org.apache.log4j.Logger;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.jupiter.api.Test;
+
+public class LogMFTest {
+    private static final String BUNDLE_NAME = "ch_qos_reload4j.reload4j.LogMFTestMessages";
+
+    @Test
+    void logrbFormatsMessageFromNamedResourceBundle() {
+        Logger logger = Logger.getLogger("reload4j.logmf." + UUID.randomUUID());
+        CapturingAppender appender = new CapturingAppender();
+        logger.setAdditivity(false);
+        logger.setLevel(Level.INFO);
+        logger.addAppender(appender);
+        try {
+            LogMF.logrb(logger, Level.INFO, BUNDLE_NAME, "processed.items", new Object[] {"Alice", 7});
+
+            assertThat(appender.messages()).containsExactly("User Alice processed 7 items");
+        } finally {
+            logger.removeAppender(appender);
+        }
+    }
+
+    private static final class CapturingAppender extends AppenderSkeleton {
+        private final List<String> messages = new ArrayList<>();
+
+        @Override
+        protected void append(LoggingEvent event) {
+            messages.add(event.getRenderedMessage());
+        }
+
+        @Override
+        public void close() {
+        }
+
+        @Override
+        public boolean requiresLayout() {
+            return false;
+        }
+
+        private List<String> messages() {
+            return messages;
+        }
+    }
+}
