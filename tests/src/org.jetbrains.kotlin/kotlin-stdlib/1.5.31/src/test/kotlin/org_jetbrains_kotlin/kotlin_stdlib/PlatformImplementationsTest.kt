1e06daeecfb25f82f32d343711beb9cee6bd805e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 12:08:37 +0200

    Exercise base Kotlin PlatformImplementations suppression

--- tests/src/org.jetbrains.kotlin/kotlin-stdlib/1.5.31/src/test/kotlin/org_jetbrains_kotlin/kotlin_stdlib/PlatformImplementationsTest.kt
+++ tests/src/org.jetbrains.kotlin/kotlin-stdlib/1.5.31/src/test/kotlin/org_jetbrains_kotlin/kotlin_stdlib/PlatformImplementationsTest.kt
@@ -4,32 +4,24 @@
  * You should have received a copy of the CC0 legalcode along with this
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
+@file:Suppress("INVISIBLE_MEMBER", "INVISIBLE_REFERENCE")
+
 package org_jetbrains_kotlin.kotlin_stdlib
 
-import java.io.Closeable
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.assertThrows
-import kotlin.io.use
 
 public class PlatformImplementationsTest {
     @Test
-    public fun suppressedExceptionFromClosingResourceIsAvailableThroughKotlinApi() {
-        val closeFailure = IllegalArgumentException("close failure")
-        val primaryFailure = assertThrows<IllegalStateException> {
-            FailingCloseable(closeFailure).use {
-                throw IllegalStateException("primary failure")
-            }
-        }
+    public fun basePlatformImplementationAddsAndReturnsSuppressedExceptions() {
+        val platformImplementations = kotlin.internal.PlatformImplementations()
+        val primaryFailure = IllegalStateException("primary failure")
+        val suppressedFailure = IllegalArgumentException("suppressed failure")
 
-        assertThat(primaryFailure.suppressedExceptions)
+        platformImplementations.addSuppressed(primaryFailure, suppressedFailure)
+
+        assertThat(platformImplementations.getSuppressed(primaryFailure))
             .singleElement()
-            .isSameAs(closeFailure)
-    }
-
-    private class FailingCloseable(private val closeFailure: RuntimeException) : Closeable {
-        override fun close() {
-            throw closeFailure
-        }
+            .isSameAs(suppressedFailure)
     }
 }
