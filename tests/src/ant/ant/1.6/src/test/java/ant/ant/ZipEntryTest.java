2063559042d42f97758a0ca7aee441004f210421
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 00:54:37 +0200

    Reset Ant ZipEntry compatibility cache in test

--- tests/src/ant/ant/1.6/src/test/java/ant/ant/ZipEntryTest.java
+++ tests/src/ant/ant/1.6/src/test/java/ant/ant/ZipEntryTest.java
@@ -8,10 +8,19 @@ package ant.ant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Field;
 import org.apache.tools.zip.ZipEntry;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class ZipEntryTest {
+    @BeforeEach
+    void resetCompressedSizeCompatibilityCache() throws ReflectiveOperationException {
+        resetStaticField("class$java$util$zip$ZipEntry", null);
+        resetStaticField("setCompressedSizeMethod", null);
+        resetStaticField("triedToGetMethod", false);
+    }
+
     @Test
     void setCompressedSizeThroughAntCompatibilityApi() {
         ZipEntry entry = new ZipEntry("content/data.txt");
@@ -19,5 +28,11 @@ public class ZipEntryTest {
         entry.setComprSize(4096L);
 
         assertThat(entry.getCompressedSize()).isEqualTo(4096L);
+    }
+
+    private static void resetStaticField(String fieldName, Object value) throws ReflectiveOperationException {
+        Field field = ZipEntry.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(null, value);
     }
 }
