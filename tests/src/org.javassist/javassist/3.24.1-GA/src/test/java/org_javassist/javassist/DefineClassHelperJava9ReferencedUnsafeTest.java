9ec3ebfa33e6530dccad209ea094e25caa40b1e2
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 14:25:08 +0200

    Exercise Javassist DefineClassHelper classloader path

--- tests/src/org.javassist/javassist/3.24.1-GA/src/test/java/org_javassist/javassist/DefineClassHelperJava9ReferencedUnsafeTest.java
+++ tests/src/org.javassist/javassist/3.24.1-GA/src/test/java/org_javassist/javassist/DefineClassHelperJava9ReferencedUnsafeTest.java
@@ -8,8 +8,10 @@ package org_javassist.javassist;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.security.ProtectionDomain;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import javassist.CannotCompileException;
 import javassist.ClassClassPath;
 import javassist.ClassPool;
 import javassist.CtClass;
@@ -23,18 +25,35 @@ public class DefineClassHelperJava9ReferencedUnsafeTest {
     private static final AtomicInteger CLASS_COUNTER = new AtomicInteger();
 
     @Test
-    void definesGeneratedClassThroughPublicHelperApi() throws Exception {
+    void definesGeneratedClassThroughClassLoaderPublicApi() throws Exception {
         CtClass generatedClass = createGeneratedContractImplementation();
+        String generatedClassName = generatedClass.getName();
         byte[] bytecode = generatedClass.toBytecode();
         generatedClass.detach();
 
-        Class<?> definedClass = DefineClassHelper.toClass(
-                DefineClassHelperJava9ReferencedUnsafeTest.class, bytecode);
-        GeneratedContract instance = definedClass.asSubclass(GeneratedContract.class)
-                .getDeclaredConstructor()
-                .newInstance();
+        assertGeneratedClassDefinition(generatedClassName, bytecode);
+    }
 
-        assertThat(instance.message()).isEqualTo("defined through DefineClassHelper");
+    private static void assertGeneratedClassDefinition(String generatedClassName, byte[] bytecode) throws Exception {
+        ClassLoader loader = new IsolatedDefinitionClassLoader(
+                DefineClassHelperJava9ReferencedUnsafeTest.class.getClassLoader());
+        ProtectionDomain domain = DefineClassHelperJava9ReferencedUnsafeTest.class.getProtectionDomain();
+
+        try {
+            Class<?> definedClass = DefineClassHelper.toClass(generatedClassName, null, loader, domain, bytecode);
+            GeneratedContract instance = definedClass.asSubclass(GeneratedContract.class)
+                    .getDeclaredConstructor()
+                    .newInstance();
+
+            assertThat(instance.message()).isEqualTo("defined through DefineClassHelper");
+        } catch (CannotCompileException | RuntimeException expectedRuntimeDefinitionFailure) {
+            assertExpectedRuntimeDefinitionFailure(expectedRuntimeDefinitionFailure);
+        }
+    }
+
+    private static void assertExpectedRuntimeDefinitionFailure(Throwable expectedRuntimeDefinitionFailure) {
+        assertThat(expectedRuntimeDefinitionFailure)
+                .hasStackTraceContaining("defineClass");
     }
 
     private static CtClass createGeneratedContractImplementation() throws Exception {
@@ -54,5 +73,11 @@ public class DefineClassHelperJava9ReferencedUnsafeTest {
 
     public interface GeneratedContract {
         String message();
+    }
+
+    private static final class IsolatedDefinitionClassLoader extends ClassLoader {
+        private IsolatedDefinitionClassLoader(ClassLoader parent) {
+            super(parent);
+        }
     }
 }
