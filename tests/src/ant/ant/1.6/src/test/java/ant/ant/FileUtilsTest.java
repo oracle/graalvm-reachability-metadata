5d54815e22f0b7dfa4359012ec67bb6e89b0a9ac
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 00:37:46 +0200

    Reset Ant FileUtils class cache in coverage test

--- tests/src/ant/ant/1.6/src/test/java/ant/ant/FileUtilsTest.java
+++ tests/src/ant/ant/1.6/src/test/java/ant/ant/FileUtilsTest.java
@@ -9,13 +9,21 @@ package ant.ant;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import org.apache.tools.ant.util.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.io.TempDir;
 
 public class FileUtilsTest {
+    @BeforeEach
+    void resetLastModifiedMethodCache() throws ReflectiveOperationException {
+        resetStaticField("class$java$io$File");
+        resetStaticField("setLastModified");
+    }
+
     @Test
     void updatesLastModifiedTimeThroughPublicApi(@TempDir Path temporaryDirectory) throws IOException {
         Path file = temporaryDirectory.resolve("sample.txt");
@@ -25,5 +33,11 @@ public class FileUtilsTest {
         FileUtils.newFileUtils().setFileLastModified(file.toFile(), expectedModifiedTime);
 
         assertThat(file.toFile().lastModified()).isEqualTo(expectedModifiedTime);
+    }
+
+    private static void resetStaticField(String fieldName) throws ReflectiveOperationException {
+        Field field = FileUtils.class.getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(null, null);
     }
 }
