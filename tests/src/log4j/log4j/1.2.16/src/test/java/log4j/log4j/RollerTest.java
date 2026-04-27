0c5b800b3adf8c8c2f4b5cff04c5ec741871488f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 03:59:30 +0200

    Add Roller isolated initialization test

---
+++ tests/src/log4j/log4j/1.2.16/src/test/java/log4j/log4j/RollerTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package log4j.log4j;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.CodeSource;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RollerTest {
+
+    @Test
+    void initializingRollerInFreshClassLoaderResolvesItsOwnClassLiteral() throws Exception {
+        try (URLClassLoader isolatedLoader = new URLClassLoader(isolatedClassPath(), ClassLoader.getPlatformClassLoader())) {
+            Class<?> rollerClass = Class.forName("org.apache.log4j.varia.Roller", true, isolatedLoader);
+
+            assertThat(rollerClass.getName()).isEqualTo("org.apache.log4j.varia.Roller");
+            assertThat(rollerClass.getClassLoader()).isSameAs(isolatedLoader);
+        }
+    }
+
+    private static URL[] isolatedClassPath() {
+        URL testClassesUrl = codeSourceUrl(RollerTest.class);
+        URL libraryClassesUrl = codeSourceUrl(Logger.class);
+        if (testClassesUrl.equals(libraryClassesUrl)) {
+            return new URL[] { testClassesUrl };
+        }
+        return new URL[] { testClassesUrl, libraryClassesUrl };
+    }
+
+    private static URL codeSourceUrl(Class<?> type) {
+        CodeSource codeSource = type.getProtectionDomain().getCodeSource();
+        assertThat(codeSource).isNotNull();
+        return codeSource.getLocation();
+    }
+}
