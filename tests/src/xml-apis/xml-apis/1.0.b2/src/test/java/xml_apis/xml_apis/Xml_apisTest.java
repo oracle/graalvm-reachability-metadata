e5beca24d7c629cfc2686f531d82347a8fa4e348
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 22:39:28 +0200

    Scaffold xml-apis:xml-apis:1.0.b2

---
+++ tests/src/xml-apis/xml-apis/1.0.b2/src/test/java/xml_apis/xml_apis/Xml_apisTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package xml_apis.xml_apis;
+
+import org.junit.jupiter.api.Test;
+
+class Xml_apisTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
