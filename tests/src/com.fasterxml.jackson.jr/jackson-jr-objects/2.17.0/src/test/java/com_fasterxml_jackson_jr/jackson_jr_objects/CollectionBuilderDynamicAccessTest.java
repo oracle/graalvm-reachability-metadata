a7e32e6f6f80c50e82fcf7d410bee4159efee599
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 12:45:51 +0200

    Expand jackson-jr CollectionBuilder dynamic access coverage

--- tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.17.0/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/CollectionBuilderDynamicAccessTest.java
+++ tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.17.0/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/CollectionBuilderDynamicAccessTest.java
@@ -7,13 +7,53 @@
 package com_fasterxml_jackson_jr.jackson_jr_objects;
 
 import com.fasterxml.jackson.jr.ob.JSON;
+import com.fasterxml.jackson.jr.ob.api.CollectionBuilder;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class CollectionBuilderDynamicAccessTest {
     @Test
-    void readsEmptyTypedArrays() throws Exception {
+    void createsEmptyTypedArraysDirectly() throws Exception {
+        CollectionBuilder builder = CollectionBuilder.defaultImpl();
+        Class<ArrayElement> elementType = runtimeArrayElementType();
+
+        Object[] values = builder.emptyArray(elementType);
+
+        assertThat(values).isEmpty();
+        assertThat(values.getClass().getComponentType()).isSameAs(elementType);
+    }
+
+    @Test
+    void createsSingletonTypedArraysDirectly() throws Exception {
+        CollectionBuilder builder = CollectionBuilder.defaultImpl();
+        Class<ArrayElement> elementType = runtimeArrayElementType();
+
+        Object[] values = builder.singletonArray(elementType, new ArrayElement("solo"));
+
+        assertThat(values).singleElement().isInstanceOf(ArrayElement.class);
+        assertThat(((ArrayElement) values[0]).name).isEqualTo("solo");
+        assertThat(values.getClass().getComponentType()).isSameAs(elementType);
+    }
+
+    @Test
+    void createsMultiValueTypedArraysDirectly() throws Exception {
+        CollectionBuilder builder = CollectionBuilder.defaultImpl();
+        Class<ArrayElement> elementType = runtimeArrayElementType();
+
+        Object[] values = builder.start()
+                .add(new ArrayElement("left"))
+                .add(new ArrayElement("right"))
+                .buildArray(elementType);
+
+        assertThat(values).hasSize(2);
+        assertThat(((ArrayElement) values[0]).name).isEqualTo("left");
+        assertThat(((ArrayElement) values[1]).name).isEqualTo("right");
+        assertThat(values.getClass().getComponentType()).isSameAs(elementType);
+    }
+
+    @Test
+    void readsEmptyTypedArraysThroughJsonApi() throws Exception {
         Class<String> elementType = runtimeStringType();
 
         Object[] values = JSON.std.arrayOfFrom(elementType, "[]");
@@ -24,7 +64,7 @@ public class CollectionBuilderDynamicAccessTest {
     }
 
     @Test
-    void readsSingletonTypedArrays() throws Exception {
+    void readsSingletonTypedArraysThroughJsonApi() throws Exception {
         Class<String> elementType = runtimeStringType();
 
         Object[] values = JSON.std.arrayOfFrom(elementType, "[\"solo\"]");
@@ -35,7 +75,7 @@ public class CollectionBuilderDynamicAccessTest {
     }
 
     @Test
-    void readsMultiValueTypedArrays() throws Exception {
+    void readsMultiValueTypedArraysThroughJsonApi() throws Exception {
         Class<String> elementType = runtimeStringType();
 
         Object[] values = JSON.std.arrayOfFrom(elementType, "[\"left\",\"right\"]");
@@ -46,12 +86,23 @@ public class CollectionBuilderDynamicAccessTest {
     }
 
     @SuppressWarnings("unchecked")
+    private static Class<ArrayElement> runtimeArrayElementType() throws Exception {
+        return (Class<ArrayElement>) JSON.std.beanFrom(Class.class, '"' + ArrayElement.class.getName() + '"');
+    }
+
+    @SuppressWarnings("unchecked")
     private static Class<String> runtimeStringType() throws Exception {
         return (Class<String>) JSON.std.beanFrom(Class.class, '"' + String.class.getName() + '"');
     }
 
     public static final class ArrayElement {
-        private ArrayElement() {
+        public String name;
+
+        public ArrayElement() {
+        }
+
+        public ArrayElement(String name) {
+            this.name = name;
         }
     }
 }
