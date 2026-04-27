9fb1804d1f061277790ae9914f4f5cb43f4c2751
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 03:50:59 +0200

    Add SocketServer dynamic access test for log4j 1.2.16

---
+++ tests/src/log4j/log4j/1.2.16/src/test/java/log4j/log4j/SocketServerTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package log4j.log4j;
+
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.security.CodeSource;
+
+import org.apache.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SocketServerTest {
+
+    @Test
+    void initializingSocketServerInFreshClassLoaderResolvesItsOwnClassLiteral() throws Exception {
+        try (URLClassLoader isolatedLoader = new URLClassLoader(isolatedClassPath(), ClassLoader.getPlatformClassLoader())) {
+            Class<?> socketServerClass = Class.forName("org.apache.log4j.net.SocketServer", true, isolatedLoader);
+
+            assertThat(socketServerClass.getName()).isEqualTo("org.apache.log4j.net.SocketServer");
+            assertThat(socketServerClass.getClassLoader()).isSameAs(isolatedLoader);
+        }
+    }
+
+    private static URL[] isolatedClassPath() {
+        URL testClassesUrl = codeSourceUrl(SocketServerTest.class);
+        URL libraryClassesUrl = codeSourceUrl(Logger.class);
+        if (testClassesUrl.equals(libraryClassesUrl)) {
+            return new URL[] { testClassesUrl };
+        }
+        return new URL[] { testClassesUrl, libraryClassesUrl };
+    }
+
+    private static URL codeSourceUrl(Class<?> type) {
+        CodeSource codeSource = type.getProtectionDomain().getCodeSource();
+        assertThat(codeSource).isNotNull();
+        return codeSource.getLocation();
+    }
+}
