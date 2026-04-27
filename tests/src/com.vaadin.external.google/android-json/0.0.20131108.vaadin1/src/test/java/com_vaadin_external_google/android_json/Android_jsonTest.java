e127820279241bdb7da068122e65d4527730a9b4
Mihailo <mihailo.markovic@oracle.com>
2026-04-27 17:42:12 +0200

    Scaffold com.vaadin.external.google:android-json:0.0.20131108.vaadin1

---
+++ tests/src/com.vaadin.external.google/android-json/0.0.20131108.vaadin1/src/test/java/com_vaadin_external_google/android_json/Android_jsonTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_vaadin_external_google.android_json;
+
+import org.junit.jupiter.api.Test;
+
+class Android_jsonTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
