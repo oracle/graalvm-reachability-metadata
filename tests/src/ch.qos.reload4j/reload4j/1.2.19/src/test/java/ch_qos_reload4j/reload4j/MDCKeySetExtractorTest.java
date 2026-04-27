a7347b7bf58cb1d77a6546d24298eab6aa06507a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 23:52:51 +0200

    Add reload4j MDC key set extractor coverage

---
+++ tests/src/ch.qos.reload4j/reload4j/1.2.19/src/test/java/ch_qos_reload4j/reload4j/MDCKeySetExtractorTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package ch_qos_reload4j.reload4j;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.MDC;
+import org.apache.log4j.helpers.MDCKeySetExtractor;
+import org.apache.log4j.spi.LoggingEvent;
+import org.junit.jupiter.api.Test;
+
+public class MDCKeySetExtractorTest {
+    private static final Logger LOGGER = Logger.getLogger(MDCKeySetExtractorTest.class);
+    private static final String LOGGER_FQCN = MDCKeySetExtractorTest.class.getName();
+
+    @Test
+    void extractsMappedDiagnosticContextKeysFromLoggingEvent() throws Exception {
+        MDC.put("requestId", "request-42");
+        MDC.put("tenant", "metadata-forge");
+        try {
+            LoggingEvent event = new LoggingEvent(LOGGER_FQCN, LOGGER, Level.INFO,
+                    "message with mapped diagnostic context", null);
+
+            Set propertyKeys = MDCKeySetExtractor.INSTANCE.getPropertyKeySet(event);
+
+            assertThat(propertyKeys).containsExactlyInAnyOrder("requestId", "tenant");
+        } finally {
+            MDC.clear();
+        }
+    }
+}
