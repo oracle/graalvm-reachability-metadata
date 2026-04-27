0e4d314f1b8ec54f178736e58f1a6944e2ccb5af
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 16:43:38 +0200

    Add Velocity IntrospectorTestCase3 coverage

---
+++ tests/src/velocity/velocity-dep/1.4/src/test/java/velocity/velocity_dep/IntrospectorTestCase3Test.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package velocity.velocity_dep;
+
+import junit.framework.TestResult;
+import org.apache.velocity.test.IntrospectorTestCase3;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class IntrospectorTestCase3Test {
+    @Test
+    void resolvesOverloadedPrimitiveMethodsWithVelocityIntrospector() {
+        TestResult result = new TestResult();
+
+        IntrospectorTestCase3.suite().run(result);
+
+        assertThat(result.errorCount()).isZero();
+        assertThat(result.failureCount()).isZero();
+        assertThat(result.runCount()).isEqualTo(1);
+    }
+}
