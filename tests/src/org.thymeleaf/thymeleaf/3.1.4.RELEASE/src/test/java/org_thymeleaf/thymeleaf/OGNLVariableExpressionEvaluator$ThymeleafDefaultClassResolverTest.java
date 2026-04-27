3534c3d0e234fb6b2128c07f2c80c3abb80a402f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 10:57:22 +0200

    Add Thymeleaf OGNL class resolver coverage test

---
+++ tests/src/org.thymeleaf/thymeleaf/3.1.4.RELEASE/src/test/java/org_thymeleaf/thymeleaf/OGNLVariableExpressionEvaluator$ThymeleafDefaultClassResolverTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_thymeleaf.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class OGNLVariableExpressionEvaluator$ThymeleafDefaultClassResolverTest {
+
+    @Test
+    void resolvesAllowedJavaTypeForStaticMethodInvocation() {
+        TemplateEngine templateEngine = new TemplateEngine();
+        Context context = new Context();
+
+        String output = templateEngine.process(
+                "<p th:text=\"${@java.lang.Math@max(2, 5)}\">placeholder</p>",
+                context);
+
+        assertThat(output).isEqualTo("<p>5</p>");
+    }
+}
