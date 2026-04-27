2ab458b5917dbfec757897dadd7d66a2642c10d9
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-27 18:11:47 +0200

    Exercise SAXBuilder parser creation directly

--- tests/src/jdom/jdom/1.0/src/test/java/jdom/jdom/SAXBuilderTest.java
+++ tests/src/jdom/jdom/1.0/src/test/java/jdom/jdom/SAXBuilderTest.java
@@ -8,16 +8,33 @@ package jdom.jdom;
 
 import org.jdom.Document;
 import org.jdom.Element;
+import org.jdom.JDOMException;
 import org.jdom.input.SAXBuilder;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
 
 import java.io.StringReader;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 public class SAXBuilderTest {
     @Test
+    @Order(1)
+    void createParserUsesDefaultJaxpParserFactory() throws Exception {
+        ExposedSAXBuilder builder = new ExposedSAXBuilder();
+
+        XMLReader parser = builder.createXmlReader();
+
+        assertThat(parser).isNotNull();
+    }
+
+    @Test
+    @Order(2)
     void buildUsesDefaultJaxpParserFactory() throws Exception {
         SAXBuilder builder = new SAXBuilder();
         InputSource source = new InputSource(new StringReader("""
@@ -35,5 +52,11 @@ public class SAXBuilderTest {
         assertThat(root.getName()).isEqualTo("catalog");
         assertThat(book.getAttributeValue("id")).isEqualTo("b1");
         assertThat(book.getChildText("title")).isEqualTo("Native XML");
+    }
+
+    private static final class ExposedSAXBuilder extends SAXBuilder {
+        XMLReader createXmlReader() throws JDOMException {
+            return createParser();
+        }
     }
 }
