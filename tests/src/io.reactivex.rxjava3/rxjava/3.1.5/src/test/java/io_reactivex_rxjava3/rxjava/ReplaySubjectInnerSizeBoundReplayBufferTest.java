02dda7c487135adc2476cc6861904d13bf330921
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 22:53:58 +0200

    Add ReplaySubject size-bound buffer coverage

---
+++ tests/src/io.reactivex.rxjava3/rxjava/3.1.5/src/test/java/io_reactivex_rxjava3/rxjava/ReplaySubjectInnerSizeBoundReplayBufferTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_reactivex_rxjava3.rxjava;
+
+import io.reactivex.rxjava3.subjects.ReplaySubject;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ReplaySubjectInnerSizeBoundReplayBufferTest {
+    @Test
+    void typedValuesSnapshotAllocatesArrayForRetainedItems() {
+        ReplaySubject<String> subject = ReplaySubject.createWithSize(2);
+
+        subject.onNext("alpha");
+        subject.onNext("beta");
+        subject.onNext("gamma");
+
+        String[] values = subject.getValues(new String[0]);
+
+        assertEquals(String[].class, values.getClass());
+        assertArrayEquals(new String[] {"beta", "gamma"}, values);
+    }
+}
