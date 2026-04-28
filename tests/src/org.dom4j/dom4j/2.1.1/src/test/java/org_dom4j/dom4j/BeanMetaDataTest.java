1570e06c61d510ab720081ced16e37f9670e2f4e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 00:36:50 +0200

    Avoid jaxen dependency in dom4j BeanMetaData test

--- tests/src/org.dom4j/dom4j/2.1.1/src/test/java/org_dom4j/dom4j/BeanMetaDataTest.java
+++ tests/src/org.dom4j/dom4j/2.1.1/src/test/java/org_dom4j/dom4j/BeanMetaDataTest.java
@@ -8,28 +8,25 @@ package org_dom4j.dom4j;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.Collections;
-import java.util.Map;
-
 import org.dom4j.DocumentFactory;
+import org.dom4j.QName;
 import org.dom4j.bean.BeanMetaData;
 import org.junit.jupiter.api.Test;
 
 public class BeanMetaDataTest {
     @Test
     void readsAndWritesBeanPropertyData() {
-        BeanMetaData metaData = BeanMetaData.get(DocumentFactory.class);
-        int index = metaData.getIndex("XPathNamespaceURIs");
+        BeanMetaData metaData = BeanMetaData.get(QName.class);
+        int index = metaData.getIndex("documentFactory");
+        QName qName = QName.get("chapter");
         DocumentFactory factory = new DocumentFactory();
-        Map<String, String> namespaces = Collections.singletonMap("example",
-                "urn:example");
 
         assertThat(index).isNotEqualTo(-1);
 
-        metaData.setData(index, factory, namespaces);
-        Object data = metaData.getData(index, factory);
+        metaData.setData(index, qName, factory);
+        Object data = metaData.getData(index, qName);
 
-        assertThat(data).isSameAs(namespaces);
-        assertThat(metaData.getQName(index).getName()).isEqualTo("XPathNamespaceURIs");
+        assertThat(data).isSameAs(factory);
+        assertThat(metaData.getQName(index).getName()).isEqualTo("documentFactory");
     }
 }
