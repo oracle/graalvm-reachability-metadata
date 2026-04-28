3d53b6648752e121f0320a38d5365000da52ac90
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 19:42:43 +0200

    Add BeanDocumentFactory dynamic access test

---
+++ tests/src/dom4j/dom4j/1.6.1/src/test/java/dom4j/dom4j/BeanDocumentFactoryTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package dom4j.dom4j;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.dom4j.Element;
+import org.dom4j.QName;
+import org.dom4j.bean.BeanDocumentFactory;
+import org.dom4j.bean.BeanElement;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.helpers.AttributesImpl;
+
+public class BeanDocumentFactoryTest {
+    @Test
+    void createsBeanBackedElementFromClassAttribute() {
+        BeanDocumentFactory factory = new BeanDocumentFactory();
+        AttributesImpl attributes = new AttributesImpl();
+        attributes.addAttribute("", "class", "class", "CDATA", BeanDocumentFactory.class.getName());
+
+        Element element = factory.createElement(QName.get("configuredBean"), attributes);
+
+        assertThat(element).isInstanceOf(BeanElement.class);
+        assertThat(element.getName()).isEqualTo("configuredBean");
+        assertThat(((BeanElement) element).getData()).isInstanceOf(BeanDocumentFactory.class).isNotSameAs(factory);
+    }
+}
