8483715dd824d10bd65221bc45b22ade7bb94f21
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 04:40:44 +0200

    Add MethodMap primitive boolean coverage test

---
+++ tests/src/org.codehaus.plexus/plexus-interpolation/1.0/src/test/java/org_codehaus_plexus/plexus_interpolation/MethodMapTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_codehaus_plexus.plexus_interpolation;
+
+import org.codehaus.plexus.interpolation.reflection.MethodMap;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MethodMapTest {
+    @Test
+    void matchesBoxedBooleanArgumentToPrimitiveBooleanParameter() throws Exception {
+        MethodMap methodMap = new MethodMap();
+        Method booleanMethod = MethodMapPrimitiveTarget.class.getMethod("accept", boolean.class);
+        methodMap.add(booleanMethod);
+
+        Method method = methodMap.find("accept", new Object[] { Boolean.TRUE });
+
+        assertThat(method).isEqualTo(booleanMethod);
+    }
+}
+
+final class MethodMapPrimitiveTarget {
+    public void accept(boolean value) {
+    }
+}
