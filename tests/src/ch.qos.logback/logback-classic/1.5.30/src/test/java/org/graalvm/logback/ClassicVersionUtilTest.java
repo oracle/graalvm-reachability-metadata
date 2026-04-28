d0e4b15d9d96e0f93d79091041f4aa23417a2b1d
Mihailo <mihailo.markovic@oracle.com>
2026-04-28 14:29:09 +0200

    Cover logback classic version resource lookup

--- tests/src/ch.qos.logback/logback-classic/1.5.30/src/test/java/org/graalvm/logback/ClassicVersionUtilTest.java
+++ tests/src/ch.qos.logback/logback-classic/1.5.30/src/test/java/org/graalvm/logback/ClassicVersionUtilTest.java
@@ -6,6 +6,11 @@
  */
 package org.graalvm.logback;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
+import ch.qos.logback.classic.ClassicConstants;
 import ch.qos.logback.classic.util.ClassicVersionUtil;
 import org.junit.jupiter.api.Test;
 
@@ -14,9 +19,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class ClassicVersionUtilTest {
 
   @Test
-  void shouldReadSelfDeclaredClassicVersionProperties() {
-    String version = ClassicVersionUtil.getVersionBySelfDeclaredProperties();
+  void shouldReadSelfDeclaredClassicVersionProperties() throws Throwable {
+    String version = getVersionBySelfDeclaredProperties(ClassicConstants.class, "logback-classic");
 
     assertThat(version).isNotBlank();
+    assertThat(version).isEqualTo(ClassicVersionUtil.getVersionBySelfDeclaredProperties());
+  }
+
+  private static String getVersionBySelfDeclaredProperties(Class<?> resourceAnchor, String moduleName) throws Throwable {
+    MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(ClassicVersionUtil.class, MethodHandles.lookup());
+    MethodHandle versionReader = lookup.findStatic(
+        ClassicVersionUtil.class,
+        "getVersionBySelfDeclaredProperties",
+        MethodType.methodType(String.class, Class.class, String.class));
+    return (String) versionReader.invokeExact(resourceAnchor, moduleName);
   }
 }
