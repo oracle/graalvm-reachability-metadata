2790ada1819528fa723e3d8fb7367b91e2d8a00a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 13:51:33 +0200

    Refine jackson-jr BeanPropertyWriter dynamic-access test

--- tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.21.0/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/BeanPropertyWriterDynamicAccessTest.java
+++ tests/src/com.fasterxml.jackson.jr/jackson-jr-objects/2.21.0/src/test/java/com_fasterxml_jackson_jr/jackson_jr_objects/BeanPropertyWriterDynamicAccessTest.java
@@ -15,27 +15,27 @@ public class BeanPropertyWriterDynamicAccessTest {
     private static final JSON JSON_WITH_FORCE_ACCESS = JSON.std.with(JSON.Feature.FORCE_REFLECTION_ACCESS);
 
     @Test
-    void serializesFieldBackedProperties() throws Exception {
-        String json = JSON_WITH_FORCE_ACCESS.asString(new FieldBackedWriterBean());
+    void serializesFieldBackedPropertiesFromNonPublicBeans() throws Exception {
+        String json = JSON_WITH_FORCE_ACCESS.asString(new NonPublicFieldBackedWriterBean());
 
         assertThat(json).isEqualTo("{\"id\":3}");
     }
 
     @Test
-    void serializesGetterBackedProperties() throws Exception {
-        String json = JSON_WITH_FORCE_ACCESS.asString(new GetterBackedWriterBean("Ada"));
+    void serializesGetterBackedPropertiesFromNonPublicBeans() throws Exception {
+        String json = JSON_WITH_FORCE_ACCESS.asString(new NonPublicGetterBackedWriterBean("Ada"));
 
         assertThat(json).isEqualTo("{\"name\":\"Ada\"}");
     }
 
-    public static final class FieldBackedWriterBean {
+    private static final class NonPublicFieldBackedWriterBean {
         public int id = 3;
     }
 
-    public static final class GetterBackedWriterBean {
+    private static final class NonPublicGetterBackedWriterBean {
         private String name;
 
-        public GetterBackedWriterBean(String name) {
+        private NonPublicGetterBackedWriterBean(String name) {
             this.name = name;
         }
 
