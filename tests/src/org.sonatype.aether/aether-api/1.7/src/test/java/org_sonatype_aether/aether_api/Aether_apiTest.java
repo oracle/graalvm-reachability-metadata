9d1ed7ac563d6958db70f1dba9b454d8a68245b4
Mihailo <mihailo.markovic@oracle.com>
2026-04-28 02:05:11 +0200

    Scaffold org.sonatype.aether:aether-api:1.7

---
+++ tests/src/org.sonatype.aether/aether-api/1.7/src/test/java/org_sonatype_aether/aether_api/Aether_apiTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_sonatype_aether.aether_api;
+
+import org.junit.jupiter.api.Test;
+
+class Aether_apiTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
