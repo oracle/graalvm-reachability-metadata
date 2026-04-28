d72b6d1e84811624a9ca32639e8a236d206d6875
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 19:47:53 +0200

    Scaffold io.dropwizard.metrics:metrics-jmx:4.2.0

---
+++ tests/src/io.dropwizard.metrics/metrics-jmx/4.2.0/src/test/java/io_dropwizard_metrics/metrics_jmx/Metrics_jmxTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_dropwizard_metrics.metrics_jmx;
+
+import org.junit.jupiter.api.Test;
+
+class Metrics_jmxTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
