28c71a0b62c33ecdcc86068580da172fc627ad98
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 09:44:07 +0200

    Exercise BshClassLoader designated loader path

--- tests/src/org.apache-extras.beanshell/bsh/2.0b6/src/test/java/org_apache_extras_beanshell/bsh/BshClassLoaderTest.java
+++ tests/src/org.apache-extras.beanshell/bsh/2.0b6/src/test/java/org_apache_extras_beanshell/bsh/BshClassLoaderTest.java
@@ -6,6 +6,7 @@
  */
 package org_apache_extras_beanshell.bsh;
 
+import bsh.Interpreter;
 import bsh.classpath.BshClassLoader;
 import bsh.classpath.ClassManagerImpl;
 import java.net.URL;
@@ -14,6 +15,18 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BshClassLoaderTest {
+
+    @Test
+    public void loadClassDelegatesToDesignatedLoaderForGeneratedScriptClass() throws Exception {
+        String scriptedClassName = "GeneratedBshClassLoaderCoverage";
+        Interpreter interpreter = new Interpreter();
+        interpreter.eval("class " + scriptedClassName + " { public String value() { return \"loaded\"; } }");
+        BshClassLoader classLoader = new BshClassLoader(interpreter.getClassManager(), new URL[0]);
+
+        Class<?> loadedClass = classLoader.loadClass(scriptedClassName);
+
+        assertThat(loadedClass.getName()).isEqualTo(scriptedClassName);
+    }
 
     @Test
     public void loadClassDelegatesToClassManagerBaseLoaderWhenNoLocalUrlMatches() throws ClassNotFoundException {
