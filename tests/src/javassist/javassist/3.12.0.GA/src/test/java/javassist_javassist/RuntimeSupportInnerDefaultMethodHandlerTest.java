e90ad9b326313d54607a33019d5f04bdeaa2d1ce
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 22:56:52 +0200

    Add Javassist default method handler coverage

---
+++ tests/src/javassist/javassist/3.12.0.GA/src/test/java/javassist_javassist/RuntimeSupportInnerDefaultMethodHandlerTest.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package javassist_javassist;
+
+import java.io.Serializable;
+
+import javassist.util.proxy.ProxyFactory;
+import javassist.util.proxy.ProxyObject;
+import javassist.util.proxy.RuntimeSupport;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class RuntimeSupportInnerDefaultMethodHandlerTest {
+    @Test
+    void defaultHandlerInvokesGeneratedProceedMethod() throws Throwable {
+        ProxyFactory factory = new ProxyFactory();
+        factory.setUseCache(false);
+        factory.setUseWriteReplace(false);
+        factory.setSuperclass(DefaultHandledService.class);
+        factory.setFilter(method -> method.getName().equals("message"));
+
+        DefaultHandledService proxy = (DefaultHandledService) factory.create(
+                new Class[] {String.class},
+                new Object[] {"base"});
+
+        assertThat(ProxyFactory.isProxyClass(proxy.getClass())).isTrue();
+        assertThat(((ProxyObject) proxy).getHandler()).isSameAs(RuntimeSupport.default_interceptor);
+        assertThat(proxy.message("javassist", 3)).isEqualTo("base:javassist:3");
+        assertThat(proxy.unhandled()).isEqualTo("base:unhandled");
+    }
+
+    public static class DefaultHandledService implements Serializable {
+        private static final long serialVersionUID = 1L;
+
+        private final String prefix;
+
+        public DefaultHandledService(String prefix) {
+            this.prefix = prefix;
+        }
+
+        public String message(String value, int count) {
+            return prefix + ":" + value + ":" + count;
+        }
+
+        public String unhandled() {
+            return prefix + ":unhandled";
+        }
+    }
+}
