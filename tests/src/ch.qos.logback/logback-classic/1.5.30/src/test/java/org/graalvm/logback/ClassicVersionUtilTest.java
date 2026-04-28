f98d71e3551a422470897028311897a643be89d0
Mihailo <mihailo.markovic@oracle.com>
2026-04-28 14:26:08 +0200

    Add logback classic version utility coverage

---
+++ tests/src/ch.qos.logback/logback-classic/1.5.30/src/test/java/org/graalvm/logback/ClassicVersionUtilTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.graalvm.logback;
+
+import ch.qos.logback.classic.util.ClassicVersionUtil;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClassicVersionUtilTest {
+
+  @Test
+  void shouldReadSelfDeclaredClassicVersionProperties() {
+    String version = ClassicVersionUtil.getVersionBySelfDeclaredProperties();
+
+    assertThat(version).isNotBlank();
+  }
+}
