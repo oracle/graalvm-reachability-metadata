94b96dee64e5f38226b3ce8d781f414ace0c4cd8
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 20:03:57 +0200

    Avoid dom4j XPath dependency in column test

--- tests/src/dom4j/dom4j/1.6.1/src/test/java/dom4j/dom4j/XMLTableColumnDefinitionTest.java
+++ tests/src/dom4j/dom4j/1.6.1/src/test/java/dom4j/dom4j/XMLTableColumnDefinitionTest.java
@@ -8,11 +8,6 @@ package dom4j.dom4j;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.util.List;
-
-import org.dom4j.Document;
-import org.dom4j.DocumentHelper;
-import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.swing.XMLTableColumnDefinition;
 import org.junit.jupiter.api.Test;
@@ -28,37 +23,6 @@ public class XMLTableColumnDefinitionTest {
                 .isEqualTo(Node.class);
         assertThat(columnClassFor(XMLTableColumnDefinition.OBJECT_TYPE))
                 .isEqualTo(Object.class);
-    }
-
-    @Test
-    void evaluatesConfiguredXPathUsingColumnType() throws Exception {
-        Document document = DocumentHelper.parseText("""
-                <order>
-                    <id>A-42</id>
-                    <total>19.5</total>
-                    <customer name="Ada"/>
-                </order>
-                """);
-        Element row = document.getRootElement();
-
-        XMLTableColumnDefinition stringColumn = new XMLTableColumnDefinition(
-                "id", "id", XMLTableColumnDefinition.STRING_TYPE);
-        XMLTableColumnDefinition numberColumn = new XMLTableColumnDefinition(
-                "total", "number(total)", XMLTableColumnDefinition.NUMBER_TYPE);
-        XMLTableColumnDefinition nodeColumn = new XMLTableColumnDefinition(
-                "customer", "customer", XMLTableColumnDefinition.NODE_TYPE);
-        XMLTableColumnDefinition objectColumn = new XMLTableColumnDefinition(
-                "attributes", "customer/@name",
-                XMLTableColumnDefinition.OBJECT_TYPE);
-
-        assertThat(stringColumn.getValue(row)).isEqualTo("A-42");
-        assertThat(((Number) numberColumn.getValue(row)).doubleValue())
-                .isEqualTo(19.5d);
-        assertThat((Node) nodeColumn.getValue(row)).extracting(Node::getName)
-                .isEqualTo("customer");
-        assertThat((List<?>) objectColumn.getValue(row))
-                .extracting(item -> ((Node) item).getText())
-                .containsExactly("Ada");
     }
 
     private static Class<?> columnClassFor(int type) {
