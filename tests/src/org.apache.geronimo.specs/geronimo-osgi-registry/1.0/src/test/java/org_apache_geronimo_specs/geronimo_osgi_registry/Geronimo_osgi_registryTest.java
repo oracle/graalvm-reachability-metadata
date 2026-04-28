f5a4508a397240dc6bd5f9680f5fa719836026e0
jvukicev <jovan.vukicevic@oracle.com>
2026-04-28 17:50:30 +0200

    Scaffold org.apache.geronimo.specs:geronimo-osgi-registry:1.0

---
+++ tests/src/org.apache.geronimo.specs/geronimo-osgi-registry/1.0/src/test/java/org_apache_geronimo_specs/geronimo_osgi_registry/Geronimo_osgi_registryTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_geronimo_specs.geronimo_osgi_registry;
+
+import org.junit.jupiter.api.Test;
+
+class Geronimo_osgi_registryTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
