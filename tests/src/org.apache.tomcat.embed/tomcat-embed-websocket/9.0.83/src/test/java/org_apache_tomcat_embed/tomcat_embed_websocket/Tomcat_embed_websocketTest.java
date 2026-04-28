0e6ed363d0ac26d01a43eba289e42419a6da98d4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-28 18:07:09 +0200

    Scaffold org.apache.tomcat.embed:tomcat-embed-websocket:9.0.83

---
+++ tests/src/org.apache.tomcat.embed/tomcat-embed-websocket/9.0.83/src/test/java/org_apache_tomcat_embed/tomcat_embed_websocket/Tomcat_embed_websocketTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_tomcat_embed.tomcat_embed_websocket;
+
+import org.junit.jupiter.api.Test;
+
+class Tomcat_embed_websocketTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
