b5267be65a2366ab2e5e5aa79ffc29a04cee61cd
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 13:47:18 +0200

    Add Formatters anonymous class fallback tests

--- tests/src/org.jboss.logmanager/jboss-logmanager/1.3.0.Final/src/test/java/org_jboss_logmanager/jboss_logmanager/FormattersAnonymous12Test.java
+++ tests/src/org.jboss.logmanager/jboss-logmanager/1.3.0.Final/src/test/java/org_jboss_logmanager/jboss_logmanager/FormattersAnonymous12Test.java
@@ -8,89 +8,71 @@ package org_jboss_logmanager.jboss_logmanager;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.invoke.MethodHandle;
 import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
 import java.lang.invoke.VarHandle;
 import java.net.URL;
-import java.security.AccessController;
 import java.security.CodeSource;
-import java.security.PrivilegedAction;
 import java.security.ProtectionDomain;
 import java.security.cert.Certificate;
 import java.util.Objects;
 import java.util.logging.Level;
 
 import org.jboss.logmanager.ExtLogRecord;
-import org.jboss.logmanager.formatters.Formatters;
 import org.jboss.logmanager.formatters.PatternFormatter;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assumptions.assumeFalse;
 
-public class Formatters12Test {
+public class FormattersAnonymous12Test {
 
     @Test
     void extendedExceptionFormattingUsesTcclDefinedClassAndResourceLookupWhenCodeSourceLocationIsMissing()
             throws Exception {
-        assumeFalse(isNativeImageRuntime(), "Runtime class definition is not supported in native-image tests");
+        final String className = FormattersAnonymous12DefinedClass.class.getName();
+        final TrackingDefinedClassLoader trackingLoader = new TrackingDefinedClassLoader(className);
 
-        String className = Formatters12DefinedClass.class.getName();
-        TrackingDefinedClassLoader trackingLoader = new TrackingDefinedClassLoader(className);
-
-        String formatted = formatWithTccl(trackingLoader, newFailure(className));
+        final String formatted = formatWithTccl(trackingLoader, newFailure(className));
 
         assertThat(formatted).contains("\tat " + className + ".invoke");
         assertThat(trackingLoader.wasResourceRequested()).isTrue();
     }
 
     @Test
-    void privilegedExtendedExceptionResourceLookupMatchesDirectClassLoaderLookup() throws Throwable {
-        assumeFalse(System.getProperty("java.vm.name", "").contains("Substrate VM"),
-                "Anonymous privileged-action construction is only needed for JVM dynamic-access coverage");
-
-        Object exceptionFormatStep = Formatters.exceptionFormatStep(true, 0, 0, true);
-        Class<?> privilegedActionClass = exceptionFormatStep.getClass().getClassLoader()
-                .loadClass(exceptionFormatStep.getClass().getName() + "$2");
-        MethodHandles.Lookup actionLookup = MethodHandles.privateLookupIn(privilegedActionClass, MethodHandles.lookup());
-        MethodHandle constructor = actionLookup.findConstructor(
-                privilegedActionClass,
-                MethodType.methodType(void.class, exceptionFormatStep.getClass(), Class.class, String.class)
-        );
-        String classResourceName = Formatters12Test.class.getName().replace('.', '/') + ".class";
-        URL expectedResource = Formatters12Test.class.getClassLoader().getResource(classResourceName);
-
-        @SuppressWarnings("unchecked")
-        PrivilegedAction<URL> privilegedAction = (PrivilegedAction<URL>) constructor.invoke(
-                exceptionFormatStep,
-                Formatters12Test.class,
-                classResourceName
+    void oneArgumentClassForNameIsUsedWhenTheContextClassLoaderRejectsTheFrameClass() {
+        final String className = String.class.getName();
+        final RejectingClassLoader rejectingLoader = new RejectingClassLoader(
+                Thread.currentThread().getContextClassLoader(),
+                className
         );
 
-        assertThat(AccessController.doPrivileged(privilegedAction)).isEqualTo(expectedResource);
+        final String formatted = formatWithTccl(rejectingLoader, newFailure(className));
+
+        assertThat(formatted).contains("\tat " + className + ".invoke");
+        assertThat(rejectingLoader.wasRejected()).isTrue();
     }
 
     @Test
-    void extendedExceptionFormattingUsesBootstrapFallbackWhenTheLibraryLoaderRejectsTheClass() throws Throwable {
-        assumeFalse(isNativeImageRuntime(), "Runtime class definition is not supported in native-image tests");
+    void bootstrapClassLoaderFallbackIsUsedWhenTheLibraryLoaderRejectsTheFrameClass() throws Throwable {
+        final String className = "java.util.HexFormat";
+        final BootstrapFallbackClassLoader bootstrapFallbackClassLoader = new BootstrapFallbackClassLoader(className);
+        final BootstrapFormattingAction formattingAction = bootstrapFallbackClassLoader.loadFormattingAction();
 
-        String className = "java.util.HexFormat";
-        BootstrapFallbackClassLoader bootstrapFallbackClassLoader = new BootstrapFallbackClassLoader(className);
-        BootstrapFormattingAction formattingAction = bootstrapFallbackClassLoader.loadFormattingAction();
-
-        String formatted = formattingAction.format(newFailure(className));
+        final String formatted = formattingAction.format(newFailure(className));
 
         assertThat(formatted).contains("\tat " + className + ".invoke");
         assertThat(bootstrapFallbackClassLoader.wasRejected()).isTrue();
     }
 
     private static String formatWithTccl(final ClassLoader contextClassLoader, final Throwable thrown) {
-        ClassLoader originalTccl = Thread.currentThread().getContextClassLoader();
+        final ClassLoader originalTccl = Thread.currentThread().getContextClassLoader();
         try {
             Thread.currentThread().setContextClassLoader(contextClassLoader);
-            PatternFormatter formatter = new PatternFormatter("%E");
-            ExtLogRecord record = new ExtLogRecord(Level.SEVERE, "coverage", Formatters12Test.class.getName());
+            final PatternFormatter formatter = new PatternFormatter("%E");
+            final ExtLogRecord record = new ExtLogRecord(
+                    Level.SEVERE,
+                    "coverage",
+                    FormattersAnonymous12Test.class.getName()
+            );
             record.setThrown(thrown);
             return formatter.format(record);
         } finally {
@@ -99,15 +81,11 @@ public class Formatters12Test {
     }
 
     private static IllegalStateException newFailure(final String className) {
-        IllegalStateException failure = new IllegalStateException("boom");
+        final IllegalStateException failure = new IllegalStateException("boom");
         failure.setStackTrace(new StackTraceElement[] {
                 new StackTraceElement(className, "invoke", "GeneratedFrame.java", 17)
         });
         return failure;
-    }
-
-    private static boolean isNativeImageRuntime() {
-        return "runtime".equals(System.getProperty("org.graalvm.nativeimage.imagecode"));
     }
 
     private static final class TrackingDefinedClassLoader extends ClassLoader {
@@ -117,7 +95,7 @@ public class Formatters12Test {
         private boolean resourceRequested;
 
         private TrackingDefinedClassLoader(final String definedClassName) throws IOException {
-            super(Formatters12Test.class.getClassLoader());
+            super(FormattersAnonymous12Test.class.getClassLoader());
             this.definedClassName = definedClassName;
             this.resourceName = definedClassName.replace('.', '/') + ".class";
             this.definedClassBytes = loadClassBytes(resourceName);
@@ -133,11 +111,11 @@ public class Formatters12Test {
                 return super.loadClass(name, resolve);
             }
             synchronized (getClassLoadingLock(name)) {
-                Class<?> alreadyLoaded = findLoadedClass(name);
+                final Class<?> alreadyLoaded = findLoadedClass(name);
                 if (alreadyLoaded != null) {
                     return alreadyLoaded;
                 }
-                Class<?> definedClass = defineClass(
+                final Class<?> definedClass = defineClass(
                         name,
                         definedClassBytes,
                         0,
@@ -152,7 +130,7 @@ public class Formatters12Test {
         }
 
         @Override
-        public java.net.URL getResource(final String name) {
+        public URL getResource(final String name) {
             if (resourceName.equals(name)) {
                 resourceRequested = true;
             }
@@ -160,7 +138,8 @@ public class Formatters12Test {
         }
 
         private static byte[] loadClassBytes(final String resourceName) throws IOException {
-            try (InputStream inputStream = Formatters12DefinedClass.class.getClassLoader().getResourceAsStream(resourceName)) {
+            try (InputStream inputStream = FormattersAnonymous12DefinedClass.class.getClassLoader()
+                    .getResourceAsStream(resourceName)) {
                 return Objects.requireNonNull(inputStream, resourceName).readAllBytes();
             }
         }
@@ -178,11 +157,11 @@ public class Formatters12Test {
 
         @Override
         public String format(final Throwable thrown) {
-            ClassLoader originalTccl = Thread.currentThread().getContextClassLoader();
+            final ClassLoader originalTccl = Thread.currentThread().getContextClassLoader();
             try {
                 Thread.currentThread().setContextClassLoader(getClass().getClassLoader());
-                PatternFormatter formatter = new PatternFormatter("%E");
-                java.util.logging.LogRecord record = new java.util.logging.LogRecord(Level.SEVERE, "coverage");
+                final PatternFormatter formatter = new PatternFormatter("%E");
+                final java.util.logging.LogRecord record = new java.util.logging.LogRecord(Level.SEVERE, "coverage");
                 record.setThrown(thrown);
                 return formatter.format(record);
             } finally {
@@ -219,13 +198,13 @@ public class Formatters12Test {
         private boolean rejected;
 
         private BootstrapFallbackClassLoader(final String rejectedClassName) {
-            super(Formatters12Test.class.getClassLoader());
+            super(FormattersAnonymous12Test.class.getClassLoader());
             this.rejectedClassName = rejectedClassName;
         }
 
         BootstrapFormattingAction loadFormattingAction() throws Throwable {
-            Class<?> actionClass = Class.forName(BootstrapFormattingInvoker.class.getName(), true, this);
-            VarHandle instanceHandle = MethodHandles.publicLookup().findStaticVarHandle(
+            final Class<?> actionClass = loadClass(BootstrapFormattingInvoker.class.getName());
+            final VarHandle instanceHandle = MethodHandles.publicLookup().findStaticVarHandle(
                     actionClass,
                     "INSTANCE",
                     BootstrapFormattingAction.class
@@ -240,7 +219,7 @@ public class Formatters12Test {
         @Override
         protected Class<?> loadClass(final String name, final boolean resolve) throws ClassNotFoundException {
             synchronized (getClassLoadingLock(name)) {
-                Class<?> alreadyLoaded = findLoadedClass(name);
+                final Class<?> alreadyLoaded = findLoadedClass(name);
                 if (alreadyLoaded != null) {
                     return alreadyLoaded;
                 }
@@ -249,7 +228,7 @@ public class Formatters12Test {
                     throw new ClassNotFoundException(name);
                 }
                 if (shouldDefineLocally(name)) {
-                    Class<?> localClass = findClass(name);
+                    final Class<?> localClass = findClass(name);
                     if (resolve) {
                         resolveClass(localClass);
                     }
@@ -261,9 +240,10 @@ public class Formatters12Test {
 
         @Override
         protected Class<?> findClass(final String name) throws ClassNotFoundException {
-            String resourceName = name.replace('.', '/') + ".class";
-            try (InputStream inputStream = Formatters12Test.class.getClassLoader().getResourceAsStream(resourceName)) {
-                byte[] classBytes = Objects.requireNonNull(inputStream, resourceName).readAllBytes();
+            final String resourceName = name.replace('.', '/') + ".class";
+            try (InputStream inputStream = FormattersAnonymous12Test.class.getClassLoader()
+                    .getResourceAsStream(resourceName)) {
+                final byte[] classBytes = Objects.requireNonNull(inputStream, resourceName).readAllBytes();
                 return defineClass(name, classBytes, 0, classBytes.length);
             } catch (IOException e) {
                 throw new ClassNotFoundException(name, e);
@@ -277,8 +257,5 @@ public class Formatters12Test {
     }
 }
 
-final class Formatters12DefinedClass {
-}
-
-final class Formatters12FallbackMarker {
+final class FormattersAnonymous12DefinedClass {
 }
