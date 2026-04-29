ecdc2ac6ecf0b8cddb6e88c547bfefe95b996e06
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 07:06:48 +0200

    Add ModelMapper bridge classloader coverage

---
+++ tests/src/org.modelmapper/modelmapper/3.2.0/src/test/java/org/modelmapper/internal/BridgeClassLoaderFactoryAccess.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.modelmapper.internal;
+
+public final class BridgeClassLoaderFactoryAccess {
+    private BridgeClassLoaderFactoryAccess() {
+    }
+
+    public static ClassLoader getClassLoader(Class<?> appType) {
+        return BridgeClassLoaderFactory.getClassLoader(appType);
+    }
+}
