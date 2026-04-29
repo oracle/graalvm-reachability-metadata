fe5f72f8caf766b3131613dd734c983cbb8ff0db
jvukicev <jovan.vukicevic@oracle.com>
2026-04-29 06:07:38 +0200

    Exercise configured AspectJ TraceFactory creation

--- tests/src/org.aspectj/aspectjweaver/1.9.7/src/test/java/org_aspectj/aspectjweaver/TraceFactoryTest.java
+++ tests/src/org.aspectj/aspectjweaver/1.9.7/src/test/java/org_aspectj/aspectjweaver/TraceFactoryTest.java
@@ -8,24 +8,24 @@ package org_aspectj.aspectjweaver;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.aspectj.weaver.tools.DefaultTraceFactory;
 import org.aspectj.weaver.tools.Trace;
 import org.aspectj.weaver.tools.TraceFactory;
 import org.junit.jupiter.api.Test;
 
 public class TraceFactoryTest {
     private static final String FACTORY_PROPERTY = "org.aspectj.tracing.factory";
-    private static final String FAILING_FACTORY_NAME = TraceFactoryUnavailableFactory.class.getName();
+    private static final String FACTORY_NAME = DefaultTraceFactory.class.getName();
 
     @Test
-    void fallsBackWhenConfiguredTraceFactoryCannotBeCreated() {
+    void createsConfiguredTraceFactory() {
         String previousFactory = System.getProperty(FACTORY_PROPERTY);
-        System.setProperty(FACTORY_PROPERTY, FAILING_FACTORY_NAME);
+        System.setProperty(FACTORY_PROPERTY, FACTORY_NAME);
         try {
             TraceFactory factory = TraceFactory.getTraceFactory();
             Trace trace = factory.getTrace(TraceFactoryTest.class);
 
-            assertThat(factory).isNotNull();
-            assertThat(factory).isNotInstanceOf(TraceFactoryUnavailableFactory.class);
+            assertThat(factory).isInstanceOf(DefaultTraceFactory.class);
             assertThat(trace).isNotNull();
         } finally {
             restoreFactoryProperty(previousFactory);
@@ -38,12 +38,5 @@ public class TraceFactoryTest {
         } else {
             System.setProperty(FACTORY_PROPERTY, previousFactory);
         }
-    }
-}
-
-class TraceFactoryUnavailableFactory extends TraceFactory {
-    @Override
-    public Trace getTrace(Class clazz) {
-        throw new AssertionError("The unavailable trace factory must not create traces");
     }
 }
