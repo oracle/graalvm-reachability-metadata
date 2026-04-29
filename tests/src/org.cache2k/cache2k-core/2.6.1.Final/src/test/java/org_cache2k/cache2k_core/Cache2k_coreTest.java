85b4758a8375295a0c00832e65cebc6eb54bae2c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 03:17:24 +0200

    Scaffold org.cache2k:cache2k-core:2.6.1.Final

---
+++ tests/src/org.cache2k/cache2k-core/2.6.1.Final/src/test/java/org_cache2k/cache2k_core/Cache2k_coreTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_cache2k.cache2k_core;
+
+import org.junit.jupiter.api.Test;
+
+class Cache2k_coreTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
