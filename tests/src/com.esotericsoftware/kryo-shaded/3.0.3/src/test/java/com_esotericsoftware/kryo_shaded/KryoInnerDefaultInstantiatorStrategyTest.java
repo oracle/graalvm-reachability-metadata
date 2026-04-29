87c47a860d54d6737c960349542a86b76262e879
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-04-30 01:12:42 +0200

    Refine Kryo default instantiator reflection coverage

--- tests/src/com.esotericsoftware/kryo-shaded/3.0.3/src/test/java/com_esotericsoftware/kryo_shaded/KryoInnerDefaultInstantiatorStrategyTest.java
+++ tests/src/com.esotericsoftware/kryo-shaded/3.0.3/src/test/java/com_esotericsoftware/kryo_shaded/KryoInnerDefaultInstantiatorStrategyTest.java
@@ -7,24 +7,59 @@
 package com_esotericsoftware.kryo_shaded;
 
 import com.esotericsoftware.kryo.Kryo;
-import com.esotericsoftware.kryo.KryoException;
+import com.esotericsoftware.kryo.KryoSerializable;
+import com.esotericsoftware.kryo.util.IntArray;
+import com.esotericsoftware.kryo.util.Util;
 import org.junit.jupiter.api.Test;
+import org.objenesis.instantiator.ObjectInstantiator;
+import org.objenesis.strategy.InstantiatorStrategy;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class KryoInnerDefaultInstantiatorStrategyTest {
     @Test
-    void reportsNonStaticMemberClassesWithoutUsingFallbackStrategy() {
-        Kryo.DefaultInstantiatorStrategy strategy = new Kryo.DefaultInstantiatorStrategy();
+    void createsPublicNoArgLibraryTypeUsingReflectionWhenReflectAsmIsBypassed() {
+        boolean previousAndroidFlag = Util.isAndroid;
+        Util.isAndroid = true;
+        try {
+            Kryo.DefaultInstantiatorStrategy strategy = new Kryo.DefaultInstantiatorStrategy();
 
-        assertThatThrownBy(() -> strategy.newInstantiatorOf(Outer.NonStaticMember.class))
-                .isInstanceOf(KryoException.class)
-                .hasMessageContaining("non-static member class")
-                .hasMessageContaining("NonStaticMember");
+            @SuppressWarnings("unchecked")
+            ObjectInstantiator<IntArray> instantiator = strategy.newInstantiatorOf(IntArray.class);
+            IntArray array = instantiator.newInstance();
+
+            assertThat(array).isInstanceOf(IntArray.class);
+            assertThat(array.ordered).isTrue();
+            assertThat(array.size).isZero();
+        } finally {
+            Util.isAndroid = previousAndroidFlag;
+        }
     }
 
-    private static class Outer {
-        class NonStaticMember {
+    @Test
+    void delegatesToFallbackAfterReflectionFindsNoNoArgConstructor() {
+        boolean previousAndroidFlag = Util.isAndroid;
+        Util.isAndroid = true;
+        try {
+            Kryo.DefaultInstantiatorStrategy strategy = new Kryo.DefaultInstantiatorStrategy(new NullFallbackStrategy());
+
+            ObjectInstantiator<?> instantiator = strategy.newInstantiatorOf(KryoSerializable.class);
+
+            assertThat(instantiator.newInstance()).isNull();
+        } finally {
+            Util.isAndroid = previousAndroidFlag;
+        }
+    }
+
+    private static final class NullFallbackStrategy implements InstantiatorStrategy {
+        @Override
+        public <T> ObjectInstantiator<T> newInstantiatorOf(Class<T> type) {
+            return new ObjectInstantiator<T>() {
+                @Override
+                public T newInstance() {
+                    return null;
+                }
+            };
         }
     }
 }
