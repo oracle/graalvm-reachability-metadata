db01520a955e2d83128883c3f7870f932d9ade2e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 06:33:28 +0200

    Cover ModelMapper JavaDispatcher security check

--- tests/src/org.modelmapper/modelmapper/3.2.0/src/test/java/org_modelmapper/modelmapper/JavaDispatcherTest.java
+++ tests/src/org.modelmapper/modelmapper/3.2.0/src/test/java/org_modelmapper/modelmapper/JavaDispatcherTest.java
@@ -8,12 +8,23 @@ package org_modelmapper.modelmapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.security.Permission;
+import java.util.concurrent.Callable;
 
 import org.junit.jupiter.api.Test;
+import org.modelmapper.internal.asm.ClassReader;
+import org.modelmapper.internal.asm.ClassWriter;
+import org.modelmapper.internal.asm.Type;
+import org.modelmapper.internal.asm.commons.ClassRemapper;
+import org.modelmapper.internal.asm.commons.Remapper;
 import org.modelmapper.internal.bytebuddy.utility.dispatcher.JavaDispatcher;
 
 public class JavaDispatcherTest {
+    private static final String SYSTEM_TYPE = "java/lang/System";
+    private static final String PROXY_SYSTEM_TYPE = Type.getInternalName(ProxySystem.class);
+
     @Test
     void createsProxyWithDefaultDispatchersWhenProxiedTypeIsUnavailable() {
         MissingTypeDispatcher dispatcher = new ConfigurableJavaDispatcher<>(
@@ -39,32 +50,14 @@ public class JavaDispatcherTest {
     }
 
     @Test
-    @SuppressWarnings("removal")
-    void checksInstalledSecurityManagerBeforeCreatingDispatcher() {
-        SecurityManager previous = System.getSecurityManager();
-        RecordingSecurityManager securityManager = new RecordingSecurityManager();
-        boolean installed = false;
-        try {
-            try {
-                System.setSecurityManager(securityManager);
-                installed = true;
-            } catch (UnsupportedOperationException ignored) {
-                installed = false;
-            }
+    void invokesSecurityManagerPermissionCheckInIsolatedDispatcher() throws Exception {
+        ProxySystem.reset();
+        ClassLoader classLoader = new SystemRedirectingClassLoader(getClass().getClassLoader());
+        Class<?> runnerType = classLoader.loadClass(IsolatedRunner.class.getName());
+        Callable<?> runner = (Callable<?>) runnerType.getConstructor().newInstance();
 
-            MissingTypeDispatcher dispatcher = new ConfigurableJavaDispatcher<>(
-                MissingTypeDispatcher.class,
-                false).run();
-
-            assertThat(dispatcher.number()).isZero();
-            if (installed) {
-                assertThat(securityManager.checkedJavaDispatcherPermission).isTrue();
-            }
-        } finally {
-            if (installed) {
-                System.setSecurityManager(previous);
-            }
-        }
+        assertThat(runner.call()).isEqualTo(Boolean.TRUE);
+        assertThat(ProxySystem.checkedJavaDispatcherPermission()).isTrue();
     }
 
     @JavaDispatcher.Defaults
@@ -78,6 +71,102 @@ public class JavaDispatcherTest {
     private static final class ConfigurableJavaDispatcher<T> extends JavaDispatcher<T> {
         private ConfigurableJavaDispatcher(Class<T> proxy, boolean generate) {
             super(proxy, JavaDispatcherTest.class.getClassLoader(), generate);
+        }
+    }
+
+    public static final class IsolatedRunner implements Callable<Boolean> {
+        @Override
+        public Boolean call() {
+            MissingTypeDispatcher dispatcher = JavaDispatcher.of(MissingTypeDispatcher.class).run();
+            return dispatcher.number() == 0 && dispatcher.text() == null;
+        }
+    }
+
+    @SuppressWarnings("removal")
+    public static final class ProxySystem {
+        private static final RecordingSecurityManager SECURITY_MANAGER = new RecordingSecurityManager();
+
+        private ProxySystem() {
+        }
+
+        public static SecurityManager getSecurityManager() {
+            return SECURITY_MANAGER;
+        }
+
+        public static String getProperty(String key, String defaultValue) {
+            return System.getProperty(key, defaultValue);
+        }
+
+        private static void reset() {
+            SECURITY_MANAGER.checkedJavaDispatcherPermission = false;
+        }
+
+        private static boolean checkedJavaDispatcherPermission() {
+            return SECURITY_MANAGER.checkedJavaDispatcherPermission;
+        }
+    }
+
+    private static final class SystemRedirectingClassLoader extends ClassLoader {
+        private SystemRedirectingClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            synchronized (getClassLoadingLock(name)) {
+                Class<?> type = findLoadedClass(name);
+                if (type == null && isChildFirst(name)) {
+                    try {
+                        type = findClass(name);
+                    } catch (ClassNotFoundException ignored) {
+                        type = super.loadClass(name, false);
+                    }
+                } else if (type == null) {
+                    type = super.loadClass(name, false);
+                }
+                if (resolve) {
+                    resolveClass(type);
+                }
+                return type;
+            }
+        }
+
+        @Override
+        protected Class<?> findClass(String name) throws ClassNotFoundException {
+            String resourceName = name.replace('.', '/') + ".class";
+            try (InputStream inputStream = getParent().getResourceAsStream(resourceName)) {
+                if (inputStream == null) {
+                    throw new ClassNotFoundException(name);
+                }
+                byte[] binaryRepresentation = inputStream.readAllBytes();
+                if (JavaDispatcher.class.getName().equals(name)) {
+                    binaryRepresentation = redirectSystemReferences(binaryRepresentation);
+                }
+                return defineClass(name, binaryRepresentation, 0, binaryRepresentation.length);
+            } catch (IOException exception) {
+                throw new ClassNotFoundException(name, exception);
+            }
+        }
+
+        private static boolean isChildFirst(String name) {
+            return name.equals(IsolatedRunner.class.getName())
+                || name.equals(MissingTypeDispatcher.class.getName())
+                || name.equals(JavaDispatcher.class.getName())
+                || name.startsWith(JavaDispatcher.class.getName() + "$");
+        }
+
+        private static byte[] redirectSystemReferences(byte[] binaryRepresentation) {
+            ClassReader reader = new ClassReader(binaryRepresentation);
+            ClassWriter writer = new ClassWriter(reader, 0);
+            reader.accept(new ClassRemapper(writer, new SystemReferenceRemapper()), 0);
+            return writer.toByteArray();
+        }
+    }
+
+    private static final class SystemReferenceRemapper extends Remapper {
+        @Override
+        public String map(String internalName) {
+            return SYSTEM_TYPE.equals(internalName) ? PROXY_SYSTEM_TYPE : internalName;
         }
     }
 
