d1b220558aa5a2aa91e82bbc595f2d9a56d7446a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 02:22:47 +0200

    Add protobuf-javalite Android64 unsafe accessor coverage

---
+++ tests/src/com.google.protobuf/protobuf-javalite/3.25.5/src/test/java/com_google_protobuf/protobuf_javalite/UnsafeUtilInnerAndroid64MemoryAccessorTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_google_protobuf.protobuf_javalite;
+
+import com.google.protobuf.MapEntryLite;
+import com.google.protobuf.SchemaUtilCoverageSupport;
+import com.google.protobuf.Value;
+import com.google.protobuf.WireFormat;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UnsafeUtilInnerAndroid64MemoryAccessorTest {
+    @Test
+    void mapDefaultEntryLookupReadsStaticHolderFieldWithAndroid64Accessor() {
+        Object defaultEntry = SchemaUtilCoverageSupport.mapDefaultEntry(
+                MapDefaultEntryMessage.class,
+                "fields");
+
+        assertThat(defaultEntry)
+                .isSameAs(MapDefaultEntryMessage.FieldsDefaultEntryHolder.defaultEntry);
+        @SuppressWarnings("unchecked")
+        MapEntryLite<String, Value> fieldsEntry = (MapEntryLite<String, Value>) defaultEntry;
+        int messageSize = fieldsEntry.computeMessageSize(
+                1,
+                "android64",
+                Value.getDefaultInstance());
+        assertThat(messageSize).isPositive();
+    }
+
+    public static final class MapDefaultEntryMessage {
+        private MapDefaultEntryMessage() {
+        }
+
+        public static final class FieldsDefaultEntryHolder {
+            public static final MapEntryLite<String, Value> defaultEntry =
+                    MapEntryLite.newDefaultInstance(
+                            WireFormat.FieldType.STRING,
+                            "",
+                            WireFormat.FieldType.MESSAGE,
+                            Value.getDefaultInstance());
+
+            private FieldsDefaultEntryHolder() {
+            }
+        }
+    }
+}
--- tests/src/com.google.protobuf/protobuf-javalite/3.25.5/src/test/java/libcore/io/Memory.java
+++ tests/src/com.google.protobuf/protobuf-javalite/3.25.5/src/test/java/libcore/io/Memory.java
@@ -10,30 +10,62 @@ public final class Memory {
     private Memory() {
     }
 
-    public static long peekLong(int address, boolean swap) {
+    public static long peekLong(long address, boolean swap) {
         return 0L;
     }
 
+    public static long peekLong(int address, boolean swap) {
+        return peekLong((long) address, swap);
+    }
+
+    public static void pokeLong(long address, long value, boolean swap) {
+    }
+
     public static void pokeLong(int address, long value, boolean swap) {
+        pokeLong((long) address, value, swap);
+    }
+
+    public static void pokeInt(long address, int value, boolean swap) {
     }
 
     public static void pokeInt(int address, int value, boolean swap) {
+        pokeInt((long) address, value, swap);
+    }
+
+    public static int peekInt(long address, boolean swap) {
+        return 0;
     }
 
     public static int peekInt(int address, boolean swap) {
-        return 0;
+        return peekInt((long) address, swap);
+    }
+
+    public static void pokeByte(long address, byte value) {
     }
 
     public static void pokeByte(int address, byte value) {
+        pokeByte((long) address, value);
     }
 
-    public static byte peekByte(int address) {
+    public static byte peekByte(long address) {
         return 0;
     }
 
+    public static byte peekByte(int address) {
+        return peekByte((long) address);
+    }
+
+    public static void pokeByteArray(long address, byte[] bytes, int offset, int count) {
+    }
+
     public static void pokeByteArray(int address, byte[] bytes, int offset, int count) {
+        pokeByteArray((long) address, bytes, offset, count);
+    }
+
+    public static void peekByteArray(long address, byte[] bytes, int offset, int count) {
     }
 
     public static void peekByteArray(int address, byte[] bytes, int offset, int count) {
+        peekByteArray((long) address, bytes, offset, count);
     }
 }
