8ae12c835a233f438b8dfcbe6bfa6a5b9fd34024
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 03:17:07 +0200

    Add Kryo shaded ClassReader coverage test

---
+++ tests/src/com.esotericsoftware/kryo-shaded/3.0.3/src/test/java/com_esotericsoftware/kryo_shaded/ClassReaderTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_esotericsoftware.kryo_shaded;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.esotericsoftware.reflectasm.shaded.org.objectweb.asm.ClassReader;
+import java.io.IOException;
+import org.junit.jupiter.api.Test;
+
+public class ClassReaderTest {
+    private static final String CLASS_READER_INTERNAL_NAME =
+            "com/esotericsoftware/reflectasm/shaded/org/objectweb/asm/ClassReader";
+
+    @Test
+    void loadsClassBytesFromSystemClassLoaderResource() throws IOException {
+        ClassReader reader = new ClassReader(ClassReader.class.getName());
+
+        assertThat(reader.getClassName()).isEqualTo(CLASS_READER_INTERNAL_NAME);
+        assertThat(reader.getSuperName()).isEqualTo("java/lang/Object");
+        assertThat(reader.getInterfaces()).isEmpty();
+    }
+}
