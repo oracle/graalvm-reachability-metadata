01dffd34f161e8bbd988c063579bc8c6cc51e7a0
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-29 23:41:05 +0200

    Use XML stack for RelayDemoRpc test

--- tests/src/org.jgroups/jgroups/5.2.0.Final/src/test/java/org_jgroups/jgroups/RelayDemoRpcTest.java
+++ tests/src/org.jgroups/jgroups/5.2.0.Final/src/test/java/org_jgroups/jgroups/RelayDemoRpcTest.java
@@ -11,32 +11,44 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class RelayDemoRpcTest {
-    private static final String TEST_STACK = "SHARED_LOOPBACK:" +
-        "SHARED_LOOPBACK_PING:" +
-        "pbcast.NAKACK2:" +
-        "UNICAST3:" +
-        "pbcast.STABLE:" +
-        "pbcast.GMS(join_timeout=1000):" +
-        "FRAG2(frag_size=8000)";
+    private static final String TEST_STACK = """
+        <config xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+                xmlns="urn:org:jgroups"
+                xsi:schemaLocation="urn:org:jgroups http://www.jgroups.org/schema/jgroups.xsd">
+            <SHARED_LOOPBACK/>
+            <SHARED_LOOPBACK_PING/>
+            <pbcast.NAKACK2/>
+            <UNICAST3/>
+            <pbcast.STABLE/>
+            <pbcast.GMS join_timeout="1000"/>
+            <FRAG2 frag_size="8000"/>
+        </config>
+        """;
 
     @Test
-    void startBuildsRpcMethodCallBeforeReadingConsoleInput() {
+    void startBuildsRpcMethodCallBeforeReadingConsoleInput() throws Exception {
         InputStream originalInput = System.in;
         RelayDemoRpcHarness demo = new RelayDemoRpcHarness();
+        Path stackFile = Files.writeString(
+            Files.createTempFile("relay-demo-rpc", ".xml"), TEST_STACK, StandardCharsets.UTF_8);
         System.setIn(new ByteArrayInputStream(new byte[0]));
         try {
-            assertThatThrownBy(() -> demo.start(TEST_STACK, "relay-demo-rpc-test"))
+            assertThatThrownBy(() -> demo.start(stackFile.toString(), "relay-demo-rpc-test"))
                 .isInstanceOf(NullPointerException.class);
             assertThat(demo.localAddress()).isNotBlank();
         }
         finally {
             demo.close();
             System.setIn(originalInput);
+            Files.deleteIfExists(stackFile);
         }
     }
 
