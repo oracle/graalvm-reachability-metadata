416c8d5b623a1ceabfe3a69acd6e1956ea980667
jvukicev <jovan.vukicevic@oracle.com>
2026-04-29 03:20:36 +0200

    Add JOL FieldData dynamic access tests

---
+++ tests/src/org.openjdk.jol/jol-core/0.2/src/test/java/org_openjdk_jol/jol_core/FieldDataTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_openjdk_jol.jol_core;
+
+import org.junit.jupiter.api.Test;
+import org.openjdk.jol.info.FieldData;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FieldDataTest {
+    @Test
+    void readsPublicFieldValueDirectly() throws NoSuchFieldException {
+        PublicFieldHolder holder = new PublicFieldHolder(7);
+        FieldData fieldData = FieldData.parse(PublicFieldHolder.class.getField("value"));
+
+        assertThat(fieldData.hostClass()).isEqualTo("PublicFieldHolder");
+        assertThat(fieldData.name()).isEqualTo("value");
+        assertThat(fieldData.typeClass()).isEqualTo("int");
+        assertThat(fieldData.safeValue(holder)).isEqualTo("7");
+    }
+
+    @Test
+    void readsPrivateFieldValueAfterMakingItAccessible() throws NoSuchFieldException {
+        PrivateFieldHolder holder = new PrivateFieldHolder(99);
+        FieldData fieldData = FieldData.parse(PrivateFieldHolder.class.getDeclaredField("value"));
+
+        assertThat(fieldData.hostClass()).isEqualTo("PrivateFieldHolder");
+        assertThat(fieldData.name()).isEqualTo("value");
+        assertThat(fieldData.typeClass()).isEqualTo("int");
+        assertThat(fieldData.safeValue(holder)).isEqualTo("99");
+    }
+
+    private static class PublicFieldHolder {
+        public final int value;
+
+        PublicFieldHolder(int value) {
+            this.value = value;
+        }
+    }
+
+    private static class PrivateFieldHolder {
+        private final int value;
+
+        PrivateFieldHolder(int value) {
+            this.value = value;
+        }
+    }
+}
