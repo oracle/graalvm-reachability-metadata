8cbb172a35a8c90366290f9d4fbe1bb7db070840
jvukicev <jovan.vukicevic@oracle.com>
2026-04-29 06:53:50 +0200

    Add AspectJ ASM constants dynamic access test

---
+++ tests/src/org.aspectj/aspectjweaver/1.9.7/src/test/java/org_aspectj/aspectjweaver/ConstantsTest.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_aspectj.aspectjweaver;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import aj.org.objectweb.asm.ClassVisitor;
+import aj.org.objectweb.asm.Opcodes;
+import org.junit.jupiter.api.Test;
+
+public class ConstantsTest {
+    @Test
+    void experimentalApiChecksVisitorBytecodeResourceBeforeRejectingNonPreviewClass() {
+        assertThatThrownBy(ExperimentalClassVisitor::new)
+                .isInstanceOf(IllegalStateException.class);
+    }
+
+    private static final class ExperimentalClassVisitor extends ClassVisitor {
+        private ExperimentalClassVisitor() {
+            super(Opcodes.ASM10_EXPERIMENTAL);
+        }
+    }
+}
