d0f392a43400e79587df7cca57bac31a7786bf14
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 04:36:57 +0200

    Add Calcite SaffronProperties helper coverage

---
+++ tests/src/org.apache.calcite/calcite-core/1.35.0/src/test/java/org_apache_calcite/calcite_core/SaffronPropertiesInnerHelperTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_calcite.calcite_core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.calcite.util.SaffronProperties;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("deprecation")
+public class SaffronPropertiesInnerHelperTest {
+    @Test
+    void initializesSingletonAndReadsDefaultProperties() {
+        SaffronProperties properties = SaffronProperties.INSTANCE;
+
+        assertThat(properties).isNotNull();
+        assertThat(properties.allowInfiniteCostConverters().get()).isTrue();
+        assertThat(properties.defaultCharset().get()).isEqualTo("ISO-8859-1");
+        assertThat(properties.defaultNationalCharset().get()).isEqualTo("ISO-8859-1");
+        assertThat(properties.defaultCollation().get()).isEqualTo("ISO-8859-1$en_US");
+        assertThat(properties.defaultCollationStrength().get()).isEqualTo("primary");
+        assertThat(properties.metadataHandlerCacheMaximumSize().get()).isEqualTo(1000);
+    }
+}
