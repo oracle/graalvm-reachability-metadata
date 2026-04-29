2942be9e6f58cf502ae06ba5e967b7b23c9df498
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-29 07:43:47 +0200

    Exercise ModelMapper DirectInvoker call sites

--- tests/src/org.modelmapper/modelmapper/3.2.0/src/test/java/org_modelmapper/modelmapper/JavaDispatcherInnerDirectInvokerTest.java
+++ tests/src/org.modelmapper/modelmapper/3.2.0/src/test/java/org_modelmapper/modelmapper/JavaDispatcherInnerDirectInvokerTest.java
@@ -8,10 +8,17 @@ package org_modelmapper.modelmapper;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.Test;
+import org.modelmapper.internal.bytebuddy.utility.Invoker;
 import org.modelmapper.internal.bytebuddy.utility.dispatcher.JavaDispatcher;
 
 public class JavaDispatcherInnerDirectInvokerTest {
+    private static final String DIRECT_INVOKER_TYPE_NAME =
+        "org.modelmapper.internal.bytebuddy.utility.dispatcher.JavaDispatcher$DirectInvoker";
+
     @Test
     void dispatchesConstructorAndInstanceMethodThroughInvoker() {
         DispatcherTargetDispatcher dispatcher = new TestJavaDispatcher<>(
@@ -22,6 +29,25 @@ public class JavaDispatcherInnerDirectInvokerTest {
 
         assertThat(returned).isSameAs(target);
         assertThat(target.value()).isEqualTo("direct-invoker");
+    }
+
+    @Test
+    void directInvokerCallsConstructorAndMethodReflectionContracts() throws Exception {
+        Invoker invoker = directInvoker();
+        Constructor<DispatcherTarget> constructor = DispatcherTarget.class.getConstructor(String.class);
+        Method append = DispatcherTarget.class.getMethod("append", String.class);
+
+        DispatcherTarget target = (DispatcherTarget) invoker.newInstance(constructor, new Object[] {"direct"});
+        Object returned = invoker.invoke(append, target, new Object[] {"-invoker"});
+
+        assertThat(returned).isSameAs(target);
+        assertThat(target.value()).isEqualTo("direct-invoker");
+    }
+
+    private static Invoker directInvoker() throws Exception {
+        Constructor<?> constructor = Class.forName(DIRECT_INVOKER_TYPE_NAME).getDeclaredConstructor();
+        constructor.setAccessible(true);
+        return (Invoker) constructor.newInstance();
     }
 
     @JavaDispatcher.Proxied("org_modelmapper.modelmapper.JavaDispatcherInnerDirectInvokerTest$DispatcherTarget")
