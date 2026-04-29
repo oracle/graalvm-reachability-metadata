8f73185b95566b27f9ade1fbe38faddb29997f1b
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-04-29 22:12:16 +0200

    Scaffold com.barchart.udt:barchart-udt-bundle:2.3.0

---
+++ tests/src/com.barchart.udt/barchart-udt-bundle/2.3.0/src/test/java/com_barchart_udt/barchart_udt_bundle/Barchart_udt_bundleTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_barchart_udt.barchart_udt_bundle;
+
+import org.junit.jupiter.api.Test;
+
+class Barchart_udt_bundleTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
