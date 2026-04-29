c2ca548bf093d21f81a390814f537df863aa71d1
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-30 00:45:23 +0200

    Refine dom4j BeanElement dynamic access test

--- tests/src/dom4j/dom4j/20040902.021138/src/test/java/dom4j/dom4j/BeanElementTest.java
+++ tests/src/dom4j/dom4j/20040902.021138/src/test/java/dom4j/dom4j/BeanElementTest.java
@@ -8,11 +8,6 @@ package dom4j.dom4j;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import java.lang.invoke.MethodHandle;
-import java.lang.invoke.MethodHandles;
-import java.lang.invoke.MethodType;
-import java.lang.reflect.Field;
-
 import org.dom4j.QName;
 import org.dom4j.bean.BeanDocumentFactory;
 import org.dom4j.bean.BeanElement;
@@ -22,18 +17,7 @@ import org.xml.sax.helpers.AttributesImpl;
 
 public class BeanElementTest {
     @Test
-    void compilerGeneratedClassLookupResolvesBeanElementType() throws Throwable {
-        MethodHandle classLookup = MethodHandles.privateLookupIn(BeanElement.class, MethodHandles.lookup())
-                .findStatic(BeanElement.class, "class$", MethodType.methodType(Class.class, String.class));
-
-        Class<?> resolvedClass = (Class<?>) classLookup.invoke(BeanElement.class.getName());
-
-        assertThat(resolvedClass).isEqualTo(BeanElement.class);
-    }
-
-    @Test
     void saxClassAttributeInitializesBackingBean() throws Exception {
-        clearCompilerGeneratedClassCache();
         BeanElement element = new BeanElement(QName.get("configuredBean"));
         AttributesImpl attributes = new AttributesImpl();
         attributes.addAttribute("", "class", "class", "CDATA", BeanDocumentFactory.class.getName());
@@ -41,11 +25,5 @@ public class BeanElementTest {
         element.setAttributes(attributes, new NamespaceStack(), false);
 
         assertThat(element.getData()).isInstanceOf(BeanDocumentFactory.class);
-    }
-
-    private static void clearCompilerGeneratedClassCache() throws Exception {
-        Field field = BeanElement.class.getDeclaredField("class$org$dom4j$bean$BeanElement");
-        field.setAccessible(true);
-        field.set(null, null);
     }
 }
