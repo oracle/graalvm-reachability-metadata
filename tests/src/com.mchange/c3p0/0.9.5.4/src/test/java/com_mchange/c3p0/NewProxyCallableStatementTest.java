aac650ed5b2f2d6a4843b5f3111e22fd96d88e08
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-30 07:08:45 +0200

    Add c3p0 callable statement raw operation test

---
+++ tests/src/com.mchange/c3p0/0.9.5.4/src/test/java/com_mchange/c3p0/NewProxyCallableStatementTest.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_mchange.c3p0;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mchange.v2.c3p0.C3P0ProxyStatement;
+import com.mchange.v2.c3p0.impl.C3P0PooledConnection;
+import com.mchange.v2.c3p0.impl.DefaultConnectionTester;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Map;
+import org.junit.jupiter.api.Test;
+
+public class NewProxyCallableStatementTest {
+    @Test
+    void rawStatementOperationInvokesMethodOnUnderlyingCallableStatement() throws Exception {
+        TrackingConnectionHandler connectionHandler = new TrackingConnectionHandler();
+        Connection physicalConnection = newConnectionProxy(connectionHandler);
+        connectionHandler.attach(physicalConnection);
+        C3P0PooledConnection pooledConnection = new C3P0PooledConnection(
+                physicalConnection,
+                new DefaultConnectionTester(),
+                false,
+                false,
+                null,
+                "new-proxy-callable-statement-test"
+        );
+
+        Connection connection = pooledConnection.getConnection();
+        CallableStatement callableStatement = connection.prepareCall("{ call read_value(?) }");
+        C3P0ProxyStatement proxyStatement = (C3P0ProxyStatement) callableStatement;
+
+        Object rawValue = proxyStatement.rawStatementOperation(
+                CallableStatement.class.getMethod("getString", int.class),
+                C3P0ProxyStatement.RAW_STATEMENT,
+                new Object[] {1}
+        );
+
+        assertThat(rawValue).isEqualTo("raw-callable-value-1");
+        assertThat(connectionHandler.callableStatementHandler.rawGetStringCalls).isEqualTo(1);
+
+        callableStatement.close();
+        connection.close();
+        pooledConnection.close();
+    }
+
+    private static Connection newConnectionProxy(TrackingConnectionHandler handler) {
+        return (Connection) Proxy.newProxyInstance(
+                NewProxyCallableStatementTest.class.getClassLoader(),
+                new Class<?>[] {Connection.class},
+                handler
+        );
+    }
+
+    private static CallableStatement newCallableStatementProxy(
+            Connection connection,
+            TrackingCallableStatementHandler handler
+    ) {
+        handler.attach(connection);
+        return (CallableStatement) Proxy.newProxyInstance(
+                NewProxyCallableStatementTest.class.getClassLoader(),
+                new Class<?>[] {CallableStatement.class},
+                handler
+        );
+    }
+
+    private static Object defaultValue(Class<?> returnType) {
+        if (returnType == Void.TYPE) {
+            return null;
+        }
+        if (returnType == Boolean.TYPE) {
+            return Boolean.FALSE;
+        }
+        if (returnType == Byte.TYPE) {
+            return (byte) 0;
+        }
+        if (returnType == Short.TYPE) {
+            return (short) 0;
+        }
+        if (returnType == Integer.TYPE) {
+            return 0;
+        }
+        if (returnType == Long.TYPE) {
+            return 0L;
+        }
+        if (returnType == Float.TYPE) {
+            return 0F;
+        }
+        if (returnType == Double.TYPE) {
+            return 0D;
+        }
+        if (returnType == Character.TYPE) {
+            return '\0';
+        }
+        return null;
+    }
+
+    private static Object handleObjectMethod(Object proxy, Method method, Object[] args, String description) {
+        switch (method.getName()) {
+            case "toString":
+                return description;
+            case "hashCode":
+                return System.identityHashCode(proxy);
+            case "equals":
+                return proxy == args[0];
+            default:
+                return null;
+        }
+    }
+
+    private static final class TrackingConnectionHandler implements InvocationHandler {
+        private final TrackingCallableStatementHandler callableStatementHandler =
+                new TrackingCallableStatementHandler();
+        private Connection connectionProxy;
+        private boolean closed;
+        private boolean autoCommit = true;
+        private boolean readOnly;
+        private int transactionIsolation = Connection.TRANSACTION_READ_COMMITTED;
+        private String catalog = "default";
+        private int holdability = ResultSet.CLOSE_CURSORS_AT_COMMIT;
+        private Map<String, Class<?>> typeMap = Collections.emptyMap();
+
+        private void attach(Connection connectionProxy) {
+            this.connectionProxy = connectionProxy;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws SQLException {
+            if (method.getDeclaringClass() == Object.class) {
+                return handleObjectMethod(proxy, method, args, "tracking-physical-connection");
+            }
+
+            String name = method.getName();
+            switch (name) {
+                case "prepareCall":
+                    return newCallableStatementProxy(connectionProxy, callableStatementHandler);
+                case "getTransactionIsolation":
+                    return transactionIsolation;
+                case "setTransactionIsolation":
+                    transactionIsolation = (Integer) args[0];
+                    return null;
+                case "getCatalog":
+                    return catalog;
+                case "setCatalog":
+                    catalog = (String) args[0];
+                    return null;
+                case "getHoldability":
+                    return holdability;
+                case "setHoldability":
+                    holdability = (Integer) args[0];
+                    return null;
+                case "getAutoCommit":
+                    return autoCommit;
+                case "setAutoCommit":
+                    autoCommit = (Boolean) args[0];
+                    return null;
+                case "isReadOnly":
+                    return readOnly;
+                case "setReadOnly":
+                    readOnly = (Boolean) args[0];
+                    return null;
+                case "getTypeMap":
+                    return typeMap;
+                case "setTypeMap":
+                    typeMap = (Map<String, Class<?>>) args[0];
+                    return null;
+                case "nativeSQL":
+                    return args[0];
+                case "isValid":
+                    return !closed;
+                case "isClosed":
+                    return closed;
+                case "close":
+                    closed = true;
+                    return null;
+                case "commit":
+                case "rollback":
+                case "clearWarnings":
+                    return null;
+                case "unwrap":
+                    throw new SQLException("No wrapped connection is available.");
+                case "isWrapperFor":
+                    return false;
+                default:
+                    return defaultValue(method.getReturnType());
+            }
+        }
+    }
+
+    private static final class TrackingCallableStatementHandler implements InvocationHandler {
+        private Connection connection;
+        private boolean closed;
+        private int rawGetStringCalls;
+
+        private void attach(Connection connection) {
+            this.connection = connection;
+        }
+
+        @Override
+        public Object invoke(Object proxy, Method method, Object[] args) throws SQLException {
+            if (method.getDeclaringClass() == Object.class) {
+                return handleObjectMethod(proxy, method, args, "tracking-raw-callable-statement");
+            }
+
+            String name = method.getName();
+            switch (name) {
+                case "getConnection":
+                    return connection;
+                case "getString":
+                    rawGetStringCalls++;
+                    return "raw-callable-value-" + args[0];
+                case "isClosed":
+                    return closed;
+                case "close":
+                    closed = true;
+                    return null;
+                case "unwrap":
+                    throw new SQLException("No wrapped callable statement is available.");
+                case "isWrapperFor":
+                    return false;
+                default:
+                    return defaultValue(method.getReturnType());
+            }
+        }
+    }
+}
