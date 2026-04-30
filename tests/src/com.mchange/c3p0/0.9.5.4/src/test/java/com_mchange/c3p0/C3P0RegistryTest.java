4ec57279146f908421961e68b0b0e0be1b7cdece
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-30 06:00:13 +0200

    Add c3p0 registry dynamic access tests

---
+++ tests/src/com.mchange/c3p0/0.9.5.4/src/test/java/com_mchange/c3p0/C3P0RegistryTest.java
@@ -0,0 +1,108 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_mchange.c3p0;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.mchange.v2.c3p0.AbstractConnectionCustomizer;
+import com.mchange.v2.c3p0.AbstractConnectionTester;
+import com.mchange.v2.c3p0.C3P0Registry;
+import com.mchange.v2.c3p0.ConnectionCustomizer;
+import com.mchange.v2.c3p0.ConnectionTester;
+import com.mchange.v2.c3p0.PooledDataSource;
+import com.mchange.v2.c3p0.management.ManagementCoordinator;
+import java.sql.Connection;
+import org.junit.jupiter.api.Test;
+
+public class C3P0RegistryTest {
+    @Test
+    void createsAndCachesConnectionTesterByConfiguredClassName() {
+        ConnectionTester tester = C3P0Registry.getConnectionTester(RegistryConnectionTester.class.getName());
+        ConnectionTester cachedTester = C3P0Registry.getConnectionTester(RegistryConnectionTester.class.getName());
+
+        assertThat(tester).isInstanceOf(RegistryConnectionTester.class);
+        assertThat(cachedTester).isSameAs(tester);
+        assertThat(tester.activeCheckConnection(null)).isEqualTo(ConnectionTester.CONNECTION_IS_OKAY);
+    }
+
+    @Test
+    void createsAndCachesConnectionCustomizerByConfiguredClassName() throws Exception {
+        String customizerClassName = RegistryConnectionCustomizer.class.getName();
+        ConnectionCustomizer customizer = C3P0Registry.getConnectionCustomizer(customizerClassName);
+        ConnectionCustomizer cachedCustomizer = C3P0Registry.getConnectionCustomizer(customizerClassName);
+
+        assertThat(customizer).isInstanceOf(RegistryConnectionCustomizer.class);
+        assertThat(cachedCustomizer).isSameAs(customizer);
+
+        customizer.onAcquire(null, "registry-test-token");
+
+        assertThat(RegistryConnectionCustomizer.lastParentDataSourceIdentityToken).isEqualTo("registry-test-token");
+    }
+
+    @Test
+    void usesManagementCoordinatorConfiguredFromC3p0Properties() {
+        C3P0Registry.getDefaultConnectionTester();
+
+        assertThat(RegistryManagementCoordinator.instancesCreated).isGreaterThanOrEqualTo(1);
+    }
+
+    public static final class RegistryConnectionTester extends AbstractConnectionTester {
+        public RegistryConnectionTester() {
+        }
+
+        @Override
+        public int activeCheckConnection(
+                Connection connection, String preferredTestQuery, Throwable[] rootCauseOutParamHolder) {
+            return ConnectionTester.CONNECTION_IS_OKAY;
+        }
+
+        @Override
+        public int statusOnException(
+                Connection connection,
+                Throwable throwable,
+                String preferredTestQuery,
+                Throwable[] rootCauseOutParamHolder) {
+            return ConnectionTester.CONNECTION_IS_INVALID;
+        }
+    }
+
+    public static final class RegistryConnectionCustomizer extends AbstractConnectionCustomizer {
+        private static String lastParentDataSourceIdentityToken;
+
+        public RegistryConnectionCustomizer() {
+        }
+
+        @Override
+        public void onAcquire(Connection connection, String parentDataSourceIdentityToken) {
+            lastParentDataSourceIdentityToken = parentDataSourceIdentityToken;
+        }
+    }
+
+    public static final class RegistryManagementCoordinator implements ManagementCoordinator {
+        private static int instancesCreated;
+
+        public RegistryManagementCoordinator() {
+            instancesCreated++;
+        }
+
+        @Override
+        public void attemptManageC3P0Registry() {
+        }
+
+        @Override
+        public void attemptUnmanageC3P0Registry() {
+        }
+
+        @Override
+        public void attemptManagePooledDataSource(PooledDataSource pooledDataSource) {
+        }
+
+        @Override
+        public void attemptUnmanagePooledDataSource(PooledDataSource pooledDataSource) {
+        }
+    }
+}
