e9f1634c36d2b509f520de30f7f1b2b0f4e26f19
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-04-30 02:57:48 +0200

    Scaffold com.jamesmurty.utils:java-xmlbuilder:0.4

---
+++ tests/src/com.jamesmurty.utils/java-xmlbuilder/0.4/src/test/java/com_jamesmurty_utils/java_xmlbuilder/Java_xmlbuilderTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_jamesmurty_utils.java_xmlbuilder;
+
+import org.junit.jupiter.api.Test;
+
+class Java_xmlbuilderTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
