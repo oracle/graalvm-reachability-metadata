15e7b459c18c8bd97fbf06c7aa564ef0bd8887c6
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-30 11:57:02 +0200

    Add ClassReader system resource coverage

--- tests/src/org.apache.xbean/xbean-asm9-shaded/4.22/src/test/java/org_apache_xbean/xbean_asm9_shaded/ClassReaderTest.java
+++ tests/src/org.apache.xbean/xbean-asm9-shaded/4.22/src/test/java/org_apache_xbean/xbean_asm9_shaded/ClassReaderTest.java
@@ -9,11 +9,22 @@ package org_apache_xbean.xbean_asm9_shaded;
 import java.io.IOException;
 
 import org.apache.xbean.asm9.ClassReader;
+import org.apache.xbean.asm9.ClassWriter;
+import org.apache.xbean.asm9.Opcodes;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClassReaderTest {
+    @Test
+    void readsGeneratedClassBytes() {
+        ClassReader classReader = new ClassReader(createClassBytes());
+
+        assertThat(classReader.getClassName()).isEqualTo("sample/Generated");
+        assertThat(classReader.getSuperName()).isEqualTo("java/lang/Object");
+        assertThat(classReader.getInterfaces()).isEmpty();
+    }
+
     @Test
     void readsClassBytesFromTheSystemClassLoader() throws IOException {
         ClassReader classReader = new ClassReader(ClassReader.class.getName());
@@ -21,5 +32,21 @@ public class ClassReaderTest {
         assertThat(classReader.getClassName()).isEqualTo("org/apache/xbean/asm9/ClassReader");
         assertThat(classReader.getSuperName()).isEqualTo("java/lang/Object");
         assertThat(classReader.getInterfaces()).isEmpty();
+    }
+
+    @Test
+    void readsApplicationClassBytesFromTheSystemClassLoader() throws IOException {
+        ClassReader classReader = new ClassReader(ClassReaderTest.class.getName());
+
+        assertThat(classReader.getClassName()).isEqualTo("org_apache_xbean/xbean_asm9_shaded/ClassReaderTest");
+        assertThat(classReader.getSuperName()).isEqualTo("java/lang/Object");
+        assertThat(classReader.getInterfaces()).isEmpty();
+    }
+
+    private static byte[] createClassBytes() {
+        ClassWriter classWriter = new ClassWriter(0);
+        classWriter.visit(Opcodes.V1_8, Opcodes.ACC_PUBLIC, "sample/Generated", null, "java/lang/Object", null);
+        classWriter.visitEnd();
+        return classWriter.toByteArray();
     }
 }
