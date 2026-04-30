5cff206012ebb82cbe743e650dcb9e1e69243d73
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-30 06:52:57 +0200

    Add pyrolite AnyClassConstructor coverage test

---
+++ tests/src/net.razorvine/pyrolite/4.30/src/test/java/net_razorvine/pyrolite/objects/AnyClassConstructorTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package net_razorvine.pyrolite.objects;
+
+import net.razorvine.pickle.objects.AnyClassConstructor;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AnyClassConstructorTest {
+    @Test
+    void constructsObjectUsingRuntimeArgumentTypes() {
+        AnyClassConstructor constructor = new AnyClassConstructor(ConstructorTarget.class);
+
+        Object value = constructor.construct(new Object[]{"sample", Integer.valueOf(7)});
+
+        assertThat(value).isInstanceOfSatisfying(ConstructorTarget.class, target -> {
+            assertThat(target.name()).isEqualTo("sample");
+            assertThat(target.count()).isEqualTo(7);
+        });
+    }
+
+    public static final class ConstructorTarget {
+        private final String name;
+        private final Integer count;
+
+        public ConstructorTarget(String name, Integer count) {
+            this.name = name;
+            this.count = count;
+        }
+
+        public String name() {
+            return name;
+        }
+
+        public Integer count() {
+            return count;
+        }
+    }
+}
