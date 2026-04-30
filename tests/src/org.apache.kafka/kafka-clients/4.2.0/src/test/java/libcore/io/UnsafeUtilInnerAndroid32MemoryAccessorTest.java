c89feab435987e65280b37864174995ecb0fa4b1
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-04-30 05:28:50 +0200

    Cover Kafka shaded protobuf Android32 accessor

--- tests/src/org.apache.kafka/kafka-clients/4.2.0/src/test/java/libcore/io/UnsafeUtilInnerAndroid32MemoryAccessorTest.java
+++ tests/src/org.apache.kafka/kafka-clients/4.2.0/src/test/java/libcore/io/UnsafeUtilInnerAndroid32MemoryAccessorTest.java
@@ -19,7 +19,7 @@ import org.apache.kafka.shaded.com.google.protobuf.InvalidProtocolBufferExceptio
 import org.apache.kafka.shaded.com.google.protobuf.Parser;
 import org.junit.jupiter.api.Test;
 
-public class UnsafeUtilTest {
+public class UnsafeUtilInnerAndroid32MemoryAccessorTest {
     @Test
     void writesProtobufPayloadUsingUnsafeArraySupport() throws Exception {
         assertEquals("libcore.io.Memory", Memory.class.getName());
@@ -112,15 +112,7 @@ final class Memory {
     private Memory() {
     }
 
-    public static long peekLong(long address, boolean swap) {
-        throw unsupported();
-    }
-
     public static long peekLong(int address, boolean swap) {
-        throw unsupported();
-    }
-
-    public static void pokeLong(long address, long value, boolean swap) {
         throw unsupported();
     }
 
@@ -128,15 +120,7 @@ final class Memory {
         throw unsupported();
     }
 
-    public static void pokeInt(long address, int value, boolean swap) {
-        throw unsupported();
-    }
-
     public static void pokeInt(int address, int value, boolean swap) {
-        throw unsupported();
-    }
-
-    public static int peekInt(long address, boolean swap) {
         throw unsupported();
     }
 
@@ -144,15 +128,7 @@ final class Memory {
         throw unsupported();
     }
 
-    public static void pokeByte(long address, byte value) {
-        throw unsupported();
-    }
-
     public static void pokeByte(int address, byte value) {
-        throw unsupported();
-    }
-
-    public static byte peekByte(long address) {
         throw unsupported();
     }
 
@@ -160,15 +136,7 @@ final class Memory {
         throw unsupported();
     }
 
-    public static void pokeByteArray(long address, byte[] bytes, int offset, int count) {
-        throw unsupported();
-    }
-
     public static void pokeByteArray(int address, byte[] bytes, int offset, int count) {
-        throw unsupported();
-    }
-
-    public static void peekByteArray(long address, byte[] bytes, int offset, int count) {
         throw unsupported();
     }
 
