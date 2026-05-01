9e26d029cf09549525ead8dd3382d1f82bf9cc4f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 00:38:41 +0200

    Dynamic-access coverage for org.eclipse.jdt.internal.compiler.lookup.ProblemReferenceBinding (45/55)

---
+++ tests/src/org.eclipse.jdt/ecj/3.26.0/src/test/java/org_eclipse_jdt/ecj/ProblemReferenceBindingTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_eclipse_jdt.ecj;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.jdt.internal.compiler.lookup.ProblemReasons;
+import org.eclipse.jdt.internal.compiler.lookup.ProblemReferenceBinding;
+import org.junit.jupiter.api.Test;
+
+public class ProblemReferenceBindingTest {
+    @Test
+    void problemReasonStringResolvesPublicProblemReasonConstant() {
+        final String reason = ProblemReferenceBinding.problemReasonString(ProblemReasons.NotVisible);
+
+        assertThat(reason).isEqualTo("ProblemReasons.NotVisible");
+    }
+
+    @Test
+    void problemReasonStringReturnsUnknownForUnmatchedProblemReason() {
+        final String reason = ProblemReferenceBinding.problemReasonString(Integer.MIN_VALUE);
+
+        assertThat(reason).isEqualTo("unknown");
+    }
+}
