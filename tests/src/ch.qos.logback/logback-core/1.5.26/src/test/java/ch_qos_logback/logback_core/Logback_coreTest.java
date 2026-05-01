7d9756eefce11af194606091112afc7ead33a0c4
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-01 14:37:13 +0200

    Scaffold ch.qos.logback:logback-core:1.5.26

---
+++ tests/src/ch.qos.logback/logback-core/1.5.26/src/test/java/ch_qos_logback/logback_core/Logback_coreTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package ch_qos_logback.logback_core;
+
+import org.junit.jupiter.api.Test;
+
+class Logback_coreTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
