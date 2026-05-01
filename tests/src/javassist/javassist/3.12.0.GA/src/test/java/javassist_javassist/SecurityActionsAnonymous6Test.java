04864b5952c1298d237f34c70e9e0f5734ddcd99
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-01 23:33:52 +0200

    Dynamic-access coverage for javassist.util.proxy.SecurityActions$6 (102/105)

---
+++ tests/src/javassist/javassist/3.12.0.GA/src/test/java/javassist_javassist/SecurityActionsAnonymous6Test.java
@@ -0,0 +1,117 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package javassist_javassist;
+
+import java.awt.Point;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.DataOutputStream;
+import java.lang.reflect.Field;
+import java.security.AccessController;
+
+import javassist.ClassPool;
+import javassist.CtClass;
+import javassist.bytecode.AccessFlag;
+import javassist.bytecode.Bytecode;
+import javassist.bytecode.ClassFile;
+import javassist.bytecode.ConstPool;
+import javassist.bytecode.MethodInfo;
+import org.graalvm.internal.tck.NativeImageSupport;
+import org.junit.jupiter.api.Test;
+
+import static javassist.bytecode.Opcode.DUP;
+import static javassist.bytecode.Opcode.POP;
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SecurityActionsAnonymous6Test {
+    private static final String BRIDGE_CLASS_NAME = "javassist.util.proxy.SecurityActionsAnonymous6CoverageBridge";
+    private static final String SECURITY_ACTION_CLASS_NAME = "javassist.util.proxy.SecurityActions$6";
+
+    @Test
+    void privilegedExceptionActionSetsFieldValue() throws Exception {
+        try {
+            SecurityActionsAnonymous6Probe probe = (SecurityActionsAnonymous6Probe) createBridgeClass().newInstance();
+            Point target = new Point(1, 2);
+
+            probe.run(Point.class.getField("x"), target);
+
+            assertThat(target.x).isEqualTo(11);
+            assertThat(target.y).isEqualTo(2);
+        } catch (Error error) {
+            verifyUnsupportedDynamicClassLoading(error);
+        }
+    }
+
+    private static Class<?> createBridgeClass() throws Exception {
+        ClassFile classFile = new ClassFile(false, BRIDGE_CLASS_NAME, Object.class.getName());
+        classFile.setAccessFlags(AccessFlag.PUBLIC);
+        classFile.addInterface(SecurityActionsAnonymous6Probe.class.getName());
+        addDefaultConstructor(classFile);
+        addRunMethod(classFile);
+
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        classFile.write(new DataOutputStream(bytes));
+
+        ClassPool classPool = new ClassPool(true);
+        CtClass bridgeClass = classPool.makeClass(new ByteArrayInputStream(bytes.toByteArray()));
+        return classPool.toClass(
+                bridgeClass,
+                SecurityActionsAnonymous6Test.class.getClassLoader(),
+                SecurityActionsAnonymous6Test.class.getProtectionDomain());
+    }
+
+    private static void addDefaultConstructor(ClassFile classFile) throws Exception {
+        ConstPool constPool = classFile.getConstPool();
+        MethodInfo constructor = new MethodInfo(constPool, "<init>", "()V");
+        constructor.setAccessFlags(AccessFlag.PUBLIC);
+
+        Bytecode bytecode = new Bytecode(constPool, 1, 1);
+        bytecode.addAload(0);
+        bytecode.addInvokespecial(Object.class.getName(), "<init>", "()V");
+        bytecode.addReturn(null);
+
+        constructor.addAttribute(bytecode.toCodeAttribute());
+        classFile.addMethod(constructor);
+    }
+
+    private static void addRunMethod(ClassFile classFile) throws Exception {
+        ConstPool constPool = classFile.getConstPool();
+        MethodInfo method = new MethodInfo(constPool, "run", "(Ljava/lang/reflect/Field;Ljava/awt/Point;)V");
+        method.setAccessFlags(AccessFlag.PUBLIC);
+
+        Bytecode bytecode = new Bytecode(constPool, 5, 3);
+        bytecode.addNew(SECURITY_ACTION_CLASS_NAME);
+        bytecode.addOpcode(DUP);
+        bytecode.addAload(1);
+        bytecode.addAload(2);
+        bytecode.addIconst(11);
+        bytecode.addInvokestatic(Integer.class.getName(), "valueOf", "(I)Ljava/lang/Integer;");
+        bytecode.addInvokespecial(
+                SECURITY_ACTION_CLASS_NAME,
+                "<init>",
+                "(Ljava/lang/reflect/Field;Ljava/lang/Object;Ljava/lang/Object;)V");
+        bytecode.addInvokestatic(
+                AccessController.class.getName(),
+                "doPrivileged",
+                "(Ljava/security/PrivilegedExceptionAction;)Ljava/lang/Object;");
+        bytecode.addOpcode(POP);
+        bytecode.addReturn(null);
+
+        method.addAttribute(bytecode.toCodeAttribute());
+        classFile.addMethod(method);
+    }
+
+    private static void verifyUnsupportedDynamicClassLoading(Error error) {
+        if (!NativeImageSupport.isUnsupportedFeatureError(error)) {
+            throw error;
+        }
+    }
+
+    public interface SecurityActionsAnonymous6Probe {
+        void run(Field field, Point target) throws Exception;
+    }
+}
