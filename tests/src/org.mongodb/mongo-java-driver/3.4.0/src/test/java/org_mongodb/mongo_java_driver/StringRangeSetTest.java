c9b6a6c2a453df0bf628844cf6f6dd5ed1349923
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 01:50:09 +0200

    Dynamic-access coverage for org.bson.types.StringRangeSet (8/8)

---
+++ tests/src/org.mongodb/mongo-java-driver/3.4.0/src/test/java/org_mongodb/mongo_java_driver/StringRangeSetTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_mongodb.mongo_java_driver;
+
+import org.bson.types.BasicBSONList;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringRangeSetTest {
+    @Test
+    void keySetCanCreateTypedArrayForAllListIndexes() {
+        final BasicBSONList list = new BasicBSONList();
+        list.put(0, "zero");
+        list.put(3, "three");
+
+        final Set<String> keys = list.keySet();
+        final String[] keyArray = keys.toArray(new String[0]);
+
+        assertThat(keyArray).containsExactly("0", "1", "2", "3");
+    }
+}
