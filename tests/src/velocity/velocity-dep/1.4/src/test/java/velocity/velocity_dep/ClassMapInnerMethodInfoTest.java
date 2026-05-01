358caddc48e25046c38fa9e40a44629f56504a37
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-01 23:58:37 +0200

    Dynamic-access coverage for org.apache.velocity.util.introspection.ClassMap$MethodInfo (51/58)

---
+++ tests/src/velocity/velocity-dep/1.4/src/test/java/velocity/velocity_dep/ClassMapInnerMethodInfoTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package velocity.velocity_dep;
+
+import java.lang.reflect.Method;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.velocity.util.introspection.ClassMap;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ClassMapInnerMethodInfoTest {
+    @Test
+    void upcastsMethodFromNonPublicImplementationToPublicInterface() throws Exception {
+        Map<String, String> values = new HashMap<>();
+        values.put("template", "merged");
+        Map<String, String> nonPublicMap = Collections.unmodifiableMap(values);
+        ClassMap classMap = new ClassMap(nonPublicMap.getClass());
+
+        Method method = classMap.findMethod("get", new Object[] { "template" });
+
+        assertNotNull(method);
+        assertEquals("get", method.getName());
+        assertEquals(Map.class, method.getDeclaringClass());
+    }
+}
