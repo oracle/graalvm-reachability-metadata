60ec1d37fc685637f08d1240e086ce7a9f930a0d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-01 11:01:15 +0200

    Dynamic-access coverage for org.eclipse.jdt.internal.compiler.apt.model.Factory (43/46)

---
+++ tests/src/org.eclipse.jdt.core.compiler/ecj/4.4.2/src/test/java/org_eclipse_jdt_core_compiler/ecj/FactoryTest.java
@@ -0,0 +1,167 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_eclipse_jdt_core_compiler.ecj;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import javax.annotation.processing.AbstractProcessor;
+import javax.annotation.processing.RoundEnvironment;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.TypeElement;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.StandardLocation;
+
+import org.eclipse.jdt.internal.compiler.tool.EclipseCompiler;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class FactoryTest {
+    private static final String TARGET_CLASS_NAME =
+            "org_eclipse_jdt_core_compiler.ecj.coverage.FactoryCoverageTarget";
+
+    @TempDir
+    Path temporaryDirectory;
+
+    @Test
+    void getAnnotationsByTypeReturnsTypedEmptyArrayForUnannotatedElement() throws IOException {
+        final Path sourceDirectory = Files.createDirectories(this.temporaryDirectory.resolve("sources"));
+        final Path classesDirectory = Files.createDirectories(this.temporaryDirectory.resolve("classes"));
+        final List<Path> sourceFiles = writeSourceFiles(sourceDirectory);
+        final FactoryCoverageProcessor processor = new FactoryCoverageProcessor();
+        final JavaCompiler compiler = new EclipseCompiler();
+
+        try (StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, UTF_8)) {
+            fileManager.setLocation(StandardLocation.CLASS_OUTPUT,
+                    Collections.singletonList(classesDirectory.toFile()));
+            final Iterable<? extends JavaFileObject> compilationUnits = fileManager.getJavaFileObjectsFromFiles(
+                    sourceFiles.stream().map(Path::toFile).toList());
+            final List<String> options = List.of("-proc:only", "-source", "1.6", "-target", "1.6");
+            final JavaCompiler.CompilationTask task = compiler.getTask(null, fileManager, null, options, null,
+                    compilationUnits);
+            task.setProcessors(Collections.singletonList(processor));
+
+            final Boolean result = callWithRecognizedClassFileVersion(task);
+
+            assertThat(result).isTrue();
+            assertThat(processor.hasVerifiedEmptyAnnotations()).isTrue();
+        }
+    }
+
+    private static List<Path> writeSourceFiles(Path sourceDirectory) throws IOException {
+        final Path javaLangDirectory = Files.createDirectories(sourceDirectory.resolve("java/lang"));
+        final Path objectSource = javaLangDirectory.resolve("Object.java");
+        final Path classSource = javaLangDirectory.resolve("Class.java");
+        final Path stringSource = javaLangDirectory.resolve("String.java");
+        final Path targetDirectory = Files.createDirectories(
+                sourceDirectory.resolve("org_eclipse_jdt_core_compiler/ecj/coverage"));
+        final Path targetSource = targetDirectory.resolve("FactoryCoverageTarget.java");
+        Files.writeString(objectSource, javaLangObjectSource(), UTF_8);
+        Files.writeString(classSource, javaLangClassSource(), UTF_8);
+        Files.writeString(stringSource, javaLangStringSource(), UTF_8);
+        Files.writeString(targetSource, source(), UTF_8);
+        return List.of(objectSource, classSource, stringSource, targetSource);
+    }
+
+    private static Boolean callWithRecognizedClassFileVersion(JavaCompiler.CompilationTask task) {
+        final String previousVersion = System.getProperty("java.class.version");
+        System.setProperty("java.class.version", "52.0");
+        try {
+            return task.call();
+        } finally {
+            if (previousVersion == null) {
+                System.clearProperty("java.class.version");
+            } else {
+                System.setProperty("java.class.version", previousVersion);
+            }
+        }
+    }
+
+    private static String javaLangObjectSource() {
+        return """
+                package java.lang;
+
+                public class Object {
+                    public Object() {
+                    }
+                }
+                """;
+    }
+
+    private static String javaLangClassSource() {
+        return """
+                package java.lang;
+
+                public final class Class<T> {
+                }
+                """;
+    }
+
+    private static String javaLangStringSource() {
+        return """
+                package java.lang;
+
+                public final class String {
+                }
+                """;
+    }
+
+    private static String source() {
+        return """
+                package org_eclipse_jdt_core_compiler.ecj.coverage;
+
+                public final class FactoryCoverageTarget {
+                }
+                """;
+    }
+
+    public @interface FactoryCoverageMarker {
+    }
+
+    private static final class FactoryCoverageProcessor extends AbstractProcessor {
+        private boolean verifiedEmptyAnnotations;
+
+        @Override
+        public Set<String> getSupportedAnnotationTypes() {
+            return Collections.singleton("*");
+        }
+
+        @Override
+        public SourceVersion getSupportedSourceVersion() {
+            return SourceVersion.RELEASE_6;
+        }
+
+        @Override
+        public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnvironment) {
+            if (roundEnvironment.processingOver() || this.verifiedEmptyAnnotations) {
+                return false;
+            }
+            final TypeElement targetElement = this.processingEnv.getElementUtils().getTypeElement(TARGET_CLASS_NAME);
+            assertThat(targetElement).isNotNull();
+
+            final FactoryCoverageMarker[] markerAnnotations = targetElement.getAnnotationsByType(
+                    FactoryCoverageMarker.class);
+
+            assertThat(markerAnnotations).isEmpty();
+            this.verifiedEmptyAnnotations = true;
+            return false;
+        }
+
+        boolean hasVerifiedEmptyAnnotations() {
+            return this.verifiedEmptyAnnotations;
+        }
+    }
+}
