1982d863f43b87511e5cc19fd9f6a08fea688365
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 01:48:13 +0200

    Dynamic-access coverage for com.mongodb.internal.connection.SslHelper (7/8)

---
+++ tests/src/org.mongodb/mongo-java-driver/3.4.0/src/test/java/org_mongodb/mongo_java_driver/SslHelperTest.java
@@ -0,0 +1,44 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_mongodb.mongo_java_driver;
+
+import com.mongodb.ServerAddress;
+import com.mongodb.internal.connection.SslHelper;
+import org.junit.jupiter.api.Test;
+
+import javax.net.ssl.SNIHostName;
+import javax.net.ssl.SNIServerName;
+import javax.net.ssl.SSLParameters;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SslHelperTest {
+    @Test
+    void enablesHostNameVerificationForSslParameters() {
+        final SSLParameters sslParameters = new SSLParameters();
+
+        SslHelper.enableHostNameVerification(sslParameters);
+
+        assertThat(sslParameters.getEndpointIdentificationAlgorithm()).isEqualTo("HTTPS");
+    }
+
+    @Test
+    void enablesServerNameIndicationForHostNames() {
+        final SSLParameters sslParameters = new SSLParameters();
+        final ServerAddress address = new ServerAddress("example.mongodb.local");
+
+        SslHelper.enableSni(address, sslParameters);
+
+        final List<SNIServerName> serverNames = sslParameters.getServerNames();
+        assertThat(serverNames).hasSize(1);
+        assertThat(serverNames.get(0)).isInstanceOf(SNIHostName.class);
+        final SNIHostName serverName = (SNIHostName) serverNames.get(0);
+        assertThat(serverName.getAsciiName()).isEqualTo(address.getHost());
+    }
+}
