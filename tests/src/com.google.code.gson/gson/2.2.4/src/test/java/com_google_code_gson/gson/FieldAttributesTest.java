3304027286cd3fe08037cbe89d8149024492270d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 09:32:54 +0200

    Dynamic-access coverage for com.google.gson.FieldAttributes (6/13)

---
+++ tests/src/com.google.code.gson/gson/2.2.4/src/test/java/com_google_code_gson/gson/FieldAttributesTest.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_google_code_gson.gson;
+
+import com.google.gson.FieldAttributes;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FieldAttributesTest {
+    @Test
+    public void exposesMetadataForTheWrappedField() throws Exception {
+        final Field field = SampleModel.class.getDeclaredField("description");
+        final FieldAttributes attributes = new FieldAttributes(field);
+
+        assertThat(attributes.getDeclaringClass()).isEqualTo(SampleModel.class);
+        assertThat(attributes.getName()).isEqualTo("description");
+        assertThat(attributes.getDeclaredType()).isEqualTo(String.class);
+        assertThat(attributes.getDeclaredClass()).isEqualTo(String.class);
+        // Checkstyle: allow direct annotation access
+        assertThat(attributes.getAnnotation(Deprecated.class)).isNotNull();
+        assertThat(attributes.getAnnotations())
+                .extracting(Annotation::annotationType)
+                .contains(Deprecated.class);
+        // Checkstyle: disallow direct annotation access
+        assertThat(attributes.hasModifier(Modifier.PUBLIC)).isTrue();
+        assertThat(attributes.hasModifier(Modifier.STATIC)).isFalse();
+    }
+
+    @Test
+    public void readsValueFromTheWrappedField() throws Exception {
+        final Field field = SampleModel.class.getDeclaredField("description");
+        final FieldAttributes attributes = new FieldAttributes(field);
+        final SampleModel sample = new SampleModel("stored field value");
+
+        assertThat(readValue(attributes, sample)).isEqualTo("stored field value");
+    }
+
+    private static Object readValue(final FieldAttributes attributes, final Object instance) throws Exception {
+        final Method getMethod = FieldAttributes.class.getDeclaredMethod("get", Object.class);
+        getMethod.setAccessible(true);
+        return getMethod.invoke(attributes, instance);
+    }
+
+    public static final class SampleModel {
+        @Deprecated
+        public String description;
+
+        public SampleModel(final String description) {
+            this.description = description;
+        }
+    }
+}
