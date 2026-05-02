39d6286cab46bb75a83d811fc673b817b3fb61cd
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 19:30:55 +0200

    Dynamic-access coverage for io.quarkus.bootstrap.workspace.LazySourceDir (6/15)

--- tests/src/io.quarkus/quarkus-bootstrap-app-model/3.33.1/src/test/java/io_quarkus/quarkus_bootstrap_app_model/Quarkus_bootstrap_app_modelTest.java
+++ tests/src/io.quarkus/quarkus-bootstrap-app-model/3.33.1/src/test/java/io_quarkus/quarkus_bootstrap_app_model/Quarkus_bootstrap_app_modelTest.java
@@ -8,7 +8,7 @@ package io_quarkus.quarkus_bootstrap_app_model;
 
 import org.junit.jupiter.api.Test;
 
-class Quarkus_bootstrap_app_modelTest {
+public class Quarkus_bootstrap_app_modelTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
