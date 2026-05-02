28ce63d090ddab14672ffd9f140e5aafa4bd0fcb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 03:01:49 +0200

    Partial dynamic-access coverage for org.apache.tools.ant.taskdefs.XSLTProcess (4/6)

--- tests/src/ant/ant/1.6/src/test/java/ant/ant/TaskAdapterTest.java
+++ tests/src/ant/ant/1.6/src/test/java/ant/ant/TaskAdapterTest.java
@@ -6,6 +6,11 @@
  */
 package ant.ant;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.VarHandle;
+
 import org.apache.tools.ant.Project;
 import org.apache.tools.ant.TaskAdapter;
 import org.junit.jupiter.api.Test;
@@ -14,6 +19,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 public class TaskAdapterTest {
+    @Test
+    void compilerGeneratedClassLookupResolvesProjectType() throws Throwable {
+        Class<?> resolvedClass = ExposedTaskAdapter.lookupCompilerGeneratedClass(
+                Project.class.getName());
+
+        assertThat(resolvedClass).isSameAs(Project.class);
+    }
+
     @Test
     void validatesAndExecutesBeanTaskThroughAdapter() {
         Project project = new Project();
@@ -27,11 +40,51 @@ public class TaskAdapterTest {
         assertThatCode(() -> adapter.checkProxyClass(AdaptedBeanTask.class))
                 .doesNotThrowAnyException();
 
+        ExposedTaskAdapter.clearCachedProjectClass();
         adapter.execute();
 
         assertThat(adapter.getProxy()).isSameAs(proxy);
         assertThat(proxy.project).isSameAs(project);
         assertThat(proxy.executed).isTrue();
+    }
+
+    private static final class ExposedTaskAdapter {
+        private static final MethodHandle CLASS_LOOKUP = classLookupMethod();
+        private static final VarHandle PROJECT_CLASS = staticField(
+                "class$org$apache$tools$ant$Project",
+                Class.class);
+
+        static Class<?> lookupCompilerGeneratedClass(String className) throws Throwable {
+            return (Class<?>) CLASS_LOOKUP.invoke(className);
+        }
+
+        static void clearCachedProjectClass() {
+            PROJECT_CLASS.set(null);
+        }
+
+        private static MethodHandle classLookupMethod() {
+            try {
+                return taskAdapterLookup().findStatic(
+                        TaskAdapter.class,
+                        "class$",
+                        MethodType.methodType(Class.class, String.class));
+            } catch (NoSuchMethodException | IllegalAccessException exception) {
+                throw new ExceptionInInitializerError(exception);
+            }
+        }
+
+        private static VarHandle staticField(String fieldName, Class<?> fieldType) {
+            try {
+                return taskAdapterLookup()
+                        .findStaticVarHandle(TaskAdapter.class, fieldName, fieldType);
+            } catch (NoSuchFieldException | IllegalAccessException exception) {
+                throw new ExceptionInInitializerError(exception);
+            }
+        }
+
+        private static MethodHandles.Lookup taskAdapterLookup() throws IllegalAccessException {
+            return MethodHandles.privateLookupIn(TaskAdapter.class, MethodHandles.lookup());
+        }
     }
 
     public static class AdaptedBeanTask {
---
+++ tests/src/ant/ant/1.6/src/test/java/ant/ant/XSLTProcessTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package ant.ant;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.XSLTLiaison;
+import org.apache.tools.ant.taskdefs.XSLTProcess;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class XSLTProcessTest {
+    private static final String TRAX_LIAISON =
+            "org.apache.tools.ant.taskdefs.optional.TraXLiaison";
+    private static final String XSLP_LIAISON =
+            "org.apache.tools.ant.taskdefs.optional.XslpLiaison";
+    private static final String XALAN_LIAISON =
+            "org.apache.tools.ant.taskdefs.optional.XalanLiaison";
+
+    @TempDir
+    Path temporaryDirectory;
+
+    @Test
+    void executesTraxProcessorLoadedByDefaultName() throws IOException {
+        Path inputFile = writeFile("input.xml", """
+                <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+                <document><title>Ant XSLT</title></document>
+                """);
+        Path stylesheetFile = writeFile("style.xsl", """
+                <?xml version=\"1.0\" encoding=\"UTF-8\"?>
+                <xsl:stylesheet version=\"1.0\" xmlns:xsl=\"http://www.w3.org/1999/XSL/Transform\">
+                    <xsl:output method=\"text\"/>
+                    <xsl:template match=\"/document\">
+                        <xsl:value-of select=\"title\"/>
+                    </xsl:template>
+                </xsl:stylesheet>
+                """);
+        Path outputFile = temporaryDirectory.resolve("result.txt");
+        ExposedXSLTProcess task = newTask("trax");
+        task.setStyle(stylesheetFile.toString());
+        task.setIn(inputFile.toFile());
+        task.setOut(outputFile.toFile());
+
+        task.execute();
+
+        assertThat(Files.readString(outputFile, StandardCharsets.UTF_8)).contains("Ant XSLT");
+    }
+
+    @Test
+    void resolvesDeprecatedXslpProcessorByName() {
+        assertOptionalProcessorResolution("xslp", XSLP_LIAISON);
+    }
+
+    @Test
+    void resolvesDeprecatedXalanProcessorByName() {
+        assertOptionalProcessorResolution("xalan", XALAN_LIAISON);
+    }
+
+    @Test
+    void resolvesCustomProcessorByClassName() {
+        ExposedXSLTProcess task = newTask(TRAX_LIAISON);
+
+        XSLTLiaison liaison = task.exposeLiaison();
+
+        assertThat(liaison.getClass().getName()).isEqualTo(TRAX_LIAISON);
+    }
+
+    @Test
+    void resolvesCustomProcessorThroughConfiguredAntClasspath() {
+        ExposedXSLTProcess task = newTask(TRAX_LIAISON);
+        task.setClasspath(new org.apache.tools.ant.types.Path(task.getProject()));
+
+        XSLTLiaison liaison = task.exposeLiaison();
+
+        assertThat(liaison.getClass().getName()).isEqualTo(TRAX_LIAISON);
+    }
+
+    private void assertOptionalProcessorResolution(String processor, String expectedClassName) {
+        ExposedXSLTProcess task = newTask(processor);
+
+        try {
+            XSLTLiaison liaison = task.exposeLiaison();
+
+            assertThat(liaison.getClass().getName()).isEqualTo(expectedClassName);
+        } catch (BuildException exception) {
+            assertThat(exception.getException())
+                    .isNotNull()
+                    .isNotInstanceOf(ClassNotFoundException.class);
+        } catch (LinkageError error) {
+            assertThat(error).isInstanceOf(NoClassDefFoundError.class);
+        }
+    }
+
+    private ExposedXSLTProcess newTask(String processor) {
+        Project project = new Project();
+        project.setBaseDir(temporaryDirectory.toFile());
+        ExposedXSLTProcess task = new ExposedXSLTProcess();
+        task.setProject(project);
+        task.setProcessor(processor);
+        return task;
+    }
+
+    private Path writeFile(String fileName, String content) throws IOException {
+        Path file = temporaryDirectory.resolve(fileName);
+        Files.writeString(file, content, StandardCharsets.UTF_8);
+        return file;
+    }
+
+    private static final class ExposedXSLTProcess extends XSLTProcess {
+        XSLTLiaison exposeLiaison() {
+            return getLiaison();
+        }
+    }
+}
