1dc725df9ae304ec7de2e62b509cfeceb5dbbdfb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 18:46:27 +0200

    Dynamic-access coverage for com.google.gson.internal.bind.ReflectiveTypeAdapterFactory$1 (5/13)

--- tests/src/com.google.code.gson/gson/2.2.4/src/test/java/com_google_code_gson/gson/GsonTest.java
+++ tests/src/com.google.code.gson/gson/2.2.4/src/test/java/com_google_code_gson/gson/GsonTest.java
@@ -8,7 +8,7 @@ package com_google_code_gson.gson;
 
 import org.junit.jupiter.api.Test;
 
-class GsonTest {
+public class GsonTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
