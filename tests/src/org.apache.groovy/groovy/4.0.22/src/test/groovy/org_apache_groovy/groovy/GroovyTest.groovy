c4c7e2d8743d17096981604806db156c9b1b48b9
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-02 06:02:34 +0200

    Checkpoint generated tests for org.apache.groovy:groovy:4.0.22 (2)

--- tests/src/org.apache.groovy/groovy/4.0.22/src/test/groovy/org_apache_groovy/groovy/GroovyTest.groovy
+++ tests/src/org.apache.groovy/groovy/4.0.22/src/test/groovy/org_apache_groovy/groovy/GroovyTest.groovy
@@ -16,11 +16,15 @@ import groovy.lang.Tuple
 import groovy.lang.Tuple2
 import groovy.util.ConfigObject
 import groovy.util.Expando
+import groovy.util.ObservableList
+import groovy.util.ObservableMap
 import org.codehaus.groovy.control.CompilerConfiguration
 import org.codehaus.groovy.runtime.InvokerHelper
 import org.graalvm.internal.tck.NativeImageSupport
 import org.junit.jupiter.api.Test
 
+import java.beans.PropertyChangeEvent
+import java.beans.PropertyChangeListener
 import java.io.StringWriter
 import java.math.BigInteger
 import java.util.Locale
@@ -118,6 +122,59 @@ public class GroovyTest {
         assertEquals(22, module.properties['minor'])
         assertFalse(module.properties.containsKey('unknown'))
         assertEquals(null, module.unknown)
+    }
+
+    @Test
+    void observableCollectionsPublishMutationEvents() {
+        ObservableMap settings = new ObservableMap([language: 'Groovy'])
+        List<PropertyChangeEvent> mapEvents = []
+        PropertyChangeListener mapListener = {
+            PropertyChangeEvent event -> mapEvents << event
+        } as PropertyChangeListener
+        settings.addPropertyChangeListener(mapListener)
+
+        assertEquals('Groovy', settings.put('language', 'Apache Groovy'))
+        settings.put('runtime', 'native')
+        settings.remove('runtime')
+
+        assertEquals('Apache Groovy', settings['language'])
+        assertEquals([
+                'language',
+                'runtime',
+                ObservableMap.SIZE_PROPERTY,
+                'runtime',
+                ObservableMap.SIZE_PROPERTY
+        ], mapEvents*.propertyName)
+        assertTrue(mapEvents[0] instanceof ObservableMap.PropertyUpdatedEvent)
+        assertEquals('Groovy', mapEvents[0].oldValue)
+        assertEquals('Apache Groovy', mapEvents[0].newValue)
+        assertTrue(mapEvents[1] instanceof ObservableMap.PropertyAddedEvent)
+        assertTrue(mapEvents[3] instanceof ObservableMap.PropertyRemovedEvent)
+
+        ObservableList phases = new ObservableList(['parse'])
+        List<PropertyChangeEvent> listEvents = []
+        PropertyChangeListener listListener = {
+            PropertyChangeEvent event -> listEvents << event
+        } as PropertyChangeListener
+        phases.addPropertyChangeListener(listListener)
+
+        assertTrue(phases.add('compile'))
+        assertEquals('parse', phases.set(0, 'analyze'))
+        assertTrue(phases.remove('compile'))
+
+        assertEquals(['analyze'], phases.content)
+        assertEquals([
+                ObservableList.CONTENT_PROPERTY,
+                ObservableList.SIZE_PROPERTY,
+                ObservableList.CONTENT_PROPERTY,
+                ObservableList.CONTENT_PROPERTY,
+                ObservableList.SIZE_PROPERTY
+        ], listEvents*.propertyName)
+        assertTrue(listEvents[0] instanceof ObservableList.ElementAddedEvent)
+        assertTrue(listEvents[2] instanceof ObservableList.ElementUpdatedEvent)
+        assertEquals('parse', listEvents[2].oldValue)
+        assertEquals('analyze', listEvents[2].newValue)
+        assertTrue(listEvents[3] instanceof ObservableList.ElementRemovedEvent)
     }
 
     @Test
