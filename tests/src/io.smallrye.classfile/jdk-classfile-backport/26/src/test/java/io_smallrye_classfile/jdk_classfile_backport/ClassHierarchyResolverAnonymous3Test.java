c23ff377cf824a208ac4c2f2e9ae5cf5dbe48e27
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-03 00:32:29 +0200

    Dynamic-access coverage for io.smallrye.classfile.ClassHierarchyResolver$3 (4/6)

---
+++ tests/src/io.smallrye.classfile/jdk-classfile-backport/26/src/test/java/io_smallrye_classfile/jdk_classfile_backport/ClassHierarchyResolverAnonymous3Test.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_smallrye_classfile.jdk_classfile_backport;
+
+import static java.lang.constant.ConstantDescs.CD_String;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.smallrye.classfile.ClassHierarchyResolver;
+import org.junit.jupiter.api.Test;
+
+public class ClassHierarchyResolverAnonymous3Test {
+    @Test
+    void classLoadingResolverResolvesKnownClassWithProvidedClassLoader() {
+        ClassHierarchyResolver resolver = ClassHierarchyResolver.ofClassLoading(ClassLoader.getSystemClassLoader());
+
+        ClassHierarchyResolver.ClassHierarchyInfo classInfo = resolver.getClassInfo(CD_String);
+
+        assertThat(classInfo).isNotNull();
+    }
+}
