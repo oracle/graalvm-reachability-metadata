a65a35a0e0a65f0cf010e815fc81fd0d8076d066
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 19:05:46 +0200

    Dynamic-access coverage for com.mchange.v2.beans.swing.PropertyComponentBindingUtility (78/93)

---
+++ tests/src/com.mchange/mchange-commons-java/0.4.0/src/test/java/com_mchange/mchange_commons_java/PropertyComponentBindingUtilityTest.java
@@ -0,0 +1,81 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_mchange.mchange_commons_java;
+
+import com.mchange.v2.beans.swing.PropertyBoundTextField;
+import org.junit.jupiter.api.Test;
+
+import javax.swing.SwingUtilities;
+import java.beans.IntrospectionException;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.lang.reflect.InvocationTargetException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PropertyComponentBindingUtilityTest {
+    @Test
+    void textFieldBindingReadsInitialBeanValueAndListensForBeanChanges()
+            throws IntrospectionException, InvocationTargetException, InterruptedException {
+        ObservableValueBean bean = new ObservableValueBean();
+        bean.setValue("initial");
+
+        PropertyBoundTextField textField = new PropertyBoundTextField(bean, "value", 20);
+        flushSwingEvents();
+
+        assertThat(textField.getText()).isEqualTo("initial");
+
+        bean.setValue("from-bean");
+
+        assertThat(textField.getText()).isEqualTo("from-bean");
+    }
+
+    @Test
+    void textFieldActionWritesUserModificationBackToBean()
+            throws IntrospectionException, InvocationTargetException, InterruptedException {
+        ObservableValueBean bean = new ObservableValueBean();
+        bean.setValue("initial");
+        PropertyBoundTextField textField = new PropertyBoundTextField(bean, "value", 20);
+        flushSwingEvents();
+
+        textField.setText("from-user");
+        textField.postActionEvent();
+
+        assertThat(bean.getValue()).isEqualTo("from-user");
+    }
+
+    private static void flushSwingEvents() throws InvocationTargetException, InterruptedException {
+        SwingUtilities.invokeAndWait(() -> {
+        });
+    }
+
+    public static class ObservableValueBean {
+        private final PropertyChangeSupport propertyChangeSupport = new PropertyChangeSupport(this);
+        private String value;
+
+        public ObservableValueBean() {
+        }
+
+        public String getValue() {
+            return value;
+        }
+
+        public void setValue(String value) {
+            String oldValue = this.value;
+            this.value = value;
+            propertyChangeSupport.firePropertyChange("value", oldValue, value);
+        }
+
+        public void addPropertyChangeListener(PropertyChangeListener listener) {
+            propertyChangeSupport.addPropertyChangeListener(listener);
+        }
+
+        public void removePropertyChangeListener(PropertyChangeListener listener) {
+            propertyChangeSupport.removePropertyChangeListener(listener);
+        }
+    }
+}
