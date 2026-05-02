e8105fd49dbaaf373d8f5adf503d74704478aaf2
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 08:20:59 +0200

    Dynamic-access coverage for com.github.fge.msgsimple.source.PropertiesMessageSource (3/3)

---
+++ tests/src/com.github.java-json-tools/msg-simple/1.2/src/test/java/com_github_java_json_tools/msg_simple/PropertiesMessageSourceTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_github_java_json_tools.msg_simple;
+
+import com.github.fge.msgsimple.source.MessageSource;
+import com.github.fge.msgsimple.source.PropertiesMessageSource;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PropertiesMessageSourceTest {
+    private static final String RESOURCE_PATH = "/com_github_java_json_tools/msg_simple/properties-message-source.properties";
+
+    @Test
+    void fromResourceLoadsMessagesFromClasspathPropertiesFile() throws Exception {
+        MessageSource source = PropertiesMessageSource.fromResource(RESOURCE_PATH);
+
+        assertThat(source.getKey("greeting")).isEqualTo("Hello from a test resource");
+        assertThat(source.getKey("accented")).isEqualTo("café");
+        assertThat(source.getKey("missing.key")).isNull();
+    }
+}
