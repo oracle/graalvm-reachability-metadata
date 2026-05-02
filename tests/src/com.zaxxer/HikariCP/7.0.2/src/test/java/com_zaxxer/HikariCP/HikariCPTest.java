b8c5a872ea0ce9b36c963de2fbcb358f0427de9f
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-02 21:23:43 +0200

    Checkpoint generated tests for com.zaxxer:HikariCP:7.0.2 (2)

--- tests/src/com.zaxxer/HikariCP/7.0.2/src/test/java/com_zaxxer/HikariCP/HikariCPTest.java
+++ tests/src/com.zaxxer/HikariCP/7.0.2/src/test/java/com_zaxxer/HikariCP/HikariCPTest.java
@@ -7,7 +7,9 @@
 package com_zaxxer.HikariCP;
 
 import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariCredentialsProvider;
 import com.zaxxer.hikari.HikariDataSource;
+import com.zaxxer.hikari.util.Credentials;
 import com.zaxxer.hikaricp.test.support.RecordingDataSource;
 import org.junit.jupiter.api.Test;
 
@@ -37,6 +39,35 @@ public class HikariCPTest {
             }
 
             assertThat(dataSource.isRunning()).isTrue();
+        }
+    }
+
+    @Test
+    void usesCredentialsProviderWhenCreatingPooledConnections() throws SQLException {
+        RecordingDataSource recordingDataSource = new RecordingDataSource();
+        StaticCredentialsProvider credentialsProvider = new StaticCredentialsProvider();
+        HikariConfig config = new HikariConfig();
+        config.setPoolName("credentials-provider-pool");
+        config.setDataSource(recordingDataSource);
+        config.setCredentialsProvider(credentialsProvider);
+        config.setMaximumPoolSize(1);
+        config.setMinimumIdle(0);
+        config.setConnectionTimeout(1_000);
+        config.setValidationTimeout(500);
+        config.setInitializationFailTimeout(1_000);
+
+        try (HikariDataSource dataSource = new HikariDataSource(config);
+                Connection connection = dataSource.getConnection()) {
+            assertThat(connection.isValid(1)).isTrue();
+            assertThat(recordingDataSource.getLastUsername()).isEqualTo("credentialed-user");
+            assertThat(recordingDataSource.getLastPassword()).isEqualTo("credentialed-password");
+        }
+    }
+
+    public static final class StaticCredentialsProvider implements HikariCredentialsProvider {
+        @Override
+        public Credentials getCredentials() {
+            return Credentials.of("credentialed-user", "credentialed-password");
         }
     }
 }
