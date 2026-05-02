bae0a3a0e66575e533c71b7f93285cdcdc69c8d5
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 04:06:32 +0200

    Partial dynamic-access coverage for org.apache.tools.ant.filters.ClassConstants (2/3)

--- tests/src/ant/ant/1.6/src/test/java/ant/ant/ClassConstantsTest.java
+++ tests/src/ant/ant/1.6/src/test/java/ant/ant/ClassConstantsTest.java
@@ -8,18 +8,67 @@ package ant.ant;
 
 import java.io.IOException;
 import java.io.StringReader;
+import java.nio.charset.StandardCharsets;
 
 import org.apache.tools.ant.filters.ClassConstants;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class ClassConstantsTest {
-    @Test
-    void readUsesJavaClassHelperToDecodeClassBytes() {
-        ClassConstants filter = new ClassConstants(new StringReader("not a Java class file"));
+    private static final byte[] CLASS_WITH_CONSTANT_BYTES = new byte[] {
+            (byte) 0xCA, (byte) 0xFE, (byte) 0xBA, (byte) 0xBE,
+            0x00, 0x03, 0x00, 0x2D,
+            0x00, 0x09,
+            0x01, 0x00, 0x0F,
+            'C', 'o', 'n', 's', 't', 'a', 'n', 't', 'F', 'i', 'x', 't', 'u', 'r', 'e',
+            0x07, 0x00, 0x01,
+            0x01, 0x00, 0x10,
+            'j', 'a', 'v', 'a', '/', 'l', 'a', 'n', 'g', '/', 'O', 'b', 'j', 'e', 'c', 't',
+            0x07, 0x00, 0x03,
+            0x01, 0x00, 0x06,
+            'A', 'N', 'S', 'W', 'E', 'R',
+            0x01, 0x00, 0x01,
+            'I',
+            0x03, 0x00, 0x00, 0x00, 0x07,
+            0x01, 0x00, 0x0D,
+            'C', 'o', 'n', 's', 't', 'a', 'n', 't', 'V', 'a', 'l', 'u', 'e',
+            0x00, 0x21,
+            0x00, 0x02,
+            0x00, 0x04,
+            0x00, 0x00,
+            0x00, 0x01,
+            0x00, 0x19,
+            0x00, 0x05,
+            0x00, 0x06,
+            0x00, 0x01,
+            0x00, 0x08,
+            0x00, 0x00, 0x00, 0x02,
+            0x00, 0x07,
+            0x00, 0x00,
+            0x00, 0x00
+    };
 
-        assertThatThrownBy(() -> filter.read())
-                .isInstanceOf(IOException.class);
+    @Test
+    void readReturnsConstantsFromClassFileBytes() throws IOException {
+        ClassConstants filter = new ClassConstants(new StringReader(classFileText()));
+
+        String constants = readFully(filter);
+
+        assertThat(constants).isEqualTo("ANSWER=7" + System.lineSeparator());
+    }
+
+    private static String classFileText() {
+        return new String(CLASS_WITH_CONSTANT_BYTES, StandardCharsets.ISO_8859_1);
+    }
+
+    private static String readFully(ClassConstants filter) throws IOException {
+        StringBuilder result = new StringBuilder();
+        int ch = filter.read();
+        while (ch != -1) {
+            result.append((char) ch);
+            ch = filter.read();
+        }
+        return result.toString();
     }
 }
