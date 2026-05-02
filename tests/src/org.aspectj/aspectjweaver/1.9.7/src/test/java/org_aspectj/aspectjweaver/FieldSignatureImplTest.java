e69df9495075f65105972b90efe07077d81ce49d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-02 06:03:13 +0200

    Dynamic-access coverage for org.aspectj.runtime.reflect.FieldSignatureImpl (146/156)

---
+++ tests/src/org.aspectj/aspectjweaver/1.9.7/src/test/java/org_aspectj/aspectjweaver/FieldSignatureImplTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_aspectj.aspectjweaver;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
+import org.aspectj.lang.reflect.FieldSignature;
+import org.aspectj.runtime.reflect.Factory;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FieldSignatureImplTest {
+    @Test
+    void resolvesDeclaredFieldFromSignature() {
+        Factory factory = new Factory("FieldSignatureImplTest.java", FieldSignatureImplTest.class);
+        FieldSignature signature = factory.makeFieldSig(
+                Modifier.PRIVATE,
+                "message",
+                FieldFixture.class,
+                String.class
+        );
+
+        Field field = signature.getField();
+
+        assertThat(field).isNotNull();
+        assertThat(field.getDeclaringClass()).isEqualTo(FieldFixture.class);
+        assertThat(field.getName()).isEqualTo("message");
+        assertThat(field.getType()).isEqualTo(String.class);
+        assertThat(Modifier.isPrivate(field.getModifiers())).isTrue();
+    }
+
+    public static final class FieldFixture {
+        private String message;
+    }
+}
