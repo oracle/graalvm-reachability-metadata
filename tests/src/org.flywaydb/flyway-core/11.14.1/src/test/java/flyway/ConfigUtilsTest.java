18f26ac3e2e744107fba7d0f73483d8ccaf086cd
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-03 21:39:35 +0200

    Dynamic-access coverage for org.flywaydb.core.internal.configuration.ConfigUtils (41/43)

---
+++ tests/src/org.flywaydb/flyway-core/11.14.1/src/test/java/flyway/ConfigUtilsTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package flyway;
+
+import java.util.List;
+
+import org.flywaydb.core.internal.configuration.ConfigUtils;
+import org.flywaydb.core.internal.configuration.models.FlywayModel;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ConfigUtilsTest {
+
+    @Test
+    void suggestsPluginNamespaceForConfigurationExtensionField() {
+        List<String> possibleConfigurations = ConfigUtils.getPossibleFlywayConfigurations(
+                "clean",
+                FlywayModel.defaults(),
+                "flyway.");
+
+        assertThat(possibleConfigurations).contains("plugins.clean");
+    }
+}
