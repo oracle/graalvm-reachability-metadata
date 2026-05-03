18182bbc4b8162e701560bccb1aea75431ed6ad9
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-03 02:48:40 +0200

    Scaffold org.eclipse.jetty.http2:http2-hpack:11.0.24

---
+++ tests/src/org.eclipse.jetty.http2/http2-hpack/11.0.24/src/test/java/org_eclipse_jetty_http2/http2_hpack/Http2_hpackTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_eclipse_jetty_http2.http2_hpack;
+
+import org.junit.jupiter.api.Test;
+
+class Http2_hpackTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
