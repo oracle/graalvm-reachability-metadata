da6a25418288396700ac229771ddf806ea562a1c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 00:24:03 +0200

    Scaffold io.quarkus:quarkus-bootstrap-runner:3.33.1

---
+++ tests/src/io.quarkus/quarkus-bootstrap-runner/3.33.1/src/test/java/io_quarkus/quarkus_bootstrap_runner/Quarkus_bootstrap_runnerTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_quarkus.quarkus_bootstrap_runner;
+
+import org.junit.jupiter.api.Test;
+
+class Quarkus_bootstrap_runnerTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
