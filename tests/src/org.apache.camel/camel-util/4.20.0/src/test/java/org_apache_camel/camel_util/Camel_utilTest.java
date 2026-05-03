78b781edf5f545597f1ae8f0d8915a09ea3304ab
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-03 23:57:24 +0200

    Scaffold org.apache.camel:camel-util:4.20.0

---
+++ tests/src/org.apache.camel/camel-util/4.20.0/src/test/java/org_apache_camel/camel_util/Camel_utilTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_camel.camel_util;
+
+import org.junit.jupiter.api.Test;
+
+class Camel_utilTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
