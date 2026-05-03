25aef1bb3e983a653f8ba67a52e002b984e5dc26
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-03 23:15:58 +0200

    Checkpoint generated tests for dev.zio:izumi-reflect-thirdparty-boopickle-shaded_3:3.0.1 (2)

--- tests/src/dev.zio/izumi-reflect-thirdparty-boopickle-shaded_3/3.0.1/src/test/scala/dev_zio/izumi_reflect_thirdparty_boopickle_shaded_3/Izumi_reflect_thirdparty_boopickle_shaded_3Test.scala
+++ tests/src/dev.zio/izumi-reflect-thirdparty-boopickle-shaded_3/3.0.1/src/test/scala/dev_zio/izumi_reflect_thirdparty_boopickle_shaded_3/Izumi_reflect_thirdparty_boopickle_shaded_3Test.scala
@@ -4,13 +4,32 @@
  * You should have received a copy of the CC0 legalcode along with this
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
-package dev_zio.izumi_reflect_thirdparty_boopickle_shaded_3
+package izumi.reflect
 
+import _root_.izumi.reflect.thirdparty.internal.boopickle.NoMacro.*
+import _root_.izumi.reflect.thirdparty.internal.boopickle.PickleImpl
+import _root_.izumi.reflect.thirdparty.internal.boopickle.PickleState
+import _root_.izumi.reflect.thirdparty.internal.boopickle.Pickler
+import _root_.izumi.reflect.thirdparty.internal.boopickle.UnpickleState
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 class Izumi_reflect_thirdparty_boopickle_shaded_3Test {
   @Test
-  def test(): Unit = {
-    println("This is just a placeholder, implement your test")
+  def nestedCollectionPicklersRoundTripOptionsAndTuples(): Unit = {
+    val value: Map[String, List[Option[(Int, Boolean)]]] = Map(
+      "enabled" -> List(Some(1 -> true), None, Some(127 -> false)),
+      "limits" -> List(Some((-4096, true)), Some((268435456, false)))
+    )
+
+    val decoded: Map[String, List[Option[(Int, Boolean)]]] = roundTrip(value)
+
+    assertEquals(value, decoded)
+  }
+
+  private def roundTrip[A](value: A)(implicit pickler: Pickler[A]): A = {
+    implicit val state: PickleState = PickleState.pickleStateSpeed
+    val bytes = PickleImpl.intoBytes(value)
+    UnpickleState(bytes).unpickle[A]
   }
 }
