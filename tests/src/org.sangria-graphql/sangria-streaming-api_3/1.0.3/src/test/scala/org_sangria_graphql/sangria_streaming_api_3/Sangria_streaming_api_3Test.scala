91066d8773d9fdb3472f8a12d8addf2e2fad713b
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-03 20:15:04 +0200

    Scaffold org.sangria-graphql:sangria-streaming-api_3:1.0.3

---
+++ tests/src/org.sangria-graphql/sangria-streaming-api_3/1.0.3/src/test/scala/org_sangria_graphql/sangria_streaming_api_3/Sangria_streaming_api_3Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_sangria_graphql.sangria_streaming_api_3
+
+import org.junit.jupiter.api.Test
+
+class Sangria_streaming_api_3Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
