ffe085124c3f2b91501a9ecb7202444b93031d70
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-03 12:36:36 +0200

    Scaffold software.amazon.awssdk:third-party-jackson-core:2.34.0

---
+++ tests/src/software.amazon.awssdk/third-party-jackson-core/2.34.0/src/test/java/software_amazon_awssdk/third_party_jackson_core/Third_party_jackson_coreTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package software_amazon_awssdk.third_party_jackson_core;
+
+import org.junit.jupiter.api.Test;
+
+class Third_party_jackson_coreTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
