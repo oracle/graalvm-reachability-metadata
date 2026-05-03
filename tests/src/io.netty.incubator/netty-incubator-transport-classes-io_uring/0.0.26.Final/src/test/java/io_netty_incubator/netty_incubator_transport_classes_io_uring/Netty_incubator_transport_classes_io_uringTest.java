7067548c2585d26eb51a411ea8cfaf7d2bf80289
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-03 10:00:50 +0200

    Scaffold io.netty.incubator:netty-incubator-transport-classes-io_uring:0.0.26.Final

---
+++ tests/src/io.netty.incubator/netty-incubator-transport-classes-io_uring/0.0.26.Final/src/test/java/io_netty_incubator/netty_incubator_transport_classes_io_uring/Netty_incubator_transport_classes_io_uringTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_netty_incubator.netty_incubator_transport_classes_io_uring;
+
+import org.junit.jupiter.api.Test;
+
+class Netty_incubator_transport_classes_io_uringTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
