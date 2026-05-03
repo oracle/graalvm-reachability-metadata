bf5cce23d3fad5b4b9ceebb5d7ae915faf9bde7f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-03 17:31:43 +0200

    Scaffold com.sun.xml.dtd-parser:dtd-parser:1.5.1

---
+++ tests/src/com.sun.xml.dtd-parser/dtd-parser/1.5.1/src/test/java/com_sun_xml_dtd_parser/dtd_parser/Dtd_parserTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_sun_xml_dtd_parser.dtd_parser;
+
+import org.junit.jupiter.api.Test;
+
+class Dtd_parserTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
