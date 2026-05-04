00e7b7fb7405a814df332d8e2a20a54971ddbdce
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 06:21:01 +0200

    Scaffold io.mockk:mockk-agent-api-jvm:1.14.9

---
+++ tests/src/io.mockk/mockk-agent-api-jvm/1.14.9/src/test/kotlin/io_mockk/mockk_agent_api_jvm/Mockk_agent_api_jvmTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_mockk.mockk_agent_api_jvm
+
+import org.junit.jupiter.api.Test
+
+class Mockk_agent_api_jvmTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
