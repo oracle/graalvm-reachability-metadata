cdf1b1bb6525399c5e452f119da88e7cd7f5ec25
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 22:16:30 +0200

    Dynamic-access coverage for com.google.protobuf.MessageSchema (36/41)

--- tests/src/com.google.protobuf/protobuf-javalite/3.25.3/src/test/java/com_google_protobuf/protobuf_javalite/MessageSchemaTest.java
+++ tests/src/com.google.protobuf/protobuf-javalite/3.25.3/src/test/java/com_google_protobuf/protobuf_javalite/MessageSchemaTest.java
@@ -6,6 +6,7 @@
  */
 package com_google_protobuf.protobuf_javalite;
 
+import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import com.google.protobuf.GeneratedMessageLite;
@@ -14,11 +15,90 @@ import org.junit.jupiter.api.Test;
 
 public class MessageSchemaTest {
     @Test
+    void schemaCreationUsesRawMessageInfoFieldNamesPresentOnTheMessageClass() {
+        assertThatCode(() -> new MessageWithRawInt32Field().hashCode())
+                .doesNotThrowAnyException();
+    }
+
+    @Test
     void schemaCreationReportsRawMessageInfoFieldNamesMissingFromTheMessageClass() {
         assertThatThrownBy(() -> new MessageWithMissingRawField().hashCode())
                 .isInstanceOf(RuntimeException.class)
                 .hasMessageContaining("Field missing_")
                 .hasMessageContaining(MessageWithMissingRawField.class.getName());
+    }
+
+    public static final class MessageWithRawInt32Field extends GeneratedMessageLite<
+            MessageWithRawInt32Field,
+            MessageWithRawInt32Field.Builder> {
+        private static final MessageWithRawInt32Field DEFAULT_INSTANCE;
+        private static volatile Parser<MessageWithRawInt32Field> parser;
+
+        @SuppressWarnings("unused")
+        private int value_;
+
+        static {
+            MessageWithRawInt32Field instance = new MessageWithRawInt32Field();
+            DEFAULT_INSTANCE = instance;
+            registerDefaultInstance(MessageWithRawInt32Field.class, instance);
+        }
+
+        private MessageWithRawInt32Field() {
+        }
+
+        @Override
+        protected Object dynamicMethod(
+                MethodToInvoke method,
+                Object firstArgument,
+                Object secondArgument
+        ) {
+            switch (method) {
+                case NEW_MUTABLE_INSTANCE:
+                    return new MessageWithRawInt32Field();
+                case NEW_BUILDER:
+                    return new Builder();
+                case BUILD_MESSAGE_INFO:
+                    return newMessageInfo(
+                            DEFAULT_INSTANCE,
+                            rawMessageInfoForOneInt32Field(),
+                            new Object[] {"value_"}
+                    );
+                case GET_DEFAULT_INSTANCE:
+                    return DEFAULT_INSTANCE;
+                case GET_PARSER:
+                    Parser<MessageWithRawInt32Field> localParser = parser;
+                    if (localParser == null) {
+                        synchronized (MessageWithRawInt32Field.class) {
+                            localParser = parser;
+                            if (localParser == null) {
+                                localParser = new DefaultInstanceBasedParser<>(DEFAULT_INSTANCE);
+                                parser = localParser;
+                            }
+                        }
+                    }
+                    return localParser;
+                case GET_MEMOIZED_IS_INITIALIZED:
+                    return (byte) 1;
+                case SET_MEMOIZED_IS_INITIALIZED:
+                    return null;
+                default:
+                    throw new UnsupportedOperationException(
+                            "Dynamic operation is not needed for this coverage test"
+                    );
+            }
+        }
+
+        private static String rawMessageInfoForOneInt32Field() {
+            return "\u0000\u0001\u0000\u0000\u0001\u0001\u0001\u0000\u0000\u0000\u0001\u0004";
+        }
+
+        public static final class Builder extends GeneratedMessageLite.Builder<
+                MessageWithRawInt32Field,
+                Builder> {
+            private Builder() {
+                super(DEFAULT_INSTANCE);
+            }
+        }
     }
 
     public static final class MessageWithMissingRawField extends GeneratedMessageLite<
