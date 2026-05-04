64fbc8d42a0c8bc2f4f7eb969e035a9d112aaa6b
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 04:33:54 +0200

    Dynamic-access coverage for org.jboss.arquillian.core.impl.loadable.SecurityActions$3 (19/26)

---
+++ tests/src/org.jboss.arquillian.core/arquillian-core-impl-base/1.1.10.Final/src/test/java/org_jboss_arquillian_core/arquillian_core_impl_base/LoadableSecurityActionsAnonymous3Test.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_jboss_arquillian_core.arquillian_core_impl_base;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.Test;
+
+public class LoadableSecurityActionsAnonymous3Test {
+    private static final String SECURITY_ACTIONS_CLASS_NAME = "org.jboss.arquillian.core.impl.loadable.SecurityActions";
+
+    @Test
+    void setFieldValueUpdatesPrivateFieldByName() throws Exception {
+        DynamicAccessTarget target = new DynamicAccessTarget("initial");
+
+        invokeSetFieldValue(DynamicAccessTarget.class, target, "value", "updated");
+
+        assertThat(target.value()).isEqualTo("updated");
+    }
+
+    private static void invokeSetFieldValue(Class<?> source, Object target, String fieldName, Object value) throws Exception {
+        Method setFieldValue = securityActionsClass().getDeclaredMethod(
+                "setFieldValue", Class.class, Object.class, String.class, Object.class);
+        setFieldValue.setAccessible(true);
+        setFieldValue.invoke(null, source, target, fieldName, value);
+    }
+
+    private static Class<?> securityActionsClass() throws ClassNotFoundException {
+        return Class.forName(SECURITY_ACTIONS_CLASS_NAME);
+    }
+
+    private static final class DynamicAccessTarget {
+        private String value;
+
+        private DynamicAccessTarget(String value) {
+            this.value = value;
+        }
+
+        private String value() {
+            return value;
+        }
+    }
+}
