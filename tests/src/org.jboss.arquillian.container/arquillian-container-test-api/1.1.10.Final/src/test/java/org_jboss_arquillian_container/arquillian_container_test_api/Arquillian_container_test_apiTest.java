2a3f80e716dc9d43e164f424accffca238a2d10e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 05:21:57 +0200

    Scaffold org.jboss.arquillian.container:arquillian-container-test-api:1.1.10.Final

---
+++ tests/src/org.jboss.arquillian.container/arquillian-container-test-api/1.1.10.Final/src/test/java/org_jboss_arquillian_container/arquillian_container_test_api/Arquillian_container_test_apiTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_jboss_arquillian_container.arquillian_container_test_api;
+
+import org.junit.jupiter.api.Test;
+
+class Arquillian_container_test_apiTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
