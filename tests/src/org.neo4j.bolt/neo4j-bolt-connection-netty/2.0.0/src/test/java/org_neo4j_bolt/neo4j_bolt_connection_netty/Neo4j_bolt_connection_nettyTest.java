d1cdddd53dba968d47d754fc01ae186a03a55376
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 23:00:07 +0200

    Scaffold org.neo4j.bolt:neo4j-bolt-connection-netty:2.0.0

---
+++ tests/src/org.neo4j.bolt/neo4j-bolt-connection-netty/2.0.0/src/test/java/org_neo4j_bolt/neo4j_bolt_connection_netty/Neo4j_bolt_connection_nettyTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_neo4j_bolt.neo4j_bolt_connection_netty;
+
+import org.junit.jupiter.api.Test;
+
+class Neo4j_bolt_connection_nettyTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
