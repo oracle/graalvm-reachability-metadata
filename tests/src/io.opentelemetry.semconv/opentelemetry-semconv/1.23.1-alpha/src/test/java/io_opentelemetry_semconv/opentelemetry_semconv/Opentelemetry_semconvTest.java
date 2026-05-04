a1bf1bc879cdc2bceaacb80735c9a39e298de823
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 04:55:50 +0200

    Scaffold io.opentelemetry.semconv:opentelemetry-semconv:1.23.1-alpha

---
+++ tests/src/io.opentelemetry.semconv/opentelemetry-semconv/1.23.1-alpha/src/test/java/io_opentelemetry_semconv/opentelemetry_semconv/Opentelemetry_semconvTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_opentelemetry_semconv.opentelemetry_semconv;
+
+import org.junit.jupiter.api.Test;
+
+class Opentelemetry_semconvTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
