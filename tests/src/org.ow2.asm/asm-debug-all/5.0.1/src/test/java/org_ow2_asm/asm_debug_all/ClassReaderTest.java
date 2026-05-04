965c61732afbe647e3e5c21cc9d03c290c67afbc
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 05:45:52 +0200

    Dynamic-access coverage for org.objectweb.asm.ClassReader (7/7)

---
+++ tests/src/org.ow2.asm/asm-debug-all/5.0.1/src/test/java/org_ow2_asm/asm_debug_all/ClassReaderTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_ow2_asm.asm_debug_all;
+
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+import org.objectweb.asm.ClassReader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ClassReaderTest {
+    @Test
+    void readsClassFromSystemResourceByBinaryName() throws IOException {
+        ClassReader classReader = new ClassReader(ClassReader.class.getName());
+
+        assertThat(classReader.getClassName()).isEqualTo("org/objectweb/asm/ClassReader");
+        assertThat(classReader.getSuperName()).isEqualTo("java/lang/Object");
+    }
+}
