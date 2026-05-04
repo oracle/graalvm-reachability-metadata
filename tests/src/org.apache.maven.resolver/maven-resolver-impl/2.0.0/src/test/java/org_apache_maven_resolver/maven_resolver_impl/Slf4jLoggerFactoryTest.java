0faa3900e736477baf9193234df795a07cc999f9
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 00:20:35 +0200

    Prepare project for org.apache.maven.resolver:maven-resolver-impl:2.0.0

---
+++ tests/src/org.apache.maven.resolver/maven-resolver-impl/2.0.0/src/test/java/org_apache_maven_resolver/maven_resolver_impl/Slf4jLoggerFactoryTest.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven_resolver.maven_resolver_impl;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.eclipse.aether.internal.impl.slf4j.Slf4jLoggerFactory;
+import org.junit.jupiter.api.Test;
+
+public class Slf4jLoggerFactoryTest {
+    @Test
+    void reportsSlf4jAvailabilityDuringFactoryInitialization() {
+        boolean slf4jAvailable = Slf4jLoggerFactory.isSlf4jAvailable();
+
+        assertThat(slf4jAvailable).isTrue();
+        assertThat(new Slf4jLoggerFactory()).isNotNull();
+    }
+}
