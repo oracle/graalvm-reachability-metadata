ed2729dddbd078af1caff7e8d3f351b186fc6583
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 21:23:48 +0200

    Dynamic-access coverage for org.bson.types.StringRangeSet (25/25)

---
+++ tests/src/org.mongodb/bson/4.11.1/src/test/java/org_mongodb/bson/StringRangeSetTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_mongodb.bson;
+
+import org.bson.types.BasicBSONList;
+import org.junit.jupiter.api.Test;
+
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StringRangeSetTest {
+    @Test
+    void copiesKeysIntoNewTypedArrayWhenInputArrayIsTooSmall() {
+        final BasicBSONList list = new BasicBSONList();
+        list.put(0, "zero");
+        list.put(1, "one");
+        list.put(2, "two");
+        final Set<String> keys = list.keySet();
+        final String[] tooSmallArray = new String[0];
+
+        final String[] keyArray = keys.toArray(tooSmallArray);
+
+        assertThat(keyArray).isNotSameAs(tooSmallArray);
+        assertThat(keyArray).containsExactly("0", "1", "2");
+    }
+}
