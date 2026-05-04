552c6485666c0c71ce3d0741beed721e8402486c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 06:54:46 +0200

    Scaffold io.mockk:mockk-core-jvm:1.14.9

---
+++ tests/src/io.mockk/mockk-core-jvm/1.14.9/src/test/kotlin/io_mockk/mockk_core_jvm/Mockk_core_jvmTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_mockk.mockk_core_jvm
+
+import org.junit.jupiter.api.Test
+
+class Mockk_core_jvmTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
