518aaf5d1e7763bc4c0c8cd2356af4e50287fbd6
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 11:37:07 +0200

    Scaffold io.opencensus:opencensus-contrib-grpc-util:0.31.1

---
+++ tests/src/io.opencensus/opencensus-contrib-grpc-util/0.31.1/src/test/java/io_opencensus/opencensus_contrib_grpc_util/Opencensus_contrib_grpc_utilTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_opencensus.opencensus_contrib_grpc_util;
+
+import org.junit.jupiter.api.Test;
+
+class Opencensus_contrib_grpc_utilTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
