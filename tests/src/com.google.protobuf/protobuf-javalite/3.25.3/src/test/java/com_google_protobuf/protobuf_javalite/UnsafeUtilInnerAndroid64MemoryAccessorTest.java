efff0c54c4eff91f60a376af73d33e8defc2f9e6
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 22:57:47 +0200

    Dynamic-access coverage for com.google.protobuf.UnsafeUtil$Android64MemoryAccessor (39/41)

---
+++ tests/src/com.google.protobuf/protobuf-javalite/3.25.3/src/test/java/com_google_protobuf/protobuf_javalite/UnsafeUtilInnerAndroid64MemoryAccessorTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_google_protobuf.protobuf_javalite;
+
+import static java.lang.invoke.MethodType.methodType;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.google.protobuf.ByteString;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.reflect.Field;
+import org.junit.jupiter.api.Test;
+
+public class UnsafeUtilInnerAndroid64MemoryAccessorTest {
+    private static final String ANDROID_64_MEMORY_ACCESSOR_CLASS_NAME =
+            "com.google.protobuf.UnsafeUtil$Android64MemoryAccessor";
+    public static final Object STATIC_VALUE = new Object();
+
+    @Test
+    void android64MemoryAccessorReadsStaticFieldValue() throws Throwable {
+        ClassLoader protobufClassLoader = ByteString.class.getClassLoader();
+        Class<?> android64MemoryAccessorClass = Class.forName(
+                ANDROID_64_MEMORY_ACCESSOR_CLASS_NAME,
+                true,
+                protobufClassLoader
+        );
+        MethodHandles.Lookup android64MemoryAccessorLookup = MethodHandles.privateLookupIn(
+                android64MemoryAccessorClass,
+                MethodHandles.lookup()
+        );
+        MethodHandle constructor = android64MemoryAccessorLookup.findConstructor(
+                android64MemoryAccessorClass,
+                methodType(void.class, sun.misc.Unsafe.class)
+        );
+        Object memoryAccessor = constructor.invoke((sun.misc.Unsafe) null);
+        MethodHandle getStaticObject = android64MemoryAccessorLookup.findVirtual(
+                android64MemoryAccessorClass,
+                "getStaticObject",
+                methodType(Object.class, Field.class)
+        );
+
+        Object value = getStaticObject.invoke(
+                memoryAccessor,
+                UnsafeUtilInnerAndroid64MemoryAccessorTest.class.getDeclaredField("STATIC_VALUE")
+        );
+
+        assertThat(value).isSameAs(STATIC_VALUE);
+    }
+}
