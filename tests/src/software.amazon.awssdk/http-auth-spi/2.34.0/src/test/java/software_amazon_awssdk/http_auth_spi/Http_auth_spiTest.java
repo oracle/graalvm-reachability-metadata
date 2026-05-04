0c3d4baf3fb35eebee6d3259272057290948bbb0
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 15:18:17 +0200

    Scaffold software.amazon.awssdk:http-auth-spi:2.34.0

---
+++ tests/src/software.amazon.awssdk/http-auth-spi/2.34.0/src/test/java/software_amazon_awssdk/http_auth_spi/Http_auth_spiTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package software_amazon_awssdk.http_auth_spi;
+
+import org.junit.jupiter.api.Test;
+
+class Http_auth_spiTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
