ce2f1a089e10c20eb63985821023f1430cc873cc
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 09:28:25 +0200

    Dynamic-access coverage for org.apache.fontbox.cmap.CMapParser (1/1)

---
+++ tests/src/org.apache.pdfbox/fontbox/3.0.7/src/test/java/org_apache_pdfbox/fontbox/CMapParserTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_pdfbox.fontbox;
+
+import org.apache.fontbox.cmap.CMap;
+import org.apache.fontbox.cmap.CMapParser;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CMapParserTest {
+    @Test
+    void parsesPredefinedCMapFromPackagedResource() throws Exception {
+        CMap cmap = new CMapParser().parsePredefined("Identity-H");
+
+        assertThat(cmap.getName()).isEqualTo("Identity-H");
+        assertThat(cmap.getRegistry()).isEqualTo("Adobe");
+        assertThat(cmap.getOrdering()).isEqualTo("Identity");
+        assertThat(cmap.getSupplement()).isZero();
+        assertThat(cmap.getType()).isEqualTo(1);
+        assertThat(cmap.getWMode()).isZero();
+        assertThat(cmap.hasCIDMappings()).isTrue();
+        assertThat(cmap.toCID(new byte[] { 0x12, 0x34 })).isEqualTo(0x1234);
+    }
+}
