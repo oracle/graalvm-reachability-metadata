0ebe51e1f9c3c105dab3209923fbc0db80f5a381
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-05-04 22:08:01 +0200

    Scaffold org.jetbrains.kotlinx:kotlinx-serialization-json-okio-jvm:1.11.0

---
+++ tests/src/org.jetbrains.kotlinx/kotlinx-serialization-json-okio-jvm/1.11.0/src/test/kotlin/org_jetbrains_kotlinx/kotlinx_serialization_json_okio_jvm/Kotlinx_serialization_json_okio_jvmTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_jetbrains_kotlinx.kotlinx_serialization_json_okio_jvm
+
+import org.junit.jupiter.api.Test
+
+class Kotlinx_serialization_json_okio_jvmTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
