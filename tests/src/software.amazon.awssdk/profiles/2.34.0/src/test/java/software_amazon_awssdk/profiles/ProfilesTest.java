713e54e931992d4441577b21f93eb6b12e81a5d4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 09:00:16 +0200

    Scaffold software.amazon.awssdk:profiles:2.34.0

---
+++ tests/src/software.amazon.awssdk/profiles/2.34.0/src/test/java/software_amazon_awssdk/profiles/ProfilesTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package software_amazon_awssdk.profiles;
+
+import org.junit.jupiter.api.Test;
+
+class ProfilesTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
