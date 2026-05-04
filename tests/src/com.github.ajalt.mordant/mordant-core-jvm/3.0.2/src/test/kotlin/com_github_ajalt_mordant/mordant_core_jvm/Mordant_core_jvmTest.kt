a269ed5574365ce821450f31f286946b15cf6c0a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 16:24:51 +0200

    Scaffold com.github.ajalt.mordant:mordant-core-jvm:3.0.2

---
+++ tests/src/com.github.ajalt.mordant/mordant-core-jvm/3.0.2/src/test/kotlin/com_github_ajalt_mordant/mordant_core_jvm/Mordant_core_jvmTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_github_ajalt_mordant.mordant_core_jvm
+
+import org.junit.jupiter.api.Test
+
+class Mordant_core_jvmTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
