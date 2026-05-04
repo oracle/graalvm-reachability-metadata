ab891f9d0f4eb8edd9a514fb4e9a04067c5c4644
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 02:12:33 +0200

    Scaffold com.github.ghik:silencer-lib_2.13.16:1.7.19

---
+++ tests/src/com.github.ghik/silencer-lib_2.13.16/1.7.19/src/test/scala/com_github_ghik/silencer_lib_2_13_16/Silencer_lib_2_13_16Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_github_ghik.silencer_lib_2_13_16
+
+import org.junit.jupiter.api.Test
+
+class Silencer_lib_2_13_16Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
