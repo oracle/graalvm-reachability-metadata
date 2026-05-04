4f4d5f8215250db4a36b2692565f7cc90ab69146
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 03:29:46 +0200

    Scaffold com.github.jnr:jnr-x86asm:1.0.2

---
+++ tests/src/com.github.jnr/jnr-x86asm/1.0.2/src/test/java/com_github_jnr/jnr_x86asm/Jnr_x86asmTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_github_jnr.jnr_x86asm;
+
+import org.junit.jupiter.api.Test;
+
+class Jnr_x86asmTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
