e9fb8157593de5f2150d7580150365b9bae56b5c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-04 21:32:59 +0200

    Dynamic-access coverage for org.apache.ivy.util.Configurator (16/31)

--- tests/src/org.apache.ivy/ivy/2.5.0/src/test/java/org_apache_ivy/ivy/IvyTest.java
+++ tests/src/org.apache.ivy/ivy/2.5.0/src/test/java/org_apache_ivy/ivy/IvyTest.java
@@ -8,7 +8,7 @@ package org_apache_ivy.ivy;
 
 import org.junit.jupiter.api.Test;
 
-class IvyTest {
+public class IvyTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
