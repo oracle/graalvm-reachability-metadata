5fa9c2049dacc160a35170826a03ece1d806d9a1
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-05 19:41:43 +0200

    Scaffold org.scala-lang:scala-compiler:2.13.8

---
+++ tests/src/org.scala-lang/scala-compiler/2.13.8/src/test/scala/org_scala_lang/scala_compiler/Scala_compilerTest.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_scala_lang.scala_compiler
+
+import org.junit.jupiter.api.Test
+
+class Scala_compilerTest {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
