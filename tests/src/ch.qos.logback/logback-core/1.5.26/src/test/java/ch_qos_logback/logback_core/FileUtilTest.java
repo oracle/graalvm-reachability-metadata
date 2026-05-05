5e15e305247711ce89f29d84f1996f86bdf5af31
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 22:01:54 +0200

    Dynamic-access coverage for ch.qos.logback.core.util.FileUtil (37/37)

---
+++ tests/src/ch.qos.logback/logback-core/1.5.26/src/test/java/ch_qos_logback/logback_core/FileUtilTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package ch_qos_logback.logback_core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import ch.qos.logback.core.ContextBase;
+import ch.qos.logback.core.util.FileUtil;
+import org.junit.jupiter.api.Test;
+
+public class FileUtilTest {
+
+    private static final String RESOURCE_NAME = "version-util-test-dependencies.properties";
+
+    @Test
+    void readsResourceFromProvidedClassLoader() {
+        FileUtil fileUtil = new FileUtil(new ContextBase());
+
+        String resourceContents = fileUtil.resourceAsString(FileUtilTest.class.getClassLoader(), RESOURCE_NAME);
+
+        assertThat(resourceContents).isNotNull();
+        assertThat(resourceContents.trim()).isEqualTo("version-util-dependency=4.5.6-test");
+    }
+}
