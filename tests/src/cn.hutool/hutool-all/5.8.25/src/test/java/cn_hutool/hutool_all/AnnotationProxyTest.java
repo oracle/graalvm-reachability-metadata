2eb5a4744be01cc2959758bdb5bdddd31184895d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 18:14:24 +0200

    Dynamic-access coverage for cn.hutool.core.annotation.AnnotationProxy (74/86)

---
+++ tests/src/cn.hutool/hutool-all/5.8.25/src/test/java/cn_hutool/hutool_all/AnnotationProxyTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package cn_hutool.hutool_all;
+
+import cn.hutool.core.annotation.AnnotationProxy;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Deprecated(since = "5.8")
+public class AnnotationProxyTest {
+
+    @Test
+    void fallsBackToAnnotationProxyMethodInvocation() throws Throwable {
+        Deprecated annotation = AnnotationProxyTest.class.getAnnotation(Deprecated.class);
+        assertThat(annotation).isNotNull();
+        AnnotationProxy<Deprecated> proxy = new AnnotationProxy<>(annotation);
+
+        Method invokeMethod = AnnotationProxy.class.getMethod("invoke", Object.class, Method.class, Object[].class);
+        Method annotationTypeMethod = AnnotationProxy.class.getMethod("annotationType");
+        Object annotationType = proxy.invoke(proxy, invokeMethod, new Object[] {proxy, annotationTypeMethod, new Object[0]});
+
+        assertThat(annotationType).isEqualTo(Deprecated.class);
+    }
+}
