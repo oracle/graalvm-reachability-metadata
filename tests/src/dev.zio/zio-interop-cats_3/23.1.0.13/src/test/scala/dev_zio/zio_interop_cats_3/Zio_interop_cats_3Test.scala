219ff25a7a04e933bc7e60c481d32a0aa91b7645
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-05-05 18:56:36 +0200

    Scaffold dev.zio:zio-interop-cats_3:23.1.0.13

---
+++ tests/src/dev.zio/zio-interop-cats_3/23.1.0.13/src/test/scala/dev_zio/zio_interop_cats_3/Zio_interop_cats_3Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package dev_zio.zio_interop_cats_3
+
+import org.junit.jupiter.api.Test
+
+class Zio_interop_cats_3Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
