6ed7092af867efd915c8ff39e92f71622c4c4236
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-05-05 19:54:39 +0200

    Scaffold com.chuusai:shapeless_2.13:2.3.13

---
+++ tests/src/com.chuusai/shapeless_2.13/2.3.13/src/test/scala/com_chuusai/shapeless_2_13/Shapeless_2_13Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_chuusai.shapeless_2_13
+
+import org.junit.jupiter.api.Test
+
+class Shapeless_2_13Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
