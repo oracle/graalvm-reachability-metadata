4e4a5d0908647c251d5ab65607938f9c53d84dc4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 12:51:23 +0200

    Scaffold cn.hutool:hutool-all:5.8.25

---
+++ tests/src/cn.hutool/hutool-all/5.8.25/src/test/java/cn_hutool/hutool_all/Hutool_allTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package cn_hutool.hutool_all;
+
+import org.junit.jupiter.api.Test;
+
+class Hutool_allTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
