72da6a3d10e61feb1d62778ec89ac13a8afc1792
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 15:01:32 +0200

    Scaffold org.bouncycastle:bc-fips:2.1.2

---
+++ tests/src/org.bouncycastle/bc-fips/2.1.2/src/test/java/org_bouncycastle/bc_fips/Bc_fipsTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_bouncycastle.bc_fips;
+
+import org.junit.jupiter.api.Test;
+
+class Bc_fipsTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
