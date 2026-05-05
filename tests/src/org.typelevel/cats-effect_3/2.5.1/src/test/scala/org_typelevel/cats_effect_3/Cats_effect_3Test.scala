5f62945bcb435774a01b8e32a36763a4cb3bc4c2
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 22:57:00 +0200

    Scaffold org.typelevel:cats-effect_3:2.5.1

---
+++ tests/src/org.typelevel/cats-effect_3/2.5.1/src/test/scala/org_typelevel/cats_effect_3/Cats_effect_3Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_typelevel.cats_effect_3
+
+import org.junit.jupiter.api.Test
+
+class Cats_effect_3Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
