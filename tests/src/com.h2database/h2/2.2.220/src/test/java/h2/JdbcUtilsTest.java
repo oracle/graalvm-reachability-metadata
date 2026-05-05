e26137638fd762ea0d178f98df34acf713d384c8
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 04:04:32 +0200

    Partial dynamic-access coverage for org.h2.util.JdbcUtils (10/12)

---
+++ tests/src/com.h2database/h2/2.2.220/src/test/java/h2/JdbcUtilsTest.java
@@ -0,0 +1,125 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package h2;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import javax.naming.InitialContext;
+import javax.naming.Name;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+import org.h2.api.JavaObjectSerializer;
+import org.h2.jdbcx.JdbcDataSource;
+import org.h2.util.JdbcUtils;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class JdbcUtilsTest {
+    private static final String JNDI_URL = "java:comp/env/jdbc/h2-jdbc-utils";
+
+    @AfterEach
+    void resetSerializer() {
+        JdbcUtils.serializer = null;
+    }
+
+    @Test
+    void loadsUserClassWithDefaultClassLookup() {
+        Class<String> type = JdbcUtils.loadUserClass(String.class.getName());
+
+        assertThat(type).isSameAs(String.class);
+    }
+
+    @Test
+    void opensConnectionWithExplicitJdbcDriver() throws SQLException {
+        try (Connection connection = JdbcUtils.getConnection(
+                "org.h2.Driver", "jdbc:h2:mem:jdbc-utils-explicit-driver", "sa", "")) {
+            assertThat(connection.isValid(1)).isTrue();
+        }
+    }
+
+    @Test
+    void opensConnectionWithJndiContextClass() throws SQLException {
+        try (Connection connection = JdbcUtils.getConnection(SimpleContext.class.getName(), JNDI_URL, "sa", "")) {
+            assertThat(connection.isValid(1)).isTrue();
+        }
+    }
+
+    @Test
+    void readsConnectionAndDatabaseMetadata() throws SQLException {
+        try (Connection connection = JdbcUtils.getConnection(
+                "org.h2.Driver", "jdbc:h2:mem:jdbc-utils-metadata", "sa", "");
+                ResultSet resultSet = JdbcUtils.getMetaResultSet(connection, "@info")) {
+            boolean productNameFound = false;
+            while (resultSet.next()) {
+                if ("meta.getDatabaseProductName".equals(resultSet.getString(1))) {
+                    productNameFound = true;
+                    assertThat(resultSet.getString(2)).contains("H2");
+                }
+            }
+            assertThat(productNameFound).isTrue();
+        }
+    }
+
+    @Test
+    void serializesWithDefaultJavaSerializationWhenCustomSerializerIsCleared() throws Exception {
+        JdbcUtils.serializer = null;
+
+        byte[] serialized = JdbcUtils.serialize("round-trip", null);
+        Object deserialized = JdbcUtils.deserialize(serialized, null);
+
+        assertThat(deserialized).isEqualTo("round-trip");
+    }
+
+    public static class CountingJavaObjectSerializer implements JavaObjectSerializer {
+        @Override
+        public byte[] serialize(Object obj) throws Exception {
+            ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+            try (ObjectOutputStream output = new ObjectOutputStream(bytes)) {
+                output.writeObject(obj);
+            }
+            return bytes.toByteArray();
+        }
+
+        @Override
+        public Object deserialize(byte[] bytes) throws Exception {
+            try (ObjectInputStream input = new ObjectInputStream(new ByteArrayInputStream(bytes))) {
+                return input.readObject();
+            }
+        }
+    }
+
+    public static class SimpleContext extends InitialContext {
+        public SimpleContext() throws NamingException {
+            super(false);
+        }
+
+        @Override
+        public Object lookup(String name) throws NamingException {
+            if (!JNDI_URL.equals(name)) {
+                throw new NamingException("Unexpected JNDI name: " + name);
+            }
+            JdbcDataSource dataSource = new JdbcDataSource();
+            dataSource.setURL("jdbc:h2:mem:jdbc-utils-jndi");
+            dataSource.setUser("sa");
+            dataSource.setPassword("");
+            return dataSource;
+        }
+
+        @Override
+        public Object lookup(Name name) throws NamingException {
+            return lookup(name.toString());
+        }
+    }
+}
