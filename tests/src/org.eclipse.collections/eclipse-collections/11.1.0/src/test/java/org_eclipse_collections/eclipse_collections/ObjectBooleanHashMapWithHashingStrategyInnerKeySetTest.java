72d0b7c72a8d5ef6468d3fa51b6bd40c1f3afc3c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 21:04:41 +0200

    Dynamic-access coverage for org.eclipse.collections.impl.map.mutable.primitive.ObjectBooleanHashMapWithHashingStrategy$KeySet (31/52)

---
+++ tests/src/org.eclipse.collections/eclipse-collections/11.1.0/src/test/java/org_eclipse_collections/eclipse_collections/ObjectBooleanHashMapWithHashingStrategyInnerKeySetTest.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_eclipse_collections.eclipse_collections;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Set;
+
+import org.eclipse.collections.impl.block.factory.HashingStrategies;
+import org.eclipse.collections.impl.map.mutable.primitive.ObjectBooleanHashMapWithHashingStrategy;
+import org.junit.jupiter.api.Test;
+
+public class ObjectBooleanHashMapWithHashingStrategyInnerKeySetTest {
+    @Test
+    void toArrayCreatesTypedKeyArrayWhenSuppliedArrayIsTooSmall() {
+        final ObjectBooleanHashMapWithHashingStrategy<String> map = ObjectBooleanHashMapWithHashingStrategy.newWithKeysValues(
+                HashingStrategies.defaultStrategy(),
+                "alpha", true,
+                "bravo", false,
+                "charlie", true);
+        final Set<String> keySet = map.keySet();
+
+        final String[] keys = keySet.toArray(new String[0]);
+
+        assertThat(keys)
+                .isExactlyInstanceOf(String[].class)
+                .containsExactlyInAnyOrder("alpha", "bravo", "charlie");
+    }
+}
