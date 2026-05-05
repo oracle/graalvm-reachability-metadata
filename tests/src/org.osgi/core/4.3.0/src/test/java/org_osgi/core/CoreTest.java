5cba60f701cdd895bba9a1bdd080ef3a3dc77545
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 12:47:01 +0200

    Dynamic-access coverage for org.osgi.framework.FrameworkUtil$FilterImpl (5/11)

--- tests/src/org.osgi/core/4.3.0/src/test/java/org_osgi/core/CoreTest.java
+++ tests/src/org.osgi/core/4.3.0/src/test/java/org_osgi/core/CoreTest.java
@@ -8,7 +8,7 @@ package org_osgi.core;
 
 import org.junit.jupiter.api.Test;
 
-class CoreTest {
+public class CoreTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
