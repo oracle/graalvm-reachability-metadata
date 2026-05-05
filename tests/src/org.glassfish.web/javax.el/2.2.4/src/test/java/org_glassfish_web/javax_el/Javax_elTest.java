1c71c91dd51cec64a0c5d65fc71a0441ac5a7310
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 19:53:05 +0200

    Dynamic-access coverage for com.sun.el.util.ReflectionUtil (6/11)

--- tests/src/org.glassfish.web/javax.el/2.2.4/src/test/java/org_glassfish_web/javax_el/Javax_elTest.java
+++ tests/src/org.glassfish.web/javax.el/2.2.4/src/test/java/org_glassfish_web/javax_el/Javax_elTest.java
@@ -8,7 +8,7 @@ package org_glassfish_web.javax_el;
 
 import org.junit.jupiter.api.Test;
 
-class Javax_elTest {
+public class Javax_elTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
