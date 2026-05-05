103ed8f04cd067a165b1519d7e10b345d08b3b0c
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-06 00:51:44 +0200

    Add OkHttp platform dynamic access tests

---
+++ tests/src/com.squareup.okhttp3/okhttp/3.6.0/src/test/java/com_squareup_okhttp3/okhttp/JdkWithJettyBootPlatformTest.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_squareup_okhttp3.okhttp;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Method;
+import java.util.Arrays;
+import javax.net.ssl.SSLSocket;
+import okhttp3.Protocol;
+import okhttp3.internal.platform.Platform;
+import org.eclipse.jetty.alpn.ALPN;
+import org.junit.jupiter.api.Test;
+
+public class JdkWithJettyBootPlatformTest {
+    @Test
+    void jettyBootPlatformConfiguresReadsAndRemovesAlpnProvider() throws Exception {
+        Platform platform = newJettyBootPlatform();
+        RecordingSslSocket socket = new RecordingSslSocket();
+
+        platform.configureTlsExtensions(socket, "example.com",
+                Arrays.asList(Protocol.HTTP_2, Protocol.HTTP_1_1));
+        ALPN.Provider provider = ALPN.get(socket);
+        ((ALPN.ClientProvider) provider).protocolSelected("h2");
+        String selectedProtocol = platform.getSelectedProtocol(socket);
+        platform.afterHandshake(socket);
+
+        assertThat(provider).isNotNull();
+        assertThat(selectedProtocol).isEqualTo("h2");
+        assertThat(ALPN.get(socket)).isNull();
+    }
+
+    private static Platform newJettyBootPlatform() throws Exception {
+        Class<?> platformClass = Class.forName(
+                "okhttp3.internal.platform.JdkWithJettyBootPlatform");
+        Constructor<?> constructor = platformClass.getDeclaredConstructor(Method.class,
+                Method.class, Method.class, Class.class, Class.class);
+        constructor.setAccessible(true);
+        return (Platform) constructor.newInstance(
+                ALPN.class.getMethod("put", SSLSocket.class, ALPN.Provider.class),
+                ALPN.class.getMethod("get", SSLSocket.class),
+                ALPN.class.getMethod("remove", SSLSocket.class),
+                ALPN.ClientProvider.class,
+                ALPN.ServerProvider.class);
+    }
+}
