af306f3a79c525b17887df8e5637219dfa41e26f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-05 10:56:30 +0200

    Partial dynamic-access coverage for org.h2.util.MemoryUnmapper (1/6)

---
+++ tests/src/com.h2database/h2/2.1.210/src/test/java/h2/MemoryUnmapperTest.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package h2;
+
+import org.h2.util.MemoryUnmapper;
+import org.junit.jupiter.api.Test;
+
+import java.nio.ByteBuffer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class MemoryUnmapperTest {
+    @Test
+    void returnsFalseForHeapByteBufferWhenCleanerHackIsEnabled() {
+        assertThat(System.getProperty("h2.nioCleanerHack")).isEqualTo("true");
+
+        ByteBuffer heapBuffer = ByteBuffer.allocate(16);
+        heapBuffer.putInt(42);
+
+        boolean unmapped = MemoryUnmapper.unmap(heapBuffer);
+
+        assertThat(unmapped).isFalse();
+    }
+}
