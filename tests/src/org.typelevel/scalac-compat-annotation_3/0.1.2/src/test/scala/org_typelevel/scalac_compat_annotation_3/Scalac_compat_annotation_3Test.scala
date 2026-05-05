b24702f1e6c5f0ae7c7340e4e821525a22179f32
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-05-05 18:56:32 +0200

    Scaffold org.typelevel:scalac-compat-annotation_3:0.1.2

---
+++ tests/src/org.typelevel/scalac-compat-annotation_3/0.1.2/src/test/scala/org_typelevel/scalac_compat_annotation_3/Scalac_compat_annotation_3Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_typelevel.scalac_compat_annotation_3
+
+import org.junit.jupiter.api.Test
+
+class Scalac_compat_annotation_3Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
