6fe096a84f1d28a0b929398533c6eada9265a1d6
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-05-06 00:37:11 +0200

    Scaffold org.tpolecat:doobie-hikari_3:0.13.4

---
+++ tests/src/org.tpolecat/doobie-hikari_3/0.13.4/src/test/scala/org_tpolecat/doobie_hikari_3/Doobie_hikari_3Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_tpolecat.doobie_hikari_3
+
+import org.junit.jupiter.api.Test
+
+class Doobie_hikari_3Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
