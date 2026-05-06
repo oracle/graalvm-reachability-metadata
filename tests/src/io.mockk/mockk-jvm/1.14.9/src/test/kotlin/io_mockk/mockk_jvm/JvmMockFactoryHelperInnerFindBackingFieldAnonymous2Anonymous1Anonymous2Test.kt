1c3e49bbd6f4f3b9899e54cbaf020553a29d06b0
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 08:44:41 +0200

    Dynamic-access coverage for io.mockk.impl.instantiation.JvmMockFactoryHelper$findBackingField$2$1$2 (14/16)

---
+++ tests/src/io.mockk/mockk-jvm/1.14.9/src/test/kotlin/io_mockk/mockk_jvm/JvmMockFactoryHelperInnerFindBackingFieldAnonymous2Anonymous1Anonymous2Test.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_mockk.mockk_jvm
+
+import io.mockk.every
+import io.mockk.spyk
+import org.assertj.core.api.Assertions.assertThat
+import org.graalvm.internal.tck.NativeImageSupport
+import org.junit.jupiter.api.Test
+
+public class JvmMockFactoryHelperInnerFindBackingFieldAnonymous2Anonymous1Anonymous2Test {
+    @Test
+    fun answerCanWriteThePropertyBackingFieldFromASpy(): Unit {
+        try {
+            val counter: MutableBackingFieldCounter = spyk(MutableBackingFieldCounter(0))
+
+            every { counter.value = any() } propertyType Int::class answers {
+                fieldValue = 42
+            }
+
+            counter.value = 1
+
+            assertThat(counter.value).isEqualTo(42)
+        } catch (error: Error) {
+            if (!NativeImageSupport.isUnsupportedFeatureError(error)) {
+                throw error
+            }
+        }
+    }
+}
+
+private class MutableBackingFieldCounter(
+    var value: Int,
+)
