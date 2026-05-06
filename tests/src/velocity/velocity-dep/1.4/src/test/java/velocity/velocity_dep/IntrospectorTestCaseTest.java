d6a85067ea4e698687ef50a5ac2410dd513b3726
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 19:10:43 +0200

    Partial dynamic-access coverage for org.apache.velocity.test.IntrospectorTestCase (8/9)

---
+++ tests/src/velocity/velocity-dep/1.4/src/test/java/velocity/velocity_dep/IntrospectorTestCaseTest.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package velocity.velocity_dep;
+
+import org.apache.velocity.test.IntrospectorTestCase;
+import org.junit.jupiter.api.Test;
+
+public class IntrospectorTestCaseTest {
+    @Test
+    void resolvesPrimitiveMethodsThroughVelocityIntrospector() {
+        IntrospectorTestCase testCase = new IntrospectorTestCase("primitiveMethods");
+
+        testCase.runTest();
+    }
+}
