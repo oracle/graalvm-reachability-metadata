0f8c3d38aa3100cf81bfbcc6d192083761958232
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 12:29:25 +0200

    Partial dynamic-access coverage for com.typesafe.sslconfig.ssl.Algorithms$ (3/5)

--- tests/src/com.typesafe/ssl-config-core_2.13/0.4.3/src/test/scala/com_typesafe/ssl_config_core_2_13/AlgorithmsTest.scala
+++ tests/src/com.typesafe/ssl-config-core_2.13/0.4.3/src/test/scala/com_typesafe/ssl_config_core_2_13/AlgorithmsTest.scala
@@ -6,7 +6,6 @@
  */
 package com_typesafe.ssl_config_core_2_13
 
-import java.lang.IllegalAccessException
 import java.security.Key
 import java.security.KeyPairGenerator
 import java.security.interfaces.ECPublicKey
@@ -18,25 +17,27 @@ import org.junit.jupiter.api.Test
 
 class AlgorithmsTest {
   @Test
-  def translatesEcPublicKeyThroughJdkEcKeyFactory(): Unit = {
+  def translatesEcPublicKeyThroughContextClassLoaderEcKeyFactory(): Unit = {
     val keyPairGenerator: KeyPairGenerator = KeyPairGenerator.getInstance("EC")
     keyPairGenerator.initialize(new ECGenParameterSpec("secp256r1"))
     val publicKey: ECPublicKey = keyPairGenerator.generateKeyPair().getPublic.asInstanceOf[ECPublicKey]
 
     val originalClassLoader: ClassLoader = Thread.currentThread().getContextClassLoader
-    Thread.currentThread().setContextClassLoader(getClass.getClassLoader)
+    Thread.currentThread().setContextClassLoader(new ClassLoader(getClass.getClassLoader) {
+      override def loadClass(name: String): Class[?] =
+        if (name == "sun.security.ec.ECKeyFactory") {
+          classOf[AlgorithmsTestEcKeyFactory]
+        } else {
+          super.loadClass(name)
+        }
+    })
     try {
-      try {
-        val translatedKey: Key = Algorithms.translateECKey(publicKey)
-        val keySize: Option[Int] = Algorithms.keySize(translatedKey)
+      val translatedKey: Key = Algorithms.translateECKey(publicKey)
+      val keySize: Option[Int] = Algorithms.keySize(translatedKey)
 
-        assertThat(translatedKey).isInstanceOf(classOf[ECPublicKey])
-        assertThat(keySize.isDefined).isTrue()
-        assertThat(keySize.get).isEqualTo(publicKey.getParams.getOrder.bitLength())
-      } catch {
-        case e: IllegalAccessException =>
-          assertThat(e.getMessage).contains("sun.security.ec.ECKeyFactory")
-      }
+      assertThat(translatedKey).isInstanceOf(classOf[ECPublicKey])
+      assertThat(keySize.isDefined).isTrue()
+      assertThat(keySize.get).isEqualTo(publicKey.getParams.getOrder.bitLength())
     } finally {
       Thread.currentThread().setContextClassLoader(originalClassLoader)
     }
