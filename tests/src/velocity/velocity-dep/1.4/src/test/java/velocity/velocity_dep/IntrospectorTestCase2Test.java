d4c98c0ce954a36b7d3a8913cce58f2fc4fbda61
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 20:51:57 +0200

    Dynamic-access coverage for org.apache.log.output.net.SocketOutputTarget (46/58)

--- tests/src/velocity/velocity-dep/1.4/src/test/java/velocity/velocity_dep/IntrospectorTestCase2Test.java
+++ tests/src/velocity/velocity-dep/1.4/src/test/java/velocity/velocity_dep/IntrospectorTestCase2Test.java
@@ -6,14 +6,38 @@
  */
 package velocity.velocity_dep;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.apache.velocity.test.IntrospectorTestCase2;
 import org.junit.jupiter.api.Test;
 
 public class IntrospectorTestCase2Test {
     @Test
-    void resolvesMostSpecificOverloadAndRejectsAmbiguousMatch() {
+    void resolvesMostSpecificOverloadAndRejectsAmbiguousMatch() throws Exception {
+        clearClassLiteralCache("class$org$apache$velocity$test$IntrospectorTestCase2$Tester");
+        clearClassLiteralCache("class$org$apache$velocity$test$IntrospectorTestCase2$Tester2");
         final IntrospectorTestCase2 testCase = new IntrospectorTestCase2("bestMatch");
 
         testCase.runTest();
+    }
+
+    @Test
+    void resolvesClassThroughCompilerGeneratedClassLiteralHelper() throws Exception {
+        final Method classResolver = IntrospectorTestCase2.class.getDeclaredMethod("class$", String.class);
+        classResolver.setAccessible(true);
+
+        final Object resolvedClass = classResolver.invoke(
+                null, "org.apache.velocity.test.IntrospectorTestCase2$Tester");
+
+        assertThat(resolvedClass).isSameAs(IntrospectorTestCase2.Tester.class);
+    }
+
+    private static void clearClassLiteralCache(String fieldName) throws Exception {
+        final Field cacheField = IntrospectorTestCase2.class.getDeclaredField(fieldName);
+        cacheField.setAccessible(true);
+        cacheField.set(null, null);
     }
 }
