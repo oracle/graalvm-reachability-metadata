4c6bfaab3c481231d5044e8469173423f50bcc7b
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 19:33:08 +0200

    Dynamic-access coverage for org.apache.velocity.test.ClassloaderChangeTest (33/58)

--- tests/src/velocity/velocity-dep/1.4/src/test/java/velocity/velocity_dep/IntrospectorTestCase3Test.java
+++ tests/src/velocity/velocity-dep/1.4/src/test/java/velocity/velocity_dep/IntrospectorTestCase3Test.java
@@ -8,12 +8,17 @@ package velocity.velocity_dep;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.apache.velocity.test.IntrospectorTestCase3;
 import org.junit.jupiter.api.Test;
 
 public class IntrospectorTestCase3Test {
     @Test
-    void createsJUnit3SuiteForUpstreamTestCase() {
+    void createsJUnit3SuiteForUpstreamTestCase() throws Exception {
+        clearClassLiteralCache("class$org$apache$velocity$test$IntrospectorTestCase3");
+
         final int testCaseCount = IntrospectorTestCase3.suite().countTestCases();
 
         assertThat(testCaseCount).isEqualTo(1);
@@ -21,8 +26,26 @@ public class IntrospectorTestCase3Test {
 
     @Test
     void resolvesPrimitiveAndNullOverloadsThroughVelocityIntrospector() throws Exception {
+        clearClassLiteralCache("class$org$apache$velocity$test$IntrospectorTestCase3$MethodProvider");
+
         final IntrospectorTestCase3 testCase = new IntrospectorTestCase3("testSimple");
 
         testCase.testSimple();
+    }
+
+    @Test
+    void resolvesClassThroughCompilerGeneratedClassLiteralHelper() throws Exception {
+        final Method classResolver = IntrospectorTestCase3.class.getDeclaredMethod("class$", String.class);
+        classResolver.setAccessible(true);
+
+        final Object resolvedClass = classResolver.invoke(null, IntrospectorTestCase3.class.getName());
+
+        assertThat(resolvedClass).isSameAs(IntrospectorTestCase3.class);
+    }
+
+    private static void clearClassLiteralCache(String fieldName) throws Exception {
+        final Field cacheField = IntrospectorTestCase3.class.getDeclaredField(fieldName);
+        cacheField.setAccessible(true);
+        cacheField.set(null, null);
     }
 }
