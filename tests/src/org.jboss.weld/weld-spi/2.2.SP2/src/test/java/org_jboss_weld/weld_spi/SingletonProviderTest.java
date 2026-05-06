21a1190af77cd3e4ee60a5b73b96d6feacfb4008
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 03:49:40 +0200

    Preserve last passing candidate for org.jboss.weld:weld-spi:2.2.SP2

--- tests/src/org.jboss.weld/weld-spi/2.2.SP2/src/test/java/org_jboss_weld/weld_spi/SingletonProviderTest.java
+++ tests/src/org.jboss.weld/weld-spi/2.2.SP2/src/test/java/org_jboss_weld/weld_spi/SingletonProviderTest.java
@@ -8,7 +8,7 @@ package org_jboss_weld.weld_spi;
 
 import org.jboss.weld.bootstrap.api.Singleton;
 import org.jboss.weld.bootstrap.api.SingletonProvider;
-import org.jboss.weld.bootstrap.api.helpers.IsolatedStaticSingletonProvider;
+import org.jboss.weld.bootstrap.api.helpers.RegistrySingletonProvider;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -30,16 +30,16 @@ public class SingletonProviderTest {
     }
 
     @Test
-    void instanceInitializesDefaultIsolatedStaticProvider() {
+    void instanceInitializesDefaultRegistryProvider() {
         SingletonProvider provider = SingletonProvider.instance();
 
-        assertThat(provider).isInstanceOf(IsolatedStaticSingletonProvider.class);
+        assertThat(provider).isInstanceOf(RegistrySingletonProvider.class);
 
         Singleton<String> singleton = provider.create(String.class);
         assertThat(singleton.isSet(SINGLETON_ID)).isFalse();
         assertThatThrownBy(() -> singleton.get(SINGLETON_ID))
                 .isInstanceOf(IllegalStateException.class)
-                .hasMessageContaining("Singleton is not set");
+                .hasMessageContaining("Singleton not set for " + SINGLETON_ID);
 
         singleton.set(SINGLETON_ID, "stored value");
         assertThat(singleton.isSet(SINGLETON_ID)).isTrue();
