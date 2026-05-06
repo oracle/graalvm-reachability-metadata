353cdb5fb8a0f8415e6e9ce99d43cd15eb2acf4b
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 15:16:37 +0200

    Partial dynamic-access coverage for javax.ws.rs.client.FactoryFinder (5/6)

---
+++ tests/src/javax.ws.rs/javax.ws.rs-api/2.0.1/src/test/java/javax_ws_rs/javax_ws_rs_api/FactoryFinderTest.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package javax_ws_rs.javax_ws_rs_api;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import javax.ws.rs.client.ClientBuilder;
+
+import org.junit.jupiter.api.Test;
+
+public class FactoryFinderTest {
+    @Test
+    void newBuilderUsesContextClassLoaderAndSystemPropertyProvider() {
+        withProviderProperty(() -> {
+            Thread currentThread = Thread.currentThread();
+            ClassLoader previousContextClassLoader = currentThread.getContextClassLoader();
+            currentThread.setContextClassLoader(FactoryFinderTest.class.getClassLoader());
+            try {
+                assertAbstractClientBuilderProviderFailsToInstantiate();
+            } finally {
+                currentThread.setContextClassLoader(previousContextClassLoader);
+            }
+        });
+    }
+
+    @Test
+    void newBuilderFallsBackToCurrentClassLoaderWhenContextClassLoaderRejectsProvider() {
+        withProviderProperty(() -> {
+            Thread currentThread = Thread.currentThread();
+            ClassLoader previousContextClassLoader = currentThread.getContextClassLoader();
+            currentThread.setContextClassLoader(new RejectingProviderClassLoader(previousContextClassLoader));
+            try {
+                assertAbstractClientBuilderProviderFailsToInstantiate();
+            } finally {
+                currentThread.setContextClassLoader(previousContextClassLoader);
+            }
+        });
+    }
+
+    @Test
+    void newBuilderUsesSystemClassLoaderWhenContextClassLoaderIsUnavailable() {
+        withProviderProperty(() -> {
+            Thread currentThread = Thread.currentThread();
+            ClassLoader previousContextClassLoader = currentThread.getContextClassLoader();
+            currentThread.setContextClassLoader(null);
+            try {
+                assertAbstractClientBuilderProviderFailsToInstantiate();
+            } finally {
+                currentThread.setContextClassLoader(previousContextClassLoader);
+            }
+        });
+    }
+
+    private static void assertAbstractClientBuilderProviderFailsToInstantiate() {
+        assertThatThrownBy(ClientBuilder::newBuilder)
+                .isInstanceOf(RuntimeException.class)
+                .hasCauseInstanceOf(ClassNotFoundException.class)
+                .hasStackTraceContaining(
+                        "Provider javax.ws.rs.client.ClientBuilder could not be instantiated");
+    }
+
+    private static synchronized void withProviderProperty(Runnable action) {
+        String previousProvider = System.getProperty(ClientBuilder.JAXRS_DEFAULT_CLIENT_BUILDER_PROPERTY);
+        System.setProperty(ClientBuilder.JAXRS_DEFAULT_CLIENT_BUILDER_PROPERTY, ClientBuilder.class.getName());
+        try {
+            action.run();
+        } finally {
+            if (previousProvider == null) {
+                System.clearProperty(ClientBuilder.JAXRS_DEFAULT_CLIENT_BUILDER_PROPERTY);
+            } else {
+                System.setProperty(ClientBuilder.JAXRS_DEFAULT_CLIENT_BUILDER_PROPERTY, previousProvider);
+            }
+        }
+    }
+
+    private static final class RejectingProviderClassLoader extends ClassLoader {
+        private RejectingProviderClassLoader(ClassLoader parent) {
+            super(parent);
+        }
+
+        @Override
+        public Class<?> loadClass(String name) throws ClassNotFoundException {
+            if (ClientBuilder.class.getName().equals(name)) {
+                throw new ClassNotFoundException(name);
+            }
+            return super.loadClass(name);
+        }
+    }
+}
