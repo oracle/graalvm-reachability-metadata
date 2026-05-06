f64c4e8f83ce02ba07c632237184bbeea8be5fea
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 13:10:00 +0200

    Update generated library support for org.apache.pekko:pekko-parsing_2.13:1.3.0

--- tests/src/org.apache.pekko/pekko-parsing_2.13/1.3.0/src/test/scala/org_apache_pekko/pekko_parsing_2_13/Pekko_parsing_2_13Test.scala
+++ tests/src/org.apache.pekko/pekko-parsing_2.13/1.3.0/src/test/scala/org_apache_pekko/pekko_parsing_2_13/Pekko_parsing_2_13Test.scala
@@ -8,118 +8,12 @@ package org_apache_pekko.pekko_parsing_2_13
 
 import org.apache.pekko.http.ccompat.{pre213macro, since213macro}
 import org.assertj.core.api.Assertions.assertThat
-import org.graalvm.internal.tck.NativeImageSupport
 import org.junit.jupiter.api.Test
-
-import java.nio.charset.StandardCharsets
-import java.nio.file.{Files, Path}
-import scala.tools.nsc.{Global, Settings}
-import scala.tools.nsc.reporters.StoreReporter
 
 class PekkoParsingCompatibilityAnnotationsTest {
   @Test
   def exposesMacroImplementationCompanions(): Unit = {
     assertThat(since213macro).isNotNull
     assertThat(pre213macro).isNotNull
-  }
-
-  @Test
-  def since213AnnotationKeepsAnnotatedMemberOnScala213(): Unit = withUnsupportedFeatureHandling {
-    val reporter: StoreReporter = compileScalaSource(
-      "CompatibilitySample.scala",
-      """
-        |import org.apache.pekko.http.ccompat.{pre213, since213}
-        |
-        |object CompatibilitySample {
-        |  @since213 def since213Only: String = "available on Scala 2.13"
-        |  @pre213 def pre213Only: String = "removed on Scala 2.13"
-        |}
-        |
-        |object CompatibilityProbe {
-        |  val selected: String = CompatibilitySample.since213Only
-        |}
-        |""".stripMargin
-    )
-
-    assertThat(reporter.hasErrors).isFalse
-  }
-
-  @Test
-  def pre213AnnotationRemovesAnnotatedMemberOnScala213(): Unit = withUnsupportedFeatureHandling {
-    val reporter: StoreReporter = compileScalaSource(
-      "CompatibilitySample.scala",
-      """
-        |import org.apache.pekko.http.ccompat.{pre213, since213}
-        |
-        |object CompatibilitySample {
-        |  @since213 def since213Only: String = "available on Scala 2.13"
-        |  @pre213 def pre213Only: String = "removed on Scala 2.13"
-        |}
-        |
-        |object CompatibilityProbe {
-        |  val removed: String = CompatibilitySample.pre213Only
-        |}
-        |""".stripMargin
-    )
-
-    assertThat(reporter.hasErrors).isTrue
-    assertThat(reporter.infos.exists { info: StoreReporter#Info =>
-      info.msg.contains("pre213Only") && info.msg.contains("is not a member")
-    }).isTrue
-  }
-
-  @Test
-  def compatibilityAnnotationsRejectMultipleAnnottees(): Unit = withUnsupportedFeatureHandling {
-    val reporter: StoreReporter = compileScalaSource(
-      "CompatibilityCompanionSample.scala",
-      """
-        |import org.apache.pekko.http.ccompat.since213
-        |
-        |@since213
-        |final class CompatibilityCompanionSample
-        |
-        |object CompatibilityCompanionSample
-        |""".stripMargin
-    )
-
-    assertThat(reporter.hasErrors).isTrue
-    assertThat(reporter.infos.exists { info: StoreReporter#Info =>
-      info.msg.contains("Please annotate single expressions")
-    }).isTrue
-  }
-
-  private def compileScalaSource(fileName: String, source: String): StoreReporter = {
-    val directory: Path = Files.createTempDirectory("pekko-parsing-macro-test")
-    val sourceFile: Path = directory.resolve(fileName)
-    val outputDirectory: Path = directory.resolve("classes")
-    Files.createDirectories(outputDirectory)
-    Files.write(sourceFile, source.getBytes(StandardCharsets.UTF_8))
-
-    val settings: Settings = new Settings
-    val arguments: List[String] = List(
-      "-Ymacro-annotations",
-      "-classpath",
-      System.getProperty("java.class.path"),
-      "-d",
-      outputDirectory.toString
-    )
-    settings.processArguments(arguments, processAll = true)
-
-    val reporter: StoreReporter = new StoreReporter
-    val compiler: Global = new Global(settings, reporter)
-    val run: compiler.Run = new compiler.Run
-    run.compile(List(sourceFile.toString))
-    reporter
-  }
-
-  private def withUnsupportedFeatureHandling(testBody: => Unit): Unit = {
-    try {
-      testBody
-    } catch {
-      case error: Error =>
-        if (!NativeImageSupport.isUnsupportedFeatureError(error)) {
-          throw error
-        }
-    }
   }
 }
