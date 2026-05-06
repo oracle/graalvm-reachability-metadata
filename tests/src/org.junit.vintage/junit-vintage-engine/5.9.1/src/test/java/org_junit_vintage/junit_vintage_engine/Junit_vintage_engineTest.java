4b5bbcd66dae2020efd3940542da1b9506a22686
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-05-06 16:59:50 +0200

    Dynamic-access coverage for org.junit.vintage.engine.support.UniqueIdStringifier (1/1)

--- tests/src/org.junit.vintage/junit-vintage-engine/5.9.1/src/test/java/org_junit_vintage/junit_vintage_engine/Junit_vintage_engineTest.java
+++ tests/src/org.junit.vintage/junit-vintage-engine/5.9.1/src/test/java/org_junit_vintage/junit_vintage_engine/Junit_vintage_engineTest.java
@@ -8,7 +8,7 @@ package org_junit_vintage.junit_vintage_engine;
 
 import org.junit.jupiter.api.Test;
 
-class Junit_vintage_engineTest {
+public class Junit_vintage_engineTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
