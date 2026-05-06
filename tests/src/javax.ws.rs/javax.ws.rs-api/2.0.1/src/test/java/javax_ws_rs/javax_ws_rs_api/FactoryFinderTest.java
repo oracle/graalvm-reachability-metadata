e6829fb61b1df4ed89064e6cee46d030fecf5529
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 15:24:00 +0200

    Dynamic-access coverage for javax.ws.rs.client.FactoryFinder (8/17)

--- tests/src/javax.ws.rs/javax.ws.rs-api/2.0.1/src/test/java/javax_ws_rs/javax_ws_rs_api/FactoryFinderTest.java
+++ tests/src/javax.ws.rs/javax.ws.rs-api/2.0.1/src/test/java/javax_ws_rs/javax_ws_rs_api/FactoryFinderTest.java
@@ -9,6 +9,7 @@ package javax_ws_rs.javax_ws_rs_api;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MultivaluedHashMap;
 
 import org.junit.jupiter.api.Test;
 
@@ -55,6 +56,14 @@ public class FactoryFinderTest {
         });
     }
 
+    @Test
+    void newBuilderInstantiatesConcreteProviderClassBeforeTypeCheck() {
+        withProviderProperty(MultivaluedHashMap.class.getName(), () ->
+                assertThatThrownBy(ClientBuilder::newBuilder)
+                        .isInstanceOf(LinkageError.class)
+                        .hasMessageContaining("attempting to cast"));
+    }
+
     private static void assertAbstractClientBuilderProviderFailsToInstantiate() {
         assertThatThrownBy(ClientBuilder::newBuilder)
                 .isInstanceOf(RuntimeException.class)
@@ -64,8 +73,12 @@ public class FactoryFinderTest {
     }
 
     private static synchronized void withProviderProperty(Runnable action) {
+        withProviderProperty(ClientBuilder.class.getName(), action);
+    }
+
+    private static synchronized void withProviderProperty(String providerClassName, Runnable action) {
         String previousProvider = System.getProperty(ClientBuilder.JAXRS_DEFAULT_CLIENT_BUILDER_PROPERTY);
-        System.setProperty(ClientBuilder.JAXRS_DEFAULT_CLIENT_BUILDER_PROPERTY, ClientBuilder.class.getName());
+        System.setProperty(ClientBuilder.JAXRS_DEFAULT_CLIENT_BUILDER_PROPERTY, providerClassName);
         try {
             action.run();
         } finally {
