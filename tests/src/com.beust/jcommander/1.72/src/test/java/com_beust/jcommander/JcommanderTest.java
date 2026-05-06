f19edba0a86b5dd4e0484b0f683eb953aeeb7766
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 19:43:19 +0200

    Dynamic-access coverage for com.beust.jcommander.Parameterized (9/31)

--- tests/src/com.beust/jcommander/1.72/src/test/java/com_beust/jcommander/JcommanderTest.java
+++ tests/src/com.beust/jcommander/1.72/src/test/java/com_beust/jcommander/JcommanderTest.java
@@ -8,7 +8,7 @@ package com_beust.jcommander;
 
 import org.junit.jupiter.api.Test;
 
-class JcommanderTest {
+public class JcommanderTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
