bd3c72ff2cd1d113edf1e57510b23a6c50427ba0
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 23:46:43 +0200

    Scaffold io.quarkus:quarkus-junit-config:3.33.1

---
+++ tests/src/io.quarkus/quarkus-junit-config/3.33.1/src/test/java/io_quarkus/quarkus_junit_config/Quarkus_junit_configTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_quarkus.quarkus_junit_config;
+
+import org.junit.jupiter.api.Test;
+
+class Quarkus_junit_configTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
