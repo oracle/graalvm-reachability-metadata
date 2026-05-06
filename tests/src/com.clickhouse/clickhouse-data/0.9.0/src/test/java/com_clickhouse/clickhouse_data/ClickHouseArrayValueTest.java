585f451d0f4b07491f6e096da830ece935d787f4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-06 03:29:46 +0200

    Dynamic-access coverage for com.clickhouse.data.value.ClickHouseArrayValue (6/37)

---
+++ tests/src/com.clickhouse/clickhouse-data/0.9.0/src/test/java/com_clickhouse/clickhouse_data/ClickHouseArrayValueTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_clickhouse.clickhouse_data;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.clickhouse.data.value.ClickHouseArrayValue;
+import com.clickhouse.data.value.ClickHouseIntegerValue;
+import org.junit.jupiter.api.Test;
+
+@SuppressWarnings("deprecation")
+public class ClickHouseArrayValueTest {
+    @Test
+    void createsTypedEmptyArrayAndTypedCopy() {
+        ClickHouseArrayValue<String> emptyValue = ClickHouseArrayValue.ofEmpty(String.class);
+        assertThat(emptyValue.getValue()).isInstanceOf(String[].class).isEmpty();
+
+        ClickHouseArrayValue<Integer> integerValue = ClickHouseArrayValue.of(new Integer[] { 1, 2, 3 });
+        Integer[] typedCopy = integerValue.asArray(Integer.class);
+
+        assertThat(typedCopy).containsExactly(1, 2, 3);
+        assertThat(typedCopy).isNotSameAs(integerValue.getValue());
+    }
+
+    @Test
+    void updateUnknownWrapsArbitraryValueInConcreteTypedArray() {
+        ClickHouseArrayValue<Object> value = ClickHouseArrayValue.ofEmpty(Object.class);
+        StringBuilder payload = new StringBuilder("payload");
+
+        value.update(payload);
+
+        Object[] rawValue = value.getValue();
+        assertThat(rawValue).isInstanceOf(StringBuilder[].class);
+        assertThat(rawValue).containsExactly(payload);
+    }
+
+    @Test
+    void setValuePromotesScalarToCompatibleNestedArrayDepth() {
+        Number[][][] initialValue = new Number[][][] { new Number[][] { new Number[] { 1 } } };
+        ClickHouseArrayValue<Number[][]> value = ClickHouseArrayValue.of(initialValue);
+
+        value.setValue(0, ClickHouseIntegerValue.of(42));
+
+        Number[][][] rawValue = value.getValue();
+        assertThat(rawValue[0]).isInstanceOf(Integer[][].class);
+        assertThat(rawValue[0].length).isEqualTo(1);
+        assertThat(rawValue[0][0]).isNull();
+    }
+}
--- tests/src/com.clickhouse/clickhouse-data/0.9.0/src/test/java/com_clickhouse/clickhouse_data/Clickhouse_dataTest.java
+++ tests/src/com.clickhouse/clickhouse-data/0.9.0/src/test/java/com_clickhouse/clickhouse_data/Clickhouse_dataTest.java
@@ -8,7 +8,7 @@ package com_clickhouse.clickhouse_data;
 
 import org.junit.jupiter.api.Test;
 
-class Clickhouse_dataTest {
+public class Clickhouse_dataTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
