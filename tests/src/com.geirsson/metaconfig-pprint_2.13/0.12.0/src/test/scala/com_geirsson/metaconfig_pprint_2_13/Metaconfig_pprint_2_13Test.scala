9419471bc2bd67a566cf100457b0e9f587f97c52
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-07 08:25:29 +0200

    Scaffold com.geirsson:metaconfig-pprint_2.13:0.12.0

---
+++ tests/src/com.geirsson/metaconfig-pprint_2.13/0.12.0/src/test/scala/com_geirsson/metaconfig_pprint_2_13/Metaconfig_pprint_2_13Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_geirsson.metaconfig_pprint_2_13
+
+import org.junit.jupiter.api.Test
+
+class Metaconfig_pprint_2_13Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
