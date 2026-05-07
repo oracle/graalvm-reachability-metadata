e9952bed394f9b4e13550c93675304db745c76b5
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-08 01:54:34 +0200

    Dynamic-access coverage for org.apache.tika.config.ConfigBase (21/123)

--- tests/src/org.apache.tika/tika-core/3.3.0/src/test/java/org_apache_tika/tika_core/ConfigBaseTest.java
+++ tests/src/org.apache.tika/tika-core/3.3.0/src/test/java/org_apache_tika/tika_core/ConfigBaseTest.java
@@ -18,9 +18,18 @@ import java.util.Map;
 import java.util.Set;
 
 import org.junit.jupiter.api.Test;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NamedNodeMap;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.TypeInfo;
+import org.w3c.dom.UserDataHandler;
 
 import org.apache.tika.config.ConfigBase;
 import org.apache.tika.exception.TikaConfigException;
+import org.apache.tika.utils.XMLReaderUtils;
 
 public class ConfigBaseTest {
 
@@ -69,6 +78,27 @@ public class ConfigBaseTest {
     }
 
     @Test
+    public void configureSetsClassListParameter() throws Exception {
+        String xml = """
+                <properties>
+                  <group>
+                    <plugins class="%s">
+                      <plugin class="%s">
+                        <name>plugin-one</name>
+                      </plugin>
+                    </plugins>
+                  </group>
+                </properties>
+                """.formatted(Plugin.class.getName(), PluginImpl.class.getName());
+
+        CompositeGroup group = ConfigurableConfig.buildCompositeFrom(wrapClassListElement(xml));
+
+        assertThat(group.plugins).hasSize(1);
+        assertThat(group.plugins.get(0)).isInstanceOf(PluginImpl.class);
+        assertThat(((PluginImpl) group.plugins.get(0)).name).isEqualTo("plugin-one");
+    }
+
+    @Test
     public void buildCompositeCreatesCompositeWithConfiguredItems() throws Exception {
         String xml = """
                 <properties>
@@ -96,6 +126,11 @@ public class ConfigBaseTest {
         return new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8));
     }
 
+    private static Element wrapClassListElement(String xml) throws Exception {
+        Document document = XMLReaderUtils.buildDOM(xmlStream(xml));
+        return new WrappingElement(document.getDocumentElement());
+    }
+
     public static class ConfigurableConfig extends ConfigBase {
         private int intValue;
         private long longValue;
@@ -116,6 +151,12 @@ public class ConfigBaseTest {
                 throws TikaConfigException, IOException {
             return buildComposite("group", CompositeGroup.class, "item", SimpleItem.class,
                     xmlStream(xml));
+        }
+
+        public static CompositeGroup buildCompositeFrom(Element element)
+                throws TikaConfigException, IOException {
+            return buildComposite("group", CompositeGroup.class, "item", SimpleItem.class,
+                    element);
         }
 
         public void setIntValue(int intValue) {
@@ -159,6 +200,20 @@ public class ConfigBaseTest {
         }
     }
 
+    public interface Plugin {
+    }
+
+    public static class PluginImpl implements Plugin {
+        private String name;
+
+        public PluginImpl() {
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+    }
+
     public static class NestedConfig {
         private String value;
 
@@ -184,6 +239,7 @@ public class ConfigBaseTest {
     public static class CompositeGroup {
         private final List<SimpleItem> items;
         private String description;
+        private List<Plugin> plugins;
 
         public CompositeGroup(List<SimpleItem> items) {
             this.items = items;
@@ -192,5 +248,397 @@ public class ConfigBaseTest {
         public void setDescription(String description) {
             this.description = description;
         }
+
+        public void setPlugins(List<Plugin> plugins) {
+            this.plugins = plugins;
+        }
+    }
+
+    private static class WrappingElement implements Element {
+        private final Element delegate;
+
+        WrappingElement(Element delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public String getTagName() {
+            return delegate.getTagName();
+        }
+
+        @Override
+        public String getAttribute(String name) {
+            return delegate.getAttribute(name);
+        }
+
+        @Override
+        public void setAttribute(String name, String value) {
+            delegate.setAttribute(name, value);
+        }
+
+        @Override
+        public void removeAttribute(String name) {
+            delegate.removeAttribute(name);
+        }
+
+        @Override
+        public Attr getAttributeNode(String name) {
+            return delegate.getAttributeNode(name);
+        }
+
+        @Override
+        public Attr setAttributeNode(Attr newAttr) {
+            return delegate.setAttributeNode(newAttr);
+        }
+
+        @Override
+        public Attr removeAttributeNode(Attr oldAttr) {
+            return delegate.removeAttributeNode(oldAttr);
+        }
+
+        @Override
+        public NodeList getElementsByTagName(String name) {
+            return delegate.getElementsByTagName(name);
+        }
+
+        @Override
+        public String getAttributeNS(String namespaceURI, String localName) {
+            return delegate.getAttributeNS(namespaceURI, localName);
+        }
+
+        @Override
+        public void setAttributeNS(String namespaceURI, String qualifiedName, String value) {
+            delegate.setAttributeNS(namespaceURI, qualifiedName, value);
+        }
+
+        @Override
+        public void removeAttributeNS(String namespaceURI, String localName) {
+            delegate.removeAttributeNS(namespaceURI, localName);
+        }
+
+        @Override
+        public Attr getAttributeNodeNS(String namespaceURI, String localName) {
+            return delegate.getAttributeNodeNS(namespaceURI, localName);
+        }
+
+        @Override
+        public Attr setAttributeNodeNS(Attr newAttr) {
+            return delegate.setAttributeNodeNS(newAttr);
+        }
+
+        @Override
+        public NodeList getElementsByTagNameNS(String namespaceURI, String localName) {
+            return delegate.getElementsByTagNameNS(namespaceURI, localName);
+        }
+
+        @Override
+        public boolean hasAttribute(String name) {
+            return delegate.hasAttribute(name);
+        }
+
+        @Override
+        public boolean hasAttributeNS(String namespaceURI, String localName) {
+            return delegate.hasAttributeNS(namespaceURI, localName);
+        }
+
+        @Override
+        public TypeInfo getSchemaTypeInfo() {
+            return delegate.getSchemaTypeInfo();
+        }
+
+        @Override
+        public void setIdAttribute(String name, boolean isId) {
+            delegate.setIdAttribute(name, isId);
+        }
+
+        @Override
+        public void setIdAttributeNS(String namespaceURI, String localName, boolean isId) {
+            delegate.setIdAttributeNS(namespaceURI, localName, isId);
+        }
+
+        @Override
+        public void setIdAttributeNode(Attr idAttr, boolean isId) {
+            delegate.setIdAttributeNode(idAttr, isId);
+        }
+
+        @Override
+        public String getNodeName() {
+            return delegate.getNodeName();
+        }
+
+        @Override
+        public String getNodeValue() {
+            return delegate.getNodeValue();
+        }
+
+        @Override
+        public void setNodeValue(String nodeValue) {
+            delegate.setNodeValue(nodeValue);
+        }
+
+        @Override
+        public short getNodeType() {
+            return delegate.getNodeType();
+        }
+
+        @Override
+        public Node getParentNode() {
+            return delegate.getParentNode();
+        }
+
+        @Override
+        public NodeList getChildNodes() {
+            return new WrappingNodeList(delegate.getChildNodes());
+        }
+
+        @Override
+        public Node getFirstChild() {
+            return wrapNode(delegate.getFirstChild());
+        }
+
+        @Override
+        public Node getLastChild() {
+            return wrapNode(delegate.getLastChild());
+        }
+
+        @Override
+        public Node getPreviousSibling() {
+            return wrapNode(delegate.getPreviousSibling());
+        }
+
+        @Override
+        public Node getNextSibling() {
+            return wrapNode(delegate.getNextSibling());
+        }
+
+        @Override
+        public NamedNodeMap getAttributes() {
+            return delegate.getAttributes();
+        }
+
+        @Override
+        public Document getOwnerDocument() {
+            return delegate.getOwnerDocument();
+        }
+
+        @Override
+        public Node insertBefore(Node newChild, Node refChild) {
+            return delegate.insertBefore(newChild, refChild);
+        }
+
+        @Override
+        public Node replaceChild(Node newChild, Node oldChild) {
+            return delegate.replaceChild(newChild, oldChild);
+        }
+
+        @Override
+        public Node removeChild(Node oldChild) {
+            return delegate.removeChild(oldChild);
+        }
+
+        @Override
+        public Node appendChild(Node newChild) {
+            return delegate.appendChild(newChild);
+        }
+
+        @Override
+        public boolean hasChildNodes() {
+            return delegate.hasChildNodes();
+        }
+
+        @Override
+        public Node cloneNode(boolean deep) {
+            return delegate.cloneNode(deep);
+        }
+
+        @Override
+        public void normalize() {
+            delegate.normalize();
+        }
+
+        @Override
+        public boolean isSupported(String feature, String version) {
+            return delegate.isSupported(feature, version);
+        }
+
+        @Override
+        public String getNamespaceURI() {
+            return delegate.getNamespaceURI();
+        }
+
+        @Override
+        public String getPrefix() {
+            return delegate.getPrefix();
+        }
+
+        @Override
+        public void setPrefix(String prefix) {
+            delegate.setPrefix(prefix);
+        }
+
+        @Override
+        public String getLocalName() {
+            return delegate.getLocalName();
+        }
+
+        @Override
+        public boolean hasAttributes() {
+            return delegate.hasAttributes();
+        }
+
+        @Override
+        public String getBaseURI() {
+            return delegate.getBaseURI();
+        }
+
+        @Override
+        public short compareDocumentPosition(Node other) {
+            return delegate.compareDocumentPosition(other);
+        }
+
+        @Override
+        public String getTextContent() {
+            return delegate.getTextContent();
+        }
+
+        @Override
+        public void setTextContent(String textContent) {
+            delegate.setTextContent(textContent);
+        }
+
+        @Override
+        public boolean isSameNode(Node other) {
+            return delegate.isSameNode(other);
+        }
+
+        @Override
+        public String lookupPrefix(String namespaceURI) {
+            return delegate.lookupPrefix(namespaceURI);
+        }
+
+        @Override
+        public boolean isDefaultNamespace(String namespaceURI) {
+            return delegate.isDefaultNamespace(namespaceURI);
+        }
+
+        @Override
+        public String lookupNamespaceURI(String prefix) {
+            return delegate.lookupNamespaceURI(prefix);
+        }
+
+        @Override
+        public boolean isEqualNode(Node arg) {
+            return delegate.isEqualNode(arg);
+        }
+
+        @Override
+        public Object getFeature(String feature, String version) {
+            return delegate.getFeature(feature, version);
+        }
+
+        @Override
+        public Object setUserData(String key, Object data, UserDataHandler handler) {
+            return delegate.setUserData(key, data, handler);
+        }
+
+        @Override
+        public Object getUserData(String key) {
+            return delegate.getUserData(key);
+        }
+    }
+
+    private static final class FlappingClassAttributeElement extends WrappingElement {
+        private final NamedNodeMap attributes;
+
+        FlappingClassAttributeElement(Element delegate) {
+            super(delegate);
+            attributes = new FlappingClassAttributeMap(delegate.getAttributes());
+        }
+
+        @Override
+        public NamedNodeMap getAttributes() {
+            return attributes;
+        }
+    }
+
+    private static class WrappingNodeList implements NodeList {
+        private final NodeList delegate;
+
+        WrappingNodeList(NodeList delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public Node item(int index) {
+            return wrapNode(delegate.item(index));
+        }
+
+        @Override
+        public int getLength() {
+            return delegate.getLength();
+        }
+    }
+
+    private static class FlappingClassAttributeMap implements NamedNodeMap {
+        private final NamedNodeMap delegate;
+        private boolean classAttributeHidden = true;
+
+        FlappingClassAttributeMap(NamedNodeMap delegate) {
+            this.delegate = delegate;
+        }
+
+        @Override
+        public Node getNamedItem(String name) {
+            if ("class".equals(name) && classAttributeHidden) {
+                classAttributeHidden = false;
+                return null;
+            }
+            return delegate.getNamedItem(name);
+        }
+
+        @Override
+        public Node setNamedItem(Node arg) {
+            return delegate.setNamedItem(arg);
+        }
+
+        @Override
+        public Node removeNamedItem(String name) {
+            return delegate.removeNamedItem(name);
+        }
+
+        @Override
+        public Node item(int index) {
+            return delegate.item(index);
+        }
+
+        @Override
+        public int getLength() {
+            return delegate.getLength();
+        }
+
+        @Override
+        public Node getNamedItemNS(String namespaceURI, String localName) {
+            return delegate.getNamedItemNS(namespaceURI, localName);
+        }
+
+        @Override
+        public Node setNamedItemNS(Node arg) {
+            return delegate.setNamedItemNS(arg);
+        }
+
+        @Override
+        public Node removeNamedItemNS(String namespaceURI, String localName) {
+            return delegate.removeNamedItemNS(namespaceURI, localName);
+        }
+    }
+
+    private static Node wrapNode(Node node) {
+        if (node instanceof Element element) {
+            if ("plugins".equals(element.getLocalName())) {
+                return new FlappingClassAttributeElement(element);
+            }
+            return new WrappingElement(element);
+        }
+        return node;
     }
 }
