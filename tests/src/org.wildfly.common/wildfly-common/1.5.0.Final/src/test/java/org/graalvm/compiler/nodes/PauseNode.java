dc851de77cd05020abe02cd2774dc58ec087edca
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-07 07:11:18 +0100

    [GenAI] Add support for org.wildfly.common:wildfly-common:1.5.0.Final using gpt-5.5 (#6228)
    * Scaffold org.wildfly.common:wildfly-common:1.5.0.Final
    
    * Checkpoint generated tests for org.wildfly.common:wildfly-common:1.5.0.Final (1)
    
    * Checkpoint generated tests for org.wildfly.common:wildfly-common:1.5.0.Final (2)
    
    * Checkpoint generated tests for org.wildfly.common:wildfly-common:1.5.0.Final (3)
    
    * Update generated library support for org.wildfly.common:wildfly-common:1.5.0.Final
    
    * Add support for org.wildfly.common:wildfly-common:1.5.0.Final
    
    * Apply local CI verification fixes

---
+++ tests/src/org.wildfly.common/wildfly-common/1.5.0.Final/src/test/java/org/graalvm/compiler/nodes/PauseNode.java
@@ -0,0 +1,18 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org.graalvm.compiler.nodes;
+
+/**
+ * Compatibility stub for WildFly Common 1.5.0.Final native-image substitutions.
+ */
+public final class PauseNode {
+    private PauseNode() {
+    }
+
+    public static void pause() {
+    }
+}
