fc47bfb8726c92e97e1fc0c45422f4089900ebfb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-07 15:34:06 +0200

    Scaffold org.typelevel:simulacrum-scalafix-annotations_3:0.5.4

---
+++ tests/src/org.typelevel/simulacrum-scalafix-annotations_3/0.5.4/src/test/scala/org_typelevel/simulacrum_scalafix_annotations_3/Simulacrum_scalafix_annotations_3Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_typelevel.simulacrum_scalafix_annotations_3
+
+import org.junit.jupiter.api.Test
+
+class Simulacrum_scalafix_annotations_3Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
