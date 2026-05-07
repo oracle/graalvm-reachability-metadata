a62be6133cac39aa035b60929aaecd2e55b01b25
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-07 06:48:40 +0200

    Prepare project for org.apache.maven:maven-artifact:3.0

---
+++ tests/src/org.apache.maven/maven-artifact/3.0/src/test/java/org_apache_maven/maven_artifact/ArtifactMetadataSourceAnonymous1Test.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven.maven_artifact;
+
+import org.apache.maven.artifact.metadata.ArtifactMetadataSource;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ArtifactMetadataSourceAnonymous1Test {
+    @Test
+    void roleInitializesArtifactMetadataSourceInterface() {
+        assertThat(ArtifactMetadataSource.ROLE).isEqualTo(ArtifactMetadataSource.class.getName());
+    }
+}
