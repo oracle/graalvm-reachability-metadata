ee94ce09ed00b80f96cadd89e0aac672683ed6a1
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-05-07 03:18:58 +0200

    Scaffold io.scalaland:chimney-macro-commons_3:2.0.0

---
+++ tests/src/io.scalaland/chimney-macro-commons_3/2.0.0/src/test/scala/io_scalaland/chimney_macro_commons_3/Chimney_macro_commons_3Test.scala
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_scalaland.chimney_macro_commons_3
+
+import org.junit.jupiter.api.Test
+
+class Chimney_macro_commons_3Test {
+  @Test
+  def test(): Unit = {
+    println("This is just a placeholder, implement your test")
+  }
+}
