54d4540e4d6f18e160bda0bb80772b75d22635de
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-07 15:35:57 +0200

    Dynamic-access coverage for org.apache.maven.model.CiManagement (3/16)

---
+++ tests/src/org.apache.maven/maven-model/2.0.10/src/test/java/org_apache_maven/maven_model/CiManagementTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven.maven_model;
+
+import org.apache.maven.model.CiManagement;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class CiManagementTest {
+    @Test
+    void rejectsNullNotifierWithTypedErrorMessage() {
+        CiManagement ciManagement = new CiManagement();
+
+        ClassCastException exception = assertThrows(ClassCastException.class,
+                () -> ciManagement.addNotifier(null));
+
+        assertThat(exception).hasMessageContaining("org.apache.maven.model.Notifier");
+        assertThat(ciManagement.getNotifiers()).isEmpty();
+    }
+}
