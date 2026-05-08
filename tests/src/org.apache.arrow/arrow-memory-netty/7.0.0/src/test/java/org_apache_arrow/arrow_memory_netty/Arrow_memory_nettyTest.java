db94658f37c357902eccf3a7e92ec6a350d0a4fb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-08 21:53:25 +0200

    Scaffold org.apache.arrow:arrow-memory-netty:7.0.0

---
+++ tests/src/org.apache.arrow/arrow-memory-netty/7.0.0/src/test/java/org_apache_arrow/arrow_memory_netty/Arrow_memory_nettyTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_arrow.arrow_memory_netty;
+
+import org.junit.jupiter.api.Test;
+
+class Arrow_memory_nettyTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
