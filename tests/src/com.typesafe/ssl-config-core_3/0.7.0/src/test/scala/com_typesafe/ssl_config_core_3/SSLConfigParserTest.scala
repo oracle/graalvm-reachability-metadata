e558d3374d4720c0a3ec2216dba0cbe11c063b85
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-08 17:11:05 +0200

    Preserve last passing candidate for com.typesafe:ssl-config-core_3:0.7.0

--- tests/src/com.typesafe/ssl-config-core_3/0.7.0/src/test/scala/com_typesafe/ssl_config_core_3/SSLConfigParserTest.scala
+++ tests/src/com.typesafe/ssl-config-core_3/0.7.0/src/test/scala/com_typesafe/ssl_config_core_3/SSLConfigParserTest.scala
@@ -6,27 +6,25 @@
  */
 package com_typesafe.ssl_config_core_3
 
+import scala.jdk.CollectionConverters.*
+
 import com.typesafe.config.{ Config, ConfigFactory }
-import com.typesafe.sslconfig.ssl.{ NoopHostnameVerifier, SSLConfigFactory, SSLConfigSettings }
+import com.typesafe.sslconfig.ssl.SSLConfigFactory
+import com.typesafe.sslconfig.ssl.SSLConfigSettings
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class SSLConfigParserTest {
   @Test
-  def parsesConfiguredHostnameVerifierClass(): Unit = {
+  def parsesConfiguredSslSettings(): Unit = {
     val config: Config = ConfigFactory.parseString(
       """
         |default = false
         |protocol = "TLSv1.2"
-        |checkRevocation = null
-        |revocationLists = []
-        |enabledCipherSuites = []
-        |enabledProtocols = []
-        |hostnameVerifierClass = "com.typesafe.sslconfig.ssl.NoopHostnameVerifier"
-        |sslParameters {
-        |  clientAuth = "default"
-        |  protocols = []
-        |}
+        |checkRevocation = true
+        |revocationLists = ["https://example.com/revoked.crl"]
+        |enabledCipherSuites = ["TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256"]
+        |enabledProtocols = ["TLSv1.2"]
         |keyManager {
         |  algorithm = null
         |  stores = []
@@ -52,9 +50,8 @@ class SSLConfigParserTest {
         |loose {
         |  allowLegacyHelloMessages = null
         |  allowUnsafeRenegotiation = null
-        |  disableHostnameVerification = false
-        |  disableSNI = false
-        |  acceptAnyCertificate = false
+        |  disableHostnameVerification = true
+        |  acceptAnyCertificate = true
         |}
         |debug {
         |  all = false
@@ -68,6 +65,15 @@ class SSLConfigParserTest {
 
     val settings: SSLConfigSettings = SSLConfigFactory.parse(config)
 
-    assertThat(settings.hostnameVerifierClass).isEqualTo(classOf[NoopHostnameVerifier])
+    val revocationLists: Seq[String] = settings.revocationLists.get.map(_.toString)
+
+    assertThat(settings.default).isFalse()
+    assertThat(settings.protocol).isEqualTo("TLSv1.2")
+    assertThat(settings.checkRevocation.get).isTrue()
+    assertThat(revocationLists.asJava).containsExactly("https://example.com/revoked.crl")
+    assertThat(settings.enabledCipherSuites.get.asJava).containsExactly("TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256")
+    assertThat(settings.enabledProtocols.get.asJava).containsExactly("TLSv1.2")
+    assertThat(settings.loose.disableHostnameVerification).isTrue()
+    assertThat(settings.loose.acceptAnyCertificate).isTrue()
   }
 }
