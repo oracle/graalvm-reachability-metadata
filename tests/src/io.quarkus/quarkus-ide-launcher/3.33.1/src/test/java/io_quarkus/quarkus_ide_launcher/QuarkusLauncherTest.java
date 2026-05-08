91b63acdca1aae15f37a6d5ff616568d785e7b3a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-08 09:54:27 +0200

    Update generated library support for io.quarkus:quarkus-ide-launcher:3.33.1

--- tests/src/io.quarkus/quarkus-ide-launcher/3.33.1/src/test/java/io_quarkus/quarkus_ide_launcher/QuarkusLauncherTest.java
+++ tests/src/io.quarkus/quarkus-ide-launcher/3.33.1/src/test/java/io_quarkus/quarkus_ide_launcher/QuarkusLauncherTest.java
@@ -11,7 +11,6 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Map;
 
 import io.quarkus.bootstrap.IDELauncherImpl;
 import io.quarkus.launcher.QuarkusLauncher;
