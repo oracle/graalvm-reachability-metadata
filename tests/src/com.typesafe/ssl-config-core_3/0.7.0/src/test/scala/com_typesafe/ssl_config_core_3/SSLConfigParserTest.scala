c7284515216b89b65bf876ace1a4fc0dbea36544
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-08 16:55:30 +0200

    Prepare project for com.typesafe:ssl-config-core_3:0.7.0

---
+++ tests/src/com.typesafe/ssl-config-core_3/0.7.0/src/test/scala/com_typesafe/ssl_config_core_3/SSLConfigParserTest.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_typesafe.ssl_config_core_3
+
+import com.typesafe.config.{ Config, ConfigFactory }
+import com.typesafe.sslconfig.ssl.{ NoopHostnameVerifier, SSLConfigFactory, SSLConfigSettings }
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SSLConfigParserTest {
+  @Test
+  def parsesConfiguredHostnameVerifierClass(): Unit = {
+    val config: Config = ConfigFactory.parseString(
+      """
+        |default = false
+        |protocol = "TLSv1.2"
+        |checkRevocation = null
+        |revocationLists = []
+        |enabledCipherSuites = []
+        |enabledProtocols = []
+        |hostnameVerifierClass = "com.typesafe.sslconfig.ssl.NoopHostnameVerifier"
+        |sslParameters {
+        |  clientAuth = "default"
+        |  protocols = []
+        |}
+        |keyManager {
+        |  algorithm = null
+        |  stores = []
+        |  prototype.stores {
+        |    type = null
+        |    path = null
+        |    data = null
+        |    password = null
+        |    classpath = false
+        |  }
+        |}
+        |trustManager {
+        |  algorithm = null
+        |  stores = []
+        |  prototype.stores {
+        |    type = null
+        |    path = null
+        |    data = null
+        |    password = null
+        |    classpath = false
+        |  }
+        |}
+        |loose {
+        |  allowLegacyHelloMessages = null
+        |  allowUnsafeRenegotiation = null
+        |  disableHostnameVerification = false
+        |  disableSNI = false
+        |  acceptAnyCertificate = false
+        |}
+        |debug {
+        |  all = false
+        |  ssl = false
+        |  sslctx = false
+        |  keymanager = false
+        |  trustmanager = false
+        |}
+        |""".stripMargin
+    ).resolve()
+
+    val settings: SSLConfigSettings = SSLConfigFactory.parse(config)
+
+    assertThat(settings.hostnameVerifierClass).isEqualTo(classOf[NoopHostnameVerifier])
+  }
+}
