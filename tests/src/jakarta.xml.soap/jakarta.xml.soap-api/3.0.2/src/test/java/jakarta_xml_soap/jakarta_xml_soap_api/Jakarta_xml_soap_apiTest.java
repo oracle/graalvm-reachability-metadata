07191d81b6c6e85ed889092397a5337cae6ae183
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-08 17:44:14 +0200

    Dynamic-access coverage for jakarta.xml.soap.ServiceLoaderUtil (4/6)

--- tests/src/jakarta.xml.soap/jakarta.xml.soap-api/3.0.2/src/test/java/jakarta_xml_soap/jakarta_xml_soap_api/Jakarta_xml_soap_apiTest.java
+++ tests/src/jakarta.xml.soap/jakarta.xml.soap-api/3.0.2/src/test/java/jakarta_xml_soap/jakarta_xml_soap_api/Jakarta_xml_soap_apiTest.java
@@ -8,7 +8,7 @@ package jakarta_xml_soap.jakarta_xml_soap_api;
 
 import org.junit.jupiter.api.Test;
 
-class Jakarta_xml_soap_apiTest {
+public class Jakarta_xml_soap_apiTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
