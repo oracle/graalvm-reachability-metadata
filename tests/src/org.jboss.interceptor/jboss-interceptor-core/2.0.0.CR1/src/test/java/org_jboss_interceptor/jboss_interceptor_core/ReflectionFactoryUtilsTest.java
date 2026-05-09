1c6ae36b04de34d0723f1a7ccf0cb8dc971c625d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 11:40:41 +0200

    Dynamic-access coverage for org.jboss.interceptor.util.ReflectionFactoryUtils (11/16)

---
+++ tests/src/org.jboss.interceptor/jboss-interceptor-core/2.0.0.CR1/src/test/java/org_jboss_interceptor/jboss_interceptor_core/ReflectionFactoryUtilsTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_jboss_interceptor.jboss_interceptor_core;
+
+import java.lang.reflect.Constructor;
+
+import org.jboss.interceptor.util.ReflectionFactoryUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ReflectionFactoryUtilsTest {
+
+    @Test
+    void createsSerializationConstructorWithoutInvokingTargetConstructor() throws Exception {
+        final Constructor<ConstructorRequiredSample> constructor = ReflectionFactoryUtils
+                .getReflectionFactoryConstructor(ConstructorRequiredSample.class);
+
+        if (ReflectionFactoryUtils.isAvailable()) {
+            assertThat(constructor).isNotNull();
+            final ConstructorRequiredSample instance = constructor.newInstance();
+
+            assertThat(instance).isNotNull();
+            assertThat(instance.value()).isNull();
+        } else {
+            assertThat(constructor).isNull();
+        }
+    }
+
+    public static final class ConstructorRequiredSample {
+        private final String value;
+
+        public ConstructorRequiredSample(String value) {
+            this.value = value;
+        }
+
+        String value() {
+            return value;
+        }
+    }
+}
