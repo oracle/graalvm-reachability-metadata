54b8b79dc420ed4fb433a32e274cb069bdf93d71
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 07:27:16 +0200

    Checkpoint generated tests for org.apache.hadoop:hadoop-mapreduce-client-jobclient:2.7.3 (2)

--- tests/src/org.apache.hadoop/hadoop-mapreduce-client-jobclient/2.7.3/src/test/java/org_apache_hadoop/hadoop_mapreduce_client_jobclient/Hadoop_mapreduce_client_jobclientTest.java
+++ tests/src/org.apache.hadoop/hadoop-mapreduce-client-jobclient/2.7.3/src/test/java/org_apache_hadoop/hadoop_mapreduce_client_jobclient/Hadoop_mapreduce_client_jobclientTest.java
@@ -6,11 +6,41 @@
  */
 package org_apache_hadoop.hadoop_mapreduce_client_jobclient;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.mapred.ClientCache;
+import org.apache.hadoop.mapred.ClientServiceDelegate;
+import org.apache.hadoop.mapreduce.JobID;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
 
-class Hadoop_mapreduce_client_jobclientTest {
+import static org.assertj.core.api.Assertions.assertThat;
+
+@Timeout(60)
+public class Hadoop_mapreduce_client_jobclientTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
+    }
+
+    @Test
+    void clientCacheReusesClientServiceDelegateForTheSameJob() throws Exception {
+        ClientCache cache = new ClientCache(clientCacheConfiguration(), null);
+        JobID firstJob = new JobID("123456789", 1);
+        JobID secondJob = new JobID("123456789", 2);
+
+        try {
+            ClientServiceDelegate firstClient = cache.getClient(firstJob);
+            ClientServiceDelegate sameJobClient = cache.getClient(firstJob);
+            ClientServiceDelegate otherJobClient = cache.getClient(secondJob);
+
+            assertThat(sameJobClient).isSameAs(firstClient);
+            assertThat(otherJobClient).isNotSameAs(firstClient);
+        } finally {
+            cache.close();
+        }
+    }
+
+    private static Configuration clientCacheConfiguration() {
+        return new Configuration(false);
     }
 }
