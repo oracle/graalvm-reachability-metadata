c560ddbd4e5cadaedf2a5e40f552aab6f1f6b807
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 18:43:02 +0200

    Scaffold org.apache.maven.shared:maven-common-artifact-filters:1.3

---
+++ tests/src/org.apache.maven.shared/maven-common-artifact-filters/1.3/src/test/java/org_apache_maven_shared/maven_common_artifact_filters/Maven_common_artifact_filtersTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven_shared.maven_common_artifact_filters;
+
+import org.junit.jupiter.api.Test;
+
+class Maven_common_artifact_filtersTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
