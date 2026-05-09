f3d01ac69a71ba727457065f9c455971e6ff6bf3
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 20:11:57 +0200

    Partial dynamic-access coverage for org.specs2.reflect.Classes (8/9)

---
+++ tests/src/org.specs2/specs2-common_3/4.20.5/src/test/scala/org_specs2/specs2_common_3/ClassesTest.scala
@@ -0,0 +1,105 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_specs2.specs2_common_3
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Assertions.fail
+import org.junit.jupiter.api.Test
+import org.specs2.control.Operation
+import org.specs2.control.runOperation
+import org.specs2.reflect.Classes
+
+class ClassesTest {
+  private val loader: ClassLoader = classOf[ClassesTest].getClassLoader
+
+  @Test
+  def loadClassEitherAndExistsClassUseSuppliedLoader(): Unit = {
+    val loaded: Either[Throwable, Class[ZeroArgumentTarget]] =
+      runSuccessfully(Classes.loadClassEither[ZeroArgumentTarget](classOf[ZeroArgumentTarget].getName, loader))
+    val existingClassFound: Boolean =
+      runSuccessfully(Classes.existsClass(classOf[ZeroArgumentTarget].getName, loader))
+    val missingClassFound: Boolean =
+      runSuccessfully(Classes.existsClass("org_specs2.specs2_common_3.DoesNotExist", loader))
+
+    loaded match {
+      case Right(klass) => assertThat(klass).isEqualTo(classOf[ZeroArgumentTarget])
+      case Left(error)  => fail(s"Expected class loading to succeed, but got $error")
+    }
+    assertThat(existingClassFound).isTrue()
+    assertThat(missingClassFound).isFalse()
+  }
+
+  @Test
+  def createInstanceFromClassUsesZeroArgumentConstructor(): Unit = {
+    val instance: ZeroArgumentTarget =
+      runSuccessfully(Classes.createInstanceFromClass(classOf[ZeroArgumentTarget], loader))
+
+    assertThat(instance.message).isEqualTo("created with no arguments")
+  }
+
+  @Test
+  def createInstanceEitherUsesAvailableConstructor(): Unit = {
+    val created: Either[Throwable, ZeroArgumentTarget] =
+      runSuccessfully(Classes.createInstanceEither[ZeroArgumentTarget](classOf[ZeroArgumentTarget].getName, loader))
+
+    created match {
+      case Right(instance) => assertThat(instance.message).isEqualTo("created with no arguments")
+      case Left(error)     => fail(s"Expected instance creation to succeed, but got $error")
+    }
+  }
+
+  @Test
+  def createInstanceFromClassReturnsScalaObjectModuleInstance(): Unit = {
+    val singletonClass: Class[AnyRef] = SingletonTarget.getClass.asInstanceOf[Class[AnyRef]]
+    val instance: AnyRef = runSuccessfully(Classes.createInstanceFromClass[AnyRef](singletonClass, loader))
+
+    assertThat(instance).isSameAs(SingletonTarget)
+  }
+
+  @Test
+  def createInstanceFromClassBuildsMissingConstructorParameter(): Unit = {
+    val instance: RequiresConstructibleParameter =
+      runSuccessfully(Classes.createInstanceFromClass(classOf[RequiresConstructibleParameter], loader))
+
+    assertThat(instance.dependency).isNotNull()
+    assertThat(instance.dependency.token).isEqualTo("constructed dependency")
+  }
+
+  @Test
+  def createInstanceFromClassUsesProvidedDefaultConstructorParameter(): Unit = {
+    val dependency: ProvidedDependency = new ProvidedDependency("provided dependency")
+    val instance: RequiresProvidedParameter =
+      runSuccessfully(Classes.createInstanceFromClass(classOf[RequiresProvidedParameter], loader, List(dependency)))
+
+    assertThat(instance.dependency).isSameAs(dependency)
+    assertThat(instance.dependency.token).isEqualTo("provided dependency")
+  }
+
+  private def runSuccessfully[A](operation: Operation[A]): A =
+    runOperation(operation) match {
+      case Right(value) => value
+      case Left(error)  => fail(s"Expected specs2 operation to succeed, but got $error")
+    }
+}
+
+final class ZeroArgumentTarget {
+  val message: String = "created with no arguments"
+}
+
+object SingletonTarget {
+  val message: String = "singleton module"
+}
+
+final class ConstructibleDependency {
+  val token: String = "constructed dependency"
+}
+
+final class RequiresConstructibleParameter(val dependency: ConstructibleDependency)
+
+final class ProvidedDependency(val token: String)
+
+final class RequiresProvidedParameter(val dependency: ProvidedDependency)
