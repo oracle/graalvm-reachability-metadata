2cef71ae09f11af0a0334b910735f880f44b51d4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 09:07:10 +0200

    Scaffold org.apache.directory.server:apacheds-kerberos-codec:2.0.0-M15

---
+++ tests/src/org.apache.directory.server/apacheds-kerberos-codec/2.0.0-M15/src/test/java/org_apache_directory_server/apacheds_kerberos_codec/Apacheds_kerberos_codecTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_directory_server.apacheds_kerberos_codec;
+
+import org.junit.jupiter.api.Test;
+
+class Apacheds_kerberos_codecTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
