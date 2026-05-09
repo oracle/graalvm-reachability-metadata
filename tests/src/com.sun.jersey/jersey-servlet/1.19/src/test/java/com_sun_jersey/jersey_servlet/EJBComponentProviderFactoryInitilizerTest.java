4409bf42ede5604dbf2996999a4d630663cbbd0e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 02:16:31 +0200

    Dynamic-access coverage for com.sun.jersey.server.impl.managedbeans.ManagedBeanComponentProviderFactory$ManagedBeanComponentProvider (8/11)

--- tests/src/com.sun.jersey/jersey-servlet/1.19/src/test/java/com_sun_jersey/jersey_servlet/EJBComponentProviderFactoryInitilizerTest.java
+++ tests/src/com.sun.jersey/jersey-servlet/1.19/src/test/java/com_sun_jersey/jersey_servlet/EJBComponentProviderFactoryInitilizerTest.java
@@ -10,20 +10,7 @@ import com.sun.jersey.api.core.DefaultResourceConfig;
 import com.sun.jersey.api.core.ResourceConfig;
 import com.sun.jersey.server.impl.ejb.EJBComponentProviderFactoryInitilizer;
 import com.sun.jersey.server.impl.ejb.EJBExceptionMapper;
-import java.util.Hashtable;
-import java.util.concurrent.atomic.AtomicReference;
-import javax.naming.Binding;
-import javax.naming.CompositeName;
-import javax.naming.Context;
-import javax.naming.Name;
-import javax.naming.NameClassPair;
-import javax.naming.NameNotFoundException;
-import javax.naming.NameParser;
-import javax.naming.NamingEnumeration;
 import javax.naming.NamingException;
-import javax.naming.spi.InitialContextFactory;
-import javax.naming.spi.InitialContextFactoryBuilder;
-import javax.naming.spi.NamingManager;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -31,20 +18,18 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class EJBComponentProviderFactoryInitilizerTest {
-    private static final String INTERCEPTOR_BINDER_NAME = "java:org.glassfish.ejb.container.interceptor_binding_spi";
-    private static final AtomicReference<InterceptorBinder> INTERCEPTOR_BINDER = new AtomicReference<InterceptorBinder>();
-
     private InterceptorBinder interceptorBinder;
 
     @BeforeAll
     static void installInitialContextFactory() throws NamingException {
-        NamingManager.setInitialContextFactoryBuilder(new TestInitialContextFactoryBuilder());
+        ManagedBeanComponentProviderFactoryInnerManagedBeanComponentProviderTest.InitialContextSupport.install();
     }
 
     @BeforeEach
     void setUp() {
         interceptorBinder = new InterceptorBinder();
-        INTERCEPTOR_BINDER.set(interceptorBinder);
+        ManagedBeanComponentProviderFactoryInnerManagedBeanComponentProviderTest.InitialContextSupport
+                .setInterceptorBinder(interceptorBinder);
     }
 
     @Test
@@ -76,169 +61,4 @@ public class EJBComponentProviderFactoryInitilizerTest {
         }
     }
 
-    private static final class TestInitialContextFactoryBuilder implements InitialContextFactoryBuilder {
-        @Override
-        public InitialContextFactory createInitialContextFactory(Hashtable<?, ?> environment) {
-            return new TestInitialContextFactory();
-        }
-    }
-
-    private static final class TestInitialContextFactory implements InitialContextFactory {
-        @Override
-        public Context getInitialContext(Hashtable<?, ?> environment) {
-            return new TestContext();
-        }
-    }
-
-    private static final class TestContext implements Context {
-        @Override
-        public Object lookup(Name name) throws NamingException {
-            return lookup(name.toString());
-        }
-
-        @Override
-        public Object lookup(String name) throws NamingException {
-            if (INTERCEPTOR_BINDER_NAME.equals(name)) {
-                return INTERCEPTOR_BINDER.get();
-            }
-            throw new NameNotFoundException(name);
-        }
-
-        @Override
-        public void bind(Name name, Object obj) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void bind(String name, Object obj) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void rebind(Name name, Object obj) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void rebind(String name, Object obj) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void unbind(Name name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void unbind(String name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void rename(Name oldName, Name newName) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void rename(String oldName, String newName) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public NamingEnumeration<NameClassPair> list(Name name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public NamingEnumeration<NameClassPair> list(String name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public NamingEnumeration<Binding> listBindings(Name name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public NamingEnumeration<Binding> listBindings(String name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void destroySubcontext(Name name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public void destroySubcontext(String name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Context createSubcontext(Name name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Context createSubcontext(String name) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Object lookupLink(Name name) throws NamingException {
-            return lookup(name);
-        }
-
-        @Override
-        public Object lookupLink(String name) throws NamingException {
-            return lookup(name);
-        }
-
-        @Override
-        public NameParser getNameParser(Name name) {
-            return CompositeName::new;
-        }
-
-        @Override
-        public NameParser getNameParser(String name) {
-            return CompositeName::new;
-        }
-
-        @Override
-        public Name composeName(Name name, Name prefix) throws NamingException {
-            Name composedName = (Name) prefix.clone();
-            composedName.addAll(name);
-            return composedName;
-        }
-
-        @Override
-        public String composeName(String name, String prefix) {
-            return prefix + name;
-        }
-
-        @Override
-        public Object addToEnvironment(String propName, Object propVal) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Object removeFromEnvironment(String propName) {
-            throw new UnsupportedOperationException();
-        }
-
-        @Override
-        public Hashtable<?, ?> getEnvironment() {
-            return new Hashtable<Object, Object>();
-        }
-
-        @Override
-        public void close() {
-            // No resources to release.
-        }
-
-        @Override
-        public String getNameInNamespace() {
-            return "";
-        }
-    }
 }
