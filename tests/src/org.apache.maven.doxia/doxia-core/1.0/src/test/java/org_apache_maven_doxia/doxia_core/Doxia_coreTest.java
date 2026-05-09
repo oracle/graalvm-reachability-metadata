1c775e1ee6be22ec077ba0e7e2cb7bd944411d0f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 19:57:19 +0200

    Dynamic-access coverage for org.apache.maven.doxia.sink.PipelineSink (3/10)

--- tests/src/org.apache.maven.doxia/doxia-core/1.0/src/test/java/org_apache_maven_doxia/doxia_core/Doxia_coreTest.java
+++ tests/src/org.apache.maven.doxia/doxia-core/1.0/src/test/java/org_apache_maven_doxia/doxia_core/Doxia_coreTest.java
@@ -8,7 +8,7 @@ package org_apache_maven_doxia.doxia_core;
 
 import org.junit.jupiter.api.Test;
 
-class Doxia_coreTest {
+public class Doxia_coreTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
