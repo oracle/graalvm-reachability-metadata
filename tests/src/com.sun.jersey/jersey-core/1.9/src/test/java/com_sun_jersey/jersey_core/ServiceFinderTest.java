6df39eca256e3a3246fa3063751c186c930eb111
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 12:39:19 +0200

    Dynamic-access coverage for com.sun.jersey.spi.service.ServiceFinder (35/59)

--- tests/src/com.sun.jersey/jersey-core/1.9/src/test/java/com_sun_jersey/jersey_core/ServiceFinderTest.java
+++ tests/src/com.sun.jersey/jersey-core/1.9/src/test/java/com_sun_jersey/jersey_core/ServiceFinderTest.java
@@ -10,16 +10,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import com.sun.jersey.spi.service.ServiceFinder;
 import java.io.IOException;
-import java.io.InputStream;
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.util.Collections;
 import java.util.Enumeration;
-import org.graalvm.internal.tck.NativeImageSupport;
 import org.junit.jupiter.api.Test;
 
 public class ServiceFinderTest {
-    private static final String SERVICE_FINDER_CLASS_NAME = "com.sun.jersey.spi.service.ServiceFinder";
-    private static final String SERVICE_FINDER_RESOURCE_NAME = "com/sun/jersey/spi/service/ServiceFinder.class";
+    private static final String MISSING_RESOURCE_NAME = "META-INF/services/com_sun_jersey.missing.ServiceFinderTest";
 
     @Test
     void createsTypedArraysWhenNoServiceConfigurationExists() {
@@ -36,18 +34,13 @@ public class ServiceFinderTest {
     }
 
     @Test
-    void initializesIsolatedCopyWhenItsClassResourceIsHidden() throws Exception {
-        try {
-            final ClassLoader classLoader = new ServiceFinderResourceHidingClassLoader();
-            final Class<?> serviceFinderClass = Class.forName(SERVICE_FINDER_CLASS_NAME, true, classLoader);
+    void resolvesMissingResourceWithServiceFinderClassLoaderFallback() throws Exception {
+        final Method getResource = ServiceFinder.class.getDeclaredMethod("getResource", String.class);
+        getResource.setAccessible(true);
 
-            assertThat(serviceFinderClass.getName()).isEqualTo(SERVICE_FINDER_CLASS_NAME);
-            assertThat(serviceFinderClass.getClassLoader()).isSameAs(classLoader);
-        } catch (final Error error) {
-            if (!NativeImageSupport.isUnsupportedFeatureError(error)) {
-                throw error;
-            }
-        }
+        final URL resource = (URL) getResource.invoke(null, MISSING_RESOURCE_NAME);
+
+        assertThat(resource).isNull();
     }
 
     public interface TestService {
@@ -61,55 +54,6 @@ public class ServiceFinderTest {
         @Override
         public Enumeration<URL> getResources(final String name) throws IOException {
             return Collections.emptyEnumeration();
-        }
-    }
-
-    private static final class ServiceFinderResourceHidingClassLoader extends ClassLoader {
-        private ServiceFinderResourceHidingClassLoader() {
-            super(ServiceFinderTest.class.getClassLoader());
-        }
-
-        @Override
-        protected Class<?> loadClass(final String name, final boolean resolve) throws ClassNotFoundException {
-            synchronized (getClassLoadingLock(name)) {
-                Class<?> loadedClass = findLoadedClass(name);
-                if (loadedClass == null) {
-                    if (SERVICE_FINDER_CLASS_NAME.equals(name)) {
-                        loadedClass = findClass(name);
-                    } else {
-                        loadedClass = super.loadClass(name, false);
-                    }
-                }
-                if (resolve) {
-                    resolveClass(loadedClass);
-                }
-                return loadedClass;
-            }
-        }
-
-        @Override
-        protected Class<?> findClass(final String name) throws ClassNotFoundException {
-            if (!SERVICE_FINDER_CLASS_NAME.equals(name)) {
-                return super.findClass(name);
-            }
-
-            try (InputStream inputStream = getParent().getResourceAsStream(SERVICE_FINDER_RESOURCE_NAME)) {
-                if (inputStream == null) {
-                    throw new ClassNotFoundException(name);
-                }
-                final byte[] bytes = inputStream.readAllBytes();
-                return defineClass(name, bytes, 0, bytes.length);
-            } catch (final IOException exception) {
-                throw new ClassNotFoundException(name, exception);
-            }
-        }
-
-        @Override
-        public URL getResource(final String name) {
-            if (SERVICE_FINDER_RESOURCE_NAME.equals(name)) {
-                return null;
-            }
-            return super.getResource(name);
         }
     }
 }
