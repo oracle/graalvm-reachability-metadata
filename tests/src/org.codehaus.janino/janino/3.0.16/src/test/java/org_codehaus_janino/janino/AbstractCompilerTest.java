b8eee2dc2de34908b292fe9f8404d8e116ac0a30
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-09 06:02:43 +0200

    Dynamic-access coverage for org.codehaus.janino.AbstractCompiler (21/24)

---
+++ tests/src/org.codehaus.janino/janino/3.0.16/src/test/java/org_codehaus_janino/janino/AbstractCompilerTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_codehaus_janino.janino;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+
+import org.codehaus.commons.compiler.ICompiler;
+import org.codehaus.janino.Compiler;
+import org.junit.jupiter.api.Test;
+
+public class AbstractCompilerTest {
+    @Test
+    void configuresClasspathThroughCompilerFacade() {
+        final Compiler compiler = new Compiler();
+
+        compiler.setClassPath(new File[0]);
+
+        assertThat(compiler).isInstanceOf(ICompiler.class);
+    }
+}
