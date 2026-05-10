53cb1edf4ef113970d1b2b49d567c7f836b41bd4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-10 10:32:43 +0200

    Scaffold tools.jackson.datatype:jackson-datatype-jsr310:3.0.0-rc2

---
+++ tests/src/tools.jackson.datatype/jackson-datatype-jsr310/3.0.0-rc2/src/test/java/tools_jackson_datatype/jackson_datatype_jsr310/Jackson_datatype_jsr310Test.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package tools_jackson_datatype.jackson_datatype_jsr310;
+
+import org.junit.jupiter.api.Test;
+
+class Jackson_datatype_jsr310Test {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
