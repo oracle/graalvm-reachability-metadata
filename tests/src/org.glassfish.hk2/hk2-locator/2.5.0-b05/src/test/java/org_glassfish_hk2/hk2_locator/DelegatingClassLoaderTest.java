7cb225cdfe886ef3290c47ae1347cb75a81455c8
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-11 23:20:15 +0200

    Dynamic-access coverage for org.jvnet.hk2.internal.ProxyUtilities$3 (9/12)

---
+++ tests/src/org.glassfish.hk2/hk2-locator/2.5.0-b05/src/test/java/org_glassfish_hk2/hk2_locator/DelegatingClassLoaderTest.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_glassfish_hk2.hk2_locator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+import java.net.URL;
+
+import org.jvnet.hk2.internal.ProxyUtilities;
+import org.jvnet.hk2.internal.ServiceLocatorImpl;
+import org.junit.jupiter.api.Test;
+
+public class DelegatingClassLoaderTest {
+    @Test
+    void proxyClassLoaderDelegatesClassAndResourceLookups() throws Exception {
+        final ServiceLocatorImpl locator = new ServiceLocatorImpl("delegating-class-loader-test", null);
+        try {
+            final GreetingService service = new ProxyUtilities().generateProxy(
+                    GreetingService.class,
+                    locator,
+                    null,
+                    null,
+                    null);
+
+            assertThat(GreetingService.class.isInstance(service)).isTrue();
+
+            final ClassLoader proxyLoader = service.getClass().getClassLoader();
+            assertThat(proxyLoader.toString()).contains("DelegatingClassLoader");
+
+            final Class<?> classFromParent = proxyLoader.loadClass(GreetingService.class.getName());
+            assertThat(classFromParent).isEqualTo(GreetingService.class);
+
+            assertThatExceptionOfType(ClassNotFoundException.class)
+                    .isThrownBy(() -> proxyLoader.loadClass("example.hk2.DoesNotExist"));
+
+            final URL missingResource = proxyLoader.getResource("example/hk2/missing-resource.txt");
+            assertThat(missingResource).isNull();
+        } finally {
+            locator.shutdown();
+        }
+    }
+
+    public interface GreetingService {
+        String greet();
+    }
+
+}
---
+++ tests/src/org.glassfish.hk2/hk2-locator/2.5.0-b05/src/test/java/org_glassfish_hk2/hk2_locator/ProxyUtilitiesAnonymous3Test.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_glassfish_hk2.hk2_locator;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.glassfish.hk2.api.ProxyCtl;
+import org.junit.jupiter.api.Test;
+import org.jvnet.hk2.internal.ProxyUtilities;
+import org.jvnet.hk2.internal.ServiceLocatorImpl;
+
+public class ProxyUtilitiesAnonymous3Test {
+    @Test
+    void generateProxyForInterfaceUsesJdkProxyCreationPath() {
+        final ServiceLocatorImpl locator = new ServiceLocatorImpl("proxy-utilities-anonymous-3-test", null);
+        try {
+            final MarkerContract proxy = new ProxyUtilities().generateProxy(
+                    MarkerContract.class,
+                    locator,
+                    null,
+                    null,
+                    null);
+
+            assertThat(proxy).isNotNull();
+            assertThat(MarkerContract.class.isInstance(proxy)).isTrue();
+            assertThat(ProxyCtl.class.isInstance(proxy)).isTrue();
+            assertThat(proxy.getClass().getClassLoader().toString()).contains("DelegatingClassLoader");
+        } finally {
+            locator.shutdown();
+        }
+    }
+
+    public interface MarkerContract {
+    }
+}
