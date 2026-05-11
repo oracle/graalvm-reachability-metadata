c47e0f1e2aee1a10074a8a2a209c8f5410baf23c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-11 08:41:46 +0200

    Checkpoint generated tests for org.apache.groovy:groovy-groovysh:4.0.24 (2)

--- tests/src/org.apache.groovy/groovy-groovysh/4.0.24/src/test/groovy/org_apache_groovy/groovy_groovysh/Groovy_groovyshTest.groovy
+++ tests/src/org.apache.groovy/groovy-groovysh/4.0.24/src/test/groovy/org_apache_groovy/groovy_groovysh/Groovy_groovyshTest.groovy
@@ -6,11 +6,70 @@
  */
 package org_apache_groovy.groovy_groovysh
 
+import org.apache.groovy.groovysh.BufferManager
 import org.junit.jupiter.api.Test
 
-class Groovy_groovyshTest {
+import static org.assertj.core.api.Assertions.assertThat
+
+public class Groovy_groovyshTest {
     @Test
-    void test() {
-        println "This is just a placeholder, implement your test"
+    void keepsSeparateContentsForSelectableShellBuffers() {
+        BufferManager manager = new BufferManager()
+
+        assertThat(manager.size()).isEqualTo(1)
+        assertThat(manager.selected).isZero()
+        assertThat(manager.current()).isEmpty()
+
+        manager.current().add('first buffer line')
+        int secondIndex = manager.create(true)
+        manager.current().add('second buffer line')
+
+        assertThat(secondIndex).isEqualTo(1)
+        assertThat(manager.selected).isEqualTo(secondIndex)
+        assertThat(manager.current()).containsExactly('second buffer line')
+
+        manager.select(0)
+        assertThat(manager.current()).containsExactly('first buffer line')
+
+        manager.updateSelected(['replacement line'])
+        assertThat(manager.current()).containsExactly('replacement line')
+
+        manager.clearSelected()
+        assertThat(manager.current()).isEmpty()
+
+        manager.select(secondIndex)
+        assertThat(manager.current()).containsExactly('second buffer line')
+    }
+
+    @Test
+    void createsUnselectedBuffersAndDeletesSelectedBufferByMovingBackward() {
+        BufferManager manager = new BufferManager()
+        manager.current().add('initial buffer line')
+
+        int unselectedIndex = manager.create(false)
+
+        assertThat(unselectedIndex).isEqualTo(1)
+        assertThat(manager.selected).isZero()
+        assertThat(manager.size()).isEqualTo(2)
+
+        manager.select(unselectedIndex)
+        manager.current().add('unselected buffer line')
+        int selectedIndex = manager.create(true)
+        manager.current().add('selected buffer line')
+
+        assertThat(selectedIndex).isEqualTo(2)
+        assertThat(manager.selected).isEqualTo(selectedIndex)
+
+        manager.deleteSelected()
+
+        assertThat(manager.size()).isEqualTo(2)
+        assertThat(manager.selected).isEqualTo(1)
+        assertThat(manager.current()).containsExactly('unselected buffer line')
+
+        manager.deleteSelected()
+
+        assertThat(manager.size()).isEqualTo(1)
+        assertThat(manager.selected).isZero()
+        assertThat(manager.current()).containsExactly('initial buffer line')
     }
 }
