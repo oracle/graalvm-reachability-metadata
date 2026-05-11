9e668092b886772cdbe37f9e98dc84f0e858a36a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-11 19:31:13 +0200

    Scaffold com.squareup.okhttp3:logging-interceptor:4.12.0

---
+++ tests/src/com.squareup.okhttp3/logging-interceptor/4.12.0/src/test/kotlin/com_squareup_okhttp3/logging_interceptor/Logging_interceptorTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_squareup_okhttp3.logging_interceptor
+
+import org.junit.jupiter.api.Test
+
+class Logging_interceptorTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
