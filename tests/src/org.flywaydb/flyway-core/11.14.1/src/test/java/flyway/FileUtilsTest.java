513917a7288d1c57f323749bcf2b231912d773ff
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-11 19:55:38 +0200

    Dynamic-access coverage for org.flywaydb.core.internal.util.FileUtils (43/43)

---
+++ tests/src/org.flywaydb/flyway-core/11.14.1/src/test/java/flyway/FileUtilsTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package flyway;
+
+import org.flywaydb.core.internal.util.FileUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FileUtilsTest {
+    private static final String MIGRATION_RESOURCE = "db/migration/V1__create_table.sql";
+
+    @Test
+    void readsResourceAsStringWithProvidedClassLoader() {
+        ClassLoader classLoader = FileUtilsTest.class.getClassLoader();
+
+        String migrationSql = FileUtils.readResourceAsString(classLoader, MIGRATION_RESOURCE);
+
+        assertThat(migrationSql).contains("CREATE TABLE test")
+                .contains("title VARCHAR NOT NULL");
+    }
+}
