83ba5b152d076714bdfc6eeaf03170083203650f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-11 14:29:49 +0100

    [GenAI] Test fix for org.glassfish.jersey.containers:jersey-container-servlet:4.0.1 using gpt-5.5 (#6792)
    * Prepare project for org.glassfish.jersey.containers:jersey-container-servlet:4.0.1
    
    * Dynamic-access coverage for org.glassfish.jersey.servlet.internal.ThreadLocalInvoker (2/2)
    
    * Native metadata for org.glassfish.jersey.servlet.internal.ThreadLocalInvoker
    
    * Preserve last passing candidate for org.glassfish.jersey.containers:jersey-container-servlet:4.0.1
    
    * Update generated library support for org.glassfish.jersey.containers:jersey-container-servlet:4.0.1
    
    * Fixed test for org.glassfish.jersey.containers:jersey-container-servlet:4.0.1

---
+++ tests/src/org.glassfish.jersey.containers/jersey-container-servlet/4.0.1/src/test/java/org_glassfish_jersey_containers/jersey_container_servlet/ThreadLocalInvokerTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_glassfish_jersey_containers.jersey_container_servlet;
+
+import java.lang.reflect.Proxy;
+import java.util.function.Function;
+
+import org.glassfish.jersey.servlet.internal.ThreadLocalInvoker;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ThreadLocalInvokerTest {
+    @Test
+    void proxyDelegatesMethodInvocationToThreadLocalInstance() {
+        ThreadLocalInvoker<Function<String, String>> invoker = new ThreadLocalInvoker<>();
+        invoker.set(name -> "Hello, " + name);
+
+        Function<String, String> proxy = createFunctionProxy(invoker);
+
+        assertThat(proxy.apply("Jersey")).isEqualTo("Hello, Jersey");
+    }
+
+    @SuppressWarnings("unchecked")
+    private static Function<String, String> createFunctionProxy(
+            final ThreadLocalInvoker<Function<String, String>> invoker) {
+        return (Function<String, String>) Proxy.newProxyInstance(
+                ThreadLocalInvokerTest.class.getClassLoader(),
+                new Class<?>[] {Function.class},
+                invoker);
+    }
+}
