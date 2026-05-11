6c9fbbbbd1cd6bb00ce2a1b22c941b98bde15fe4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-11 18:35:06 +0200

    Dynamic-access coverage for retrofit2.Retrofit (2/4)

--- tests/src/com.squareup.retrofit2/retrofit/2.5.0/src/test/kotlin/com_squareup_retrofit2/retrofit/RetrofitTest.kt
+++ tests/src/com.squareup.retrofit2/retrofit/2.5.0/src/test/kotlin/com_squareup_retrofit2/retrofit/RetrofitTest.kt
@@ -6,11 +6,30 @@
  */
 package com_squareup_retrofit2.retrofit
 
+import okhttp3.ResponseBody
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.http.GET
 
-class RetrofitTest {
+public class RetrofitTest {
+    public interface ExampleService {
+        @GET("users")
+        public fun users(): Call<ResponseBody>
+    }
+
     @Test
-    fun test() {
-        println("This is just a placeholder, implement your test")
+    public fun createsServiceProxyAndEagerlyValidatesDeclaredMethods(): Unit {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://example.com/")
+            .validateEagerly(true)
+            .build()
+
+        val service: ExampleService = retrofit.create(ExampleService::class.java)
+        val call: Call<ResponseBody> = service.users()
+
+        assertThat(service).isInstanceOf(ExampleService::class.java)
+        assertThat(call.request().url().toString()).isEqualTo("https://example.com/users")
     }
 }
