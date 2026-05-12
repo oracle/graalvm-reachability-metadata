57a709bd1b30ceb2fd0308552a2195ef46f5d4f6
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-13 00:46:43 +0200

    Scaffold org.apache.maven.wagon:wagon-ssh-external:1.0-beta-2

---
+++ tests/src/org.apache.maven.wagon/wagon-ssh-external/1.0-beta-2/src/test/java/org_apache_maven_wagon/wagon_ssh_external/Wagon_ssh_externalTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven_wagon.wagon_ssh_external;
+
+import org.junit.jupiter.api.Test;
+
+class Wagon_ssh_externalTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
