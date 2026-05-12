5cfec02b8b6ca9f5d707f0740c1e0b24aece5ba1
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-13 01:57:16 +0200

    Dynamic-access coverage for org.codehaus.plexus.archiver.util.ArchiveEntryUtils (3/6)

--- tests/src/org.codehaus.plexus/plexus-archiver/2.1/src/test/java/org_codehaus_plexus/plexus_archiver/Plexus_archiverTest.java
+++ tests/src/org.codehaus.plexus/plexus-archiver/2.1/src/test/java/org_codehaus_plexus/plexus_archiver/Plexus_archiverTest.java
@@ -8,7 +8,7 @@ package org_codehaus_plexus.plexus_archiver;
 
 import org.junit.jupiter.api.Test;
 
-class Plexus_archiverTest {
+public class Plexus_archiverTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
