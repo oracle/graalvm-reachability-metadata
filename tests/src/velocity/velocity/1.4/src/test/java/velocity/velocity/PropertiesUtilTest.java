cd0b74102ff1085d02efa806acb194e680920c80
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-12 18:41:37 +0200

    Dynamic-access coverage for org.apache.velocity.texen.util.PropertiesUtil (48/56)

---
+++ tests/src/velocity/velocity/1.4/src/test/java/velocity/velocity/PropertiesUtilTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package velocity.velocity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Properties;
+
+import org.apache.velocity.texen.util.PropertiesUtil;
+import org.junit.jupiter.api.Test;
+
+public class PropertiesUtilTest {
+    private static final String PROPERTIES_RESOURCE = "velocity/velocity/texen-test.properties";
+
+    @Test
+    public void loadsPropertiesResourceFromClasspath() {
+        ClasspathPropertiesUtil propertiesUtil = new ClasspathPropertiesUtil();
+
+        Properties properties = propertiesUtil.loadClasspathResource(PROPERTIES_RESOURCE);
+
+        assertThat(properties)
+                .containsEntry("texen.resource.loaded", "true")
+                .containsEntry("texen.resource.name", "PropertiesUtil");
+    }
+
+    private static final class ClasspathPropertiesUtil extends PropertiesUtil {
+        private Properties loadClasspathResource(String propertiesFile) {
+            return loadFromClassPath(propertiesFile);
+        }
+    }
+}
