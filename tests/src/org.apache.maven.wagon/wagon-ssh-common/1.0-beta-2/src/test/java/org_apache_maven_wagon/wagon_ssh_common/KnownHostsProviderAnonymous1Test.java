552059f164a8d0d60e7d6e864c21f6a7b5c37937
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-12 23:07:11 +0200

    Dynamic-access coverage for org.apache.maven.wagon.providers.ssh.knownhost.KnownHostsProvider$1 (2/2)

---
+++ tests/src/org.apache.maven.wagon/wagon-ssh-common/1.0-beta-2/src/test/java/org_apache_maven_wagon/wagon_ssh_common/KnownHostsProviderAnonymous1Test.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven_wagon.wagon_ssh_common;
+
+import org.apache.maven.wagon.providers.ssh.knownhost.KnownHostsProvider;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class KnownHostsProviderAnonymous1Test {
+    @Test
+    void roleUsesKnownHostsProviderClassName() {
+        assertThat(KnownHostsProvider.ROLE)
+                .isEqualTo(KnownHostsProvider.class.getName());
+    }
+}
