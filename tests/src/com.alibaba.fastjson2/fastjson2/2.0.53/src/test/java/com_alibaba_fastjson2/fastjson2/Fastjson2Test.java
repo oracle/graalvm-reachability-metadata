b28e5ea79de7c1a1fa668d3b82b3814d29003c23
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-12 17:36:58 +0200

    Scaffold com.alibaba.fastjson2:fastjson2:2.0.53

---
+++ tests/src/com.alibaba.fastjson2/fastjson2/2.0.53/src/test/java/com_alibaba_fastjson2/fastjson2/Fastjson2Test.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_alibaba_fastjson2.fastjson2;
+
+import org.junit.jupiter.api.Test;
+
+class Fastjson2Test {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
