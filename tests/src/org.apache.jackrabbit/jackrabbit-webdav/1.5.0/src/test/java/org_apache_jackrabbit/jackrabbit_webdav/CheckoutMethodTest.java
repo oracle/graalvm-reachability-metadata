b375507b154b97a5c6e1f7ed5877138e857181a1
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-13 08:52:04 +0200

    Dynamic-access coverage for org.apache.jackrabbit.webdav.client.methods.CheckoutMethod (29/83)

---
+++ tests/src/org.apache.jackrabbit/jackrabbit-webdav/1.5.0/src/test/java/org_apache_jackrabbit/jackrabbit_webdav/CheckoutMethodTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_jackrabbit.jackrabbit_webdav;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.apache.jackrabbit.webdav.DavMethods;
+import org.apache.jackrabbit.webdav.client.methods.CheckoutMethod;
+import org.junit.jupiter.api.Test;
+
+public class CheckoutMethodTest {
+    @Test
+    public void constructorCreatesCheckoutRequest() {
+        ExposedCheckoutMethod method = new ExposedCheckoutMethod("http://localhost:8080/repository/document.txt");
+
+        assertThat(method.getName()).isEqualTo(DavMethods.METHOD_CHECKOUT);
+        assertThat(method.getPath()).isEqualTo("/repository/document.txt");
+        assertThat(method.isSuccessfulStatus(200)).isTrue();
+        assertThat(method.isSuccessfulStatus(201)).isFalse();
+        assertThat(method.isSuccessfulStatus(204)).isFalse();
+    }
+
+    private static final class ExposedCheckoutMethod extends CheckoutMethod {
+        private ExposedCheckoutMethod(String uri) {
+            super(uri);
+        }
+
+        private boolean isSuccessfulStatus(int statusCode) {
+            return isSuccess(statusCode);
+        }
+    }
+}
