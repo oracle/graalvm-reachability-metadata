cbdf08a7ff8b02dceba44b06b81759fcb01b45b4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-18 20:57:55 +0200

    Preserve last passing candidate for org.sonatype.plexus:plexus-cipher:1.6

--- tests/src/org.sonatype.plexus/plexus-cipher/1.6/src/test/java/org_sonatype_plexus/plexus_cipher/PlexusCipherAnonymous1Test.java
+++ tests/src/org.sonatype.plexus/plexus-cipher/1.6/src/test/java/org_sonatype_plexus/plexus_cipher/PlexusCipherAnonymous1Test.java
@@ -9,11 +9,22 @@ package org_sonatype_plexus.plexus_cipher;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
+import org.sonatype.plexus.components.cipher.DefaultPlexusCipher;
 import org.sonatype.plexus.components.cipher.PlexusCipher;
+import org.sonatype.plexus.components.cipher.PlexusCipherException;
 
 public class PlexusCipherAnonymous1Test {
     @Test
-    void roleConstantInitializesPlexusCipherInterface() {
-        assertThat(PlexusCipher.ROLE).isEqualTo(PlexusCipher.class.getName());
+    void defaultCipherDecoratesAndUndecoratesThroughPlexusCipherInterface() throws PlexusCipherException {
+        PlexusCipher cipher = new DefaultPlexusCipher();
+
+        String value = "secret";
+        String decorated = cipher.decorate(value);
+
+        assertThat(decorated).startsWith(String.valueOf(PlexusCipher.ENCRYPTED_STRING_DECORATION_START));
+        assertThat(decorated).endsWith(String.valueOf(PlexusCipher.ENCRYPTED_STRING_DECORATION_STOP));
+        assertThat(cipher.isEncryptedString(decorated)).isTrue();
+        assertThat(cipher.unDecorate(decorated)).isEqualTo(value);
+        assertThat(cipher.isEncryptedString(value)).isFalse();
     }
 }
