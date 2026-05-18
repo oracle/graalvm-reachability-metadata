575fe33594031048352747e510ab8b3174c76b28
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-19 01:08:46 +0200

    Dynamic-access coverage for org.springframework.amqp.rabbit.listener.DirectMessageListenerContainer (4/8)

---
+++ tests/src/org.springframework.amqp/spring-rabbit/4.0.2/src/test/java/org_springframework_amqp/spring_rabbit/DirectMessageListenerContainerTest.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_springframework_amqp.spring_rabbit;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.listener.DirectMessageListenerContainer;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DirectMessageListenerContainerTest {
+
+    @Test
+    void createsLocalRabbitAdminWhenMissingQueuesAreFatalAndNoAdminIsConfigured() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory("localhost");
+        TestDirectMessageListenerContainer container = new TestDirectMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setMissingQueuesFatal(true);
+
+        try {
+            container.start();
+
+            AmqpAdmin amqpAdmin = container.getConfiguredAmqpAdmin();
+            assertThat(amqpAdmin).isInstanceOf(RabbitAdmin.class);
+        }
+        finally {
+            container.stop();
+            connectionFactory.destroy();
+        }
+    }
+
+    private static final class TestDirectMessageListenerContainer extends DirectMessageListenerContainer {
+
+        AmqpAdmin getConfiguredAmqpAdmin() {
+            return getAmqpAdmin();
+        }
+
+    }
+
+}
