14bf689845103f04acdd7ea46a5805779b7a460f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-19 18:14:51 +0200

    Scaffold tools.jackson.datatype:jackson-datatype-joda:3.0.4

---
+++ tests/src/tools.jackson.datatype/jackson-datatype-joda/3.0.4/src/test/java/tools_jackson_datatype/jackson_datatype_joda/Jackson_datatype_jodaTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package tools_jackson_datatype.jackson_datatype_joda;
+
+import org.junit.jupiter.api.Test;
+
+class Jackson_datatype_jodaTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
