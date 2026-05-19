7484d4cd40477762572184b571389857ff130a49
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-19 19:54:25 +0200

    Dynamic-access coverage for org.apache.pekko.protobufv3.internal.ExtensionSchemas (53/54)

--- tests/src/org.apache.pekko/pekko-protobuf-v3_2.13/2.0.0-M2/src/test/scala/org_apache_pekko/pekko_protobuf_v3_2_13/ExtensionSchemasTest.scala
+++ tests/src/org.apache.pekko/pekko-protobuf-v3_2.13/2.0.0-M2/src/test/scala/org_apache_pekko/pekko_protobuf_v3_2_13/ExtensionSchemasTest.scala
@@ -6,6 +6,11 @@
  */
 package org_apache_pekko.pekko_protobuf_v3_2_13
 
+import java.lang.invoke.MethodHandle
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType.methodType
+
+import org.apache.pekko.protobufv3.internal.DescriptorProtos
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -17,5 +22,32 @@ class ExtensionSchemasTest {
     message.parseEmptyInputThroughSchema()
 
     assertThat(message.getDescriptorForType.getFile.toProto.getSyntax).isEqualTo("proto2")
+  }
+
+  @Test
+  def fullRuntimeSchemaFactoryLoadsExtensionSchemaFullConstructor(): Unit = {
+    val protobufClass: Class[?] = Class.forName("org.apache.pekko.protobufv3.internal.Protobuf")
+    val schemaClass: Class[?] = Class.forName("org.apache.pekko.protobufv3.internal.Schema")
+    val lookup: MethodHandles.Lookup = MethodHandles.privateLookupIn(
+      protobufClass,
+      MethodHandles.lookup()
+    )
+    val getInstance: MethodHandle = lookup.findStatic(
+      protobufClass,
+      "getInstance",
+      methodType(protobufClass)
+    )
+    val schemaFor: MethodHandle = lookup.findVirtual(
+      protobufClass,
+      "schemaFor",
+      methodType(schemaClass, classOf[Class[_]])
+    )
+    val protobuf: Object = getInstance.invokeWithArguments()
+    val schema: Object = schemaFor.invokeWithArguments(
+      protobuf,
+      classOf[DescriptorProtos.FileDescriptorSet]
+    )
+
+    assertThat(schema).isNotNull
   }
 }
