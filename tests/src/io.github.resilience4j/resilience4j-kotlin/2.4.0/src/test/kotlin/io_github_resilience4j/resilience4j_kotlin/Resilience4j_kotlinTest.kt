92f980a19a9f6fc07b4c2b6215304a61159ee713
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-20 03:09:52 +0200

    Scaffold io.github.resilience4j:resilience4j-kotlin:2.4.0

---
+++ tests/src/io.github.resilience4j/resilience4j-kotlin/2.4.0/src/test/kotlin/io_github_resilience4j/resilience4j_kotlin/Resilience4j_kotlinTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_github_resilience4j.resilience4j_kotlin
+
+import org.junit.jupiter.api.Test
+
+class Resilience4j_kotlinTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
