b38992b10a06e799f65b7ca21c56297e6c216cb3
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-20 13:57:33 +0200

    Checkpoint generated tests for org.wildfly.common:wildfly-common-jdk9-supplement:1.0.Final (2)

--- tests/src/org.wildfly.common/wildfly-common-jdk9-supplement/1.0.Final/src/test/java/org_wildfly_common/wildfly_common_jdk9_supplement/Wildfly_common_jdk9_supplementTest.java
+++ tests/src/org.wildfly.common/wildfly-common-jdk9-supplement/1.0.Final/src/test/java/org_wildfly_common/wildfly_common_jdk9_supplement/Wildfly_common_jdk9_supplementTest.java
@@ -11,10 +11,34 @@ import static org.wildfly.common.cpu.ProcessorInfo.availableProcessors;
 import static org.wildfly.common.os.Process.getProcessId;
 import static org.wildfly.common.os.Process.getProcessName;
 
+import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 
+@TestMethodOrder(OrderAnnotation.class)
 public class Wildfly_common_jdk9_supplementTest {
+    private static final String JBOSS_PROCESS_NAME_PROPERTY = "jboss.process.name";
+
     @Test
+    @Order(1)
+    void processNameUsesConfiguredJbossProcessName() {
+        String configuredProcessName = "wildfly-common-jdk9-supplement-test";
+        String originalProcessName = System.getProperty(JBOSS_PROCESS_NAME_PROPERTY);
+        System.setProperty(JBOSS_PROCESS_NAME_PROPERTY, configuredProcessName);
+        try {
+            assertThat(getProcessName()).isEqualTo(configuredProcessName);
+        } finally {
+            if (originalProcessName == null) {
+                System.clearProperty(JBOSS_PROCESS_NAME_PROPERTY);
+            } else {
+                System.setProperty(JBOSS_PROCESS_NAME_PROPERTY, originalProcessName);
+            }
+        }
+    }
+
+    @Test
+    @Order(2)
     void processorInfoReportsTheRuntimeProcessorCount() {
         int runtimeProcessors = Runtime.getRuntime().availableProcessors();
         int wildflyProcessors = availableProcessors();
@@ -24,6 +48,7 @@ public class Wildfly_common_jdk9_supplementTest {
     }
 
     @Test
+    @Order(3)
     void processIdMatchesTheCurrentJavaProcess() {
         long processId = getProcessId();
 
@@ -33,6 +58,7 @@ public class Wildfly_common_jdk9_supplementTest {
     }
 
     @Test
+    @Order(4)
     void processNameIsResolvedAndStable() {
         String processName = getProcessName();
 
