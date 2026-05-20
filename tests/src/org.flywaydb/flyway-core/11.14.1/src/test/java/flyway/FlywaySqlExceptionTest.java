f86ef85e7efa99cacc3a77812c5167214a6aeccf
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-05-20 11:04:09 +0200

    Issue-requested metadata coverage for org.flywaydb:flyway-core:11.14.1

--- tests/src/org.flywaydb/flyway-core/11.14.1/src/test/java/flyway/FlywaySqlExceptionTest.java
+++ tests/src/org.flywaydb/flyway-core/11.14.1/src/test/java/flyway/FlywaySqlExceptionTest.java
@@ -6,26 +6,34 @@
  */
 package flyway;
 
+import java.io.PrintWriter;
 import java.security.cert.CertPathBuilderException;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
+import java.util.logging.Logger;
 
-import org.flywaydb.core.internal.exception.FlywaySqlException;
+import javax.sql.DataSource;
+
+import org.flywaydb.core.Flyway;
 import org.flywaydb.core.internal.exception.sqlExceptions.FlywaySqlNoDriversForInteractiveAuthException;
 import org.flywaydb.core.internal.exception.sqlExceptions.FlywaySqlNoIntegratedAuthException;
 import org.flywaydb.core.internal.exception.sqlExceptions.FlywaySqlServerUntrustedCertificateSqlException;
+import org.flywaydb.core.internal.exception.sqlExceptions.FlywaySqlUnableToConnectToDbException;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 public class FlywaySqlExceptionTest {
 
     @Test
-    void checksEverySpecificSqlExceptionMatcherBeforeIgnoringUnrecognizedSqlExceptions() {
+    void checksEverySpecificSqlExceptionMatcherBeforeReportingUnrecognizedConnectionFailures() {
         SQLException sqlException = new SQLException("ordinary database failure", "42000");
 
-        assertThatCode(() -> FlywaySqlException.throwFlywayExceptionIfPossible(sqlException, null))
-                .doesNotThrowAnyException();
+        assertThatThrownBy(() -> migrateWithFailingDataSource(sqlException))
+                .isInstanceOf(FlywaySqlUnableToConnectToDbException.class)
+                .hasCause(sqlException)
+                .hasMessageContaining("Unable to obtain connection from database");
     }
 
     @Test
@@ -35,9 +43,10 @@ public class FlywaySqlExceptionTest {
                 "08S01",
                 new CertPathBuilderException("unable to build certification path"));
 
-        assertThatThrownBy(() -> FlywaySqlException.throwFlywayExceptionIfPossible(sqlException, null))
+        assertThatThrownBy(() -> migrateWithFailingDataSource(sqlException))
                 .isInstanceOf(FlywaySqlServerUntrustedCertificateSqlException.class)
-                .hasCause(sqlException);
+                .hasCause(sqlException)
+                .hasMessageContaining("The server certificate is not trusted");
     }
 
     @Test
@@ -46,17 +55,83 @@ public class FlywaySqlExceptionTest {
                 "This driver is not configured for integrated authentication",
                 "08S01");
 
-        assertThatThrownBy(() -> FlywaySqlException.throwFlywayExceptionIfPossible(sqlException, null))
+        assertThatThrownBy(() -> migrateWithFailingDataSource(sqlException))
                 .isInstanceOf(FlywaySqlNoIntegratedAuthException.class)
-                .hasCause(sqlException);
+                .hasCause(sqlException)
+                .hasMessageContaining("To setup integrated authentication");
     }
 
     @Test
     void throwsSpecificExceptionForMissingInteractiveAuthenticationDrivers() {
         SQLException sqlException = new SQLException("MSAL4J classes are not available");
 
-        assertThatThrownBy(() -> FlywaySqlException.throwFlywayExceptionIfPossible(sqlException, null))
+        assertThatThrownBy(() -> migrateWithFailingDataSource(sqlException))
                 .isInstanceOf(FlywaySqlNoDriversForInteractiveAuthException.class)
-                .hasCause(sqlException);
+                .hasCause(sqlException)
+                .hasMessageContaining("interactive authentication");
+    }
+
+    private void migrateWithFailingDataSource(SQLException sqlException) {
+        Flyway.configure()
+                .dataSource(new FailingDataSource(sqlException))
+                .resourceProvider(new FixedResourceProvider())
+                .connectRetries(0)
+                .load()
+                .migrate();
+    }
+
+    private static final class FailingDataSource implements DataSource {
+        private final SQLException sqlException;
+        private PrintWriter logWriter;
+        private int loginTimeout;
+
+        private FailingDataSource(SQLException sqlException) {
+            this.sqlException = sqlException;
+        }
+
+        @Override
+        public Connection getConnection() throws SQLException {
+            throw sqlException;
+        }
+
+        @Override
+        public Connection getConnection(String username, String password) throws SQLException {
+            throw sqlException;
+        }
+
+        @Override
+        public PrintWriter getLogWriter() {
+            return logWriter;
+        }
+
+        @Override
+        public void setLogWriter(PrintWriter out) {
+            logWriter = out;
+        }
+
+        @Override
+        public void setLoginTimeout(int seconds) {
+            loginTimeout = seconds;
+        }
+
+        @Override
+        public int getLoginTimeout() {
+            return loginTimeout;
+        }
+
+        @Override
+        public Logger getParentLogger() throws SQLFeatureNotSupportedException {
+            throw new SQLFeatureNotSupportedException();
+        }
+
+        @Override
+        public <T> T unwrap(Class<T> iface) throws SQLException {
+            throw new SQLException("Not a wrapper for " + iface.getName());
+        }
+
+        @Override
+        public boolean isWrapperFor(Class<?> iface) {
+            return false;
+        }
     }
 }
