0eb6138272a62ba90e55de1a57e33e7c1676e9ba
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-21 01:22:16 +0200

    Dynamic-access coverage for io.smallrye.mutiny.helpers.queues.SingletonQueue (1/1)

---
+++ tests/src/io.smallrye.reactive/mutiny/2.0.0/src/test/java/io_smallrye_reactive/mutiny/SingletonQueueTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_smallrye_reactive.mutiny;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Queue;
+import java.util.function.Supplier;
+
+import io.smallrye.mutiny.helpers.queues.Queues;
+import org.junit.jupiter.api.Test;
+
+public class SingletonQueueTest {
+
+    @Test
+    public void toArrayExpandsTypedArrayUsingElementComponentType() {
+        Supplier<Queue<String>> supplier = Queues.get(1);
+        Queue<String> queue = supplier.get();
+
+        assertTrue(queue.offer("mutiny"));
+
+        String[] values = queue.toArray(new String[0]);
+
+        assertArrayEquals(new String[] { "mutiny" }, values);
+    }
+}
