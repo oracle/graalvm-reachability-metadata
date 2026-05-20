e196228480713bd48d62a6ab2844fc25baa53ffa
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-20 13:55:32 +0200

    Checkpoint generated tests for org.wildfly.common:wildfly-common-jdk9-supplement:1.0.Final (1)

--- tests/src/org.wildfly.common/wildfly-common-jdk9-supplement/1.0.Final/src/test/java/org_wildfly_common/wildfly_common_jdk9_supplement/Wildfly_common_jdk9_supplementTest.java
+++ tests/src/org.wildfly.common/wildfly-common-jdk9-supplement/1.0.Final/src/test/java/org_wildfly_common/wildfly_common_jdk9_supplement/Wildfly_common_jdk9_supplementTest.java
@@ -6,11 +6,37 @@
  */
 package org_wildfly_common.wildfly_common_jdk9_supplement;
 
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.wildfly.common.cpu.ProcessorInfo.availableProcessors;
+import static org.wildfly.common.os.Process.getProcessId;
+import static org.wildfly.common.os.Process.getProcessName;
+
 import org.junit.jupiter.api.Test;
 
-class Wildfly_common_jdk9_supplementTest {
+public class Wildfly_common_jdk9_supplementTest {
     @Test
-    void test() throws Exception {
-        System.out.println("This is just a placeholder, implement your test");
+    void processorInfoReportsTheRuntimeProcessorCount() {
+        int runtimeProcessors = Runtime.getRuntime().availableProcessors();
+        int wildflyProcessors = availableProcessors();
+
+        assertThat(wildflyProcessors).isEqualTo(runtimeProcessors);
+        assertThat(wildflyProcessors).isPositive();
+    }
+
+    @Test
+    void processIdMatchesTheCurrentJavaProcess() {
+        long processId = getProcessId();
+
+        assertThat(processId).isEqualTo(ProcessHandle.current().pid());
+        assertThat(processId).isPositive();
+        assertThat(getProcessId()).isEqualTo(processId);
+    }
+
+    @Test
+    void processNameIsResolvedAndStable() {
+        String processName = getProcessName();
+
+        assertThat(processName).isNotBlank();
+        assertThat(getProcessName()).isSameAs(processName);
     }
 }
