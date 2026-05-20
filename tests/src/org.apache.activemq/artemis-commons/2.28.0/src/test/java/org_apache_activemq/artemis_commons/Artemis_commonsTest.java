d0984aa3e5bdb8967264d8f36104d83472403f30
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-20 19:23:52 +0200

    Dynamic-access coverage for org.apache.activemq.artemis.utils.ClassloadingUtil (11/40)

--- tests/src/org.apache.activemq/artemis-commons/2.28.0/src/test/java/org_apache_activemq/artemis_commons/Artemis_commonsTest.java
+++ tests/src/org.apache.activemq/artemis-commons/2.28.0/src/test/java/org_apache_activemq/artemis_commons/Artemis_commonsTest.java
@@ -8,7 +8,7 @@ package org_apache_activemq.artemis_commons;
 
 import org.junit.jupiter.api.Test;
 
-class Artemis_commonsTest {
+public class Artemis_commonsTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
