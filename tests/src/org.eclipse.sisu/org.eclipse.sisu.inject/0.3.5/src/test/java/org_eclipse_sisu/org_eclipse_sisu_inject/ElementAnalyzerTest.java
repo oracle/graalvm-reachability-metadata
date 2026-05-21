14b53b876e5dee2a475fdc2c4906e7e0daf302a7
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-21 06:50:57 +0200

    Dynamic-access coverage for org.eclipse.sisu.wire.ElementAnalyzer (32/32)

--- tests/src/org.eclipse.sisu/org.eclipse.sisu.inject/0.3.5/src/test/java/org_eclipse_sisu/org_eclipse_sisu_inject/ElementAnalyzerTest.java
+++ tests/src/org.eclipse.sisu/org.eclipse.sisu.inject/0.3.5/src/test/java/org_eclipse_sisu/org_eclipse_sisu_inject/ElementAnalyzerTest.java
@@ -8,21 +8,43 @@ package org_eclipse_sisu.org_eclipse_sisu_inject;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.annotation.Annotation;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.sisu.wire.WireModule;
 import org.junit.jupiter.api.Test;
+import org.sonatype.guice.bean.locators.BeanLocator;
+import org.sonatype.inject.BeanEntry;
+import org.sonatype.inject.Mediator;
 
 import com.google.inject.AbstractModule;
 import com.google.inject.Binder;
-import com.google.inject.Guice;
+import com.google.inject.Binding;
 import com.google.inject.Inject;
-import com.google.inject.Injector;
 import com.google.inject.Key;
 import com.google.inject.Module;
+import com.google.inject.spi.Element;
+import com.google.inject.spi.Elements;
 
 public class ElementAnalyzerTest {
+    @Test
+    void wireModuleAnalyzesLegacyLocatorBindingsWhenCompatibilityApiIsPresent() {
+        BeanLocator legacyLocator = new LegacyBeanLocator();
+        Module applicationModule = new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(BeanLocator.class).toInstance(legacyLocator);
+            }
+        };
+
+        List<Key<?>> recordedBindingKeys = bindingKeys(Elements.getElements(new WireModule(applicationModule)));
+
+        assertThat(BeanLocator.class.getName()).isEqualTo("org.sonatype.guice.bean.locators.BeanLocator");
+        assertThat(recordedBindingKeys).contains(Key.get(BeanLocator.class));
+    }
+
     @Test
     void wireModuleRoutesUnresolvedConstructorDependenciesToCustomWiring() {
         Collaborator collaborator = new WiredCollaborator();
@@ -38,14 +60,36 @@ public class ElementAnalyzerTest {
             wiredKeys.add(key);
             if (Key.get(Collaborator.class).equals(key)) {
                 binder.bind(Collaborator.class).toInstance(collaborator);
+                return true;
             }
-            return true;
+            return false;
         });
 
-        Injector injector = Guice.createInjector(wireModule);
+        List<Key<?>> recordedBindingKeys = bindingKeys(Elements.getElements(wireModule));
 
-        assertThat(injector.getInstance(NeedsCollaborator.class).collaborator()).isSameAs(collaborator);
         assertThat(wiredKeys).containsExactly(Key.get(Collaborator.class));
+        assertThat(recordedBindingKeys).contains(Key.get(NeedsCollaborator.class), Key.get(Collaborator.class));
+    }
+
+    private static List<Key<?>> bindingKeys(Iterable<Element> elements) {
+        List<Key<?>> keys = new ArrayList<>();
+        for (Element element : elements) {
+            if (element instanceof Binding<?>) {
+                keys.add(((Binding<?>) element).getKey());
+            }
+        }
+        return keys;
+    }
+
+    private static final class LegacyBeanLocator implements BeanLocator {
+        @Override
+        public <Q extends Annotation, T> Iterable<BeanEntry<Q, T>> locate(Key<T> key) {
+            return Collections.emptyList();
+        }
+
+        @Override
+        public <Q extends Annotation, T, W> void watch(Key<T> key, Mediator<Q, T, W> mediator, W watcher) {
+        }
     }
 
     private interface Collaborator {
