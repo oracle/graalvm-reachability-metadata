e4727359180a609917a0ccca618576831bd77521
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-21 02:17:56 +0200

    Scaffold com.google.http-client:google-http-client-gson:1.42.2

---
+++ tests/src/com.google.http-client/google-http-client-gson/1.42.2/src/test/java/com_google_http_client/google_http_client_gson/Google_http_client_gsonTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_google_http_client.google_http_client_gson;
+
+import org.junit.jupiter.api.Test;
+
+class Google_http_client_gsonTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
