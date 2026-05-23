b4bd5aeaeb75bea5f47d05785d8678d7d09a0e83
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-23 13:55:20 +0200

    Dynamic-access coverage for gnu.trove.map.hash.TIntObjectHashMap$MapBackedView (27/47)

---
+++ tests/src/net.sf.trove4j/core/3.1.0/src/test/java/net_sf_trove4j/core/TIntObjectHashMapInnerMapBackedViewTest.java
@@ -0,0 +1,31 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package net_sf_trove4j.core;
+
+import gnu.trove.map.hash.TIntObjectHashMap;
+import org.junit.jupiter.api.Test;
+
+import java.util.Collection;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TIntObjectHashMapInnerMapBackedViewTest {
+    @Test
+    void valueCollectionToArrayCreatesTypedArrayWhenProvidedArrayIsTooSmall() {
+        TIntObjectHashMap<String> map = new TIntObjectHashMap<>();
+        map.put(1, "alpha");
+        map.put(2, "beta");
+
+        Collection<String> values = map.valueCollection();
+        String[] result = values.toArray(new String[0]);
+
+        assertThat(result).containsExactlyInAnyOrder("alpha", "beta");
+        assertThat(result.getClass()).isEqualTo(String[].class);
+        assertThat(values.contains("alpha")).isTrue();
+        assertThat(values.contains("beta")).isTrue();
+    }
+}
