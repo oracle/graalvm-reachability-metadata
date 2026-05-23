924bfd40b3fee750241380df0d95183f214a15dc
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-23 16:56:50 +0200

    Update generated library support for org.mongodb.morphia:morphia:1.3.2

--- tests/src/org.mongodb.morphia/morphia/1.3.2/src/test/java/org_mongodb_morphia/morphia/DefaultCreatorTest.java
+++ tests/src/org.mongodb.morphia/morphia/1.3.2/src/test/java/org_mongodb_morphia/morphia/DefaultCreatorTest.java
@@ -9,14 +9,8 @@ package org_mongodb_morphia.morphia;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.mongodb.BasicDBObject;
-import java.util.ArrayList;
-import java.util.List;
 import org.junit.jupiter.api.Test;
-import org.mongodb.morphia.annotations.ConstructorArgs;
-import org.mongodb.morphia.annotations.Embedded;
 import org.mongodb.morphia.mapping.DefaultCreator;
-import org.mongodb.morphia.mapping.MappedClass;
-import org.mongodb.morphia.mapping.MappedField;
 import org.mongodb.morphia.mapping.Mapper;
 import org.mongodb.morphia.mapping.MapperOptions;
 
@@ -54,37 +48,6 @@ public class DefaultCreatorTest {
         assertThat(creator.getClassNameCache()).containsEntry(className, CachedClassNameFixture.class);
     }
 
-    @Test
-    void createInstanceUsesConstructorArgsAnnotationForValueObjects() {
-        DefaultCreator creator = new DefaultCreator();
-        MappedField mappedField = mappedField(ConstructorArgsHolder.class, "value");
-        BasicDBObject dbObject = new BasicDBObject("name", "morphia");
-
-        Object value = creator.createInstance(new Mapper(), mappedField, dbObject);
-
-        assertThat(value).isInstanceOfSatisfying(ConstructorArgValue.class,
-                constructed -> assertThat(constructed.name).isEqualTo("morphia"));
-    }
-
-    @Test
-    @SuppressWarnings("unchecked")
-    void createListUsesMappedFieldConstructorWhenAvailable() {
-        DefaultCreator creator = new DefaultCreator();
-        MappedField mappedField = mappedField(CollectionHolder.class, "values");
-
-        List<String> values = creator.createList(mappedField);
-
-        assertThat(values).isInstanceOf(CustomStringList.class)
-                .containsExactly("constructed");
-    }
-
-    private static MappedField mappedField(Class<?> holderType, String fieldName) {
-        MappedClass mappedClass = new Mapper().addMappedClass(holderType);
-        MappedField mappedField = mappedClass.getMappedFieldByJavaField(fieldName);
-        assertThat(mappedField).isNotNull();
-        return mappedField;
-    }
-
     public static class PrivateNoArgsFixture {
         private final String value;
 
@@ -100,33 +63,6 @@ public class DefaultCreatorTest {
 
     public static class CachedClassNameFixture {
         private CachedClassNameFixture() {
-        }
-    }
-
-    @Embedded
-    public static class ConstructorArgsHolder {
-        @ConstructorArgs("name")
-        private ConstructorArgValue value;
-    }
-
-    public static class ConstructorArgValue {
-        private final String name;
-
-        private ConstructorArgValue(String name) {
-            this.name = name;
-        }
-    }
-
-    @Embedded
-    public static class CollectionHolder {
-        private CustomStringList values;
-    }
-
-    public static class CustomStringList extends ArrayList<String> {
-        private static final long serialVersionUID = 1L;
-
-        private CustomStringList() {
-            add("constructed");
         }
     }
 }
