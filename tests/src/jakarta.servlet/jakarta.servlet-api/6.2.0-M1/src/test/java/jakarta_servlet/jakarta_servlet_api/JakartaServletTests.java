b1b6ff0e3e7c283f692577652f46043d111a98a1
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-05-26 16:22:37 +0200

    Dynamic-access coverage for jakarta.servlet.http.HttpServlet (6/8)

--- tests/src/jakarta.servlet/jakarta.servlet-api/6.2.0-M1/src/test/java/jakarta_servlet/jakarta_servlet_api/JakartaServletTests.java
+++ tests/src/jakarta.servlet/jakarta.servlet-api/6.2.0-M1/src/test/java/jakarta_servlet/jakarta_servlet_api/JakartaServletTests.java
@@ -12,7 +12,7 @@ import jakarta.servlet.GenericServlet;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServlet;
+
 import org.junit.jupiter.api.Test;
 
 
@@ -22,19 +22,10 @@ public class JakartaServletTests {
         new CustomServlet();
     }
 
-    @Test
-    void instantiateHttpServlet() {
-        new CustomHttpServlet();
-    }
-
     static class CustomServlet extends GenericServlet {
         @Override
         public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 
         }
-    }
-
-    static class CustomHttpServlet extends HttpServlet {
-
     }
 }
