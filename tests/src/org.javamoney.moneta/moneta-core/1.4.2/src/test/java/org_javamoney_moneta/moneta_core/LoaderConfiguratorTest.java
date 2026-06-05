b86cf23819921f5f7718810e5ebf29b78990722c
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-05 22:01:26 +0200

    Prepare project for org.javamoney.moneta:moneta-core:1.4.2

---
+++ tests/src/org.javamoney.moneta/moneta-core/1.4.2/src/test/java/org_javamoney_moneta/moneta_core/LoaderConfiguratorTest.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_javamoney_moneta.moneta_core;
+
+import org.javamoney.moneta.spi.loader.DefaultLoaderService;
+import org.javamoney.moneta.spi.loader.LoaderService;
+import org.javamoney.moneta.spi.loader.LoaderService.UpdatePolicy;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class LoaderConfiguratorTest {
+    private static final String RESOURCE_ID = "loader-configurator-absolute";
+
+    @Test
+    public void loadRegistersResourceFoundThroughOwningClass() {
+        LoaderService loaderService = new DefaultLoaderService();
+
+        assertThat(loaderService.isResourceRegistered(RESOURCE_ID)).isTrue();
+        assertThat(loaderService.getUpdatePolicy(RESOURCE_ID)).isEqualTo(UpdatePolicy.LAZY);
+        assertThat(loaderService.getUpdateConfiguration(RESOURCE_ID))
+                .containsEntry("resource", "/javamoney.properties")
+                .containsEntry("type", "LAZY");
+    }
+}
