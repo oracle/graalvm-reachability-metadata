b76883cb42c37fe820592663501f5deaf003aa8a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 01:57:45 +0200

    Update generated library support for io.lettuce:lettuce-core:6.2.6.RELEASE

--- tests/src/io.lettuce/lettuce-core/6.2.6.RELEASE/src/test/java/io_lettuce/lettuce_core/ClusterFutureSyncInvocationHandlerTest.java
+++ tests/src/io.lettuce/lettuce-core/6.2.6.RELEASE/src/test/java/io_lettuce/lettuce_core/ClusterFutureSyncInvocationHandlerTest.java
@@ -28,7 +28,6 @@ import java.net.SocketTimeoutException;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
