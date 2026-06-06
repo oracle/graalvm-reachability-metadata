ee9e2d1b2a1186ef43b3b340571d440f6d23ce0e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 10:47:59 +0200

    Prepare project for org.apache.pekko:pekko-protobuf-v3_3:2.0.0-M2

---
+++ tests/src/org.apache.pekko/pekko-protobuf-v3_3/2.0.0-M2/src/test/scala/org_apache_pekko/pekko_protobuf_v3_3/ExtensionSchemasTest.scala
@@ -0,0 +1,22 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_pekko.pekko_protobuf_v3_3
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class ExtensionSchemasTest {
+  @Test
+  def initializesFullRuntimeExtensionSchemaForProto2GeneratedMessages(): Unit = {
+    val message: ExtensionSchemasProto2Probe = new ExtensionSchemasProto2Probe()
+
+    message.initializeEmptyPayloadSchema()
+
+    assertThat(message.getDescriptorForType.getFile.toProto.hasSyntax).isFalse
+    assertThat(message.getDescriptorForType.getName).isEqualTo("ExtensionSchemasProto2Probe")
+  }
+}
---
+++ tests/src/org.apache.pekko/pekko-protobuf-v3_3/2.0.0-M2/src/test/scala/org_apache_pekko/pekko_protobuf_v3_3/GeneratedMessageAnonymous4Test.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_pekko.pekko_protobuf_v3_3
+
+import org.apache.pekko.protobufv3.internal.Descriptors
+import org.apache.pekko.protobufv3.internal.GeneratedMessage
+import org.apache.pekko.protobufv3.internal.Message
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class GeneratedMessageAnonymous4Test {
+  @Test
+  def fileScopedExtensionLoadsDescriptorFromGeneratedDescriptorClass(): Unit = {
+    val extension: GeneratedMessage.GeneratedExtension[Message, String] =
+      GeneratedMessage.newFileScopedGeneratedExtension[Message, String](classOf[String], null)
+    val extensionDescriptor: Descriptors.FieldDescriptor =
+      GeneratedMessageAnonymous4DescriptorProbe.descriptor.findExtensionByName("covered_extension")
+    extension.internalInit(extensionDescriptor)
+
+    val descriptor: Descriptors.FieldDescriptor = extension.getDescriptor
+
+    assertThat(descriptor.getName).isEqualTo("covered_extension")
+    assertThat(descriptor.getNumber).isEqualTo(100)
+    assertThat(descriptor.getJavaType).isEqualTo(Descriptors.FieldDescriptor.JavaType.STRING)
+    assertThat(descriptor.getFile).isSameAs(GeneratedMessageAnonymous4DescriptorProbe.descriptor)
+  }
+}
