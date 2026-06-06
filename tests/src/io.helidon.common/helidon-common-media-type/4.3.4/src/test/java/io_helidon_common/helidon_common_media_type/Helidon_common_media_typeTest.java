ac29cceff0b354c73d037039a4ba02b3e28a7a33
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 13:49:12 +0200

    Checkpoint generated tests for io.helidon.common:helidon-common-media-type:4.3.4 (3)

--- tests/src/io.helidon.common/helidon-common-media-type/4.3.4/src/test/java/io_helidon_common/helidon_common_media_type/Helidon_common_media_typeTest.java
+++ tests/src/io.helidon.common/helidon-common-media-type/4.3.4/src/test/java/io_helidon_common/helidon_common_media_type/Helidon_common_media_typeTest.java
@@ -12,6 +12,7 @@ import java.util.Optional;
 
 import io.helidon.common.media.type.MediaType;
 import io.helidon.common.media.type.MediaTypes;
+import io.helidon.common.media.type.ParserMode;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -37,6 +38,38 @@ public class Helidon_common_media_typeTest {
                            MediaTypes.APPLICATION_JSON);
         assertDetectedType(MediaTypes.detectType(URI.create("https://example.test/docs/index.html").toURL()),
                            MediaTypes.TEXT_HTML);
+    }
+
+    @Test
+    void createsMediaTypesAndExposesStructuredParts() {
+        MediaType vendorJson = MediaTypes.create("application/vnd.example+json");
+
+        assertThat(vendorJson.type()).isEqualTo("application");
+        assertThat(vendorJson.subtype()).isEqualTo("vnd.example+json");
+        assertThat(vendorJson.text()).isEqualTo("application/vnd.example+json");
+        assertThat(vendorJson.fullType()).isEqualTo("application/vnd.example+json");
+        assertThat(vendorJson.hasSuffix("json")).isTrue();
+        assertThat(vendorJson.hasSuffix("+json")).isTrue();
+        assertThat(vendorJson.hasSuffix("xml")).isFalse();
+    }
+
+    @Test
+    void createsRelaxedTextMediaType() {
+        MediaType relaxedText = MediaTypes.create("text", ParserMode.RELAXED);
+
+        assertThat(relaxedText).isEqualTo(MediaTypes.TEXT_PLAIN);
+        assertThat(relaxedText.type()).isEqualTo("text");
+        assertThat(relaxedText.subtype()).isEqualTo("plain");
+    }
+
+    @Test
+    void identifiesWildcardMediaTypeParts() {
+        MediaType wildcardSubtype = MediaTypes.create("application/*");
+
+        assertThat(wildcardSubtype.isWildcardType()).isFalse();
+        assertThat(wildcardSubtype.isWildcardSubtype()).isTrue();
+        assertThat(MediaTypes.WILDCARD.isWildcardType()).isTrue();
+        assertThat(MediaTypes.WILDCARD.isWildcardSubtype()).isTrue();
     }
 
     private static void assertDetectedType(Optional<MediaType> actual, MediaType expected) {
