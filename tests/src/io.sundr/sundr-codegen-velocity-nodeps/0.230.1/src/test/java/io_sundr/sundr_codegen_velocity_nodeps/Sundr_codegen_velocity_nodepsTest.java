891cfb17f719dc6f3e62db3f248cfef8c8bb5788
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 05:35:28 +0200

    Scaffold io.sundr:sundr-codegen-velocity-nodeps:0.230.1

---
+++ tests/src/io.sundr/sundr-codegen-velocity-nodeps/0.230.1/src/test/java/io_sundr/sundr_codegen_velocity_nodeps/Sundr_codegen_velocity_nodepsTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_sundr.sundr_codegen_velocity_nodeps;
+
+import org.junit.jupiter.api.Test;
+
+class Sundr_codegen_velocity_nodepsTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
