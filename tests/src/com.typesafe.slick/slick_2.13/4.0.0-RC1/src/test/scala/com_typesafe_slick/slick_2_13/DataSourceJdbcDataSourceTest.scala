1ce898213b79204d70ac8b9573be028624ef2729
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 23:56:06 +0200

    Preserve last passing candidate for com.typesafe.slick:slick_2.13:4.0.0-RC1

--- tests/src/com.typesafe.slick/slick_2.13/4.0.0-RC1/src/test/scala/com_typesafe_slick/slick_2_13/DataSourceJdbcDataSourceTest.scala
+++ tests/src/com.typesafe.slick/slick_2.13/4.0.0-RC1/src/test/scala/com_typesafe_slick/slick_2_13/DataSourceJdbcDataSourceTest.scala
@@ -6,6 +6,7 @@
  */
 package com_typesafe_slick.slick_2_13
 
+import com.typesafe.config.Config
 import com.typesafe.config.ConfigFactory
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
@@ -15,22 +16,40 @@ import slick.jdbc.JdbcDataSource
 class DataSourceJdbcDataSourceTest {
   @Test
   def createsDataSourceFromConfiguredClassName(): Unit = {
-    val dataSourceClassName = "slick.jdbc.DriverDataSource"
-    val config = ConfigFactory.parseString(s"""
+    val dataSourceClassName: String = "slick.jdbc.DriverDataSource"
+    val config: Config = ConfigFactory.parseString(s"""
       connectionPool = "disabled"
       dataSourceClassName = "$dataSourceClassName"
       maxConnections = 7
       """)
 
-    val jdbcDataSource = JdbcDataSource.forConfig(
+    val jdbcDataSource: JdbcDataSource = JdbcDataSource.forConfig(
       config,
       driver = null,
       name = "configured-data-source",
       classLoader = getClass.getClassLoader)
 
     assertThat(jdbcDataSource).isInstanceOf(classOf[DataSourceJdbcDataSource])
-    val dataSource = jdbcDataSource.asInstanceOf[DataSourceJdbcDataSource]
+    val dataSource: DataSourceJdbcDataSource = jdbcDataSource.asInstanceOf[DataSourceJdbcDataSource]
     assertThat(dataSource.ds.getClass.getName).isEqualTo(dataSourceClassName)
     assertThat(dataSource.maxConnections).isEqualTo(Some(7))
+  }
+
+  @Test
+  def factoryInstantiatesConfiguredDataSourceClass(): Unit = {
+    val dataSourceClassName: String = "org.postgresql.ds.PGSimpleDataSource"
+    val config: Config = ConfigFactory.parseString(s"""
+      dataSourceClassName = "$dataSourceClassName"
+      maxConnections = 3
+      """)
+
+    val dataSource: DataSourceJdbcDataSource = DataSourceJdbcDataSource.forConfig(
+      config,
+      driver = null,
+      name = "direct-configured-data-source",
+      classLoader = getClass.getClassLoader)
+
+    assertThat(dataSource.ds.getClass.getName).isEqualTo(dataSourceClassName)
+    assertThat(dataSource.maxConnections).isEqualTo(Some(3))
   }
 }
