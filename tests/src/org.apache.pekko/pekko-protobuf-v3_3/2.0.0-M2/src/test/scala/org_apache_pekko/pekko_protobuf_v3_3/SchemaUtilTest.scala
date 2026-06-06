a0302ccea58c3d21520224986e69e12ab5331e0a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 11:13:57 +0200

    Dynamic-access coverage for org.apache.pekko.protobufv3.internal.SchemaUtil (53/54)

--- tests/src/org.apache.pekko/pekko-protobuf-v3_3/2.0.0-M2/src/test/scala/org_apache_pekko/pekko_protobuf_v3_3/SchemaUtilTest.scala
+++ tests/src/org.apache.pekko/pekko-protobuf-v3_3/2.0.0-M2/src/test/scala/org_apache_pekko/pekko_protobuf_v3_3/SchemaUtilTest.scala
@@ -6,6 +6,11 @@
  */
 package org_apache_pekko.pekko_protobuf_v3_3
 
+import java.lang.invoke.MethodHandle
+import java.lang.invoke.MethodHandles
+import java.lang.invoke.MethodType.methodType
+
+import org.apache.pekko.protobufv3.internal.GeneratedMessageV3
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
@@ -15,9 +20,50 @@ class SchemaUtilTest {
     val message: SchemaUtilMapFieldProbe = new SchemaUtilMapFieldProbe()
 
     message.initializeEmptyMapSchema()
+    val messageInfo: AnyRef = descriptorMessageInfoFactoryMessageInfoFor.invokeWithArguments(
+      descriptorMessageInfoFactoryGetInstance.invokeWithArguments(),
+      classOf[SchemaUtilMapFieldProbe]
+    ).asInstanceOf[AnyRef]
 
     val entriesField = message.getDescriptorForType.findFieldByName("entries")
+    assertThat(messageInfo).isNotNull
     assertThat(entriesField.isMapField).isTrue()
     assertThat(message.getEntriesMap).isEmpty()
+  }
+
+  private def descriptorMessageInfoFactoryGetInstance: MethodHandle = {
+    descriptorMessageInfoFactoryLookup.findStatic(
+      descriptorMessageInfoFactoryClass,
+      "getInstance",
+      methodType(descriptorMessageInfoFactoryClass)
+    )
+  }
+
+  private def descriptorMessageInfoFactoryMessageInfoFor: MethodHandle = {
+    descriptorMessageInfoFactoryLookup.findVirtual(
+      descriptorMessageInfoFactoryClass,
+      "messageInfoFor",
+      methodType(messageInfoClass, classOf[Class[_]])
+    )
+  }
+
+  private def descriptorMessageInfoFactoryLookup: MethodHandles.Lookup = {
+    MethodHandles.privateLookupIn(descriptorMessageInfoFactoryClass, MethodHandles.lookup())
+  }
+
+  private def descriptorMessageInfoFactoryClass: Class[_] = {
+    Class.forName(
+      "org.apache.pekko.protobufv3.internal.DescriptorMessageInfoFactory",
+      true,
+      classOf[GeneratedMessageV3].getClassLoader
+    )
+  }
+
+  private def messageInfoClass: Class[_] = {
+    Class.forName(
+      "org.apache.pekko.protobufv3.internal.MessageInfo",
+      true,
+      classOf[GeneratedMessageV3].getClassLoader
+    )
   }
 }
