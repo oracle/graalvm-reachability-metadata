bc2e0207f349317648ed69a7d2264840f49bc17a
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 03:55:21 +0200

    Prepare project for org.javamoney.moneta:moneta-core:1.4.3

---
+++ tests/src/org.javamoney.moneta/moneta-core/1.4.3/src/test/java/org_javamoney_moneta/moneta_core/DefaultMonetaryAmountsSingletonSpiTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_javamoney_moneta.moneta_core;
+
+import org.javamoney.moneta.FastMoney;
+import org.javamoney.moneta.spi.DefaultMonetaryAmountsSingletonSpi;
+import org.javamoney.moneta.spi.MonetaryConfig;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DefaultMonetaryAmountsSingletonSpiTest {
+    private static final String DEFAULT_AMOUNT_TYPE_PROPERTY = "org.javamoney.moneta.Money.defaults.amountType";
+
+    @Test
+    public void getDefaultAmountTypeLoadsConfiguredAmountClass() {
+        Optional<String> previousValue = MonetaryConfig.getString(DEFAULT_AMOUNT_TYPE_PROPERTY);
+        try {
+            MonetaryConfig.setValue(DEFAULT_AMOUNT_TYPE_PROPERTY, FastMoney.class.getName());
+
+            DefaultMonetaryAmountsSingletonSpi amounts = new DefaultMonetaryAmountsSingletonSpi();
+
+            assertThat(amounts.getDefaultAmountType()).isEqualTo(FastMoney.class);
+            assertThat(amounts.getAmountTypes()).contains(FastMoney.class);
+        } finally {
+            MonetaryConfig.setValue(DEFAULT_AMOUNT_TYPE_PROPERTY, previousValue.orElse(null));
+        }
+    }
+}
