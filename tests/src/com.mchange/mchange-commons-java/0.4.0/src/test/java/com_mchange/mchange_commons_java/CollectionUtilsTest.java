fcfd8c255b17d0dd16b13a212c52e1d3d34066ac
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-06 18:45:37 +0200

    Dynamic-access coverage for com.mchange.v2.util.CollectionUtils (86/93)

--- tests/src/com.mchange/mchange-commons-java/0.4.0/src/test/java/com_mchange/mchange_commons_java/CollectionUtilsTest.java
+++ tests/src/com.mchange/mchange-commons-java/0.4.0/src/test/java/com_mchange/mchange_commons_java/CollectionUtilsTest.java
@@ -9,10 +9,14 @@ package com_mchange.mchange_commons_java;
 import com.mchange.v2.util.CollectionUtils;
 import org.junit.jupiter.api.Test;
 
+import java.util.AbstractCollection;
+import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
@@ -77,5 +81,182 @@ public class CollectionUtilsTest {
         assertThat(cloned.get("one")).isEqualTo(1);
         assertThat(cloned.get("two")).isEqualTo(2);
         assertThat(cloned).isNotSameAs(original);
+    }
+
+    @Test
+    void attemptCloneClonesCollectionUsingPublicCloneMethod() throws NoSuchMethodException {
+        ReflectionCloneCollection original = new ReflectionCloneCollection();
+        original.add("alpha");
+        original.add("beta");
+
+        Collection<?> cloned = CollectionUtils.attemptClone(original);
+
+        assertThat(cloned).isInstanceOf(ReflectionCloneCollection.class);
+        assertThat(cloned.contains("alpha")).isTrue();
+        assertThat(cloned.contains("beta")).isTrue();
+        assertThat(cloned).isNotSameAs(original);
+    }
+
+    @Test
+    void attemptCloneClonesCollectionUsingCollectionConstructor() throws NoSuchMethodException {
+        CollectionConstructorCollection original = new CollectionConstructorCollection();
+        original.add("alpha");
+        original.add("beta");
+
+        Collection<?> cloned = CollectionUtils.attemptClone(original);
+
+        assertThat(cloned).isInstanceOf(CollectionConstructorCollection.class);
+        assertThat(cloned.contains("alpha")).isTrue();
+        assertThat(cloned.contains("beta")).isTrue();
+        assertThat(cloned).isNotSameAs(original);
+    }
+
+    @Test
+    void attemptCloneClonesCollectionUsingSameClassConstructor() throws NoSuchMethodException {
+        SameClassConstructorCollection original = new SameClassConstructorCollection();
+        original.add("alpha");
+        original.add("beta");
+
+        Collection<?> cloned = CollectionUtils.attemptClone(original);
+
+        assertThat(cloned).isInstanceOf(SameClassConstructorCollection.class);
+        assertThat(cloned.contains("alpha")).isTrue();
+        assertThat(cloned.contains("beta")).isTrue();
+        assertThat(cloned).isNotSameAs(original);
+    }
+
+    @Test
+    void attemptCloneClonesMapUsingPublicCloneMethod() throws NoSuchMethodException {
+        ReflectionCloneMap original = new ReflectionCloneMap();
+        original.put("one", 1);
+        original.put("two", 2);
+
+        Map<?, ?> cloned = CollectionUtils.attemptClone(original);
+
+        assertThat(cloned).isInstanceOf(ReflectionCloneMap.class);
+        assertThat(cloned.get("one")).isEqualTo(1);
+        assertThat(cloned.get("two")).isEqualTo(2);
+        assertThat(cloned).isNotSameAs(original);
+    }
+
+    @Test
+    void attemptCloneClonesMapUsingMapConstructor() throws NoSuchMethodException {
+        MapConstructorMap original = new MapConstructorMap();
+        original.put("one", 1);
+        original.put("two", 2);
+
+        Map<?, ?> cloned = CollectionUtils.attemptClone(original);
+
+        assertThat(cloned).isInstanceOf(MapConstructorMap.class);
+        assertThat(cloned.get("one")).isEqualTo(1);
+        assertThat(cloned.get("two")).isEqualTo(2);
+        assertThat(cloned).isNotSameAs(original);
+    }
+
+    @Test
+    void attemptCloneClonesMapUsingSameClassConstructor() throws NoSuchMethodException {
+        SameClassConstructorMap original = new SameClassConstructorMap();
+        original.put("one", 1);
+        original.put("two", 2);
+
+        Map<?, ?> cloned = CollectionUtils.attemptClone(original);
+
+        assertThat(cloned).isInstanceOf(SameClassConstructorMap.class);
+        assertThat(cloned.get("one")).isEqualTo(1);
+        assertThat(cloned.get("two")).isEqualTo(2);
+        assertThat(cloned).isNotSameAs(original);
+    }
+
+    public static class ReflectionCloneCollection extends TestCollection {
+        public ReflectionCloneCollection() {
+        }
+
+        @Override
+        public ReflectionCloneCollection clone() {
+            ReflectionCloneCollection clone = new ReflectionCloneCollection();
+            clone.values.addAll(values);
+            return clone;
+        }
+    }
+
+    public static class CollectionConstructorCollection extends TestCollection {
+        public CollectionConstructorCollection() {
+        }
+
+        public CollectionConstructorCollection(Collection<String> source) {
+            values.addAll(source);
+        }
+    }
+
+    public static class SameClassConstructorCollection extends TestCollection {
+        public SameClassConstructorCollection() {
+        }
+
+        public SameClassConstructorCollection(SameClassConstructorCollection source) {
+            values.addAll(source.values);
+        }
+    }
+
+    public abstract static class TestCollection extends AbstractCollection<String> {
+        protected final Collection<String> values = new ArrayList<>();
+
+        @Override
+        public Iterator<String> iterator() {
+            return values.iterator();
+        }
+
+        @Override
+        public int size() {
+            return values.size();
+        }
+
+        @Override
+        public boolean add(String value) {
+            return values.add(value);
+        }
+    }
+
+    public static class ReflectionCloneMap extends TestMap {
+        public ReflectionCloneMap() {
+        }
+
+        @Override
+        public ReflectionCloneMap clone() {
+            ReflectionCloneMap clone = new ReflectionCloneMap();
+            clone.values.putAll(values);
+            return clone;
+        }
+    }
+
+    public static class MapConstructorMap extends TestMap {
+        public MapConstructorMap() {
+        }
+
+        public MapConstructorMap(Map<String, Integer> source) {
+            values.putAll(source);
+        }
+    }
+
+    public static class SameClassConstructorMap extends TestMap {
+        public SameClassConstructorMap() {
+        }
+
+        public SameClassConstructorMap(SameClassConstructorMap source) {
+            values.putAll(source.values);
+        }
+    }
+
+    public abstract static class TestMap extends AbstractMap<String, Integer> {
+        protected final Map<String, Integer> values = new HashMap<>();
+
+        @Override
+        public Set<Entry<String, Integer>> entrySet() {
+            return values.entrySet();
+        }
+
+        @Override
+        public Integer put(String key, Integer value) {
+            return values.put(key, value);
+        }
     }
 }
