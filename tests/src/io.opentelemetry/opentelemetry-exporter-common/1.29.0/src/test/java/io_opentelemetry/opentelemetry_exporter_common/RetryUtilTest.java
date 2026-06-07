c5ea5421577c05bb1a2cdc31d3063d707cf45386
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-06-08 00:33:28 +0200

    Preserve last passing candidate for io.opentelemetry:opentelemetry-exporter-common:1.29.0

--- tests/src/io.opentelemetry/opentelemetry-exporter-common/1.29.0/src/test/java/io_opentelemetry/opentelemetry_exporter_common/RetryUtilTest.java
+++ tests/src/io.opentelemetry/opentelemetry-exporter-common/1.29.0/src/test/java/io_opentelemetry/opentelemetry_exporter_common/RetryUtilTest.java
@@ -8,8 +8,8 @@ package io_opentelemetry.opentelemetry_exporter_common;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-import io.opentelemetry.exporter.internal.grpc.GrpcStatusUtil;
-import io.opentelemetry.exporter.internal.retry.RetryUtil;
+import io.opentelemetry.exporter.internal.RetryUtil;
+import io.opentelemetry.exporter.internal.grpc.GrpcExporterUtil;
 import org.junit.jupiter.api.Test;
 
 public class RetryUtilTest {
@@ -19,13 +19,13 @@ public class RetryUtilTest {
                 .containsExactlyInAnyOrder(429, 502, 503, 504);
         assertThat(RetryUtil.retryableGrpcStatusCodes())
                 .containsExactlyInAnyOrder(
-                        GrpcStatusUtil.GRPC_STATUS_CANCELLED,
-                        GrpcStatusUtil.GRPC_STATUS_DEADLINE_EXCEEDED,
-                        GrpcStatusUtil.GRPC_STATUS_RESOURCE_EXHAUSTED,
-                        GrpcStatusUtil.GRPC_STATUS_ABORTED,
-                        GrpcStatusUtil.GRPC_STATUS_OUT_OF_RANGE,
-                        GrpcStatusUtil.GRPC_STATUS_UNAVAILABLE,
-                        GrpcStatusUtil.GRPC_STATUS_DATA_LOSS);
+                        String.valueOf(GrpcExporterUtil.GRPC_STATUS_CANCELLED),
+                        String.valueOf(GrpcExporterUtil.GRPC_STATUS_DEADLINE_EXCEEDED),
+                        String.valueOf(GrpcExporterUtil.GRPC_STATUS_RESOURCE_EXHAUSTED),
+                        String.valueOf(GrpcExporterUtil.GRPC_STATUS_ABORTED),
+                        String.valueOf(GrpcExporterUtil.GRPC_STATUS_OUT_OF_RANGE),
+                        String.valueOf(GrpcExporterUtil.GRPC_STATUS_UNAVAILABLE),
+                        String.valueOf(GrpcExporterUtil.GRPC_STATUS_DATA_LOSS));
     }
 
     @Test
