7afa55f2fb6649a80d4dac10056913abfe7fa1fb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-08 15:10:56 +0200

    Update generated library support for com.google.collections:google-collections:1.0

--- tests/src/com.google.collections/google-collections/1.0/src/test/java/com_google_collections/google_collections/MultimapsInnerCustomListMultimapTest.java
+++ tests/src/com.google.collections/google-collections/1.0/src/test/java/com_google_collections/google_collections/MultimapsInnerCustomListMultimapTest.java
@@ -23,7 +23,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 
 public class MultimapsInnerCustomListMultimapTest {
     @Test
