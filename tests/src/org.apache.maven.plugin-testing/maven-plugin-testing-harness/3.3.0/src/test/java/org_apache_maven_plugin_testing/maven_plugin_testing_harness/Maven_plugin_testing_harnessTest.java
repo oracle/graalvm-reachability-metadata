50c1235047cd6385a497ae7f784ac736da83c85b
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 19:30:04 +0200

    Scaffold org.apache.maven.plugin-testing:maven-plugin-testing-harness:3.3.0

---
+++ tests/src/org.apache.maven.plugin-testing/maven-plugin-testing-harness/3.3.0/src/test/java/org_apache_maven_plugin_testing/maven_plugin_testing_harness/Maven_plugin_testing_harnessTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven_plugin_testing.maven_plugin_testing_harness;
+
+import org.junit.jupiter.api.Test;
+
+class Maven_plugin_testing_harnessTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
