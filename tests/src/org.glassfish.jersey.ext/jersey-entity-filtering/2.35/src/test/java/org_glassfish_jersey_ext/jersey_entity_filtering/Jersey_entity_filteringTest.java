79a50ef3e01d22ee445b679dfb22a59426315584
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 21:30:03 +0200

    Scaffold org.glassfish.jersey.ext:jersey-entity-filtering:2.35

---
+++ tests/src/org.glassfish.jersey.ext/jersey-entity-filtering/2.35/src/test/java/org_glassfish_jersey_ext/jersey_entity_filtering/Jersey_entity_filteringTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_glassfish_jersey_ext.jersey_entity_filtering;
+
+import org.junit.jupiter.api.Test;
+
+class Jersey_entity_filteringTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
