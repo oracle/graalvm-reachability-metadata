278be81df1a7da31387e2c9c58d84a40e3910cbf
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 05:37:19 +0200

    Dynamic-access coverage for javassist.LoaderClassPath (81/105)

--- tests/src/javassist/javassist/3.12.0.GA/src/test/java/javassist/javassist/ClassPoolTest.java
+++ tests/src/javassist/javassist/3.12.0.GA/src/test/java/javassist/javassist/ClassPoolTest.java
@@ -8,6 +8,10 @@ package javassist.javassist;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+
 import javassist.CannotCompileException;
 import javassist.ClassClassPath;
 import javassist.ClassPool;
@@ -40,9 +44,24 @@ public class ClassPoolTest {
         }
     }
 
+    @Test
+    void callsCompilerGeneratedClassLookupHelper() throws Throwable {
+        MethodHandles.Lookup lookup = MethodHandles.privateLookupIn(
+                ClassPool.class, MethodHandles.lookup());
+        MethodHandle classLookup = lookup.findStatic(
+                ClassPool.class,
+                "class$",
+                MethodType.methodType(Class.class, String.class));
+
+        Object result = classLookup.invoke(ClassPool.class.getName());
+
+        assertThat(result).isSameAs(ClassPool.class);
+    }
+
     private static ClassPool newInitializedClassPool() {
         ClassPool classPool = new ClassPool(true);
         classPool.insertClassPath(new ClassClassPath(ClassPoolTest.class));
+        classPool.insertClassPath(new ClassClassPath(ClassPool.class));
         return classPool;
     }
 
