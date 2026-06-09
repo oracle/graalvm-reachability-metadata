b821400048a1f6449fc6a47b26c7bffe7752a32f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 20:45:55 +0200

    Dynamic-access coverage for org.springframework.test.context.DefaultMethodInvoker (8/10)

---
+++ tests/src/org.springframework/spring-test/7.0.0/src/test/java/org_springframework/spring_test/DefaultMethodInvokerTest.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_springframework.spring_test;
+
+import java.lang.reflect.Method;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.test.context.MethodInvoker;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DefaultMethodInvokerTest {
+    @Test
+    void invokesNoArgumentMethodWithDefaultInvoker() throws Exception {
+        DefaultInvokerTarget target = new DefaultInvokerTarget();
+        Method method = DefaultInvokerTarget.class.getDeclaredMethod("message");
+
+        Object result = MethodInvoker.DEFAULT_INVOKER.invoke(method, target);
+
+        assertThat(result).isEqualTo("invoked");
+        assertThat(target.invoked).isTrue();
+    }
+
+    static class DefaultInvokerTarget {
+        private boolean invoked;
+
+        String message() {
+            this.invoked = true;
+            return "invoked";
+        }
+    }
+}
