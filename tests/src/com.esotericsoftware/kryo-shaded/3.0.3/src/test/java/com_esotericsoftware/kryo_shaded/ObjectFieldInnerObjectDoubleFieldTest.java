f989afef286fc1c4fbf40f92ae398301769731c4
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 12:21:37 +0200

    Dynamic-access coverage for com.esotericsoftware.kryo.serializers.ObjectField$ObjectDoubleField (64/113)

---
+++ tests/src/com.esotericsoftware/kryo-shaded/3.0.3/src/test/java/com_esotericsoftware/kryo_shaded/ObjectFieldInnerObjectDoubleFieldTest.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_esotericsoftware.kryo_shaded;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.esotericsoftware.kryo.Kryo;
+import com.esotericsoftware.kryo.Registration;
+import com.esotericsoftware.kryo.io.Input;
+import com.esotericsoftware.kryo.io.Output;
+import com.esotericsoftware.kryo.serializers.FieldSerializer;
+import com.esotericsoftware.kryo.serializers.FieldSerializer.CachedField;
+import java.lang.reflect.Method;
+import org.junit.jupiter.api.Test;
+import org.objenesis.instantiator.ObjectInstantiator;
+
+public class ObjectFieldInnerObjectDoubleFieldTest {
+    @Test
+    void serializesReadsAndCopiesPrivateDoubleFieldWithObjectCachedField() {
+        Kryo kryo = newKryoWithDoubleFieldSerializer();
+
+        DoubleFieldBean original = new DoubleFieldBean(12345.6789d);
+        byte[] bytes = writeObject(kryo, original);
+        DoubleFieldBean restored = kryo.readObject(new Input(bytes), DoubleFieldBean.class);
+        DoubleFieldBean copied = kryo.copy(original);
+
+        assertThat(restored.getValue()).isEqualTo(original.getValue());
+        assertThat(copied).isNotSameAs(original);
+        assertThat(copied.getValue()).isEqualTo(original.getValue());
+    }
+
+    @Test
+    void getsPrivateDoubleFieldThroughObjectCachedFieldMethod() throws ReflectiveOperationException {
+        Kryo kryo = newKryoWithDoubleFieldSerializer();
+        CachedField cachedField = ((FieldSerializer<?>) kryo.getSerializer(DoubleFieldBean.class)).getField("value");
+
+        assertThat(invokeObjectDoubleFieldGetField(cachedField, new DoubleFieldBean(Math.PI))).isEqualTo(Math.PI);
+    }
+
+    private static Kryo newKryoWithDoubleFieldSerializer() {
+        Kryo kryo = new Kryo();
+        kryo.setRegistrationRequired(true);
+        kryo.setReferences(false);
+        kryo.setAsmEnabled(true);
+
+        FieldSerializer<DoubleFieldBean> serializer = new FieldSerializer<>(kryo, DoubleFieldBean.class);
+        Registration registration = kryo.register(DoubleFieldBean.class, serializer);
+        registration.setInstantiator(new ObjectInstantiator<DoubleFieldBean>() {
+            @Override
+            public DoubleFieldBean newInstance() {
+                return new DoubleFieldBean();
+            }
+        });
+
+        assertThat(serializer.getField("value").getClass().getName())
+                .isEqualTo("com.esotericsoftware.kryo.serializers.ObjectField$ObjectDoubleField");
+        return kryo;
+    }
+
+    private static Object invokeObjectDoubleFieldGetField(CachedField cachedField, DoubleFieldBean bean)
+            throws ReflectiveOperationException {
+        Method getField = cachedField.getClass().getMethod("getField", Object.class);
+        getField.setAccessible(true);
+        return getField.invoke(cachedField, bean);
+    }
+
+    private static byte[] writeObject(Kryo kryo, DoubleFieldBean original) {
+        Output output = new Output(32, -1);
+        kryo.writeObject(output, original);
+        output.close();
+        return output.toBytes();
+    }
+
+    public static class DoubleFieldBean {
+        private double value;
+
+        public DoubleFieldBean() {
+        }
+
+        DoubleFieldBean(double value) {
+            this.value = value;
+        }
+
+        public double getValue() {
+            return value;
+        }
+    }
+}
