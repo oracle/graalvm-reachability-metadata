f1539f4a87c64c32088716f88176a3fea7ab9e4e
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 09:40:44 +0200

    Dynamic-access coverage for shaded.parquet.com.fasterxml.jackson.databind.type.TypeFactory (55/70)

---
+++ tests/src/org.apache.parquet/parquet-jackson/1.12.2/src/test/java/org_apache_parquet/parquet_jackson/TypeFactoryTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_parquet.parquet_jackson;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import shaded.parquet.com.fasterxml.jackson.databind.type.TypeFactory;
+
+public class TypeFactoryTest {
+    private static final String TARGET_CLASS_NAME =
+            "org_apache_parquet.parquet_jackson.TypeFactoryTestTarget";
+
+    @Test
+    void findsClassByNameWithThreadContextClassLoader() throws Exception {
+        final TypeFactory typeFactory = TypeFactory.defaultInstance();
+
+        final Class<?> targetClass = typeFactory.findClass(TARGET_CLASS_NAME);
+
+        assertThat(targetClass).isEqualTo(TypeFactoryTestTarget.class);
+    }
+
+    @Test
+    void fallsBackToDefaultClassLookupWhenThreadContextClassLoaderCannotLoadClass() throws Exception {
+        final ClassLoader originalClassLoader = Thread.currentThread().getContextClassLoader();
+        Thread.currentThread().setContextClassLoader(new RejectingClassLoader());
+        try {
+            final TypeFactory typeFactory = TypeFactory.defaultInstance();
+
+            final Class<?> targetClass = typeFactory.findClass(TARGET_CLASS_NAME);
+
+            assertThat(targetClass).isEqualTo(TypeFactoryTestTarget.class);
+        } finally {
+            Thread.currentThread().setContextClassLoader(originalClassLoader);
+        }
+    }
+
+    private static final class RejectingClassLoader extends ClassLoader {
+        RejectingClassLoader() {
+            super(null);
+        }
+
+        @Override
+        protected Class<?> loadClass(String name, boolean resolve) throws ClassNotFoundException {
+            throw new ClassNotFoundException(name);
+        }
+    }
+}
+
+final class TypeFactoryTestTarget {
+}
