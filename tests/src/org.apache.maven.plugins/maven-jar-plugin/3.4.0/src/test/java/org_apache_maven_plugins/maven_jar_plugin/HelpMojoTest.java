dd5d01ff98661007c7049ad31e92a8aae5eefd00
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 13:13:52 +0200

    [GenAI] Test fix for org.apache.maven.plugins:maven-jar-plugin:3.4.0 using gpt-5.5 (#8239)
    * Prepare project for org.apache.maven.plugins:maven-jar-plugin:3.4.0
    
    * Preserve last passing candidate for org.apache.maven.plugins:maven-jar-plugin:3.4.0
    
    * Update generated library support for org.apache.maven.plugins:maven-jar-plugin:3.4.0
    
    * Fixed test for org.apache.maven.plugins:maven-jar-plugin:3.4.0

---
+++ tests/src/org.apache.maven.plugins/maven-jar-plugin/3.4.0/src/test/java/org_apache_maven_plugins/maven_jar_plugin/HelpMojoTest.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_maven_plugins.maven_jar_plugin;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+import org.apache.maven.plugins.maven_jar_plugin.HelpMojo;
+import org.junit.jupiter.api.Test;
+
+public class HelpMojoTest {
+    @Test
+    void executeLoadsPluginHelpResource() {
+        HelpMojo mojo = new HelpMojo();
+
+        assertThatCode(mojo::execute).doesNotThrowAnyException();
+    }
+}
