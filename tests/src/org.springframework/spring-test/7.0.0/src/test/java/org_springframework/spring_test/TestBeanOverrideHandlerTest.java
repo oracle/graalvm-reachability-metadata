1a50eba199f41d0837185fd424a4bfb2b250be1d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-09 17:42:45 +0200

    Dynamic-access coverage for org.springframework.test.context.bean.override.convention.TestBeanOverrideHandler (10/10)

---
+++ tests/src/org.springframework/spring-test/7.0.0/src/test/java/org_springframework/spring_test/TestBeanOverrideHandlerTest.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_springframework.spring_test;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestContextManager;
+import org.springframework.test.context.bean.override.convention.TestBean;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class TestBeanOverrideHandlerTest {
+    @Test
+    void invokesTestBeanFactoryMethodWhenPreparingSpringTestContext() throws Exception {
+        TestContextManager manager = new TestContextManager(UsesTestBean.class);
+        UsesTestBean testInstance = new UsesTestBean();
+
+        try {
+            manager.prepareTestInstance(testInstance);
+
+            ApplicationContext applicationContext = manager.getTestContext().getApplicationContext();
+            MessageService bean = applicationContext.getBean(MessageService.class);
+
+            assertThat(testInstance.messageService).isSameAs(bean);
+            assertThat(bean.message()).isEqualTo("test override");
+        }
+        finally {
+            manager.afterTestClass();
+        }
+    }
+
+    @ContextConfiguration(classes = TestConfiguration.class)
+    static class UsesTestBean {
+        @TestBean(methodName = "overrideMessageService")
+        private MessageService messageService;
+
+        private static MessageService overrideMessageService() {
+            return new MessageService("test override");
+        }
+    }
+
+    @Configuration(proxyBeanMethods = false)
+    static class TestConfiguration {
+        @Bean
+        MessageService messageService() {
+            return new MessageService("production");
+        }
+    }
+
+    record MessageService(String message) {
+    }
+}
