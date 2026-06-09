0213356fa9869e8ecd266d6903961fdb8b8f4fc6
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-06-09 21:41:54 +0200

    Dynamic-access coverage for net.sf.saxon.expr.PJConverter (19/34)

---
+++ tests/src/net.sf.saxon/Saxon-HE/9.9.1-6/src/test/java/net_sf_saxon/Saxon_HE/PJConverterTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package net_sf_saxon.Saxon_HE;
+
+import net.sf.saxon.Configuration;
+import net.sf.saxon.expr.PJConverter;
+import net.sf.saxon.expr.StaticProperty;
+import net.sf.saxon.s9api.XdmAtomicValue;
+import net.sf.saxon.type.BuiltInAtomicType;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PJConverterTest {
+    @Test
+    void allocatesConverterForUntypedAtomicValueToTargetWithStringConstructor() throws Exception {
+        Configuration configuration = Configuration.newConfiguration();
+
+        PJConverter converter = PJConverter.allocate(
+                configuration,
+                BuiltInAtomicType.UNTYPED_ATOMIC,
+                StaticProperty.ALLOWS_ONE,
+                XdmAtomicValue.class);
+
+        assertThat(converter).isNotNull();
+    }
+}
