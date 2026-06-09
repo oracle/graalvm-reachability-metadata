5e3293277d0d30e7569ada0bb0cec213c90396ae
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-10 01:59:34 +0200

    Scaffold com.google.cloud:google-cloud-spanner:6.116.1

---
+++ tests/src/com.google.cloud/google-cloud-spanner/6.116.1/src/test/java/com_google_cloud/google_cloud_spanner/Google_cloud_spannerTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_google_cloud.google_cloud_spanner;
+
+import org.junit.jupiter.api.Test;
+
+class Google_cloud_spannerTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
