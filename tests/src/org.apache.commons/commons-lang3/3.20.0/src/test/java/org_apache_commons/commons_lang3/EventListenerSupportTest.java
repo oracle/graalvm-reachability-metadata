fbb62bdae7c1664c4d8fce2fda4dd8583a9bc160
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-06-10 17:32:46 +0200

    Update generated library support for org.apache.commons:commons-lang3:3.20.0

--- tests/src/org.apache.commons/commons-lang3/3.20.0/src/test/java/org_apache_commons/commons_lang3/EventListenerSupportTest.java
+++ tests/src/org.apache.commons/commons-lang3/3.20.0/src/test/java/org_apache_commons/commons_lang3/EventListenerSupportTest.java
@@ -6,62 +6,11 @@
  */
 package org_apache_commons.commons_lang3;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.commons.lang3.event.EventListenerSupport;
-import org.junit.jupiter.api.Test;
-
 public class EventListenerSupportTest {
-
-    @Test
-    public void serializationRoundTripKeepsSerializableListenersAndDropsNonSerializableListeners()
-            throws IOException, ClassNotFoundException {
-        EventListenerSupport<AuditListener> listenerSupport = EventListenerSupport.create(AuditListener.class);
-        SerializableAuditListener serializableListener = new SerializableAuditListener("persisted");
-        NonSerializableAuditListener nonSerializableListener = new NonSerializableAuditListener("transient");
-
-        listenerSupport.addListener(serializableListener);
-        listenerSupport.addListener(nonSerializableListener);
-        listenerSupport.fire().onAudit("before-serialization");
-
-        EventListenerSupport<AuditListener> deserializedListenerSupport = roundTrip(listenerSupport);
-        AuditListener[] restoredListeners = deserializedListenerSupport.getListeners();
-
-        assertThat(restoredListeners).hasSize(1);
-        assertThat(restoredListeners[0]).isInstanceOf(SerializableAuditListener.class);
-
-        SerializableAuditListener restoredListener = (SerializableAuditListener) restoredListeners[0];
-        assertThat(restoredListener.events()).containsExactly("persisted:before-serialization");
-
-        deserializedListenerSupport.fire().onAudit("after-deserialization");
-
-        assertThat(restoredListener.events())
-                .containsExactly("persisted:before-serialization", "persisted:after-deserialization");
-        assertThat(nonSerializableListener.events()).containsExactly("transient:before-serialization");
-    }
-
-    @SuppressWarnings("unchecked")
-    private static EventListenerSupport<AuditListener> roundTrip(final EventListenerSupport<AuditListener> listenerSupport)
-            throws IOException, ClassNotFoundException {
-        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
-        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(byteArrayOutputStream)) {
-            objectOutputStream.writeObject(listenerSupport);
-        }
-
-        try (ObjectInputStream objectInputStream = new ObjectInputStream(
-                new ByteArrayInputStream(byteArrayOutputStream.toByteArray()))) {
-            return (EventListenerSupport<AuditListener>) objectInputStream.readObject();
-        }
-    }
 
     public interface AuditListener {
 
