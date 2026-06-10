00a0c4a33359ae373433b78c088a3cb5609fc911
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-10 05:41:10 +0200

    Update generated library support for org.springframework:spring-test:7.0.0

---
+++ tests/src/org.springframework/spring-test/7.0.0/src/test/java/org_springframework/spring_test/TestBeanOverrideHandlerTest_TestBeanTestCase__TestContext001_ApplicationContextInitializer.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_springframework.spring_test;
+
+import org.springframework.aot.generate.Generated;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.annotation.ContextAnnotationAutowireCandidateResolver;
+import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.core.annotation.AnnotationAwareOrderComparator;
+
+/**
+ * {@link ApplicationContextInitializer} to restore an application context based
+ * on previous AOT processing.
+ */
+@Generated
+public class TestBeanOverrideHandlerTest_TestBeanTestCase__TestContext001_ApplicationContextInitializer implements ApplicationContextInitializer<GenericApplicationContext> {
+    @Override
+    public void initialize(GenericApplicationContext applicationContext) {
+        DefaultListableBeanFactory beanFactory = applicationContext.getDefaultListableBeanFactory();
+        beanFactory.setAutowireCandidateResolver(new ContextAnnotationAutowireCandidateResolver());
+        beanFactory.setDependencyComparator(AnnotationAwareOrderComparator.INSTANCE);
+        new TestBeanOverrideHandlerTest_TestBeanTestCase__TestContext001_BeanFactoryRegistrations().registerBeanDefinitions(beanFactory);
+        new TestBeanOverrideHandlerTest_TestBeanTestCase__TestContext001_BeanFactoryRegistrations().registerAliases(beanFactory);
+    }
+}
