dd2327eec2d9e0c501bd31bcfd304f1685bc103a
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-06-10 03:14:47 +0200

    Dynamic-access coverage for org.apache.commons.lang3.function.MethodInvokers (68/68)

---
+++ tests/src/org.apache.commons/commons-lang3/3.19.0/src/test/java/org_apache_commons/commons_lang3/MethodInvokersTest.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_commons.commons_lang3;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.lang.reflect.Method;
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import org.apache.commons.lang3.function.MethodInvokers;
+import org.junit.jupiter.api.Test;
+
+public class MethodInvokersTest {
+
+    @Test
+    public void asFunctionInvokesInstanceSupplierMethod() throws Exception {
+        Method method = String.class.getMethod("length");
+
+        Function<String, Integer> length = MethodInvokers.asFunction(method);
+
+        assertThat(length.apply("commons")).isEqualTo(7);
+    }
+
+    @Test
+    public void asBiFunctionInvokesInstanceMethodWithArgument() throws Exception {
+        Method method = String.class.getMethod("charAt", int.class);
+
+        BiFunction<String, Integer, Character> charAt = MethodInvokers.asBiFunction(method);
+
+        assertThat(charAt.apply("lang", 2)).isEqualTo('n');
+    }
+
+    @Test
+    public void asBiConsumerInvokesInstanceConsumerMethod() throws Exception {
+        Method method = MutableText.class.getMethod("append", String.class);
+        MutableText target = new MutableText();
+
+        BiConsumer<MutableText, String> append = MethodInvokers.asBiConsumer(method);
+        append.accept(target, "native-image");
+
+        assertThat(target.getText()).isEqualTo("native-image");
+    }
+
+    @Test
+    public void asSupplierInvokesStaticSupplierMethod() throws Exception {
+        Method method = MethodInvokersTest.class.getMethod("greeting");
+
+        Supplier<String> supplier = MethodInvokers.asSupplier(method);
+
+        assertThat(supplier.get()).isEqualTo("hello commons-lang");
+    }
+
+    public static String greeting() {
+        return "hello commons-lang";
+    }
+
+    public static class MutableText {
+        private final StringBuilder builder = new StringBuilder();
+
+        public void append(String value) {
+            builder.append(value);
+        }
+
+        public String getText() {
+            return builder.toString();
+        }
+    }
+}
