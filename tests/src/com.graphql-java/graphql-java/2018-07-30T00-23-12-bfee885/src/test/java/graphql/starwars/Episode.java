d178e1614d7cdbadc8ad75c3adc1c06db269f4eb
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-26 18:32:51 +0200

    Prepare project for com.graphql-java:graphql-java:2018-07-30T00-23-12-bfee885

---
+++ tests/src/com.graphql-java/graphql-java/2018-07-30T00-23-12-bfee885/src/test/java/graphql/starwars/Episode.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package graphql.starwars;
+
+/**
+ * @author Brian Clozel
+ */
+public enum Episode {
+  NEWHOPE,
+  EMPIRE,
+  JEDI
+}
