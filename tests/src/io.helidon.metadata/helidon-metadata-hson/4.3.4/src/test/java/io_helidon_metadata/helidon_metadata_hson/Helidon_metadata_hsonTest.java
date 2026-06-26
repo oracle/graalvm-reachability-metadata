e39d35ff6790ca75e4347f4a0c5ea9a99b26ae8f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-26 13:00:06 +0200

    Scaffold io.helidon.metadata:helidon-metadata-hson:4.3.4

---
+++ tests/src/io.helidon.metadata/helidon-metadata-hson/4.3.4/src/test/java/io_helidon_metadata/helidon_metadata_hson/Helidon_metadata_hsonTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_helidon_metadata.helidon_metadata_hson;
+
+import org.junit.jupiter.api.Test;
+
+class Helidon_metadata_hsonTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
