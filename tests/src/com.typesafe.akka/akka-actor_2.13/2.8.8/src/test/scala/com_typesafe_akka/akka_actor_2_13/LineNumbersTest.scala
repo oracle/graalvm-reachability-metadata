90219ede65fa5ccf75d911c9aedabb222732c29d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-27 16:45:19 +0200

    Partial dynamic-access coverage for akka.util.LineNumbers$ (1/4)

---
+++ tests/src/com.typesafe.akka/akka-actor_2.13/2.8.8/src/test/scala/com_typesafe_akka/akka_actor_2_13/LineNumbersTest.scala
@@ -0,0 +1,43 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_typesafe_akka.akka_actor_2_13
+
+import akka.util.LineNumbers
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class LineNumbersTest {
+
+  @Test
+  def readsSourceInformationFromAnOrdinaryObjectClassResource(): Unit = {
+    val target: PlainLineNumberTarget = new PlainLineNumberTarget("ordinary-object")
+
+    val result: LineNumbers.Result = LineNumbers(target)
+
+    assertThat(result).isNotEqualTo(LineNumbers.NoSourceInfo)
+    assertThat(result).isNotInstanceOf(classOf[LineNumbers.UnknownSourceFormat])
+    assertThat(result.toString).contains("LineNumbersTest.scala")
+  }
+
+  @Test
+  def readsSourceInformationFromASerializableLambdaImplementationClassResource(): Unit = {
+    val target: SerializableThunk = () => "serializable-lambda"
+
+    assertThat(target.value()).isEqualTo("serializable-lambda")
+    val result: LineNumbers.Result = LineNumbers(target.asInstanceOf[AnyRef])
+
+    assertThat(result).isNotEqualTo(LineNumbers.NoSourceInfo)
+    assertThat(result).isNotInstanceOf(classOf[LineNumbers.UnknownSourceFormat])
+    assertThat(result.toString).contains("LineNumbersTest.scala")
+  }
+
+  private final class PlainLineNumberTarget(val name: String)
+
+  private trait SerializableThunk extends Serializable {
+    def value(): String
+  }
+}
