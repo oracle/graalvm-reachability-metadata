00d82055fcd5a0ab3970c1b8b45f18941a63b0d2
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-27 15:09:10 +0200

    Update generated library support for org.apache.pekko:pekko-actor_2.13:1.1.5

--- tests/src/org.apache.pekko/pekko-actor_2.13/1.1.5/src/test/scala/org_apache_pekko/pekko_actor_2_13/AbstractPropsTest.scala
+++ tests/src/org.apache.pekko/pekko-actor_2.13/1.1.5/src/test/scala/org_apache_pekko/pekko_actor_2_13/AbstractPropsTest.scala
@@ -9,11 +9,8 @@ package org_apache_pekko.pekko_actor_2_13
 import org.apache.pekko.actor.Actor
 import org.apache.pekko.actor.Props
 import org.apache.pekko.japi.Creator
-import org_apache_pekko.pekko_actor_2_13.AbstractPropsJavaCreators.JavaConstructorProbeActor
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertThrows
 import org.junit.jupiter.api.Test
-import org.junit.jupiter.api.function.Executable
 
 class AbstractPropsTest {
   @Test
@@ -22,20 +19,6 @@ class AbstractPropsTest {
     val props: Props = Props.create(classOf[ConstructorProbeActor], creator)
 
     assertThat(props.actorClass()).isEqualTo(classOf[ConstructorProbeActor])
-  }
-
-  @Test
-  def rejectsNonStaticJavaAnonymousInnerCreator(): Unit = {
-    val factory: AbstractPropsJavaCreators = new AbstractPropsJavaCreators
-    val creator: Creator[JavaConstructorProbeActor] = factory.newAnonymousInnerCreator()
-    val exception: IllegalArgumentException = assertThrows(classOf[IllegalArgumentException], new Executable {
-      override def execute(): Unit = {
-        Props.create(classOf[JavaConstructorProbeActor], creator)
-        ()
-      }
-    })
-
-    assertThat(exception).hasMessageContaining("cannot use non-static local Creator to create actors")
   }
 
   final class PublicConstructorCreator extends Creator[ConstructorProbeActor] {
