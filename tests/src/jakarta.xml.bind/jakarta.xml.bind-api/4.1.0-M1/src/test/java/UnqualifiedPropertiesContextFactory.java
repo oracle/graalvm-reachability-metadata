263a50e4590dee8561ea9ab7a1033a880d4dd141
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-27 15:01:58 +0200

    [GenAI] Test fix for jakarta.xml.bind:jakarta.xml.bind-api:4.1.0-M1 using gpt-5.5 (#8659)
    * Prepare project for jakarta.xml.bind:jakarta.xml.bind-api:4.1.0-M1
    
    * Dynamic-access coverage for jakarta.xml.bind.ServiceLoaderUtil (25/25)
    
    * Native metadata for jakarta.xml.bind.ServiceLoaderUtil
    
    * Preserve last passing candidate for jakarta.xml.bind:jakarta.xml.bind-api:4.1.0-M1
    
    * Update generated library support for jakarta.xml.bind:jakarta.xml.bind-api:4.1.0-M1
    
    * Fixed test for jakarta.xml.bind:jakarta.xml.bind-api:4.1.0-M1

---
+++ tests/src/jakarta.xml.bind/jakarta.xml.bind-api/4.1.0-M1/src/test/java/UnqualifiedPropertiesContextFactory.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+import java.util.Map;
+
+import jakarta.xml.bind.JAXBContext;
+import jakarta_xml_bind.jakarta_xml_bind_api.support.PropertiesContextFactory;
+
+public final class UnqualifiedPropertiesContextFactory {
+    private UnqualifiedPropertiesContextFactory() {
+    }
+
+    public static JAXBContext createContext(String contextPath, ClassLoader classLoader, Map<?, ?> properties) {
+        return PropertiesContextFactory.createContext(contextPath, classLoader, properties);
+    }
+
+    public static JAXBContext createContext(Class<?>[] classes, Map<?, ?> properties) {
+        return PropertiesContextFactory.createContext(classes, properties);
+    }
+}
