5ad9078d185b6a5fd8e4f17a6c5ad23793038fc1
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 01:13:05 +0200

    [GenAI] Add support for io.getquill:quill-sql_2.13:4.8.5 using gpt-5.5 (#8746)
    * Scaffold io.getquill:quill-sql_2.13:4.8.5
    
    * Dynamic-access coverage for io.getquill.context.sql.dsl.SqlDsl$ForUpdate$$anon$2 (2/4)
    
    * Dynamic-access coverage for io.getquill.context.sql.dsl.SqlDsl$Like$$anon$1 (4/4)
    
    * Native metadata for io.getquill.context.sql.dsl.SqlDsl$Like$$anon$1__native_batch_2
    
    * Update generated library support for io.getquill:quill-sql_2.13:4.8.5
    
    * Add support for io.getquill:quill-sql_2.13:4.8.5

---
+++ tests/src/io.getquill/quill-sql_2.13/4.8.5/src/test/scala/io_getquill/quill_sql_2_13/SqlDslInnerForUpdateAnonymous2Test.scala
@@ -0,0 +1,30 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_getquill.quill_sql_2_13
+
+import io.getquill.Literal
+import io.getquill.MirrorSqlDialect
+import io.getquill.Query
+import io.getquill.Quoted
+import io.getquill.context.sql.SqlContext
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+final case class LockableRow(id: Int, name: String)
+
+class SqlDslInnerForUpdateAnonymous2Test {
+  @Test
+  def forUpdateCreatesQuotedQueryWithLockingClause(): Unit = {
+    val sqlContext: SqlContext[MirrorSqlDialect.type, Literal.type] = null
+    val sourceQuery: Query[LockableRow] = null.asInstanceOf[Query[LockableRow]]
+    val lockedRows: Quoted[Query[LockableRow]] =
+      new sqlContext.ForUpdate[LockableRow](sourceQuery).forUpdate()
+
+    assertThat(lockedRows.ast.toString)
+      .contains("FOR UPDATE")
+  }
+}
