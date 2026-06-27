ca8c45aab66e65a2f3378c679b53116f56cd5061
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-27 02:36:55 +0200

    Scaffold com.openai:openai-java-core:4.38.0

---
+++ tests/src/com.openai/openai-java-core/4.38.0/src/test/kotlin/com_openai/openai_java_core/Openai_java_coreTest.kt
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_openai.openai_java_core
+
+import org.junit.jupiter.api.Test
+
+class Openai_java_coreTest {
+    @Test
+    fun test() {
+        println("This is just a placeholder, implement your test")
+    }
+}
