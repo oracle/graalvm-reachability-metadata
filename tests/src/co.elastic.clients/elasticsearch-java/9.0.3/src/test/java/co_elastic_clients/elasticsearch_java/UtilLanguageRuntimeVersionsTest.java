1594d420d8e1213244491b52b840e66fea11f574
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 12:32:02 +0200

    Dynamic-access coverage for co.elastic.clients.util.LanguageRuntimeVersions (12/17)

---
+++ tests/src/co.elastic.clients/elasticsearch-java/9.0.3/src/test/java/co_elastic_clients/elasticsearch_java/UtilLanguageRuntimeVersionsTest.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package co_elastic_clients.elasticsearch_java;
+
+import co.elastic.clients.util.LanguageRuntimeVersions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class UtilLanguageRuntimeVersionsTest {
+    @Test
+    void detectsAvailableJvmLanguageRuntimeVersions() {
+        assertThat(LanguageRuntimeVersions.kotlinVersion()).matches("\\d+\\.\\d+");
+        assertThat(LanguageRuntimeVersions.scalaVersion()).matches("\\d+\\.\\d+");
+        assertThat(LanguageRuntimeVersions.getRuntimeMetadata())
+            .containsPattern("(^|,)kt=\\d+\\.\\d+($|,)")
+            .containsPattern("(^|,)sc=\\d+\\.\\d+($|,)");
+    }
+}
