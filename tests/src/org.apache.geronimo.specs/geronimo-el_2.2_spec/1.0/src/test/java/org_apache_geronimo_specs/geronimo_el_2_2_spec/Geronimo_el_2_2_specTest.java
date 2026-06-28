b44692495bd4cb6ed0bad1d69a076b3efa8f6617
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 16:28:09 +0200

    Dynamic-access coverage for javax.el.BeanELResolver (13/17)

--- tests/src/org.apache.geronimo.specs/geronimo-el_2.2_spec/1.0/src/test/java/org_apache_geronimo_specs/geronimo_el_2_2_spec/Geronimo_el_2_2_specTest.java
+++ tests/src/org.apache.geronimo.specs/geronimo-el_2.2_spec/1.0/src/test/java/org_apache_geronimo_specs/geronimo_el_2_2_spec/Geronimo_el_2_2_specTest.java
@@ -8,7 +8,7 @@ package org_apache_geronimo_specs.geronimo_el_2_2_spec;
 
 import org.junit.jupiter.api.Test;
 
-class Geronimo_el_2_2_specTest {
+public class Geronimo_el_2_2_specTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
