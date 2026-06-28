48f5af7af44147dae102686158b6bc097635c6bf
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 05:21:17 +0200

    Partial dynamic-access coverage for net.sf.ehcache.util.ClassLoaderUtil (3/4)

---
+++ tests/src/net.sf.ehcache/ehcache-core/2.4.4/src/test/java/net_sf_ehcache/ehcache_core/ClassLoaderUtilTest.java
@@ -0,0 +1,43 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package net_sf_ehcache.ehcache_core;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import net.sf.ehcache.config.CacheConfiguration;
+import net.sf.ehcache.util.ClassLoaderUtil;
+
+import org.junit.jupiter.api.Test;
+
+public class ClassLoaderUtilTest {
+    @Test
+    void loadsEhcacheClassWithStandardClassLoader() throws ClassNotFoundException {
+        Class<?> loadedClass = ClassLoaderUtil.loadClass(CacheConfiguration.class.getName());
+
+        assertThat(loadedClass).isSameAs(CacheConfiguration.class);
+    }
+
+    @Test
+    void createsEhcacheInstanceWithNoArgumentConstructor() {
+        Object instance = ClassLoaderUtil.createNewInstance(CacheConfiguration.class.getName());
+
+        assertThat(instance).isInstanceOf(CacheConfiguration.class);
+    }
+
+    @Test
+    void createsEhcacheInstanceWithPublicConstructorArguments() {
+        Object instance = ClassLoaderUtil.createNewInstance(
+                CacheConfiguration.class.getName(),
+                new Class[] {String.class, int.class},
+                new Object[] {"configured-cache", 128});
+
+        assertThat(instance).isInstanceOf(CacheConfiguration.class);
+        CacheConfiguration configuration = (CacheConfiguration) instance;
+        assertThat(configuration.getName()).isEqualTo("configured-cache");
+        assertThat(configuration.getMaxElementsInMemory()).isEqualTo(128);
+    }
+}
