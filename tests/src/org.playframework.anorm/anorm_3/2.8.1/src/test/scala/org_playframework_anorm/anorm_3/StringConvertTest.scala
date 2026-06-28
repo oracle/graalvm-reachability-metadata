89dce7cd306af184aa8dc20fcf89829223079e88
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 09:52:22 +0200

    Dynamic-access coverage for org.joda.convert.StringConvert (44/105)

--- tests/src/org.playframework.anorm/anorm_3/2.8.1/src/test/scala/org_playframework_anorm/anorm_3/StringConvertTest.scala
+++ tests/src/org.playframework.anorm/anorm_3/2.8.1/src/test/scala/org_playframework_anorm/anorm_3/StringConvertTest.scala
@@ -6,17 +6,25 @@
  */
 package org_playframework_anorm.anorm_3
 
+import java.lang.module.Configuration
+import java.lang.module.FindException
+import java.lang.module.ModuleFinder
 import java.lang.reflect.Type
+import java.nio.file.Path
+import java.nio.file.Paths
 import java.util.OptionalDouble
 import java.util.OptionalInt
 import java.util.OptionalLong
+import java.util.Set
 
 import com.google.common.reflect.TypeToken
+import org.graalvm.internal.tck.NativeImageSupport
 import org.joda.convert.StringConvert
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 import scala.annotation.static
+import scala.jdk.CollectionConverters.*
 
 class StringConvertTest {
   @Test
@@ -118,6 +126,75 @@ class StringConvertTest {
 
     assertEquals("from-char-sequence-constructor", parsed.asText)
     assertEquals("from-char-sequence-constructor", convert.convertToString(parsed))
+  }
+
+  @Test
+  def constructorAddsAReadEdgeToGuavaWhenLoadedAsNamedModules(): Unit = {
+    try {
+      assertEquals("java.lang.String", convertGuavaTypeTokenInNamedModuleLayer())
+    } catch {
+      case error: Error if NativeImageSupport.isUnsupportedFeatureError(error) =>
+      case exception: FindException if isNativeImageCodeSourceFailure(exception) =>
+        // Native Image code sources point at the executable, not the original modular JARs.
+    }
+  }
+
+  private def convertGuavaTypeTokenInNamedModuleLayer(): String = {
+    val jodaConvertJar: Path = jarPath(classOf[StringConvert])
+    val classPathTypeTokenClass: Class[?] = Class.forName("com.google.common.reflect.TypeToken")
+    val guavaJar: Path = jarPath(classPathTypeTokenClass)
+    val failureAccessClass: Class[?] = Class.forName(
+      "com.google.common.util.concurrent.internal.InternalFutureFailureAccess"
+    )
+    val failureAccessJar: Path = jarPath(failureAccessClass)
+    val finder: ModuleFinder = ModuleFinder.of(jodaConvertJar, guavaJar, failureAccessJar)
+    val jodaConvertModuleName: String = moduleName(finder, "org.joda.convert")
+    val guavaModuleName: String = moduleName(finder, "com.google.common.reflect")
+    val configuration: Configuration = ModuleLayer.boot().configuration().resolve(
+      finder,
+      ModuleFinder.of(),
+      Set.of(jodaConvertModuleName, guavaModuleName)
+    )
+    val layer: ModuleLayer = ModuleLayer.boot().defineModulesWithOneLoader(
+      configuration,
+      ClassLoader.getPlatformClassLoader
+    )
+    val loader: ClassLoader = layer.findLoader(jodaConvertModuleName)
+    val thread: Thread = Thread.currentThread()
+    val originalLoader: ClassLoader = thread.getContextClassLoader
+    thread.setContextClassLoader(loader)
+    try {
+      val stringConvertType: Class[?] = Class.forName("org.joda.convert.StringConvert", true, loader)
+      val convert: Object = stringConvertType.getConstructor().newInstance().asInstanceOf[Object]
+      val typeTokenClass: Class[?] = Class.forName("com.google.common.reflect.TypeToken", true, loader)
+      val token: Object = typeTokenClass.getMethod("of", classOf[Type]).invoke(null, classOf[String])
+
+      stringConvertType
+        .getMethod("convertToString", classOf[Class[?]], classOf[Object])
+        .invoke(convert, typeTokenClass, token)
+        .asInstanceOf[String]
+    } finally {
+      thread.setContextClassLoader(originalLoader)
+    }
+  }
+
+  private def jarPath(`type`: Class[?]): Path = {
+    Paths.get(`type`.getProtectionDomain.getCodeSource.getLocation.toURI)
+  }
+
+  private def isNativeImageCodeSourceFailure(exception: FindException): Boolean = {
+    val message: String = Option(exception.getMessage).getOrElse("")
+    message.contains("Unable to derive module descriptor") || message.contains("Module format not recognized")
+  }
+
+  private def moduleName(finder: ModuleFinder, packageName: String): String = {
+    finder
+      .findAll()
+      .asScala
+      .find(reference => reference.descriptor().packages().contains(packageName))
+      .getOrElse(throw new IllegalStateException(s"No module contains package $packageName"))
+      .descriptor()
+      .name()
   }
 }
 
