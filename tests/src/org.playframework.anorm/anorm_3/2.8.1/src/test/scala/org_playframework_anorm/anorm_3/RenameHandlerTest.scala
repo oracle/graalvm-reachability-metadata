ca43a678b2c0a63b17e23ff02f69d01b77c7555f
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 10:37:05 +0200

    Partial dynamic-access coverage for org.joda.convert.RenameHandler (1/2)

---
+++ tests/src/org.playframework.anorm/anorm_3/2.8.1/src/test/scala/org_playframework_anorm/anorm_3/RenameHandlerTest.scala
@@ -0,0 +1,20 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_playframework_anorm.anorm_3
+
+import org.joda.convert.RenameHandler
+import org.junit.jupiter.api.Assertions.assertSame
+import org.junit.jupiter.api.Test
+
+class RenameHandlerTest {
+  @Test
+  def loadsRenameConfigurationResourcesFromClasspath(): Unit = {
+    val handler: RenameHandler = RenameHandler.create(true)
+
+    assertSame(classOf[RenameHandlerTest], handler.lookupType("example.LegacyAnormName"))
+  }
+}
