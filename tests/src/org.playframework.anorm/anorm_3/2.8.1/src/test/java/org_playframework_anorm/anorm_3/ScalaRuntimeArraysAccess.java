13ef3de40ce81db6aa809a9b626de07eb2d860ef
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 11:26:00 +0200

    Dynamic-access coverage for scala.runtime.Arrays$ (105/105)

---
+++ tests/src/org.playframework.anorm/anorm_3/2.8.1/src/test/java/org_playframework_anorm/anorm_3/ScalaRuntimeArraysAccess.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_playframework_anorm.anorm_3;
+
+final class ScalaRuntimeArraysAccess {
+    private ScalaRuntimeArraysAccess() {
+    }
+
+    static String[][] newStringMatrix(int rows, int columns) {
+        return scala.runtime.Arrays$.MODULE$.newArray(
+                String.class,
+                String[][].class,
+                new int[] { rows, columns });
+    }
+
+    static int[][] newIntMatrix(int rows, int columns) {
+        return scala.runtime.Arrays$.MODULE$.newArray(
+                int.class,
+                int[][].class,
+                new int[] { rows, columns });
+    }
+}
