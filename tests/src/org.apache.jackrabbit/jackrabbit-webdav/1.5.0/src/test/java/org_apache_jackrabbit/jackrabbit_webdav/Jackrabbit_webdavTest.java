d950488ecf1d2c81fdf8a6a38c7a47089cfd242d
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-28 22:25:42 +0200

    Dynamic-access coverage for org.apache.jackrabbit.webdav.version.report.ReportType (13/83)

--- tests/src/org.apache.jackrabbit/jackrabbit-webdav/1.5.0/src/test/java/org_apache_jackrabbit/jackrabbit_webdav/Jackrabbit_webdavTest.java
+++ tests/src/org.apache.jackrabbit/jackrabbit-webdav/1.5.0/src/test/java/org_apache_jackrabbit/jackrabbit_webdav/Jackrabbit_webdavTest.java
@@ -8,7 +8,7 @@ package org_apache_jackrabbit.jackrabbit_webdav;
 
 import org.junit.jupiter.api.Test;
 
-class Jackrabbit_webdavTest {
+public class Jackrabbit_webdavTest {
     @Test
     void test() throws Exception {
         System.out.println("This is just a placeholder, implement your test");
