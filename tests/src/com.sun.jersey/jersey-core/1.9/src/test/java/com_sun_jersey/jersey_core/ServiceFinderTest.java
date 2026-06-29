b3c4811850c2bc83d6bfb3a64707dd6d3d5989f2
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-29 04:20:22 +0200

    Partial dynamic-access coverage for com.sun.jersey.spi.service.ServiceFinder (5/6)

---
+++ tests/src/com.sun.jersey/jersey-core/1.9/src/test/java/com_sun_jersey/jersey_core/ServiceFinderTest.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_sun_jersey.jersey_core;
+
+import com.sun.jersey.core.impl.provider.header.MediaTypeProvider;
+import com.sun.jersey.spi.HeaderDelegateProvider;
+import com.sun.jersey.spi.service.ServiceFinder;
+import javax.ws.rs.core.Application;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.Variant.VariantListBuilder;
+import javax.ws.rs.ext.RuntimeDelegate;
+import javax.ws.rs.ext.RuntimeDelegate.HeaderDelegate;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ServiceFinderTest {
+    @BeforeAll
+    static void setRuntimeDelegate() {
+        RuntimeDelegate.setInstance(new MinimalRuntimeDelegate());
+    }
+
+    @Test
+    void discoversHeaderDelegateProvidersAsInstancesAndClasses() {
+        ClassLoader classLoader = ServiceFinderTest.class.getClassLoader();
+        ServiceFinder<HeaderDelegateProvider> finder = ServiceFinder.find(
+                HeaderDelegateProvider.class, classLoader);
+
+        HeaderDelegateProvider[] providers = finder.toArray();
+        Class<HeaderDelegateProvider>[] providerClasses = finder.toClassArray();
+
+        assertThat(providers)
+                .isNotEmpty()
+                .anySatisfy(provider -> assertThat(provider.supports(MediaType.class)).isTrue());
+        assertThat(providerClasses)
+                .extracting(Class::getName)
+                .contains(MediaTypeProvider.class.getName());
+    }
+
+    @Test
+    void discoversHeaderDelegateProviderClassesWithSystemLookupWhenLoaderIsNull() {
+        ServiceFinder<HeaderDelegateProvider> finder = ServiceFinder.find(
+                HeaderDelegateProvider.class, null);
+
+        Class<HeaderDelegateProvider>[] providerClasses = finder.toClassArray();
+
+        assertThat(providerClasses)
+                .extracting(Class::getName)
+                .contains(MediaTypeProvider.class.getName());
+    }
+
+    private static final class MinimalRuntimeDelegate extends RuntimeDelegate {
+        @Override
+        public UriBuilder createUriBuilder() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public ResponseBuilder createResponseBuilder() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public VariantListBuilder createVariantListBuilder() {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public <T> T createEndpoint(Application application, Class<T> endpointType) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        @SuppressWarnings("unchecked")
+        public <T> HeaderDelegate<T> createHeaderDelegate(Class<T> type) {
+            return (HeaderDelegate<T>) NoOpHeaderDelegate.INSTANCE;
+        }
+    }
+
+    private enum NoOpHeaderDelegate implements HeaderDelegate<Object> {
+        INSTANCE;
+
+        @Override
+        public Object fromString(String value) {
+            throw new UnsupportedOperationException();
+        }
+
+        @Override
+        public String toString(Object value) {
+            throw new UnsupportedOperationException();
+        }
+    }
+}
