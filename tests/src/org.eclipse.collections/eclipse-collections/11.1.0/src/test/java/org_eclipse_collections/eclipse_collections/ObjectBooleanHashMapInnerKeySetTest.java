6e4308023011a290ffb2c495721d95b3f06168a1
Vojin Jovanovic <vojin.jovanovic@oracle.com>
2026-06-29 12:34:57 +0200

    Dynamic-access coverage for org.eclipse.collections.impl.map.mutable.primitive.ObjectBooleanHashMap$KeySet (30/52)

---
+++ tests/src/org.eclipse.collections/eclipse-collections/11.1.0/src/test/java/org_eclipse_collections/eclipse_collections/ObjectBooleanHashMapInnerKeySetTest.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_eclipse_collections.eclipse_collections;
+
+import java.util.Set;
+
+import org.eclipse.collections.impl.map.mutable.primitive.ObjectBooleanHashMap;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ObjectBooleanHashMapInnerKeySetTest {
+
+    @Test
+    void createsTypedKeyArrayWhenTargetArrayIsTooSmall() {
+        ObjectBooleanHashMap<String> map = ObjectBooleanHashMap.newWithKeysValues(
+                "alpha",
+                true,
+                "beta",
+                false,
+                "gamma",
+                true);
+        Set<String> keys = map.keySet();
+
+        CharSequence[] result = keys.toArray(new CharSequence[0]);
+
+        assertThat(result.getClass()).isEqualTo(CharSequence[].class);
+        assertThat(result).containsExactlyInAnyOrder("alpha", "beta", "gamma");
+    }
+}
