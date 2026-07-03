c91e2370ae119937f081197378eb8a0b5d659b4c
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-07-03 14:54:22 +0200

    Dynamic-access coverage for org.wildfly.common.net.Inet (8/8)

---
+++ tests/src/org.wildfly.common/wildfly-common/1.5.0.Final/src/test/java/org_wildfly_common/wildfly_common/InetTest.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_wildfly_common.wildfly_common;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.net.Inet4Address;
+
+import org.junit.jupiter.api.Test;
+import org.wildfly.common.net.Inet;
+
+public class InetTest {
+    @Test
+    void resolvesAllAddressesUsingTheRequestedConcreteType() throws Exception {
+        Inet4Address[] addresses = Inet.getAllAddressesByNameAndType("127.0.0.1", Inet4Address.class);
+
+        assertThat(addresses).hasSize(1);
+        assertThat(addresses[0].getHostAddress()).isEqualTo("127.0.0.1");
+    }
+}
