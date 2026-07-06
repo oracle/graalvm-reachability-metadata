a7e785b8acd382fdc67b6712433f723e9119cff5
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-07-06 12:28:55 +0200

    Partial dynamic-access coverage for lombok.launch.ShadowClassLoader (4/9)

--- tests/src/org.projectlombok/lombok/1.18.38/src/test/java/org_projectlombok/lombok/ShadowClassLoaderTest.java
+++ tests/src/org.projectlombok/lombok/1.18.38/src/test/java/org_projectlombok/lombok/ShadowClassLoaderTest.java
@@ -6,9 +6,18 @@
  */
 package org_projectlombok.lombok;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
 import java.util.ServiceLoader;
 
 import javax.annotation.processing.Processor;
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.StandardJavaFileManager;
+import javax.tools.ToolProvider;
 
 import lombok.Builder;
 import lombok.Value;
@@ -40,6 +49,47 @@ public class ShadowClassLoaderTest {
 
         assertThat(lombokProcessor).isNotNull();
         assertThat(lombokProcessor.getSupportedAnnotationTypes()).isNotEmpty();
+    }
+
+    @Test
+    void processesLombokAnnotationThroughJavaCompiler() throws IOException {
+        Processor lombokProcessor = null;
+        for (Processor processor : ServiceLoader.load(Processor.class)) {
+            if (processor.getClass().getName().equals("lombok.launch.AnnotationProcessorHider$AnnotationProcessor")) {
+                lombokProcessor = processor;
+                break;
+            }
+        }
+        assertThat(lombokProcessor).isNotNull();
+
+        JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        assertThat(compiler).isNotNull();
+        Path sourceFile = Files.createTempFile("RuntimeProcessed", ".java");
+        Files.writeString(sourceFile, """
+                import lombok.Getter;
+
+                @Getter
+                class RuntimeProcessed {
+                    private final String name = "Ada";
+                }
+                """);
+
+        try (StandardJavaFileManager fileManager = compiler.getStandardFileManager(null, null, null)) {
+            DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+            Iterable<? extends JavaFileObject> sourceFiles = fileManager.getJavaFileObjects(sourceFile.toFile());
+            JavaCompiler.CompilationTask compilation = compiler.getTask(
+                    null,
+                    fileManager,
+                    diagnostics,
+                    List.of("-proc:only", "-classpath", System.getProperty("java.class.path")),
+                    null,
+                    sourceFiles);
+            compilation.setProcessors(List.of(lombokProcessor));
+
+            assertThat(compilation.call()).as(diagnostics.getDiagnostics().toString()).isTrue();
+        } finally {
+            Files.deleteIfExists(sourceFile);
+        }
     }
 
     @Value
