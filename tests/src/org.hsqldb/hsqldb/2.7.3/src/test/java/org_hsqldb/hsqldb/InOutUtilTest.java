3243aeb6abbb1e093c6720f91fff54314f302c4a
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-07-08 04:25:14 +0200

    Dynamic-access coverage for org.hsqldb.lib.InOutUtil (63/82)

--- tests/src/org.hsqldb/hsqldb/2.7.3/src/test/java/org_hsqldb/hsqldb/InOutUtilTest.java
+++ tests/src/org.hsqldb/hsqldb/2.7.3/src/test/java/org_hsqldb/hsqldb/InOutUtilTest.java
@@ -8,6 +8,8 @@ package org_hsqldb.hsqldb;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
+
 import org.hsqldb.lib.InOutUtil;
 import org.junit.jupiter.api.Test;
 
@@ -21,5 +23,17 @@ public class InOutUtilTest {
 
         assertThat(serialized).isNotEmpty();
         assertThat(deserialized).isEqualTo(value);
+    }
+
+    @Test
+    void deserializesSerializableCollection() throws Exception {
+        ArrayList<String> values = new ArrayList<>();
+        values.add("first");
+        values.add("second");
+
+        byte[] serialized = InOutUtil.serialize(values);
+        Object deserialized = InOutUtil.deserialize(serialized);
+
+        assertThat(deserialized).isEqualTo(values);
     }
 }
