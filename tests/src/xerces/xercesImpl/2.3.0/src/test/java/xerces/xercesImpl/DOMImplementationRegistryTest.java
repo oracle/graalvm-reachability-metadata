d24ab4651df383acfd165b55372db79d36c9081d
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-07-24 14:21:02 +0200

    Partial dynamic-access coverage for org.apache.xerces.dom3.DOMImplementationRegistry (5/6)

---
+++ tests/src/xerces/xercesImpl/2.3.0/src/test/java/xerces/xercesImpl/DOMImplementationRegistryTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package xerces.xercesImpl;
+
+import org.apache.xerces.dom3.DOMImplementationRegistry;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Exercises DOM implementation source discovery through its public registry API.
+ * §FS-repository-functional-spec.5.2
+ */
+public class DOMImplementationRegistryTest {
+    private static final String DOM_IMPLEMENTATION_SOURCE_LIST =
+            "org.w3c.dom.DOMImplementationSourceList";
+    private static final String XERCES_DOM_IMPLEMENTATION_SOURCE =
+            "org.apache.xerces.dom.DOMImplementationSourceImpl";
+
+    @Test
+    void discoversConfiguredDomImplementationSource() throws Exception {
+        String originalSourceList = System.getProperty(DOM_IMPLEMENTATION_SOURCE_LIST);
+        try {
+            System.setProperty(DOM_IMPLEMENTATION_SOURCE_LIST, XERCES_DOM_IMPLEMENTATION_SOURCE);
+
+            DOMImplementationRegistry registry = DOMImplementationRegistry.newInstance();
+
+            assertThat(registry).isNotNull();
+        } finally {
+            if (originalSourceList == null) {
+                System.clearProperty(DOM_IMPLEMENTATION_SOURCE_LIST);
+            } else {
+                System.setProperty(DOM_IMPLEMENTATION_SOURCE_LIST, originalSourceList);
+            }
+        }
+    }
+}
