aca7aff667c8b753a8237e79fc01dc97f99ef359
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-07-24 17:56:45 +0200

    Dynamic-access coverage for org.bouncycastle.jcajce.provider.ProvDHPrivateKey (17/73)

--- tests/src/org.bouncycastle/bc-fips/2.1.2/src/test/java/org_bouncycastle/bc_fips/Prov15EdDSAPublicKeyTest.java
+++ tests/src/org.bouncycastle/bc-fips/2.1.2/src/test/java/org_bouncycastle/bc_fips/Prov15EdDSAPublicKeyTest.java
@@ -10,11 +10,13 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.security.KeyFactory;
 import java.security.KeyPair;
 import java.security.KeyPairGenerator;
 import java.security.PublicKey;
 import java.security.Security;
 import java.security.Signature;
+import java.security.spec.X509EncodedKeySpec;
 
 import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
 import org.junit.jupiter.api.BeforeAll;
@@ -33,11 +35,16 @@ public class Prov15EdDSAPublicKeyTest {
         KeyPairGenerator generator = KeyPairGenerator.getInstance(
             "Ed25519", BouncyCastleFipsProvider.PROVIDER_NAME);
         KeyPair keyPair = generator.generateKeyPair();
-
-        PublicKey restoredPublicKey = deserialize(serialize(keyPair.getPublic()));
+        KeyFactory keyFactory = KeyFactory.getInstance(
+            "Ed25519", BouncyCastleFipsProvider.PROVIDER_NAME);
+        PublicKey publicKey = keyFactory.generatePublic(
+            new X509EncodedKeySpec(keyPair.getPublic().getEncoded()));
+        PublicKey restoredPublicKey = deserialize(serialize(publicKey));
+        PublicKey restoredUnsharedPublicKey = deserializeUnshared(serializeUnshared(publicKey));
 
         assertThat(restoredPublicKey.getAlgorithm()).isEqualTo("Ed25519");
-        assertThat(restoredPublicKey.getEncoded()).isEqualTo(keyPair.getPublic().getEncoded());
+        assertThat(restoredPublicKey.getEncoded()).isEqualTo(publicKey.getEncoded());
+        assertThat(restoredUnsharedPublicKey.getEncoded()).isEqualTo(publicKey.getEncoded());
         assertThat(verifiesSignature(keyPair, restoredPublicKey, "serialized public key")).isTrue();
     }
 
@@ -49,9 +56,23 @@ public class Prov15EdDSAPublicKeyTest {
         return bytes.toByteArray();
     }
 
+    private byte[] serializeUnshared(PublicKey publicKey) throws Exception {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        try (ObjectOutputStream output = new ObjectOutputStream(bytes)) {
+            output.writeUnshared(publicKey);
+        }
+        return bytes.toByteArray();
+    }
+
     private PublicKey deserialize(byte[] serializedPublicKey) throws Exception {
         try (ObjectInputStream input = new ObjectInputStream(new ByteArrayInputStream(serializedPublicKey))) {
             return (PublicKey) input.readObject();
+        }
+    }
+
+    private PublicKey deserializeUnshared(byte[] serializedPublicKey) throws Exception {
+        try (ObjectInputStream input = new ObjectInputStream(new ByteArrayInputStream(serializedPublicKey))) {
+            return (PublicKey) input.readUnshared();
         }
     }
 
---
+++ tests/src/org.bouncycastle/bc-fips/2.1.2/src/test/java/org_bouncycastle/bc_fips/ProvDHPrivateKeyTest.java
@@ -0,0 +1,80 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_bouncycastle.bc_fips;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Security;
+
+import javax.crypto.KeyAgreement;
+import javax.crypto.interfaces.DHPrivateKey;
+import javax.crypto.spec.DHParameterSpec;
+
+import org.bouncycastle.jcajce.provider.BouncyCastleFipsProvider;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ProvDHPrivateKeyTest {
+    @BeforeAll
+    static void registerProvider() {
+        Security.addProvider(new BouncyCastleFipsProvider());
+    }
+
+    @Test
+    void serializesAndRestoresADiffieHellmanPrivateKey() throws Exception {
+        DHParameterSpec parameters = parametersFromJdkProvider();
+        KeyPairGenerator generator = KeyPairGenerator.getInstance(
+            "DH", BouncyCastleFipsProvider.PROVIDER_NAME);
+        generator.initialize(parameters);
+        KeyPair keyPair = generator.generateKeyPair();
+        KeyPair peerKeyPair = generator.generateKeyPair();
+
+        PrivateKey restoredPrivateKey = deserialize(serialize(keyPair.getPrivate()));
+
+        assertThat(restoredPrivateKey.getAlgorithm()).isEqualTo("DH");
+        assertThat(restoredPrivateKey.getEncoded()).isEqualTo(keyPair.getPrivate().getEncoded());
+        assertThat(agree(restoredPrivateKey, peerKeyPair.getPublic()))
+            .isEqualTo(agree(peerKeyPair.getPrivate(), keyPair.getPublic()));
+    }
+
+    private DHParameterSpec parametersFromJdkProvider() throws Exception {
+        KeyPairGenerator generator = KeyPairGenerator.getInstance("DH", "SunJCE");
+        generator.initialize(2048);
+        KeyPair keyPair = generator.generateKeyPair();
+        return ((DHPrivateKey)keyPair.getPrivate()).getParams();
+    }
+
+    private byte[] serialize(PrivateKey privateKey) throws Exception {
+        ByteArrayOutputStream bytes = new ByteArrayOutputStream();
+        try (ObjectOutputStream output = new ObjectOutputStream(bytes)) {
+            output.writeObject(privateKey);
+        }
+        return bytes.toByteArray();
+    }
+
+    private PrivateKey deserialize(byte[] serializedPrivateKey) throws Exception {
+        try (ObjectInputStream input = new ObjectInputStream(
+            new ByteArrayInputStream(serializedPrivateKey))) {
+            return (PrivateKey)input.readObject();
+        }
+    }
+
+    private byte[] agree(PrivateKey privateKey, PublicKey publicKey) throws Exception {
+        KeyAgreement agreement = KeyAgreement.getInstance("DH", BouncyCastleFipsProvider.PROVIDER_NAME);
+        agreement.init(privateKey);
+        agreement.doPhase(publicKey, true);
+        return agreement.generateSecret();
+    }
+}
