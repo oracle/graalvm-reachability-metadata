2f1cc90091bbd6ae6890105c8ba966ac0ffa6d43
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-07-28 10:08:19 +0200

    Fix isolated test metadata fixtures

--- tests/src/org.springframework/spring-test/6.0.0/src/test/java/org/springframework/test/context/aot/AotTestContextInitializers__Generated.java
+++ tests/src/org.springframework/spring-test/6.0.0/src/test/java/org/springframework/test/context/aot/AotTestContextInitializers__Generated.java
@@ -4,7 +4,7 @@
  * You should have received a copy of the CC0 legalcode along with this
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
-package org_springframework.spring_test;
+package org.springframework.test.context.aot;
 
 import java.util.Map;
 import java.util.function.Supplier;
