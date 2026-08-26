aac387c12103d24aa882b1554e5aaee14163390b
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-08-26 11:34:48 +0200

    Partial dynamic-access coverage for org.apache.tools.ant.AntClassLoader (14/15)

--- tests/src/org.apache.ant/ant/1.8.1/src/test/java/org_apache_ant/ant/AntClassLoaderTest.java
+++ tests/src/org.apache.ant/ant/1.8.1/src/test/java/org_apache_ant/ant/AntClassLoaderTest.java
@@ -9,11 +9,16 @@ package org_apache_ant.ant;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import org.apache.tools.ant.AntClassLoader;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.Echo;
+import org.apache.tools.ant.types.Path;
+import org.graalvm.internal.tck.NativeImageSupport;
 import org.junit.jupiter.api.Test;
 
 public class AntClassLoaderTest {
@@ -22,6 +27,25 @@ public class AntClassLoaderTest {
     @Test
     void initializesClassThroughPublicApi() {
         AntClassLoader.initializeClass(ConstructorProbe.class);
+    }
+
+    @Test
+    void forceLoadsClassFromConfiguredClasspath() throws Exception {
+        Project project = new Project();
+        Path classpath = new Path(project);
+        URL artifactLocation = Echo.class.getProtectionDomain().getCodeSource().getLocation();
+        classpath.setLocation(new File(artifactLocation.toURI()));
+        AntClassLoader loader = new AntClassLoader(getClass().getClassLoader(), project, classpath, false);
+
+        try {
+            assertThat(loader.forceLoadClass(Echo.class.getName()).getName()).isEqualTo(Echo.class.getName());
+        } catch (Error error) {
+            if (!NativeImageSupport.isUnsupportedFeatureError(error)) {
+                throw error;
+            }
+        } finally {
+            loader.cleanup();
+        }
     }
 
     @Test
@@ -45,6 +69,7 @@ public class AntClassLoaderTest {
         }
 
         loader.setParentFirst(false);
+        assertThat(loader.getResource(TASK_DEFINITIONS)).isNotNull();
         assertThat(loader.getNamedResources(TASK_DEFINITIONS).hasMoreElements()).isTrue();
 
         loader.setIsolated(true);
