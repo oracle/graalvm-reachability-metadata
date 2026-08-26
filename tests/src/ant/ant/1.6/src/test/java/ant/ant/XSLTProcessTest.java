b85f50376d66c8f61c98bfae77d22d4837c4a62f
Jovan Vukićević <jovan.vukicevic@oracle.com>
2026-08-26 15:54:08 +0200

    Partial dynamic-access coverage for org.apache.tools.ant.taskdefs.XSLTProcess (4/6)

---
+++ tests/src/ant/ant/1.6/src/test/java/ant/ant/XSLTProcessTest.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package ant.ant;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.taskdefs.XSLTLiaison;
+import org.apache.tools.ant.taskdefs.XSLTProcess;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+public class XSLTProcessTest {
+    @Test
+    void transformsDocumentsWithTheTraxProcessor(@TempDir Path temporaryDirectory) throws IOException {
+        Path input = Files.writeString(temporaryDirectory.resolve("input.xml"), "<document>Ant</document>");
+        Path stylesheet = Files.writeString(
+                temporaryDirectory.resolve("stylesheet.xsl"),
+                """
+                <xsl:stylesheet version="1.0" xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+                  <xsl:template match="/document"><result><xsl:value-of select="."/></result></xsl:template>
+                </xsl:stylesheet>
+                """);
+        Path output = temporaryDirectory.resolve("output.xml");
+
+        XSLTProcess process = newProcess(temporaryDirectory);
+        process.setProcessor("trax");
+        process.setStyle(stylesheet.toString());
+        process.setIn(input.toFile());
+        process.setOut(output.toFile());
+        process.execute();
+
+        assertThat(Files.readString(output)).contains("<result>Ant</result>");
+    }
+
+    @Test
+    void loadsAConfiguredLiaisonFromTheTaskClasspath(@TempDir Path temporaryDirectory) throws IOException {
+        Path input = Files.writeString(temporaryDirectory.resolve("input.xml"), "Ant");
+        Path stylesheet = Files.writeString(temporaryDirectory.resolve("stylesheet.xsl"), "unused");
+        Path output = temporaryDirectory.resolve("output.xml");
+
+        XSLTProcess process = newProcess(temporaryDirectory);
+        process.setProcessor(CopyingLiaison.class.getName());
+        process.createClasspath();
+        process.setStyle(stylesheet.toString());
+        process.setIn(input.toFile());
+        process.setOut(output.toFile());
+        process.execute();
+
+        assertThat(Files.readString(output)).isEqualTo("transformed: Ant");
+    }
+
+    private static XSLTProcess newProcess(Path baseDirectory) {
+        Project project = new Project();
+        project.setBaseDir(baseDirectory.toFile());
+        project.init();
+
+        XSLTProcess process = new XSLTProcess();
+        process.setProject(project);
+        process.init();
+        return process;
+    }
+
+    public static final class CopyingLiaison implements XSLTLiaison {
+        @Override
+        public void setStylesheet(File stylesheet) {
+        }
+
+        @Override
+        public void transform(File input, File output) throws IOException {
+            Files.writeString(
+                    output.toPath(), "transformed: " + Files.readString(input.toPath()), StandardCharsets.UTF_8);
+        }
+
+        @Override
+        public void addParam(String name, String expression) {
+        }
+    }
+}
