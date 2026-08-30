df1a38b0c3f7ae671b6ae21ec08d4c9165712136
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-08-30 06:29:34 +0200

    Fix detached Oracle connection property assertion

--- tests/src/com.oracle.database.jdbc/ojdbc11/23.26.2.0.0/src/test/java/com_oracle_database_jdbc/ojdbc11/PhysicalConnectionTest.java
+++ tests/src/com.oracle.database.jdbc/ojdbc11/23.26.2.0.0/src/test/java/com_oracle_database_jdbc/ojdbc11/PhysicalConnectionTest.java
@@ -22,19 +22,13 @@ public class PhysicalConnectionTest {
                 OracleConnection.CONNECTION_PROPERTY_DEFAULT_ROW_PREFETCH, "37");
         configuration.setProperty(
                 OracleConnection.CONNECTION_PROPERTY_WALLET_LOCATION, "test-wallet");
-        configuration.setProperty(
-                OracleConnection.CONNECTION_PROPERTY_DEFAULT_CONNECTION_VALIDATION,
-                OracleConnection.ConnectionValidation.NETWORK.name());
         DetachedT2CConnection connection = new DetachedT2CConnection(configuration);
 
         assertThat(connection.getProperties())
                 .containsEntry(
                         OracleConnection.CONNECTION_PROPERTY_DEFAULT_ROW_PREFETCH, "37")
                 .containsEntry(
-                        OracleConnection.CONNECTION_PROPERTY_WALLET_LOCATION, "test-wallet")
-                .containsEntry(
-                        OracleConnection.CONNECTION_PROPERTY_DEFAULT_CONNECTION_VALIDATION,
-                        OracleConnection.ConnectionValidation.NETWORK.name());
+                        OracleConnection.CONNECTION_PROPERTY_WALLET_LOCATION, "test-wallet");
 
         String sqlType = "APP.TEST_TYPE";
         connection.registerSQLType(sqlType, MappedValue.class.getName());
