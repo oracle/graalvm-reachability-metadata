3fc01c1788f0ebbf78306dea68f5ede30c7908e8
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-08-30 04:48:29 +0200

    Use compatible bytecode in CGLIB duplicate test

--- tests/src/org.springframework/spring-core/5.3.15/src/test/java/org_springframework/spring_core/DuplicatesPredicateTest.java
+++ tests/src/org.springframework/spring-core/5.3.15/src/test/java/org_springframework/spring_core/DuplicatesPredicateTest.java
@@ -10,34 +10,24 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.List;
+import java.util.Comparator;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.cglib.core.DuplicatesPredicate;
+import org.springframework.core.ExceptionDepthComparator;
 
 /** Verifies duplicate filtering when a generic override contributes a bridge method. */
 public class DuplicatesPredicateTest {
     @Test
     void filtersDuplicateBridgeSignature() throws NoSuchMethodException {
-        Method bridge = Arrays.stream(StringProcessor.class.getDeclaredMethods())
+        Method bridge = Arrays.stream(ExceptionDepthComparator.class.getDeclaredMethods())
                 .filter(Method::isBridge)
                 .findFirst()
                 .orElseThrow(AssertionError::new);
-        Method parentMethod = GenericProcessor.class.getDeclaredMethod("process", Object.class);
-        DuplicatesPredicate predicate = new DuplicatesPredicate(List.of(bridge, parentMethod));
+        Method comparatorMethod = Comparator.class.getMethod("compare", Object.class, Object.class);
+        DuplicatesPredicate predicate = new DuplicatesPredicate(Arrays.asList(bridge, comparatorMethod));
 
         assertThat(predicate.evaluate(bridge)).isTrue();
-        assertThat(predicate.evaluate(parentMethod)).isFalse();
-    }
-
-    public abstract static class GenericProcessor<T> {
-        public abstract T process(T value);
-    }
-
-    public static final class StringProcessor extends GenericProcessor<String> {
-        @Override
-        public String process(String value) {
-            return value.toUpperCase();
-        }
+        assertThat(predicate.evaluate(comparatorMethod)).isFalse();
     }
 }
