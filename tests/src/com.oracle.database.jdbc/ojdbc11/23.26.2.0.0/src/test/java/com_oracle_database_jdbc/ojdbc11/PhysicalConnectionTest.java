34da9e43c3056052162ec73149f9f25586865d21
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-02 03:27:03 +0200

    Remove unsupported JDBC property assertion

--- tests/src/com.oracle.database.jdbc/ojdbc11/23.26.2.0.0/src/test/java/com_oracle_database_jdbc/ojdbc11/PhysicalConnectionTest.java
+++ tests/src/com.oracle.database.jdbc/ojdbc11/23.26.2.0.0/src/test/java/com_oracle_database_jdbc/ojdbc11/PhysicalConnectionTest.java
@@ -22,18 +22,13 @@ public class PhysicalConnectionTest {
                 OracleConnection.CONNECTION_PROPERTY_DEFAULT_ROW_PREFETCH, "37");
         configuration.setProperty(
                 OracleConnection.CONNECTION_PROPERTY_WALLET_LOCATION, "test-wallet");
-        configuration.setProperty(
-                OracleConnection.CONNECTION_PROPERTY_DEFAULT_CONNECTION_VALIDATION, "NETWORK");
         DetachedT2CConnection connection = new DetachedT2CConnection(configuration);
 
         assertThat(connection.getProperties())
                 .containsEntry(
                         OracleConnection.CONNECTION_PROPERTY_DEFAULT_ROW_PREFETCH, "37")
                 .containsEntry(
-                        OracleConnection.CONNECTION_PROPERTY_WALLET_LOCATION, "test-wallet")
-                .containsEntry(
-                        OracleConnection.CONNECTION_PROPERTY_DEFAULT_CONNECTION_VALIDATION,
-                        "NETWORK");
+                        OracleConnection.CONNECTION_PROPERTY_WALLET_LOCATION, "test-wallet");
 
         String sqlType = "APP.TEST_TYPE";
         connection.registerSQLType(sqlType, MappedValue.class.getName());
@@ -51,7 +46,6 @@ public class PhysicalConnectionTest {
     private static final class DetachedT2CConnection extends T2CConnection {
         private DetachedT2CConnection(Properties properties) throws SQLException {
             super("jdbc:oracle:oci:@", properties, null);
-            readConnectionProperties("jdbc:oracle:oci:@", properties);
         }
     }
 }
