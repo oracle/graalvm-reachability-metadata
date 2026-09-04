045d2f78d5adce5a79445c77b1050942a4203a00
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-04 05:07:15 +0200

    Scaffold io.micronaut.serde:micronaut-serde-jackson:3.1.1

---
+++ tests/src/io.micronaut.serde/micronaut-serde-jackson/3.1.1/src/test/java/io_micronaut_serde/micronaut_serde_jackson/Micronaut_serde_jacksonTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package io_micronaut_serde.micronaut_serde_jackson;
+
+import org.junit.jupiter.api.Test;
+
+class Micronaut_serde_jacksonTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
