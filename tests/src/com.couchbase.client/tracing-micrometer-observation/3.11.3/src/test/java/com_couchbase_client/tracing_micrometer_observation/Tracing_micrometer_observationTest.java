59fa1d207711452c2c15aa4b4061a2679102405f
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-04 09:15:51 +0200

    Scaffold com.couchbase.client:tracing-micrometer-observation:3.11.3

---
+++ tests/src/com.couchbase.client/tracing-micrometer-observation/3.11.3/src/test/java/com_couchbase_client/tracing_micrometer_observation/Tracing_micrometer_observationTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package com_couchbase_client.tracing_micrometer_observation;
+
+import org.junit.jupiter.api.Test;
+
+class Tracing_micrometer_observationTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
