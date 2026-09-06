5f1c089139508263b60652374d3a6cf51dfe01b3
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-06 21:04:21 +0200

    Issue-requested metadata coverage for org.apache.activemq:activemq-broker:6.0.0

--- tests/src/org.apache.activemq/activemq-broker/6.0.0/src/test/java/org_apache_activemq/activemq_broker/ManagementContextTest.java
+++ tests/src/org.apache.activemq/activemq-broker/6.0.0/src/test/java/org_apache_activemq/activemq_broker/ManagementContextTest.java
@@ -10,6 +10,8 @@ import org.apache.activemq.broker.jmx.ManagementContext;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
 
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.util.concurrent.TimeUnit;
 
 import javax.management.MBeanServer;
@@ -33,4 +35,33 @@ public class ManagementContextTest {
         }
     }
 
+    @Test
+    void startsConnectorBackedManagementContext() throws Exception {
+        ManagementContext context = new ManagementContext();
+        context.setCreateConnector(true);
+        context.setConnectorHost("127.0.0.1");
+        context.setConnectorPort(availablePort());
+
+        try {
+            context.start();
+            awaitConnectorStart(context);
+
+            assertThat(context.isConnectorStarted()).isTrue();
+        } finally {
+            context.stop();
+        }
+    }
+
+    private static int availablePort() throws IOException {
+        try (ServerSocket socket = new ServerSocket(0)) {
+            return socket.getLocalPort();
+        }
+    }
+
+    private static void awaitConnectorStart(ManagementContext context) throws InterruptedException {
+        long deadline = System.nanoTime() + TimeUnit.SECONDS.toNanos(20L);
+        while (!context.isConnectorStarted() && System.nanoTime() < deadline) {
+            Thread.sleep(10L);
+        }
+    }
 }
