9d0a269fde49b27938b44fe5f69b91a809a44b66
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-07 03:09:08 +0200

    Dynamic-access coverage for kotlin.internal.PlatformImplementationsKt (18/18)

--- tests/src/org.jetbrains.kotlin/kotlin-stdlib/1.7.10/src/test/kotlin/kotlinstdlib/PlatformImplementationsKtTest.kt
+++ tests/src/org.jetbrains.kotlin/kotlin-stdlib/1.7.10/src/test/kotlin/kotlinstdlib/PlatformImplementationsKtTest.kt
@@ -8,15 +8,24 @@ package kotlinstdlib
 
 import kotlin.random.Random
 import kotlin.test.Test
-import kotlin.test.assertSame
+import kotlin.test.assertTrue
 
 public class PlatformImplementationsKtTest {
     @Test
-    public fun defaultRandomFillsAndReturnsTheProvidedArray(): Unit {
-        val bytes: ByteArray = ByteArray(32)
+    public fun defaultRandomProducesAValueWithJavaSevenPlatformSelection(): Unit {
+        val originalJavaSpecificationVersion: String? = System.getProperty("java.specification.version")
+        try {
+            System.setProperty("java.specification.version", "1.7")
 
-        val result: ByteArray = Random.Default.nextBytes(bytes)
+            val value: Int = Random.Default.nextInt(100, 200)
 
-        assertSame(bytes, result)
+            assertTrue(value in 100 until 200)
+        } finally {
+            if (originalJavaSpecificationVersion == null) {
+                System.clearProperty("java.specification.version")
+            } else {
+                System.setProperty("java.specification.version", originalJavaSpecificationVersion)
+            }
+        }
     }
 }
