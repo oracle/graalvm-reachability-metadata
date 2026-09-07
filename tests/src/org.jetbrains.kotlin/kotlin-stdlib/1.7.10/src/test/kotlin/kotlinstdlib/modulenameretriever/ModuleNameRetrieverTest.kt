f2eecc80ac372063534fe8301c729586b4d5d460
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-07 03:00:07 +0200

    Dynamic-access coverage for kotlin.coroutines.jvm.internal.ModuleNameRetriever (17/18)

--- tests/src/org.jetbrains.kotlin/kotlin-stdlib/1.7.10/src/test/kotlin/kotlinstdlib/modulenameretriever/ModuleNameRetrieverTest.kt
+++ tests/src/org.jetbrains.kotlin/kotlin-stdlib/1.7.10/src/test/kotlin/kotlinstdlib/modulenameretriever/ModuleNameRetrieverTest.kt
@@ -4,7 +4,7 @@
  * You should have received a copy of the CC0 legalcode along with this
  * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
  */
-package kotlinstdlib
+package kotlinstdlib.modulenameretriever
 
 import kotlin.coroutines.Continuation
 import kotlin.coroutines.CoroutineContext
@@ -17,9 +17,11 @@ import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
 
+private const val MODULE_NAME: String = "kotlinstdlib.modulenameretrievertest"
+
 public class ModuleNameRetrieverTest {
     @Test
-    public fun generatedCoroutineProvidesADebugStackTraceElement(): Unit {
+    public fun generatedCoroutineInNamedModuleProvidesADebugStackTraceElement(): Unit {
         val block: suspend () -> Unit = {
             suspendForModuleNameInspection()
         }
@@ -30,6 +32,11 @@ public class ModuleNameRetrieverTest {
         continuation.resumeWith(Result.success(Unit))
         val stackTraceElement: StackTraceElement = assertNotNull(generatedFrame.getStackTraceElement())
 
+        assertEquals(MODULE_NAME, stackTraceElement.className.substringBefore('/'))
+        assertTrue(
+            stackTraceElement.className.substringAfter('/')
+                .startsWith("kotlinstdlib.modulenameretriever.ModuleNameRetrieverTest")
+        )
         assertEquals("ModuleNameRetrieverTest.kt", stackTraceElement.fileName)
         assertTrue(stackTraceElement.lineNumber > 0)
         assertTrue(generatedFrame.toString().contains("ModuleNameRetrieverTest.kt"))
