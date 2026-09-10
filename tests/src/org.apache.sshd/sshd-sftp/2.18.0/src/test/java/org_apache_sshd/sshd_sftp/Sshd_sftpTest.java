41fbe2aa09adb7f76e9a2bdbc2793d1327c12706
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-10 08:16:08 +0200

    Scaffold org.apache.sshd:sshd-sftp:2.18.0

---
+++ tests/src/org.apache.sshd/sshd-sftp/2.18.0/src/test/java/org_apache_sshd/sshd_sftp/Sshd_sftpTest.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_sshd.sshd_sftp;
+
+import org.junit.jupiter.api.Test;
+
+class Sshd_sftpTest {
+    @Test
+    void test() throws Exception {
+        System.out.println("This is just a placeholder, implement your test");
+    }
+}
