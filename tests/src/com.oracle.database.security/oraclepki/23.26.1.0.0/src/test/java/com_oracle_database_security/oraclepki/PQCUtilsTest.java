544a4c0357975e6f7ddc073e87edd7f73a61fd59
Milan Cugurovic <milan.c.cugurovic@oracle.com>
2026-09-17 13:31:35 +0200

    Revert "Test Oracle PKI PQC key parameter access"
    This reverts commit 8c4912c0b06c072c8c1efc2f93335826c0a8fcc5.

--- tests/src/com.oracle.database.security/oraclepki/23.26.1.0.0/src/test/java/com_oracle_database_security/oraclepki/PQCUtilsTest.java
+++ tests/src/com.oracle.database.security/oraclepki/23.26.1.0.0/src/test/java/com_oracle_database_security/oraclepki/PQCUtilsTest.java
@@ -6,10 +6,8 @@
  */
 package com_oracle_database_security.oraclepki;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
-import oracle.security.pki.internal.pqc.util.OraclePKIPQCKeyPair;
 import oracle.security.pki.internal.pqc.util.PQCUtils;
 import org.junit.jupiter.api.Test;
 
@@ -17,13 +15,5 @@ public class PQCUtilsTest {
     @Test
     void validatesKnownPqcAlgorithmIdentifier() {
         assertThatCode(() -> PQCUtils.a("ML-DSA-44")).doesNotThrowAnyException();
-    }
-
-    @Test
-    void readsNamedParametersAndSizeFromPqcKeys() throws Exception {
-        OraclePKIPQCKeyPair keyPair = OraclePKIPQCKeyPair.a("ML-DSA-44");
-
-        assertThat(PQCUtils.a(keyPair.getPublicKey(), "Public")).isEqualTo("ML-DSA-44");
-        assertThat(PQCUtils.b(keyPair.getPublicKey())).isPositive();
     }
 }
