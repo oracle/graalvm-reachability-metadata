8fc63b11a22796dc616765c7a9f9453910301a15
Milan Cugurovic <milan.c.cugurovic@oracle.com>
2026-09-19 21:10:05 +0200

    Prepare project for org.springframework:spring-aop:6.1.7

---
+++ tests/src/org.springframework/spring-aop/6.1.7/src/test/java/org_springframework/spring_aop/AbstractAspectJAdvisorFactoryTest.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_springframework.spring_aop;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.junit.jupiter.api.Test;
+
+import org.springframework.aop.aspectj.annotation.ReflectiveAspectJAdvisorFactory;
+
+public class AbstractAspectJAdvisorFactoryTest {
+
+    @Test
+    void rejectsAnnotationStyleAspectWithAjcCompiledMarkerField() {
+        ReflectiveAspectJAdvisorFactory factory = new ReflectiveAspectJAdvisorFactory();
+
+        boolean aspect = factory.isAspect(AjcCompiledMarkerAspect.class);
+
+        assertThat(aspect).isFalse();
+    }
+
+    @Test
+    void rejectsClassWithoutAspectAnnotation() {
+        ReflectiveAspectJAdvisorFactory factory = new ReflectiveAspectJAdvisorFactory();
+
+        boolean aspect = factory.isAspect(NonAspect.class);
+
+        assertThat(aspect).isFalse();
+    }
+
+    @Test
+    void acceptsAnnotationStyleAspectWithoutAjcCompiledMarkerField() {
+        ReflectiveAspectJAdvisorFactory factory = new ReflectiveAspectJAdvisorFactory();
+
+        boolean aspect = factory.isAspect(AnnotationStyleAspect.class);
+
+        assertThat(aspect).isTrue();
+    }
+
+    @Aspect
+    public static class AjcCompiledMarkerAspect {
+        private Object ajc$perSingletonInstance;
+    }
+
+    @Aspect
+    public static class AnnotationStyleAspect {
+    }
+
+    public static class NonAspect {
+    }
+}
