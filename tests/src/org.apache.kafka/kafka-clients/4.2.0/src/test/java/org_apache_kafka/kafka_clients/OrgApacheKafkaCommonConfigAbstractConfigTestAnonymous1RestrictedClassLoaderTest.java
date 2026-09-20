343eed2f2e54615b51e2d99545617ee2ac84c836
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-20 21:36:09 +0200

    Improve Kafka clients dynamic-access coverage

---
+++ tests/src/org.apache.kafka/kafka-clients/4.2.0/src/test/java/org_apache_kafka/kafka_clients/OrgApacheKafkaCommonConfigAbstractConfigTestAnonymous1RestrictedClassLoaderTest.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_kafka.kafka_clients;
+
+import org.apache.kafka.common.config.AbstractConfigTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Timeout;
+
+public class OrgApacheKafkaCommonConfigAbstractConfigTestAnonymous1RestrictedClassLoaderTest {
+
+    @Test
+    @Timeout(60)
+    void resolvesClassConfigurationWithRestrictedContextClassLoader() {
+        AbstractConfigTest configTest = new AbstractConfigTest();
+        configTest.setup();
+        try {
+            configTest.testClassConfigs();
+        } finally {
+            configTest.teardown();
+        }
+    }
+}
