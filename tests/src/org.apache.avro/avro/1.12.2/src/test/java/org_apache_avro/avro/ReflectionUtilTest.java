f87ba11e12836b8fe1acc56a228f328aa42c1942
Milan Cugurovic <milan.c.cugurovic@oracle.com>
2026-09-21 13:21:24 +0200

    Native-test gate fixes for org.apache.avro.reflect.ReflectionUtil

--- tests/src/org.apache.avro/avro/1.12.2/src/test/java/org_apache_avro/avro/ReflectionUtilTest.java
+++ tests/src/org.apache.avro/avro/1.12.2/src/test/java/org_apache_avro/avro/ReflectionUtilTest.java
@@ -28,15 +28,6 @@ public class ReflectionUtilTest {
     }
 
     @Test
-    void createsInstanceUsingOneArgumentConstructorFunction() {
-        Function<String, ConstructedWithString> constructor = ReflectionUtil.getConstructorAsFunction(String.class,
-                ConstructedWithString.class);
-
-        assertThat(constructor).isNotNull();
-        assertThat(constructor.apply("value").value()).isEqualTo("value");
-    }
-
-    @Test
     void returnsNullWhenOneArgumentConstructorIsNotAccessible() {
         Function<String, PrivateStringConstructor> constructor = ReflectionUtil.getConstructorAsFunction(String.class,
                 PrivateStringConstructor.class);
