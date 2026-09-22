b4698083be3deeb0915f509ae6d56845e24a38f3
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-22 06:29:14 +0200

    Expand Tomcat reflective startup coverage

---
+++ tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.22/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/DigesterGeneratedCodeLoader.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_tomcat_embed.tomcat_embed_core;
+
+import org.apache.tomcat.util.digester.Digester;
+
+public class DigesterGeneratedCodeLoader implements Digester.GeneratedCodeLoader {
+    private static String requestedClassName;
+
+    public DigesterGeneratedCodeLoader() {
+    }
+
+    @Override
+    public Object loadGeneratedCode(String className) {
+        requestedClassName = className;
+        return null;
+    }
+
+    public static String getRequestedClassName() {
+        return requestedClassName;
+    }
+}
