afbfdfae6797c582c6eb9e69fa3e231df1344db9
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-22 02:27:27 +0200

    Expand Tomcat dynamic access integration coverage

---
+++ tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.22/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/BaseCatalinaMBeanTest.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_tomcat_embed.tomcat_embed_core;
+
+import org.apache.catalina.connector.Request;
+import org.apache.catalina.connector.Response;
+import org.apache.catalina.core.StandardContext;
+import org.apache.catalina.mbeans.ContainerMBean;
+import org.apache.catalina.valves.ValveBase;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class BaseCatalinaMBeanTest {
+
+    @Test
+    void addsConfiguredValveToManagedContainer() throws Exception {
+        StandardContext context = new StandardContext();
+        ContainerMBean mBean = new ContainerMBean();
+        mBean.setManagedResource(context, "ObjectReference");
+
+        String objectName = mBean.addValve(TestValve.class.getName());
+
+        assertThat(objectName).isNull();
+        assertThat(context.getPipeline().getValves()).anyMatch(TestValve.class::isInstance);
+    }
+
+    public static final class TestValve extends ValveBase {
+
+        public TestValve() {
+        }
+
+        @Override
+        public void invoke(Request request, Response response) {
+        }
+    }
+}
---
+++ tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.22/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/ResourceLinkFactoryTest.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright and related rights waived via CC0
+ *
+ * You should have received a copy of the CC0 legalcode along with this
+ * work. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+package org_apache_tomcat_embed.tomcat_embed_core;
+
+import java.util.Hashtable;
+
+import org.apache.naming.NamingContext;
+import org.apache.naming.ResourceLinkRef;
+import org.apache.naming.factory.ResourceLinkFactory;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ResourceLinkFactoryTest {
+
+    @Test
+    void resolvesRegisteredGlobalResourceWithExpectedType() throws Exception {
+        NamingContext globalContext = new NamingContext(new Hashtable<>(), "global");
+        globalContext.bind("shared/message", "configured-value");
+        ResourceLinkFactory.setGlobalContext(globalContext);
+        ResourceLinkFactory.registerGlobalResourceAccess(globalContext, "local/message", "shared/message");
+        ResourceLinkRef reference = new ResourceLinkRef(String.class.getName(), "shared/message", null, null);
+
+        try {
+            Object resource = new ResourceLinkFactory().getObjectInstance(reference, null, null, null);
+
+            assertThat(resource).isEqualTo("configured-value");
+        } finally {
+            ResourceLinkFactory.deregisterGlobalResourceAccess(globalContext, "local/message");
+        }
+    }
+}
