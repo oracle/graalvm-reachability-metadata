73b02d16cae21ade09dd8dfba90e6c83d808db20
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-23 02:15:06 +0200

    Issue-requested metadata coverage for com.twelvemonkeys.common:common-image:3.9.4

--- tests/src/com.twelvemonkeys.common/common-image/3.9.4/src/test/java/com_twelvemonkeys_common/common_image/BufferedImageFactoryInnerConsumerTest.java
+++ tests/src/com.twelvemonkeys.common/common-image/3.9.4/src/test/java/com_twelvemonkeys_common/common_image/BufferedImageFactoryInnerConsumerTest.java
@@ -10,6 +10,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.awt.image.BufferedImage;
 import java.awt.image.ColorModel;
+import java.awt.image.IndexColorModel;
 import java.awt.image.MemoryImageSource;
 
 import com.twelvemonkeys.image.BufferedImageFactory;
@@ -29,5 +30,21 @@ public class BufferedImageFactoryInnerConsumerTest {
         assertThat(image.getWidth()).isEqualTo(2);
         assertThat(image.getHeight()).isEqualTo(2);
         assertThat(image.getRGB(0, 0, 2, 2, null, 0, 2)).containsExactly(pixels);
+    }
+
+    @Test
+    void preservesIndexedPixelsProducedAsBytes() {
+        byte[] red = {0x00, 0x20, 0x40, 0x60};
+        byte[] green = {0x10, 0x30, 0x50, 0x70};
+        byte[] blue = {0x20, 0x40, 0x60, (byte) 0x80};
+        IndexColorModel colorModel = new IndexColorModel(8, 4, red, green, blue);
+        byte[] pixels = {0, 1, 2, 3};
+        MemoryImageSource source = new MemoryImageSource(2, 2, colorModel, pixels, 0, 2);
+
+        BufferedImage image = new BufferedImageFactory(source).getBufferedImage();
+
+        assertThat(image.getColorModel()).isSameAs(colorModel);
+        assertThat(image.getRGB(0, 0, 2, 2, null, 0, 2))
+                .containsExactly(0xff001020, 0xff203040, 0xff405060, 0xff607080);
     }
 }
