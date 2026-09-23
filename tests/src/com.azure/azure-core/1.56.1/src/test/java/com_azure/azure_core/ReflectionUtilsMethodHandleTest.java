23f2ab6ae5586b79b4bfada729e65f893865212b
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-23 11:14:42 +0200

    Exercise Azure reflective invoker entry points

--- tests/src/com.azure/azure-core/1.56.1/src/test/java/com_azure/azure_core/ReflectionUtilsMethodHandleTest.java
+++ tests/src/com.azure/azure-core/1.56.1/src/test/java/com_azure/azure_core/ReflectionUtilsMethodHandleTest.java
@@ -9,6 +9,7 @@ package com_azure.azure_core;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.azure.core.http.HttpHeaders;
+import com.azure.core.http.HttpMethod;
 import com.azure.core.http.HttpRequest;
 import com.azure.core.http.rest.SimpleResponse;
 import com.azure.core.implementation.ReflectiveInvoker;
@@ -19,10 +20,17 @@ import org.junit.jupiter.api.Timeout;
 @Timeout(60)
 public class ReflectionUtilsMethodHandleTest {
     @Test
-    void createsAConstructorInvokerForAResponseType() {
+    void createsAndInvokesAConstructorInvokerForAResponseType() throws Exception {
         ReflectiveInvoker constructor = new ResponseConstructorsCache().get(CustomResponse.class);
+        HttpRequest request = new HttpRequest(HttpMethod.GET, "https://localhost/widgets/7");
+        HttpHeaders headers = new HttpHeaders().set("etag", "widget-7");
+
+        CustomResponse response = (CustomResponse) constructor.invokeStatic(request, 200, headers, "available");
 
         assertThat(constructor.getParameterCount()).isEqualTo(4);
+        assertThat(response.getStatusCode()).isEqualTo(200);
+        assertThat(response.getHeaders().getValue("etag")).isEqualTo("widget-7");
+        assertThat(response.getValue()).isEqualTo("available");
     }
 
     public static final class CustomResponse extends SimpleResponse<String> {
