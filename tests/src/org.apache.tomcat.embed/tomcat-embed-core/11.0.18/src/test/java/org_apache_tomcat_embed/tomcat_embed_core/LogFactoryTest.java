a33be1479b1151dada38021bbce5930a598340d7
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-25 22:37:12 +0200

    Test Tomcat reflective protocol and logging paths

--- tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.18/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/LogFactoryTest.java
+++ tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.18/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/LogFactoryTest.java
@@ -6,9 +6,6 @@
  */
 package org_apache_tomcat_embed.tomcat_embed_core;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.junit.jupiter.api.Order;
@@ -16,24 +13,129 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@Order(1)
+@Order(0)
 public class LogFactoryTest {
 
     @Test
-    void createsLoggerForRequestedName() {
-        String loggerName = "factory-created-logger";
-        Logger julLogger = Logger.getLogger(loggerName);
-        Level originalLevel = julLogger.getLevel();
+    void createsDiscoveredLoggerWithRequestedName() {
+        Log log = LogFactory.getLog("factory-created-logger");
 
-        try {
-            julLogger.setLevel(Level.INFO);
+        assertThat(log).isInstanceOf(TestLog.class);
+        TestLog testLog = (TestLog) log;
+        testLog.info("ready");
 
-            Log log = LogFactory.getLog(loggerName);
+        assertThat(testLog.getName()).isEqualTo("factory-created-logger");
+        assertThat(testLog.getMessage()).isEqualTo("ready");
+    }
 
-            assertThat(log.isInfoEnabled()).isTrue();
-            assertThat(log.isDebugEnabled()).isFalse();
-        } finally {
-            julLogger.setLevel(originalLevel);
+    public static final class TestLog implements Log {
+        private final String name;
+        private Object message;
+
+        public TestLog() {
+            this("service-provider");
+        }
+
+        public TestLog(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public Object getMessage() {
+            return message;
+        }
+
+        @Override
+        public boolean isDebugEnabled() {
+            return false;
+        }
+
+        @Override
+        public boolean isErrorEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isFatalEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isInfoEnabled() {
+            return true;
+        }
+
+        @Override
+        public boolean isTraceEnabled() {
+            return false;
+        }
+
+        @Override
+        public boolean isWarnEnabled() {
+            return true;
+        }
+
+        @Override
+        public void trace(Object value) {
+            message = value;
+        }
+
+        @Override
+        public void trace(Object value, Throwable throwable) {
+            message = value;
+        }
+
+        @Override
+        public void debug(Object value) {
+            message = value;
+        }
+
+        @Override
+        public void debug(Object value, Throwable throwable) {
+            message = value;
+        }
+
+        @Override
+        public void info(Object value) {
+            message = value;
+        }
+
+        @Override
+        public void info(Object value, Throwable throwable) {
+            message = value;
+        }
+
+        @Override
+        public void warn(Object value) {
+            message = value;
+        }
+
+        @Override
+        public void warn(Object value, Throwable throwable) {
+            message = value;
+        }
+
+        @Override
+        public void error(Object value) {
+            message = value;
+        }
+
+        @Override
+        public void error(Object value, Throwable throwable) {
+            message = value;
+        }
+
+        @Override
+        public void fatal(Object value) {
+            message = value;
+        }
+
+        @Override
+        public void fatal(Object value, Throwable throwable) {
+            message = value;
         }
     }
 }
