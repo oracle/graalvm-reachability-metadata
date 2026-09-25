c837bd91b9091a78df0ec78b4027d36fc2a4d50d
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-25 17:07:47 +0200

    Restore Tomcat default logging tests

--- tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.22/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/LogFactoryTest.java
+++ tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.22/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/LogFactoryTest.java
@@ -6,6 +6,9 @@
  */
 package org_apache_tomcat_embed.tomcat_embed_core;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import org.apache.juli.logging.Log;
 import org.apache.juli.logging.LogFactory;
 import org.junit.jupiter.api.Order;
@@ -17,128 +20,20 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class LogFactoryTest {
 
     @Test
-    void createsServiceProvidedLoggerForRequestedName() {
-        Log log = LogFactory.getLog("factory-created-logger");
+    void createsLoggerForRequestedName() {
+        String loggerName = "factory-created-logger";
+        Logger julLogger = Logger.getLogger(loggerName);
+        Level originalLevel = julLogger.getLevel();
 
-        assertThat(log).isInstanceOf(TestLog.class);
-        TestLog testLog = (TestLog) log;
-        assertThat(testLog.getName()).isEqualTo("factory-created-logger");
+        try {
+            julLogger.setLevel(Level.INFO);
 
-        testLog.info("service provider message");
+            Log log = LogFactory.getLog(loggerName);
 
-        assertThat(testLog.getLastMessage()).isEqualTo("service provider message");
-        assertThat(log.isInfoEnabled()).isTrue();
-        assertThat(log.isDebugEnabled()).isFalse();
-    }
-
-    public static final class TestLog implements Log {
-        private final String name;
-        private Object lastMessage;
-
-        public TestLog(String name) {
-            this.name = name;
-        }
-
-        public String getName() {
-            return name;
-        }
-
-        public Object getLastMessage() {
-            return lastMessage;
-        }
-
-        @Override
-        public boolean isDebugEnabled() {
-            return false;
-        }
-
-        @Override
-        public boolean isErrorEnabled() {
-            return true;
-        }
-
-        @Override
-        public boolean isFatalEnabled() {
-            return true;
-        }
-
-        @Override
-        public boolean isInfoEnabled() {
-            return true;
-        }
-
-        @Override
-        public boolean isTraceEnabled() {
-            return false;
-        }
-
-        @Override
-        public boolean isWarnEnabled() {
-            return true;
-        }
-
-        @Override
-        public void trace(Object message) {
-            record(message);
-        }
-
-        @Override
-        public void trace(Object message, Throwable throwable) {
-            record(message);
-        }
-
-        @Override
-        public void debug(Object message) {
-            record(message);
-        }
-
-        @Override
-        public void debug(Object message, Throwable throwable) {
-            record(message);
-        }
-
-        @Override
-        public void info(Object message) {
-            record(message);
-        }
-
-        @Override
-        public void info(Object message, Throwable throwable) {
-            record(message);
-        }
-
-        @Override
-        public void warn(Object message) {
-            record(message);
-        }
-
-        @Override
-        public void warn(Object message, Throwable throwable) {
-            record(message);
-        }
-
-        @Override
-        public void error(Object message) {
-            record(message);
-        }
-
-        @Override
-        public void error(Object message, Throwable throwable) {
-            record(message);
-        }
-
-        @Override
-        public void fatal(Object message) {
-            record(message);
-        }
-
-        @Override
-        public void fatal(Object message, Throwable throwable) {
-            record(message);
-        }
-
-        private void record(Object message) {
-            lastMessage = message;
+            assertThat(log.isInfoEnabled()).isTrue();
+            assertThat(log.isDebugEnabled()).isFalse();
+        } finally {
+            julLogger.setLevel(originalLevel);
         }
     }
 }
