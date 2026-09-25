4c122701c9f216718bc097f68d3de4ed1f50dd04
Mihailo Markovic <mihailo.markovic@oracle.com>
2026-09-25 23:31:31 +0200

    Dynamic-access coverage for org.apache.naming.StringManager (421/432)

--- tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.18/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/NamingStringManagerTest.java
+++ tests/src/org.apache.tomcat.embed/tomcat-embed-core/11.0.18/src/test/java/org_apache_tomcat_embed/tomcat_embed_core/NamingStringManagerTest.java
@@ -6,6 +6,8 @@
  */
 package org_apache_tomcat_embed.tomcat_embed_core;
 
+import java.net.URL;
+
 import org.apache.naming.StringManager;
 import org.junit.jupiter.api.Test;
 
@@ -13,10 +15,50 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class NamingStringManagerTest {
 
+    private static final String BUNDLE_PACKAGE =
+            "org_apache_tomcat_embed.tomcat_embed_core.contextbundle";
+    private static final String BUNDLE_RESOURCE =
+            "org_apache_tomcat_embed/tomcat_embed_core/contextbundle/LocalStrings.properties";
+    private static final String NAMING_RESOURCE = "org/apache/naming/LocalStrings.properties";
+
     @Test
     void representsPackageWithoutAMessageBundle() {
         StringManager manager = StringManager.getManager(NamingStringManagerTest.class);
 
         assertThat(manager.getString("missing-key")).isNull();
+    }
+
+    @Test
+    void loadsTrustedApplicationMessagesFromTheContextClassLoader() {
+        Thread thread = Thread.currentThread();
+        ClassLoader originalClassLoader = thread.getContextClassLoader();
+        ClassLoader resourceClassLoader = new ResourceMappingClassLoader(originalClassLoader);
+
+        try {
+            thread.setContextClassLoader(resourceClassLoader);
+            StringManager manager = StringManager.getManager(BUNDLE_PACKAGE);
+
+            assertThat(manager.getString("namingContext.invalidName")).isEqualTo("Name is not valid");
+        } finally {
+            thread.setContextClassLoader(originalClassLoader);
+        }
+    }
+
+    private static final class ResourceMappingClassLoader extends ClassLoader {
+
+        private final ClassLoader resourceLoader;
+
+        private ResourceMappingClassLoader(ClassLoader resourceLoader) {
+            super(null);
+            this.resourceLoader = resourceLoader;
+        }
+
+        @Override
+        public URL getResource(String name) {
+            if (BUNDLE_RESOURCE.equals(name)) {
+                return resourceLoader.getResource(NAMING_RESOURCE);
+            }
+            return null;
+        }
     }
 }
